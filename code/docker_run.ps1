5d68619665ba1db50443c6cb042ce527805797aa
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-12-23 14:02:36 +0100

    Good-bye HTML ;-( (#109)
    * Good-bye HTML ;-(
    
    * remove docker scripts

--- code/docker_run.ps1
+++
@@ -1,1 +0,0 @@
-docker run --rm -it -p 8080:8080 -u "${id -u}:${id -g}" -v "${pwd}:/code" -w "/code" gradle:7.2.0-jdk11 gradle html:superDev
--- code/docker_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run --rm -it -p 8080:8080 -u "$(id -u):$(id -g)" -v "$(pwd):/code" -w "/code" gradle:7.2.0-jdk11 gradle html:superDev
