1c70792c4a0100d6b243e0b083b5319980148c63
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2022-10-05 13:25:33 +0200

    split ILevel into three interfaces  (#391)
    * split ILevel into three
    
    * move getLayout in ITileable
    
    Co-authored-by: Andre Matutat <andre.matutat@fh-bielefelde.de>

--- code/core/src/level/elements/ILevel.java
+++ code/core/src/level/elements/ILevel.java
@@ -1,117 +1,14 @@
 package level.elements;
 
-import basiselements.DungeonElement;
-import com.badlogic.gdx.ai.pfa.Connection;
-import com.badlogic.gdx.ai.pfa.DefaultGraphPath;
-import com.badlogic.gdx.ai.pfa.GraphPath;
-import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder;
-import com.badlogic.gdx.ai.pfa.indexed.IndexedGraph;
-import com.badlogic.gdx.utils.Array;
-import java.util.Random;
-import level.elements.astar.TileHeuristic;
-import level.tools.Coordinate;
 import level.tools.LevelElement;
 import level.tools.TileTextureFactory;
-import tools.Point;
 
-public interface ILevel extends IndexedGraph<Tile> {
-    Random RANDOM = new Random();
-
-    /**
-     * Starts the indexed A* pathfinding algorithm a returns a path
-     *
-     * @param start Start tile
-     * @param end End tile
-     * @return Generated path
-     */
-    default GraphPath<Tile> findPath(Tile start, Tile end) {
-        GraphPath<Tile> path = new DefaultGraphPath<>();
-        new IndexedAStarPathFinder<>(this).searchNodePath(start, end, getTileHeuristic(), path);
-        return path;
-    }
-
-    /**
-     * Checks if the passed entity is on the tile to the next level.
-     *
-     * @param entity entity to check for.
-     * @return if the passed entity is on the tile to the next level
-     */
-    default boolean isOnEndTile(DungeonElement entity) {
-        return entity.getPosition().toCoordinate().equals(getEndTile().getCoordinate());
-    }
-
-    /**
-     * Get a tile on the global position.
-     *
-     * @param globalPoint Position form where to get the tile.
-     * @return The tile on that point. null if there is no Tile or the Coordinate is out of bound
-     */
-    default Tile getTileAt(Coordinate globalPoint) {
-        try {
-            return getLayout()[globalPoint.y][globalPoint.x];
-        } catch (IndexOutOfBoundsException e) {
-            return null;
-        }
-    }
-
-    /**
-     * @return a random Tile in the Level
-     */
-    default Tile getRandomTile() {
-        return getLayout()[RANDOM.nextInt(getLayout().length)][
-                RANDOM.nextInt(getLayout()[0].length)];
-    }
-
-    /**
-     * Get a random Tile
-     *
-     * @param elementType Type of the Tile
-     * @return A random Tile of the given Type
-     */
-    default Tile getRandomTile(LevelElement elementType) {
-        Tile randomTile = getRandomTile();
-        if (randomTile.getLevelElement() == elementType) {
-            return randomTile;
-        } else {
-            return getRandomTile(elementType);
-        }
-    }
-
-    /**
-     * Get the position of a random Tile as Point
-     *
-     * @return Position of the Tile as Point
-     */
-    default Point getRandomTilePoint() {
-        return getRandomTile().getCoordinate().toPoint();
-    }
-
-    /**
-     * Get the position of a random Tile as Point
-     *
-     * @param elementTyp Type of the Tile
-     * @return Position of the Tile as Point
-     */
-    default Point getRandomTilePoint(LevelElement elementTyp) {
-        return getRandomTile(elementTyp).getCoordinate().toPoint();
-    }
-
-    /**
-     * @return The layout of the level
-     */
-    Tile[][] getLayout();
+public interface ILevel extends ITileable {
 
     /** Mark a random tile as start */
     default void setRandomStart() {
         setStartTile(getRandomTile(LevelElement.FLOOR));
     }
-
-    /**
-     * Get the start tile.
-     *
-     * @return The start tile.
-     */
-    Tile getStartTile();
 
     /**
      * Set the start tile.
@@ -126,36 +23,11 @@ public interface ILevel extends IndexedGraph<Tile> {
     }
 
     /**
-     * Get the end tile.
-     *
-     * @return The end tile.
-     */
-    Tile getEndTile();
-
-    /**
      * Set the end tile.
      *
      * @param end The end tile.
      */
     void setEndTile(Tile end);
-
-    /**
-     * Change the type of tile (including changing texture)
-     *
-     * @param tile The Tile you want to change
-     * @param changeInto The LevelElement to change the Tile into.
-     */
-    default void changeTileElementType(Tile tile, LevelElement changeInto) {
-        tile.setLevelElement(
-                changeInto, TileTextureFactory.findTexturePath(tile, getLayout(), changeInto));
-    }
-
-    /**
-     * For libGDX pathfinding algorithms
-     *
-     * @return nodeCount
-     */
-    int getNodeCount();
 
     /**
      * F=Floor, W=Wall, E=Exit, S=Skip/Blank
@@ -181,18 +53,14 @@ public interface ILevel extends IndexedGraph<Tile> {
         return output.toString();
     }
 
-    @Override
-    default int getIndex(Tile tile) {
-        return tile.getIndex();
-    }
-
-    @Override
-    default Array<Connection<Tile>> getConnections(Tile fromNode) {
-        return fromNode.getConnections();
-    }
-
     /**
-     * @return the TileHeuristic for the Level
+     * Change the type of tile (including changing texture)
+     *
+     * @param tile The Tile you want to change
+     * @param changeInto The LevelElement to change the Tile into.
      */
-    TileHeuristic getTileHeuristic();
+    default void changeTileElementType(Tile tile, LevelElement changeInto) {
+        tile.setLevelElement(
+                changeInto, TileTextureFactory.findTexturePath(tile, getLayout(), changeInto));
+    }
 }
