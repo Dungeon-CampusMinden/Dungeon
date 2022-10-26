fb05c5bb2d6e3c23a4e9b7a1f307f60a99ef98fa
Andre Matutat <andre.matutat@fh-bielefeld.de>
2022-10-26 14:47:25 +0200

    refactor graph

--- code/game/src/graph/EdgeDirection.java
+++ code/game/src/graph/EdgeDirection.java
@@ -1,8 +1,23 @@
 package graph;
 
 public enum EdgeDirection {
-    UP,
-    RIGHT,
-    LEFT,
-    DOWN
+    UP(0),
+    RIGHT(1),
+    LEFT(2),
+    DOWN(3);
+
+    public int value;
+
+    EdgeDirection(int value) {
+        this.value = value;
+    }
+
+    public static EdgeDirection getOppsit(EdgeDirection direction) {
+        return switch (direction) {
+            case UP -> DOWN;
+            case DOWN -> UP;
+            case LEFT -> RIGHT;
+            case RIGHT -> LEFT;
+        };
+    }
 }
--- code/game/src/graph/Node.java
+++ code/game/src/graph/Node.java
@@ -1,14 +1,38 @@
 package graph;
 
 public class Node<T extends Object> {
-    T value;
+    private T value;
+    private Node[] neighbours;
 
     public Node(T value) {
         this.value = value;
+        neighbours = new Node[4];
+    }
+
+    public boolean connect(Node other, EdgeDirection direction, boolean skipLoop) {
+        if (neighbours[direction.value] == null) {
+            if (skipLoop || other.connect(this, EdgeDirection.getOppsit(direction), true)) {
+                neighbours[direction.value] = other;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean connect(Node other, EdgeDirection direction) {
+        return connect(other, direction, false);
     }
 
     public void setValue(T value) {
         this.value = value;
+    }
+
+    public Node getNeighbour(EdgeDirection direction) {
+        return neighbours[direction.value];
+    }
+
+    public Node[] getNeighbours() {
+        return neighbours;
     }
 
     public T getValue() {
