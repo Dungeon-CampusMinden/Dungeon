b6605494932cff9d8235f2e546ff2b1e2382482c
mr <malt.r@mail.de>
2022-11-03 15:28:31 +0100

    Add first dsl test

---
+++ code/dsl/test/parser/TestDungeonASTConverter.java
@@ -0,0 +1,36 @@
+package parser;
+
+import antlr.main.DungeonDSLParser;
+import helpers.Helpers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import parser.AST.IdNode;
+import parser.AST.Node;
+import starter.DesktopLauncher;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DesktopLauncher.class})
+public class TestDungeonASTConverter {
+
+    @Test
+    public void testSimpleDotDef() {
+        String program = "graph g { a -- b }";
+
+        var ast = Helpers.getASTFromString(program);
+
+        // TODO: checking the structure in this way is very verbose and
+        // gets old quickly..
+        var dot_def = ast.getChild(0);
+        assertEquals(dot_def.type, Node.Type.DotDefinition);
+
+        var id = dot_def.getChild(0);
+        assertEquals(id.type, Node.Type.Identifier);
+        var idNode = (IdNode)id;
+        assertEquals("g", idNode.getName());
+    }
+}
