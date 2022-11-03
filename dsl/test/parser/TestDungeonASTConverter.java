f82d8641c27a7e84bba6f3c31537fa4fc1c5764b
mr <malt.r@mail.de>
2022-11-03 20:39:05 +0100

    finish first dsl test

--- dsl/test/parser/TestDungeonASTConverter.java
+++ dsl/test/parser/TestDungeonASTConverter.java
@@ -7,22 +7,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import parser.AST.IdNode;
-import parser.AST.Node;
+import parser.AST.*;
 import starter.DesktopLauncher;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({DesktopLauncher.class})
 public class TestDungeonASTConverter {
 
+    // TODO: checking the structure in this way is very verbose and
+    // gets old quickly, should create more comfortable way of testing AST-Structure..
     @Test
     public void testSimpleDotDef() {
         String program = "graph g { a -- b }";
 
         var ast = Helpers.getASTFromString(program);
 
-        // TODO: checking the structure in this way is very verbose and
-        // gets old quickly..
         var dot_def = ast.getChild(0);
         assertEquals(dot_def.type, Node.Type.DotDefinition);
 
@@ -30,5 +29,33 @@ public class TestDungeonASTConverter {
         assertEquals(id.type, Node.Type.Identifier);
         var idNode = (IdNode) id;
         assertEquals("g", idNode.getName());
+
+        var edgeStmt = dot_def.getChild(1);
+        assertEquals(edgeStmt.type, Node.Type.DotEdgeStmt);
+        var edgeStmtNode = (EdgeStmtNode) edgeStmt;
+
+        var lhsId = edgeStmtNode.getLhsId();
+        assertEquals(lhsId.type, Node.Type.Identifier);
+        var lhsIdNode = (IdNode) lhsId;
+        assertEquals(lhsIdNode.getName(), "a");
+
+        var rhsStmts = edgeStmtNode.getRhsStmts();
+        assertEquals(rhsStmts.size(), 1);
+
+        var rhsEdge = rhsStmts.get(0);
+        assertEquals(rhsEdge.type, Node.Type.DotEdgeRHS);
+
+        var rhsEdgeNode = (EdgeRhsNode) rhsEdge;
+        var edgeOp = rhsEdgeNode.getEdgeOpNode();
+        assertEquals(edgeOp.type, Node.Type.DotEdgeOp);
+
+        var edgeOpNode = (EdgeOpNode)edgeOp;
+        assertEquals(edgeOpNode.getEdgeOpType(), EdgeOpNode.Type.doubleLine);
+
+        var rhsId = rhsEdgeNode.getIdNode();
+        assertEquals(rhsId.type, Node.Type.Identifier);
+
+        var rhsIdNode = (IdNode)rhsId;
+        assertEquals(rhsIdNode.getName(), "b");
     }
 }
