9689e1a992b714f3a9bed527d0687d61e7ad5ad0
mr <malt.r@mail.de>
2022-11-08 10:06:01 +0100

    Convert Property to record

--- game/src/graph/Property.java
+++ game/src/graph/Property.java
@@ -1,21 +1,5 @@
 package graph;
 
-public class Property<T> {
-    /*enum AttributeType {
-        STRING,
-        INTEGER,
-        FLOAT
-    }*/
-
-    public static Property NONE = new Property<Void>(null);
-
-    private final T value;
-
-    public T getValue() {
-        return this.value;
-    }
-
-    public Property(T value) {
-        this.value = value;
-    }
+public record Property<T>(T value) {
+    public static Property<Void> NONE = new Property<>(null);
 }
