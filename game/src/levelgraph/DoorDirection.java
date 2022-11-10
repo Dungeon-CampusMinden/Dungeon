1807fe40feabf8eb318349c2ee8ae9229fb0fb85
Andre Matutat <andre.matutat@fh-bielefeld.de>
2022-11-10 17:08:54 +0100

    add getValue, make value private

--- game/src/levelgraph/DoorDirection.java
+++ game/src/levelgraph/DoorDirection.java
@@ -1,17 +1,32 @@
 package levelgraph;
 
+/**
+ * Defines on which side of the room the door is located
+ *
+ * @quthor Andre Matutat
+ */
 public enum DoorDirection {
     UP(0),
     RIGHT(1),
     LEFT(2),
     DOWN(3);
 
-    public int value;
+    private int value;
 
     DoorDirection(int value) {
         this.value = value;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Returns the opposite side of the room
+     *
+     * @param direction
+     * @return opposite side of the room
+     */
     public static DoorDirection getOppsit(DoorDirection direction) {
         return switch (direction) {
             case UP -> DOWN;
