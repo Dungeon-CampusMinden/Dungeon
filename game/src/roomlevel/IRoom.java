7280a348c4109836dc1ae1da74893733a813bfbc
Andre Matutat <andre.matutat@fh-bielefeld.de>
2022-11-10 17:19:24 +0100

    add javadoc

--- game/src/roomlevel/IRoom.java
+++ game/src/roomlevel/IRoom.java
@@ -3,7 +3,11 @@ package roomlevel;
 import java.util.LinkedHashSet;
 
 public interface IRoom {
+    /**
+     * @return all DoorTiles in this room
+     */
     LinkedHashSet<DoorTile> getDoors();
 
+    /** remove the ExitTile in this room if exist */
     void removeExit();
 }
