bef056c943abf89df1be3ec640c35513b1c8ecc8
mr <malt.r@mail.de>
2022-11-15 09:37:20 +0100

    Add method for creation of AST from resource file in Helpers

--- dsl/test/helpers/Helpers.java
+++ dsl/test/helpers/Helpers.java
@@ -2,19 +2,29 @@ package helpers;
 
 import antlr.main.DungeonDSLLexer;
 import antlr.main.DungeonDSLParser;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import parser.DungeonASTConverter;
 
 public class Helpers {
-    public static DungeonDSLParser.ProgramContext getParseTree(String program) {
-        var stream = CharStreams.fromString(program);
+
+    private static DungeonDSLParser.ProgramContext getParseTreeFromCharStream(CharStream stream) {
         var lexer = new DungeonDSLLexer(stream);
 
         var tokenStream = new CommonTokenStream(lexer);
         var parser = new DungeonDSLParser(tokenStream);
 
         return parser.program();
+    }
+
+    public static DungeonDSLParser.ProgramContext getParseTree(String program) {
+        var stream = CharStreams.fromString(program);
+        return getParseTreeFromCharStream(stream);
     }
 
     public static parser.AST.Node convertToAST(DungeonDSLParser.ProgramContext parseTree) {
@@ -24,6 +34,15 @@ public class Helpers {
 
     public static parser.AST.Node getASTFromString(String program) {
         var parseTree = getParseTree(program);
+        return convertToAST(parseTree);
+    }
+
+    public static parser.AST.Node getASTFromResourceFile(URL fileResourceURL)
+            throws URISyntaxException, IOException {
+        var file = new File(fileResourceURL.toURI());
+        var stream = CharStreams.fromFileName(file.getAbsolutePath());
+
+        var parseTree = getParseTreeFromCharStream(stream);
         return convertToAST(parseTree);
     }
 }
