6e3def2c0ea6129fd05cb4186dbc2b1d527422b4
mr <malt.r@mail.de>
2022-11-16 14:41:24 +0100

    Modify high level config parsing test

--- dsl/test/interpreter/TestDSLInterpreter.java
+++ dsl/test/interpreter/TestDSLInterpreter.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.junit.Assert;
 import org.junit.Test;
+import parser.AST.Node;
 
 public class TestDSLInterpreter {
     @Test
@@ -26,12 +26,15 @@ public class TestDSLInterpreter {
     }
 
     @Test
-    public void testQuestConfigParsing() throws URISyntaxException, IOException {
+    public void questConfigHighLevel() throws URISyntaxException, IOException {
         URL resource = getClass().getClassLoader().getResource("program.ds");
         assert resource != null;
         var ast = Helpers.getASTFromResourceFile(resource);
-        Assert.assertNotNull(ast);
 
-        Assert.assertNotNull(null);
+        var firstChild = ast.getChild(0);
+        assertEquals(Node.Type.DotDefinition, firstChild.type);
+
+        var secondChild = ast.getChild(1);
+        assertEquals(Node.Type.ObjectDefinition, secondChild.type);
     }
 }
