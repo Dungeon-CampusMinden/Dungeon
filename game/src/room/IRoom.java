64e8a32b176b76be00161ba032b97036d6f472b9
Andre Matutat <andre.matutat@fh-bielefeld.de>
2022-11-17 14:33:57 +0100

    connect doors

--- game/src/room/IRoom.java
+++ game/src/room/IRoom.java
@@ -2,10 +2,13 @@ package room;
 
 import java.util.LinkedHashSet;
 import level.elements.tile.DoorTile;
+import level.elements.tile.Tile;
 
 public interface IRoom {
     /**
      * @return all DoorTiles in this room
      */
     LinkedHashSet<DoorTile> getDoors();
+
+    Tile[][] getLayout();
 }
