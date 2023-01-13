282acab6fd10a494ecec982c4fa34648e5e9b425
mr <malt.r@mail.de>
2023-01-13 12:15:44 +0100

    Rename aggregateTypeWithDefaults to Prototype

--- dsl/src/runtime/Prototype.java
+++ dsl/src/runtime/Prototype.java
@@ -1,22 +1,23 @@
 package runtime;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import semanticAnalysis.Scope;
 import semanticAnalysis.Symbol;
 import semanticAnalysis.types.AggregateType;
 import semanticAnalysis.types.IType;
 
-// TODO: rename this to "Prototype"?
-public class AggregateTypeWithDefaults extends Value implements IType {
-    public static AggregateTypeWithDefaults NONE =
-            new AggregateTypeWithDefaults(new AggregateType("NO_TYPE_NAME", Scope.NULL), -1);
+// TODO: make this a subclass of AggregateType
+public class Prototype extends Value implements IType {
+    public static Prototype NONE = new Prototype(new AggregateType("NO_TYPE_NAME", Scope.NULL), -1);
     private final HashMap<String, Value> defaultValues;
 
     private AggregateType internalType() {
         return (AggregateType) this.dataType;
     }
 
-    public AggregateTypeWithDefaults(AggregateType internalType, int symbolIdx) {
+    public Prototype(AggregateType internalType, int symbolIdx) {
         super(internalType, Value.NONE, symbolIdx);
         defaultValues = new HashMap<>();
     }
@@ -33,6 +34,10 @@ public class AggregateTypeWithDefaults extends Value implements IType {
 
     public Value getDefaultValue(String name) {
         return defaultValues.getOrDefault(name, Value.NONE);
+    }
+
+    public Set<Map.Entry<String, Value>> getDefaultValues() {
+        return this.defaultValues.entrySet();
     }
 
     @Override
