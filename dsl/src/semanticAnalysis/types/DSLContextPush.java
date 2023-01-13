06a0c81221b32fb50f9d19e22234b05fe883ff85
mr <malt.r@mail.de>
2023-01-13 12:25:54 +0100

    Add DSLContext

--- dsl/src/semanticAnalysis/types/DSLContextPush.java
+++ dsl/src/semanticAnalysis/types/DSLContextPush.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER})
-public @interface DSLContextMember {
+@Target(ElementType.TYPE)
+public @interface DSLContextPush {
     public String name() default "";
 }
