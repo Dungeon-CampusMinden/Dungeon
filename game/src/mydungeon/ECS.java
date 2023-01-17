3c1e694a7cf1e0cd62550b9e18924c4862a12a03
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-01-17 11:44:02 +0100

    load new level on EndTile

--- game/src/mydungeon/ECS.java
+++ game/src/mydungeon/ECS.java
@@ -3,12 +3,14 @@ package mydungeon;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import controller.Game;
+import ecs.components.PositionComponent;
 import ecs.entities.Entity;
 import ecs.entities.Hero;
 import ecs.systems.*;
 import java.util.*;
 import level.LevelAPI;
 import level.elements.ILevel;
+import level.elements.tile.Tile;
 import level.generator.postGeneration.WallGenerator;
 import level.generator.randomwalk.RandomWalkGenerator;
 import starter.DesktopLauncher;
@@ -43,12 +45,15 @@ public class ECS extends Game {
     protected void frame() {
         camera.setFocusPoint(hero.getPositionComponent().getPosition());
 
+        if (isOnEndTile(hero)) levelAPI.loadLevel();
         if (Gdx.input.isKeyJustPressed(Input.Keys.P)) togglePause();
     }
 
     @Override
     public void onLevelLoad() {
         currentLevel = levelAPI.getCurrentLevel();
+        entities.clear();
+        entities.add(hero);
         hero.getPositionComponent()
                 .setPosition(currentLevel.getStartTile().getCoordinate().toPoint());
     }
@@ -58,6 +63,15 @@ public class ECS extends Game {
         if (systems != null) {
             systems.forEach(s -> s.toggleRun());
         }
+    }
+
+    private boolean isOnEndTile(Entity entity) {
+        PositionComponent pc = (PositionComponent) entity.getComponent(PositionComponent.name);
+        if (pc != null) {
+            Tile currentTile = currentLevel.getTileAt(pc.getPosition().toCoordinate());
+            if (currentTile.equals(currentLevel.getEndTile())) return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
