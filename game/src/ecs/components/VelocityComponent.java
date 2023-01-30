a541e40df09c83fe7b64baf0bd68a8809c2da5f9
AHeinisch <aheinisch@fh-bielefeld.de>
2023-01-30 12:29:56 +0100

    add missingTexture to VelocityComponent

--- game/src/ecs/components/VelocityComponent.java
+++ game/src/ecs/components/VelocityComponent.java
@@ -6,9 +6,12 @@ import semanticAnalysis.types.DSLContextMember;
 import semanticAnalysis.types.DSLType;
 import semanticAnalysis.types.DSLTypeMember;
 
+import java.util.List;
+
 /** VelocityComponent is a component that stores the x, y movement direction */
 @DSLType
 public class VelocityComponent extends Component {
+    private static List<String> missingTexture = List.of("animation/missingTexture.png");
     public static String name = "VelocityComponent";
     private float currentXVelocity;
     private float currentYVelocity;
@@ -67,8 +70,8 @@ public class VelocityComponent extends Component {
         this.currentYVelocity = 0;
         this.xVelocity = 0;
         this.yVelocity = 0;
-        this.moveLeftAnimation = null;
-        this.moveRightAnimation = null;
+        this.moveLeftAnimation = new Animation(missingTexture, 100);
+        this.moveRightAnimation = new Animation(missingTexture, 100);
     }
 
     /**
