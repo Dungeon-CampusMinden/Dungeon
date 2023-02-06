f48340ffa6bcbf9fe406bbbe05fe9cc50cd76eb6
mr <malt.r@mail.de>
2023-02-06 10:17:35 +0100

    Store builder method in AggregateTypeAdapter

--- dsl/src/semanticAnalysis/types/AggregateTypeAdapter.java
+++ dsl/src/semanticAnalysis/types/AggregateTypeAdapter.java
@@ -2,9 +2,15 @@ package semanticAnalysis.types;
 
 import semanticAnalysis.IScope;
 
+import java.lang.reflect.Method;
+
 public class AggregateTypeAdapter extends AggregateType {
-    public AggregateTypeAdapter(String name, IScope parentScope, Class<?> originType) {
+
+    final Method builderMethod;
+
+    public AggregateTypeAdapter(String name, IScope parentScope, Class<?> originType, Method builderMethod) {
         super(name, parentScope, originType);
+        this.builderMethod = builderMethod;
     }
 
     @Override
