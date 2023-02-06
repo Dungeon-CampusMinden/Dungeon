b48f9360b32d743e058473cff35ae77143b984a2
mr <malt.r@mail.de>
2023-02-06 17:15:50 +0100

    Handle anonymous aggregate value definition in symboltableparser

--- dsl/src/interpreter/DSLInterpreter.java
+++ dsl/src/interpreter/DSLInterpreter.java
@@ -111,7 +111,7 @@ public class DSLInterpreter implements AstVisitor<Object> {
             var propertyDefNode = (PropertyDefNode) propDef;
             var rhsValue = (Value) propertyDefNode.getStmtNode().accept(this);
 
-            Value value = (Value)rhsValue.clone();
+            Value value = (Value) rhsValue.clone();
 
             // indicate, that the value is "dirty", which means it was set
             // explicitly and needs to be set in the java object corresponding
