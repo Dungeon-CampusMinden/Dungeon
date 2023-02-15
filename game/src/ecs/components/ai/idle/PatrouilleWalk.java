fe8a03dd203bd9bff46e465ace4013b17ee7c926
Finn Watermann <finn.watermann@arcor.de>
2023-02-15 17:15:06 +0100

    Add class PatrouilleWalk

---
+++ game/src/ecs/components/ai/idle/PatrouilleWalk.java
@@ -0,0 +1,133 @@
+package ecs.components.ai.idle;
+
+import com.badlogic.gdx.ai.pfa.GraphPath;
+import ecs.components.MissingComponentException;
+import ecs.components.PositionComponent;
+import ecs.components.ai.AITools;
+import ecs.entities.Entity;
+import level.elements.tile.Tile;
+import mydungeon.ECS;
+import tools.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PatrouilleWalk implements IIdleAI {
+
+    public enum MODE {
+        RANDOM,
+        LOOP,
+        BACK_AND_FORTH
+    }
+
+    private final List<Tile> checkpoints = new ArrayList<>();
+    private final int numberCheckpoints;
+    private final int pauseTime;
+    private final float radius;
+    private final MODE mode;
+    private GraphPath<Tile> currentPath;
+    private boolean initialized = false;
+    private boolean forward = true;
+    private long arrivalTime = 0;
+    private int currentCheckpoint = 0;
+
+    /**
+     * Walks a random pattern in a radius around the entity. The checkpoints will be chosen randomly at first idle.
+     * After being initialized the checkpoints won't change anymore, only the order may be.
+     * @param radius Max distance from the entity to walk
+     * @param numberCheckpoints Number of checkpoints to walk to
+     * @param pauseTime Max time in milliseconds to wait on a checkpoint. The actual time is a random number between 0 and this value
+     */
+    public PatrouilleWalk(float radius, int numberCheckpoints, int pauseTime, MODE mode) {
+        this.radius = radius;
+        this.numberCheckpoints = numberCheckpoints;
+        this.pauseTime = pauseTime;
+        this.mode = mode;
+    }
+
+    private void init(Entity entity) {
+        initialized = true;
+        PositionComponent position = (PositionComponent) entity.getComponent(PositionComponent.class)
+            .orElseThrow(() -> new MissingComponentException("PositionComponent"));
+        Point center = position.getPosition();
+        Tile tile = ECS.currentLevel.getTileAt(position.getPosition().toCoordinate());
+
+        if(tile == null) {
+            return;
+        }
+
+        List<Tile> accessibleTiles = AITools.getAccessibleTilesInRange(center, radius);
+
+        if(accessibleTiles.isEmpty()) {
+            return;
+        }
+
+        Random rnd = new Random();
+        int maxTries = 0;
+        while(this.checkpoints.size() < numberCheckpoints || accessibleTiles.size() == this.checkpoints.size() || maxTries >= 1000) {
+            Tile t = accessibleTiles.get(rnd.nextInt(accessibleTiles.size()));
+            if(!this.checkpoints.contains(t)) {
+                this.checkpoints.add(t);
+            }
+            maxTries ++;
+        }
+    }
+
+    @Override
+    public void idle(Entity entity) {
+        if(!initialized) {
+            this.init(entity);
+        }
+
+        if(currentPath != null && !AITools.pathFinishedOrLeft(entity, currentPath)) {
+            AITools.move(entity, currentPath);
+            return;
+        }
+
+        if(currentPath != null && AITools.pathFinishedOrLeft(entity, currentPath)) {
+            arrivalTime = System.currentTimeMillis();
+            currentPath = null;
+            return;
+        }
+
+        if(System.currentTimeMillis() - arrivalTime < pauseTime) {
+            return;
+        }
+
+        //HERE: Path to checkpoint finished + pause time over
+        PositionComponent position = (PositionComponent) entity.getComponent(PositionComponent.class)
+            .orElseThrow(() -> new MissingComponentException("PositionComponent"));
+        this.arrivalTime = 0;
+
+        switch (mode) {
+            case RANDOM -> {
+                Random rnd = new Random();
+                currentCheckpoint = rnd.nextInt(checkpoints.size());
+                currentPath = AITools.calculatePath(position.getPosition(), this.checkpoints.get(currentCheckpoint).getCoordinate().toPoint());
+            }
+            case LOOP -> {
+                currentCheckpoint = (currentCheckpoint + 1) % checkpoints.size();
+                currentPath = AITools.calculatePath(position.getPosition(), this.checkpoints.get(currentCheckpoint).getCoordinate().toPoint());
+            }
+            case BACK_AND_FORTH -> {
+                if (forward) {
+                    currentCheckpoint = (currentCheckpoint + 1) % checkpoints.size();
+                    if (currentCheckpoint == 0) {
+                        forward = false;
+                    }
+                } else {
+                    currentCheckpoint = (currentCheckpoint - 1) % checkpoints.size();
+                    if (currentCheckpoint == checkpoints.size() - 1) {
+                        forward = true;
+                    }
+                }
+                currentPath = AITools.calculatePath(position.getPosition(), this.checkpoints.get(currentCheckpoint).getCoordinate().toPoint());
+            }
+            default -> {}
+        }
+
+    }
+
+
+}
