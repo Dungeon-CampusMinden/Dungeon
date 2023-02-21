c98e5fe4145fd7fa75dcf00117929e4300a01b68
Finn Watermann <finn.watermann@arcor.de>
2023-02-21 20:12:21 +0100

    level up entity if enough xp points are reached for next level

--- game/src/ecs/systems/XPSystem.java
+++ game/src/ecs/systems/XPSystem.java
@@ -1,10 +1,21 @@
 package ecs.systems;
 
-public class XPSystem extends ECS_System{
+import ecs.components.xp.XPComponent;
+import mydungeon.ECS;
+
+public class XPSystem extends ECS_System {
 
     @Override
     public void update() {
-
+        ECS.entities.stream()
+                .flatMap(e -> e.getComponent(XPComponent.class).stream())
+                .forEach(
+                        component -> {
+                            XPComponent comp = (XPComponent) component;
+                            if (comp.getXPToNextLevel() <= 0) {
+                                comp.setCurrentLevel(comp.getCurrentLevel() + 1);
+                                comp.levelUp(comp.getCurrentLevel());
+                            }
+                        });
     }
-
 }
