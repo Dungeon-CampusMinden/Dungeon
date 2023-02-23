809fc0df5f624af67f7018f4cdbbbb6f21aa9c3e
Finn Watermann <finn.watermann@arcor.de>
2023-02-23 16:33:20 +0100

    Add docs to tests + removed @before block

--- game/test/ecs/components/XPComponentTest.java
+++ game/test/ecs/components/XPComponentTest.java
@@ -6,35 +6,55 @@ import ecs.components.xp.XPComponent;
 import ecs.entities.Entity;
 import ecs.systems.SystemController;
 import mydungeon.ECS;
-import org.junit.Before;
 import org.junit.Test;
 
 public class XPComponentTest {
 
-    private XPComponent xpComponent;
-
-    @Before
-    public void setup() {
+    /**
+     * Test if the xp component ist initialized with zero xp and level zero.
+     */
+    @Test
+    public void testStartXP() {
+        /* Prepare */
         ECS.entities.clear();
         ECS.systems = new SystemController();
         Entity entity = new Entity();
-        xpComponent = new XPComponent(entity, null);
-    }
+        XPComponent xpComponent = new XPComponent(entity, null);
 
-    @Test
-    public void startXP() {
+        /* Test */
         assertEquals(0, xpComponent.getCurrentXP());
+        assertEquals(0, xpComponent.getCurrentLevel());
     }
 
+    /**
+     * Test if xp is added correctly.
+     */
     @Test
-    public void addXP2() {
+    public void testAddXP2() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        XPComponent xpComponent = new XPComponent(entity, null);
+
+        /* Test */
         assertEquals(0, xpComponent.getCurrentXP());
         xpComponent.addXP(10);
         assertEquals(10, xpComponent.getCurrentXP());
     }
 
+    /**
+     * Test if xp is added correctly.
+     */
     @Test
-    public void addXP3() {
+    public void testAddXP3() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        XPComponent xpComponent = new XPComponent(entity, null);
+
+        /* Test */
         assertEquals(0, xpComponent.getCurrentXP());
         xpComponent.addXP(10);
         xpComponent.addXP(10);
--- game/test/ecs/systems/XPSystemTest.java
+++ game/test/ecs/systems/XPSystemTest.java
@@ -6,68 +6,150 @@ import ecs.components.xp.ILevelUp;
 import ecs.components.xp.XPComponent;
 import ecs.entities.Entity;
 import mydungeon.ECS;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class XPSystemTest {
 
-    private Entity entity;
-    private XPComponent xpComponent;
-    private XPSystem xpSystem;
-    private ILevelUp levelUp;
-
-    @Before
-    public void setup() {
+    /**
+     * Test if the xp component ist initialized with zero xp and level zero.
+     */
+    @Test
+    public void testStartingWithZero() {
+        /* Prepare */
         ECS.entities.clear();
         ECS.systems = new SystemController();
-        this.entity = new Entity();
-        this.levelUp = Mockito.mock(ILevelUp.class);
-        this.xpComponent = new XPComponent(entity, this.levelUp);
-        this.xpSystem = new XPSystem();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        assertEquals(0, xpComponent.getCurrentXP());
+        assertEquals(0, xpComponent.getCurrentLevel());
+        xpSystem.update();
+        assertEquals(0, xpComponent.getCurrentXP());
+        assertEquals(0, xpComponent.getCurrentLevel());
     }
 
+    /**
+     * Test if level up is not triggered if the xp is not enough.
+     */
     @Test
-    public void noLevelUp() {
-        assertEquals(0, this.xpComponent.getCurrentLevel());
-        this.xpComponent.addXP(99); // First level is reached with 100 XP
-        this.xpSystem.update();
-        assertEquals(0, this.xpComponent.getCurrentLevel());
+    public void testNoLevelUp() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        /* Test */
+        xpComponent.addXP(99); // First level is reached with 100 XP
+        xpSystem.update();
+        assertEquals(0, xpComponent.getCurrentLevel());
     }
 
+    /**
+     * Test if level up is triggered if the xp is exact the needed amount.
+     */
     @Test
-    public void levelUpExact() {
-        assertEquals(0, this.xpComponent.getCurrentLevel());
-        this.xpComponent.addXP(100); // First level is reached with 100 XP
-        this.xpSystem.update();
-        assertEquals(1, this.xpComponent.getCurrentLevel());
+    public void testLevelUpExact() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        /* Test */
+        xpComponent.addXP(100); // First level is reached with 100 XP
+        xpSystem.update();
+        assertEquals(1, xpComponent.getCurrentLevel());
+        assertEquals(0, xpComponent.getCurrentXP());
     }
 
+    /**
+     * Test if level up is triggered if the xp is more than the needed amount.
+     */
     @Test
-    public void levelUpOverflow() {
-        assertEquals(0, this.xpComponent.getCurrentLevel());
-        this.xpComponent.addXP(120); // First level is reached with 100 XP
-        this.xpSystem.update();
-        assertEquals(1, this.xpComponent.getCurrentLevel());
-        assertEquals(20, this.xpComponent.getCurrentXP());
+    public void testLevelUpOverflow() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        /* Test */
+        assertEquals(0, xpComponent.getCurrentLevel());
+        xpComponent.addXP(120); // First level is reached with 100 XP
+        xpSystem.update();
+        assertEquals(1, xpComponent.getCurrentLevel());
+        assertEquals(20, xpComponent.getCurrentXP());
     }
 
+    /**
+     * Test if two level ups are triggered if the xp is exact the needed amount for two levels.
+     * First Level is reached with 100 XP, second level is reached with 141 XP (100 + 141 = 241)
+     */
     @Test
-    public void levelUpMultipleExact() {
-        assertEquals(0, this.xpComponent.getCurrentLevel());
-        // First Level is reached with 100 XP, second level is reached with 141 XP (100 + 141 = 241)
-        this.xpComponent.addXP(241);
-        this.xpSystem.update();
-        assertEquals(2, this.xpComponent.getCurrentLevel());
+    public void testLevelUpMultipleExact() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        /* Test */
+        xpComponent.addXP(241);
+        xpSystem.update();
+        assertEquals(2, xpComponent.getCurrentLevel());
+        assertEquals(0, xpComponent.getCurrentXP());
     }
 
+    /**
+     * Test if two level ups are triggered if the xp is more than the needed amount for two levels.
+     * First Level is reached with 100 XP, second level is reached with 141 XP (100 + 141 = 241)
+     */
     @Test
-    public void levelUpMultipleOverflow() {
-        assertEquals(0, this.xpComponent.getCurrentLevel());
-        // First Level is reached with 100 XP, second level is reached with 141 XP (100 + 141 = 241)
-        this.xpComponent.addXP(261);
-        this.xpSystem.update();
-        assertEquals(2, this.xpComponent.getCurrentLevel());
-        assertEquals(20, this.xpComponent.getCurrentXP());
+    public void testLevelUpMultipleOverflow() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        /* Test */
+        xpComponent.addXP(261);
+        xpSystem.update();
+        assertEquals(2, xpComponent.getCurrentLevel());
+        assertEquals(20, xpComponent.getCurrentXP());
     }
+
+    /**
+     * Test if negative xp is not allowed.
+     */
+    @Test
+    public void testNegativeXP() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        ILevelUp levelUp = Mockito.mock(ILevelUp.class);
+        XPComponent xpComponent = new XPComponent(entity, levelUp);
+        XPSystem xpSystem = new XPSystem();
+
+        /* Test */
+        xpComponent.addXP(-1);
+        xpSystem.update();
+        assertEquals(0, xpComponent.getCurrentXP());
+    }
+
 }
