c3521c859a9122f62307bf2dd10a6a808ca8a8b9
Finn Watermann <finn.watermann@arcor.de>
2023-02-23 16:37:51 +0100

    Extracted performing levelup to utility method for documentation

--- game/src/ecs/systems/XPSystem.java
+++ game/src/ecs/systems/XPSystem.java
@@ -14,10 +14,22 @@ public class XPSystem extends ECS_System {
                             XPComponent comp = (XPComponent) component;
                             long xpLeft;
                             while ((xpLeft = comp.getXPToNextLevel()) <= 0) {
-                                comp.setCurrentLevel(comp.getCurrentLevel() + 1);
-                                comp.setCurrentXP(xpLeft * -1);
-                                comp.levelUp(comp.getCurrentLevel());
+                                this.performLevelUp(comp, (int) xpLeft);
                             }
                         });
+    }
+
+    /**
+     * Perform a level up by increasing the current level and resetting the current XP. If the
+     * current XP is greater than the needed amount for the level up the remaining xp are added to
+     * the current XP.
+     *
+     * @param comp XPComponent of entity
+     * @param xpLeft XP left to level up (can be negative if greater the needed amount)
+     */
+    private void performLevelUp(XPComponent comp, int xpLeft) {
+        comp.setCurrentLevel(comp.getCurrentLevel() + 1);
+        comp.setCurrentXP(xpLeft * -1);
+        comp.levelUp(comp.getCurrentLevel());
     }
 }
