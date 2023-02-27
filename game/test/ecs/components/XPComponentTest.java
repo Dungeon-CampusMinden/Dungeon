50a3d3b5f7d06ba62801171e945811f7f4fdddbd
Finn Watermann <finn.watermann@arcor.de>
2023-02-27 16:57:20 +0100

    Add Tests for XPToNextLevel Method

--- game/test/ecs/components/XPComponentTest.java
+++ game/test/ecs/components/XPComponentTest.java
@@ -52,4 +52,44 @@ public class XPComponentTest {
         xpComponent.addXP(10);
         assertEquals(20, xpComponent.getCurrentXP());
     }
+
+    @Test
+    public void testXPToNextLevel1() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        XPComponent xpComponent = new XPComponent(entity, null);
+
+        /* Test */
+        xpComponent.addXP(10);
+        assertEquals(90, xpComponent.getXPToNextLevel());
+    }
+
+    @Test
+    public void testXPToNextLevel2() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        XPComponent xpComponent = new XPComponent(entity, null);
+
+        /* Test */
+        xpComponent.addXP(100);
+        assertEquals(0, xpComponent.getXPToNextLevel());
+    }
+
+    @Test
+    public void testXPToNextLevel3() {
+        /* Prepare */
+        ECS.entities.clear();
+        ECS.systems = new SystemController();
+        Entity entity = new Entity();
+        XPComponent xpComponent = new XPComponent(entity, null);
+
+        /* Test */
+        xpComponent.addXP(120);
+        assertEquals(-20, xpComponent.getXPToNextLevel());
+    }
+
 }
