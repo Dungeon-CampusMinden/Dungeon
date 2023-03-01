5bacacfb40c932565b78d3a919cb89b82873e4ab
Finn Watermann <finn.watermann@arcor.de>
2023-03-01 22:14:36 +0100

    Apply spotless

--- game/src/ecs/components/xp/ILevelUp.java
+++ game/src/ecs/components/xp/ILevelUp.java
@@ -4,6 +4,7 @@ public interface ILevelUp {
 
     /**
      * Implements the LevelUp behavior of a XPComponent having entity
+     *
      * @param nexLevel is the new level of the entity
      */
     void onLevelUp(long nexLevel);
