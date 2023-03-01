be82bfeaeaf5ec138ffdff954bf8891e1c3900b2
Finn Watermann <finn.watermann@arcor.de>
2023-03-01 22:06:26 +0100

    Add Javadoc

--- game/src/ecs/components/xp/ILevelUp.java
+++ game/src/ecs/components/xp/ILevelUp.java
@@ -2,5 +2,9 @@ package ecs.components.xp;
 
 public interface ILevelUp {
 
+    /**
+     * Implements the LevelUp behavior of a XPComponent having entity
+     * @param nexLevel is the new level of the entity
+     */
     void onLevelUp(long nexLevel);
 }
