1b906c48d3fcd861338e3d4c0b35569c9ed15ab8
Finn Watermann <finn.watermann@arcor.de>
2023-03-06 12:55:28 +0100

    Add javadoc

--- game/src/configuration/values/ConfigCharValue.java
+++ game/src/configuration/values/ConfigCharValue.java
@@ -2,15 +2,31 @@ package configuration.values;
 
 public class ConfigCharValue extends ConfigValue<Character> {
 
+    /**
+     * Creates a new ConfigValue of type Character.
+     *
+     * @param value
+     */
     public ConfigCharValue(Character value) {
         super(value);
     }
 
+    /**
+     * Serialize the char value to a string.
+     *
+     * @return The serialized value.
+     */
     @Override
     public String serialize() {
         return value.toString();
     }
 
+    /**
+     * Deserialize the char value from a string.
+     *
+     * @param value The string to deserialize.
+     * @return The deserialized value.
+     */
     @Override
     public Character deserialize(String value) {
         if (value.length() > 1) {
