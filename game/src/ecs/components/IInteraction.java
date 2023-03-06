67ff923a6eae14556db6ab40fed573571a022e5d
AHeinisch <aheinisch@fh-bielefeld.de>
2023-03-06 23:08:54 +0100

    add missing javadoc for IInteraction

--- game/src/ecs/components/IInteraction.java
+++ game/src/ecs/components/IInteraction.java
@@ -3,5 +3,11 @@ package ecs.components;
 import ecs.entities.Entity;
 
 public interface IInteraction {
+
+    /**
+     * Implements the interaction behavior of an Interactive entity
+     *
+     * @param entity
+     */
     void onInteraction(Entity entity);
 }
