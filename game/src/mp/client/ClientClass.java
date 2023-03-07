dc569b0006221ee46e3881e5af183195207c8a04
Benjamin Krüger <benni.k94@web.de>
2023-03-07 22:36:26 +0100

    completed first exemplary server-client-communication

--- game/src/mp/client/ClientClass.java
+++ game/src/mp/client/ClientClass.java
@@ -1,4 +1,44 @@
 package mp.client;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Client;
+import mp.packages.request.PingRequest;
+import mp.packages.response.PingResponse;
+import mp.server.ServerListener;
+
+import java.io.IOException;
+
 public class ClientClass {
+
+    // TODO: Outsource config parameters
+    private static final Integer connectionTimeout = 5000;
+    private static final Integer serverPort = 25444;
+    private static final String serverAddress = "127.0.0.1";
+
+    public static void main(String[] args) {
+        final Client client = new Client();
+
+        client.start();
+
+        try {
+            client.connect(connectionTimeout, serverAddress, 25444);
+        } catch (
+        IOException e) {
+            e.printStackTrace();
+        }
+
+        client.addListener(new ClientListener());
+
+        Kryo kryo = client.getKryo();
+        // register all packages that should be able to be received and sent
+        kryo.register(PingRequest .class);
+        kryo.register(PingResponse .class);
+
+        PingRequest pingRequest = new PingRequest();
+        client.sendTCP(pingRequest);
+
+        while (true) {
+
+        }
+    }
 }
--- game/src/mp/packages/response/PingResponse.java
+++ game/src/mp/packages/response/PingResponse.java
@@ -3,4 +3,8 @@ package mp.packages.response;
 public class PingResponse {
 
     long time = System.currentTimeMillis();
+
+    public long getTime() {
+        return time;
+    }
 }
