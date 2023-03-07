dc11fc5de6c902e712b5c2efb004b2874e1168dc
AHeinisch <aheinisch@fh-bielefeld.de>
2023-03-07 12:16:08 +0100

    add IReachable Javadoc

--- game/src/ecs/tools/interaction/IReachable.java
+++ game/src/ecs/tools/interaction/IReachable.java
@@ -1,5 +1,11 @@
 package ecs.tools.interaction;
 
 public interface IReachable {
+    /**
+     * Împlementation of a Check if the Interaction is valid and could trigger the interaction
+     *
+     * @param interactionData the prepared Data of an Interaction between two Entities
+     * @return true if the interaction is valid, otherwise false
+     */
     boolean cheackReachable(InteractionData interactionData);
 }
