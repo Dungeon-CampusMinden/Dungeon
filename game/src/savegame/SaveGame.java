85ec4bf11fe8618fb17eb8d6ddc514126aba214c
Finn Watermann <finn.watermann@arcor.de>
2023-03-14 01:58:26 +0100

    Add timestamp to file name

--- game/src/savegame/SaveGame.java
+++ game/src/savegame/SaveGame.java
@@ -7,12 +7,16 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import starter.Game;
 
 public class SaveGame {
 
     private static final JsonValue.PrettyPrintSettings PRETTY_PRINT_SETTINGS =
             new JsonValue.PrettyPrintSettings();
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 
     static {
         PRETTY_PRINT_SETTINGS.outputType = JsonWriter.OutputType.json;
@@ -43,22 +47,24 @@ public class SaveGame {
         return entities;
     }
 
-    /**
-     * Save the current game state to a file
-     *
-     * @throws IOException If the file could not be written
-     */
-    public static void save() throws IOException {
+    /** Save the current game state to a file */
+    public static void save() {
         System.out.println(getEntityData().toJson(JsonWriter.OutputType.json));
 
         JsonValue root = new JsonValue(JsonValue.ValueType.object);
         root.addChild("entities", getEntityData());
         String jsonString = root.prettyPrint(PRETTY_PRINT_SETTINGS);
 
-        File file = new File("savegame.json");
-        file.createNewFile();
-        FileOutputStream fos = new FileOutputStream(file, false);
-        fos.write(jsonString.getBytes(StandardCharsets.UTF_8));
-        fos.close();
+        try {
+            File file = new File(String.format("savegame_%s.json", DATE_FORMAT.format(new Date())));
+            if (!file.createNewFile()) {
+                throw new RuntimeException("Could not create savegame file!");
+            }
+            FileOutputStream fos = new FileOutputStream(file, false);
+            fos.write(jsonString.getBytes(StandardCharsets.UTF_8));
+            fos.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not save game!", e);
+        }
     }
 }
