bf36bdec5e9535de51e12d22edaced650e34293d
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2023-03-20 13:37:19 +0100

    use fireball texture for fireball skill  (#487)
    * rename fireBall_Down files
    
    * use fireball texture

--- game/src/ecs/components/skill/FireballSkill.java
+++ game/src/ecs/components/skill/FireballSkill.java
@@ -7,7 +7,7 @@ import tools.Point;
 public class FireballSkill extends DamageProjectileSkill {
     public FireballSkill(ITargetSelection targetSelection) {
         super(
-                "monster/imp/", // TODO change texture
+                "skills/fireball/fireBall_Down/",
                 0.5f,
                 new Damage(1, DamageType.FIRE, null),
                 new Point(10, 10),
