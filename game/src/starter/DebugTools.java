ae560d3404a920217c2e3493545da7cbd1a7a2ad
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-03-21 10:49:02 +0100

    add DebugTools#ZOOM_CAMERA

--- game/src/starter/DebugTools.java
+++ game/src/starter/DebugTools.java
@@ -1,7 +1,14 @@
 package starter;
 
-/**
- * Collection of functions for easy debugging
- */
+/** Collection of functions for easy debugging */
 public class DebugTools {
+
+    /**
+     * Zoom in or out
+     *
+     * @param amount Zoom length
+     */
+    public static void ZOOM_CAMERA(float amount) {
+        Game.camera.zoom = Math.max(0.1f, Game.camera.zoom + amount);
+    }
 }
