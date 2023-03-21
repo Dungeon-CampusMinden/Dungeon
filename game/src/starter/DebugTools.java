551b1fd8e3becba4e3b29e5ffcb70a024196623c
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-03-21 10:56:23 +0100

    add teleport to cursor

--- game/src/starter/DebugTools.java
+++ game/src/starter/DebugTools.java
@@ -1,5 +1,10 @@
 package starter;
 
+import ecs.components.MissingComponentException;
+import ecs.components.PositionComponent;
+import ecs.components.skill.SkillTools;
+import tools.Point;
+
 /** Collection of functions for easy debugging */
 public class DebugTools {
 
@@ -10,5 +15,20 @@ public class DebugTools {
      */
     public static void ZOOM_CAMERA(float amount) {
         Game.camera.zoom = Math.max(0.1f, Game.camera.zoom + amount);
+    }
+
+    /** Teleport the Hero to the current cursor Position */
+    public static void TELEPORT_TO_CURSOR() {
+        PositionComponent pc =
+                (PositionComponent)
+                        Game.hero
+                                .getComponent(PositionComponent.class)
+                                .orElseThrow(
+                                        () ->
+                                                new MissingComponentException(
+                                                        "Hero is missing PositionComponent"));
+        Point cP = SkillTools.getCursorPositionAsPoint();
+        if (Game.currentLevel.getTileAt(cP.toCoordinate()).isAccessible())
+            pc.setPosition(SkillTools.getCursorPositionAsPoint());
     }
 }
