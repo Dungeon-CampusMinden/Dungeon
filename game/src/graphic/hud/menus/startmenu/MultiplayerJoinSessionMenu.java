84e83cadfa299bee598870cbc09ecce30d6992e5
Benjamin Krüger <benni.k94@web.de>
2023-03-25 22:10:38 +0100

    Split game mode decision events on start menu into separate events for each mode

--- game/src/graphic/hud/menus/startmenu/MultiplayerJoinSessionMenu.java
+++ game/src/graphic/hud/menus/startmenu/MultiplayerJoinSessionMenu.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Null;
 import graphic.hud.menus.Menu;
@@ -19,7 +18,7 @@ import java.net.UnknownHostException;
 
 public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
 
-    private final ScreenInput inputHostPort;
+    private final ScreenInput inputHostIpPort;
     private final ScreenButton buttonJoin;
 
     public MultiplayerJoinSessionMenu() {
@@ -42,7 +41,7 @@ public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
         }
         BitmapFont inputFont = new BitmapFont();
         inputFont.getData().setScale(2f, 2f);
-        inputHostPort = new ScreenInput(
+        inputHostIpPort = new ScreenInput(
             String.format("%s:%d", deviceIpAddress, 25444),
             new Point(0, 0),
             new TextFieldStyleBuilder(inputFont)
@@ -51,12 +50,12 @@ public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
                 .build()
         );
         GlyphLayout glyphLayout = new GlyphLayout();
-        glyphLayout.setText(inputHostPort.getStyle().font, inputHostPort.getText());
+        glyphLayout.setText(inputHostIpPort.getStyle().font, inputHostIpPort.getText());
         float prefWidth = glyphLayout.width;
-        inputHostPort.setWidth(prefWidth);
-        inputHostPort.setPosition(
+        inputHostIpPort.setWidth(prefWidth);
+        inputHostIpPort.setPosition(
             (Constants.WINDOW_WIDTH) / 2f,
-            (Constants.WINDOW_HEIGHT) / 1.5f + inputHostPort.getHeight(),
+            (Constants.WINDOW_HEIGHT) / 1.5f + inputHostIpPort.getHeight(),
             Align.center | Align.bottom);
 
         buttonJoin = new ScreenButton(
@@ -73,13 +72,13 @@ public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
             Align.center | Align.bottom);
         buttonJoin.getLabel().setFontScale(buttonTextLabelScale);
 
-        add((T) inputHostPort);
+        add((T) inputHostIpPort);
         add((T) buttonJoin);
         hideMenu();
     }
 
-    public ScreenInput getInputHostPort() {
-        return inputHostPort;
+    public ScreenInput getInputHostIpPort() {
+        return inputHostIpPort;
     }
 
     public ScreenButton getButtonJoin() {
