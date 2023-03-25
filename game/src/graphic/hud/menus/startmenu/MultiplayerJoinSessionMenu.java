f30d80554bf52bb98258911a2ce280c5646578b0
Benjamin Krüger <benni.k94@web.de>
2023-03-25 18:37:52 +0100

    Removed Open to lan menu -> directly open

--- game/src/graphic/hud/menus/startmenu/MultiplayerJoinSessionMenu.java
+++ game/src/graphic/hud/menus/startmenu/MultiplayerJoinSessionMenu.java
@@ -1,9 +1,12 @@
 package graphic.hud.menus.startmenu;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Null;
 import graphic.hud.menus.Menu;
@@ -11,9 +14,12 @@ import graphic.hud.widgets.*;
 import tools.Constants;
 import tools.Point;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
 
-    private final ScreenInput inputSessionInfo;
+    private final ScreenInput inputHostPort;
     private final ScreenButton buttonJoin;
 
     public MultiplayerJoinSessionMenu() {
@@ -28,16 +34,29 @@ public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
      */
     public MultiplayerJoinSessionMenu(SpriteBatch batch, @Null Stage stage) {
         super(batch, stage);
-        inputSessionInfo = new ScreenInput(
-            "Lets come together",
+        String deviceIpAddress;
+        try {
+            deviceIpAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            deviceIpAddress = "Error";
+        }
+        BitmapFont inputFont = new BitmapFont();
+        inputFont.getData().setScale(2f, 2f);
+        inputHostPort = new ScreenInput(
+            String.format("%s:%d", deviceIpAddress, 25444),
             new Point(0, 0),
-            new TextFieldStyleBuilder(FontBuilder.DEFAULT_FONT)
+            new TextFieldStyleBuilder(inputFont)
                 .setFontColor(Color.RED)
+                .setBackground(Color.WHITE)
                 .build()
         );
-        inputSessionInfo.setPosition(
+        GlyphLayout glyphLayout = new GlyphLayout();
+        glyphLayout.setText(inputHostPort.getStyle().font, inputHostPort.getText());
+        float prefWidth = glyphLayout.width;
+        inputHostPort.setWidth(prefWidth);
+        inputHostPort.setPosition(
             (Constants.WINDOW_WIDTH) / 2f,
-            (Constants.WINDOW_HEIGHT) / 1.5f + inputSessionInfo.getHeight(),
+            (Constants.WINDOW_HEIGHT) / 1.5f + inputHostPort.getHeight(),
             Align.center | Align.bottom);
 
         buttonJoin = new ScreenButton(
@@ -54,13 +73,13 @@ public class MultiplayerJoinSessionMenu<T extends Actor> extends Menu<T> {
             Align.center | Align.bottom);
         buttonJoin.getLabel().setFontScale(buttonTextLabelScale);
 
-        add((T) inputSessionInfo);
+        add((T) inputHostPort);
         add((T) buttonJoin);
         hideMenu();
     }
 
-    public ScreenInput getInputSessionInfo() {
-        return inputSessionInfo;
+    public ScreenInput getInputHostPort() {
+        return inputHostPort;
     }
 
     public ScreenButton getButtonJoin() {
