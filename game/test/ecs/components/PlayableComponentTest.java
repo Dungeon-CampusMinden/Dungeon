d26e224a2745a06a9becc44607c81683af39c877
Lena241 <33090382+Lena241@users.noreply.github.com>
2023-04-02 18:57:09 +0200

    Update PlayableComponentTest.java

--- game/test/ecs/components/PlayableComponentTest.java
+++ game/test/ecs/components/PlayableComponentTest.java
@@ -2,6 +2,7 @@ package ecs.components;
 
 import static org.junit.Assert.*;
 
+import ecs.components.skill.Skill;
 import ecs.entities.Entity;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,5 +22,25 @@ public class PlayableComponentTest {
         assertTrue(playableComponent.isPlayable());
         playableComponent.setPlayable(false);
         assertFalse(playableComponent.isPlayable());
+    }
+
+    @Test
+    public void setSkillSlot1() {
+        Skill s = Mockito.mock(Skill.class);
+        playableComponent.setSkillSlot1(s);
+        assertEquals(s, playableComponent.getSkillSlot1().get());
+        Skill s2 = Mockito.mock(Skill.class);
+        playableComponent.setSkillSlot1(s2);
+        assertEquals(s2, playableComponent.getSkillSlot1().get());
+    }
+
+    @Test
+    public void setSkillSlot2() {
+        Skill s = Mockito.mock(Skill.class);
+        playableComponent.setSkillSlot2(s);
+        assertEquals(s, playableComponent.getSkillSlot2().get());
+        Skill s2 = Mockito.mock(Skill.class);
+        playableComponent.setSkillSlot2(s2);
+        assertEquals(s2, playableComponent.getSkillSlot2().get());
     }
 }
