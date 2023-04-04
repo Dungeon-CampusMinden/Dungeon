1314c267b1551f59284164b715d96933a2722247
dkirshner <diana.kirzhner@fh-bielefeld.de>
2023-04-04 11:15:57 +0200

    update of the game class, occurs due to error during rebase

--- game/src/starter/Game.java
+++ game/src/starter/Game.java
@@ -12,7 +12,6 @@ import configuration.Configuration;
 import configuration.KeyboardConfig;
 import controller.AbstractController;
 import controller.SystemController;
-import dslToGame.QuestConfig;
 import ecs.components.MissingComponentException;
 import ecs.components.PositionComponent;
 import ecs.entities.Entity;
@@ -20,14 +19,11 @@ import ecs.entities.Hero;
 import ecs.systems.*;
 import graphic.DungeonCamera;
 import graphic.Painter;
-import graphic.hud.EventResponsiveDialogue;
-import interpreter.DSLInterpreter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
+
+import graphic.hud.EventResponsiveDialogue;
 import level.IOnLevelLoader;
 import level.LevelAPI;
 import level.elements.ILevel;
@@ -39,19 +35,19 @@ import level.tools.LevelSize;
 import tools.Constants;
 import tools.Point;
 
+
 /** The heart of the framework. From here all strings are pulled. */
 public class Game extends ScreenAdapter implements IOnLevelLoader {
 
     private final LevelSize LEVELSIZE = LevelSize.SMALL;
 
-    private static final String exceptSystemName = "DrawSystem";
     /**
      * The batch is necessary to draw ALL the stuff. Every object that uses draw need to know the
      * batch.
      */
     protected SpriteBatch batch;
 
-    /** Contais all Controller of the Dungeon */
+    /** Contains all Controller of the Dungeon */
     protected List<AbstractController<?>> controller;
 
     public static DungeonCamera camera;
@@ -62,64 +58,33 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
     /** Generates the level */
     protected IGenerator generator;
 
-    private boolean doFirstFrame = true;
+    private boolean doSetup = true;
 
     /** All entities that are currently active in the dungeon */
-    private static Set<Entity> entities = new HashSet<>();
+    private static final Set<Entity> entities = new HashSet<>();
     /** All entities to be removed from the dungeon in the next frame */
-    private static Set<Entity> entitiesToRemove = new HashSet<>();
+    private static final Set<Entity> entitiesToRemove = new HashSet<>();
     /** All entities to be added from the dungeon in the next frame */
-    private static Set<Entity> entitiesToAdd = new HashSet<>();
+    private static final Set<Entity> entitiesToAdd = new HashSet<>();
 
     /** List of all Systems in the ECS */
     public static SystemController systems;
 
     public static ILevel currentLevel;
-    private static EventResponsiveDialogue eventResponsiveDialogue;
-    private PositionComponent heroPositionComponent;
-    public static Hero hero;
+    private static final String exceptSystemName = "DrawSystem";
+
+    private EventResponsiveDialogue eventResponsiveDialogue;
+    private static Entity hero;
     private Logger gameLogger;
 
-    /** Called once at the beginning of the game. */
-    protected void setup() {
-        initBaseLogger();
-        gameLogger = Logger.getLogger(this.getClass().getName());
-        controller.clear();
-        systems = new SystemController();
-        controller.add(systems);
-        hero = new Hero(new Point(0, 0));
-        heroPositionComponent =
-                (PositionComponent)
-                        hero.getComponent(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
-        levelAPI = new LevelAPI(batch, painter, new WallGenerator(new RandomWalkGenerator()), this);
-        levelAPI.loadLevel();
-
-        new VelocitySystem();
-        new DrawSystem(painter);
-        new PlayerSystem();
-        new AISystem();
-        new CollisionSystem();
-        new HealthSystem();
-        new XPSystem();
-        new SkillSystem();
-        new ProjectileSystem();
-    }
-
-    /** Called at the beginning of each frame. Before the controllers call <code>update</code>. */
-    protected void frame() {
-        camera.setFocusPoint(heroPositionComponent.getPosition());
-        entities.removeAll(entitiesToRemove);
-        entities.addAll(entitiesToAdd);
-        for (Entity entity : entitiesToRemove) {
-            gameLogger.info("Entity '" + entity.getClass().getSimpleName() + "' was deleted.");
+    public static void main(String[] args) {
+        // start the game
+        try {
+            Configuration.loadAndGetConfiguration("dungeon_config.json", KeyboardConfig.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        entitiesToRemove.clear();
-        if (isOnEndTile()) levelAPI.loadLevel();
-        manageEventResponsiveDialogue(controller, systems);
-        entitiesToAdd.clear();
-        if (isOnEndTile()) levelAPI.loadLevel(LEVELSIZE);
+        DesktopLauncher.run(new Game());
     }
 
     /**
@@ -130,177 +95,99 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
      */
     @Override
     public void render(float delta) {
-        if (doFirstFrame) {
-            firstFrame();
-        }
+        if (doSetup) setup();
         batch.setProjectionMatrix(camera.combined);
-        if (runLoop()) {
-            frame();
-            if (runLoop()) {
-                clearScreen();
-                levelAPI.update();
-                if (runLoop()) {
-                    controller.forEach(AbstractController::update);
-                    if (runLoop()) {
-                        camera.update();
-                    }
-                }
-            }
-        }
+        frame();
+        clearScreen();
+        levelAPI.update();
+        controller.forEach(AbstractController::update);
+        camera.update();
     }
 
-    private void clearScreen() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
-    }
-
-    private void firstFrame() {
-        doFirstFrame = false;
+    /** Called once at the beginning of the game. */
+    protected void setup() {
+        doSetup = false;
         controller = new ArrayList<>();
         setupCameras();
         painter = new Painter(batch, camera);
         generator = new RandomWalkGenerator();
         levelAPI = new LevelAPI(batch, painter, generator, this);
-        setup();
+        initBaseLogger();
+        gameLogger = Logger.getLogger(this.getClass().getName());
+        systems = new SystemController();
+        controller.add(systems);
+        hero = new Hero(new Point(0, 0));
+        levelAPI = new LevelAPI(batch, painter, new WallGenerator(new RandomWalkGenerator()), this);
+        levelAPI.loadLevel(LEVELSIZE);
+        createSystems();
     }
 
-    public void setSpriteBatch(SpriteBatch batch) {
-        this.batch = batch;
-    }
-
-    protected boolean runLoop() {
-        return true;
-    }
-
-    private void setupCameras() {
-        camera = new DungeonCamera(null, Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT);
-        camera.zoom = Constants.DEFAULT_ZOOM_FACTOR;
-
-        // See also:
-        // https://stackoverflow.com/questions/52011592/libgdx-set-ortho-camera
+    /** Called at the beginning of each frame. Before the controllers call <code>update</code>. */
+    protected void frame() {
+        setCameraFocus();
+        manageEntitiesSets();
+        getHero().ifPresent(this::loadNextLevelIfEntityIsOnEndTile);
+        manageEventResponsiveDialogue(controller, systems);
     }
 
     @Override
     public void onLevelLoad() {
         currentLevel = levelAPI.getCurrentLevel();
-
         entities.clear();
+        getHero().ifPresent(this::placeOnLevelStart);
+    }
+
+    private void manageEntitiesSets() {
+        entities.removeAll(entitiesToRemove);
+        entities.addAll(entitiesToAdd);
+        for (Entity entity : entitiesToRemove) {
+            gameLogger.info("Entity '" + entity.getClass().getSimpleName() + "' was deleted.");
+        }
+        for (Entity entity : entitiesToAdd) {
+            gameLogger.info("Entity '" + entity.getClass().getSimpleName() + "' was added.");
+        }
+        entitiesToRemove.clear();
+        entitiesToAdd.clear();
+    }
+
+    private void setCameraFocus() {
+        if (getHero().isPresent()) {
+            PositionComponent pc =
+                (PositionComponent)
+                    getHero()
+                        .get()
+                        .getComponent(PositionComponent.class)
+                        .orElseThrow(
+                            () ->
+                                new MissingComponentException(
+                                    "PositionComponent"));
+            camera.setFocusPoint(pc.getPosition());
+
+        } else camera.setFocusPoint(new Point(0, 0));
+    }
+
+    private void loadNextLevelIfEntityIsOnEndTile(Entity hero) {
+        if (isOnEndTile(hero)) levelAPI.loadLevel(LEVELSIZE);
+    }
+
+    private boolean isOnEndTile(Entity entity) {
+        PositionComponent pc =
+            (PositionComponent)
+                entity.getComponent(PositionComponent.class)
+                    .orElseThrow(
+                        () -> new MissingComponentException("PositionComponent"));
+        Tile currentTile = currentLevel.getTileAt(pc.getPosition().toCoordinate());
+        return currentTile.equals(currentLevel.getEndTile());
+    }
+
+    private void placeOnLevelStart(Entity hero) {
         entities.add(hero);
-        heroPositionComponent.setPosition(currentLevel.getStartTile().getCoordinate().toPoint());
-
-        // TODO: when calling this before currentLevel is set, the default ctor of PositionComponent
-        // triggers NullPointerException
-        setupDSLInput();
-    }
-
-    private boolean isOnEndTile() {
-        Tile currentTile =
-                currentLevel.getTileAt(heroPositionComponent.getPosition().toCoordinate());
-        if (currentTile.equals(currentLevel.getEndTile())) return true;
-
-        return false;
-    }
-
-    private void setupDSLInput() {
-        String program =
-                """
-        game_object monster {
-            position_component {
-            },
-            velocity_component {
-            x_velocity: 0.1,
-            y_velocity: 0.1,
-            move_right_animation:"monster/imp/runRight",
-            move_left_animation: "monster/imp/runLeft"
-            },
-            animation_component{
-                idle_left: "monster/imp/idleLeft",
-                idle_right: "monster/imp/idleRight",
-                current_animation: "monster/imp/idleLeft"
-            },
-            ai_component {
-            },
-            hitbox_component {
-            }
-        }
-
-        quest_config config {
-            entity: monster
-        }
-        """;
-        DSLInterpreter interpreter = new DSLInterpreter();
-        QuestConfig config = (QuestConfig) interpreter.getQuestConfig(program);
-        entities.add(config.entity());
-    }
-    /**
-     * Key Event creates a dialogue which pauses the game and is formatted according to the event.
-     * After leaving the dialogue, the pause is cancelled. The dialogue object is deleted.
-     *
-     * @param controller manages elements of a certain type and is based on a layer system
-     * @param systems ECS_Systems, which control components of all entities in game loop
-     */
-    private void manageEventResponsiveDialogue(
-            List<AbstractController<?>> controller, SystemController systems) {
-        if (EventResponsiveDialogue.isDialogCalledByKeyboardEvent()) {
-            generateEventResponsiveDialogue(controller, systems);
-        } else if (eventResponsiveDialogue != null && !eventResponsiveDialogue.isEnable()) {
-            deleteEventResponsiveDialogue(controller, systems);
-        }
-    }
-
-    /**
-     * If no dialogue is created, a new dialogue is created according to the pressed key (button).
-     * Pause all systems except DrawSystem
-     *
-     * @param controller manages elements of a certain type and is based on a layer system
-     * @param systems ECS_Systems, which control components of all entities in game loop
-     */
-    private void generateEventResponsiveDialogue(
-            List<AbstractController<?>> controller, SystemController systems) {
-        if (eventResponsiveDialogue != null) deleteEventResponsiveDialogue(controller, systems);
-
-        String msg = "";
-        eventResponsiveDialogue =
-                new EventResponsiveDialogue(
-                        msg, new Skin(Gdx.files.internal(Constants.SKIN_FOR_DIALOG)), Color.WHITE);
-
-        if (controller != null) controller.add(eventResponsiveDialogue);
-
-        if (systems != null) {
-            for (ECS_System system : systems) {
-                system.notRunExceptSystems(exceptSystemName);
-            }
-        }
-    }
-
-    /**
-     * After leaving the dialogue, it is removed from the stage, the game is unpaused by releasing
-     * all systems and deleting the dialogue obejct.
-     *
-     * @param controller manages elements of a certain type and is based on a layer system
-     * @param systems ECS_Systems, which control components of all entities in game loop
-     */
-    private void deleteEventResponsiveDialogue(
-            List<AbstractController<?>> controller, SystemController systems) {
-        if (eventResponsiveDialogue == null) return;
-
-        if (controller != null && controller.contains(eventResponsiveDialogue))
-            controller.remove(eventResponsiveDialogue);
-
-        if (systems != null) {
-            systems.forEach(ECS_System::allRun);
-        }
-        eventResponsiveDialogue = null;
-    }
-
-    public static void main(String[] args) {
-        // start the game
-        try {
-            Configuration.loadAndGetConfiguration("dungeon_config.json", KeyboardConfig.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        DesktopLauncher.run(new Game());
+        PositionComponent pc =
+            (PositionComponent)
+                hero.getComponent(PositionComponent.class)
+                    .orElseThrow(
+                        () -> new MissingComponentException("PositionComponent"));
+        pc.setPosition(currentLevel.getStartTile().getCoordinate().toPoint());
     }
 
     /**
@@ -341,4 +228,110 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
     public static Set<Entity> getEntitiesToRemove() {
         return entitiesToRemove;
     }
+
+    /**
+     * @return the player character, can be null if not initialized
+     */
+    public static Optional<Entity> getHero() {
+        return Optional.ofNullable(hero);
+    }
+
+    /**
+     * set the reference of the playable character careful: old hero will not be removed from the
+     * game
+     *
+     * @param hero new reference of hero
+     */
+    public static void setHero(Entity hero) {
+        Game.hero = hero;
+    }
+
+    public void setSpriteBatch(SpriteBatch batch) {
+        this.batch = batch;
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+    private void setupCameras() {
+        camera = new DungeonCamera(null, Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT);
+        camera.zoom = Constants.DEFAULT_ZOOM_FACTOR;
+
+        // See also:
+        // https://stackoverflow.com/questions/52011592/libgdx-set-ortho-camera
+    }
+
+    private void createSystems() {
+        new VelocitySystem();
+        new DrawSystem(painter);
+        new PlayerSystem();
+        new AISystem();
+        new CollisionSystem();
+        new HealthSystem();
+        new XPSystem();
+        new SkillSystem();
+        new ProjectileSystem();
+    }
+
+    /**
+     * Key Event creates a dialogue which pauses the game and is formatted according to the event.
+     * After leaving the dialogue, the pause is cancelled. The dialogue object is deleted.
+     *
+     * @param controller manages elements of a certain type and is based on a layer system
+     * @param systems ECS_Systems, which control components of all entities in game loop
+     */
+    private void manageEventResponsiveDialogue(
+        List<AbstractController<?>> controller, SystemController systems) {
+        if (EventResponsiveDialogue.isDialogCalledByKeyboardEvent()) {
+            generateEventResponsiveDialogue(controller, systems);
+        } else if (eventResponsiveDialogue != null && !eventResponsiveDialogue.isEnable()) {
+            deleteEventResponsiveDialogue(controller, systems);
+        }
+    }
+
+    /**
+     * After leaving the dialogue, it is removed from the stage, the game is unpaused by releasing
+     * all systems and deleting the dialogue obejct.
+     *
+     * @param controller manages elements of a certain type and is based on a layer system
+     * @param systems ECS_Systems, which control components of all entities in game loop
+     */
+    private void deleteEventResponsiveDialogue( List<AbstractController<?>> controller, SystemController systems) {
+        if (eventResponsiveDialogue == null) return;
+
+        if (controller != null && controller.contains(eventResponsiveDialogue))
+            controller.remove(eventResponsiveDialogue);
+
+        if (systems != null) {
+            systems.forEach(ECS_System::allRun);
+        }
+        eventResponsiveDialogue = null;
+    }
+
+    /**
+     * If no dialogue is created, a new dialogue is created according to the pressed key (button).
+     * Pause all systems except DrawSystem
+     *
+     * @param controller manages elements of a certain type and is based on a layer system
+     * @param systems ECS_Systems, which control components of all entities in game loop
+     */
+    private void generateEventResponsiveDialogue( List<AbstractController<?>> controller, SystemController systems) {
+        if (eventResponsiveDialogue != null) deleteEventResponsiveDialogue(controller, systems);
+
+        String msg = "";
+        eventResponsiveDialogue =
+            new EventResponsiveDialogue(
+                msg, new Skin(Gdx.files.internal(Constants.SKIN_FOR_DIALOG)), Color.WHITE);
+
+        if (controller != null) controller.add(eventResponsiveDialogue);
+
+        if (systems != null) {
+            for (ECS_System system : systems) {
+                system.notRunExceptSystems(exceptSystemName);
+            }
+        }
+    }
+
 }
