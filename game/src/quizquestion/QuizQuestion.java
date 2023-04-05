ead9e64f4a832cb53cf874dd0274ae130c599808
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-04-05 08:02:32 +0200

    make QuizQuestion record

--- game/src/quizquestion/QuizQuestion.java
+++ game/src/quizquestion/QuizQuestion.java
@@ -1,11 +1,6 @@
 package quizquestion;
 
-public class QuizQuestion {
-    private QuizQuestionContent question;
-    private QuizQuestionContent[] answers;
-    private QuizQuestionType type;
-
-    // later move into type builder  (bitte Kommentar übernehmen)
+public record QuizQuestion(QuizQuestionContent question,QuizQuestionContent[] answers,QuizQuestionType type) {
     public void askQuizQuestionWithUI() {
         QuizQuestionUI.showQuizQuestion(this);
     }
