dd3f69cdd83ed7ffd2ddba259bb34dc42ef4c5ce
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-04-05 08:24:46 +0200

    add javadoc for QuizQuestin

--- game/src/quizquestion/QuizQuestion.java
+++ game/src/quizquestion/QuizQuestion.java
@@ -1,7 +1,18 @@
 package quizquestion;
-
+/**
+ * The QuizQuestion class represents a single quiz question, including the question itself, possible
+ * answer choices, and the type of question.
+ *
+ * @param question the QuizQuestionContent object representing the question text
+ * @param answers an array of QuizQuestionContent objects representing the answer choices
+ * @param type the QuizQuestionType representing the type of question
+ */
 public record QuizQuestion(
         QuizQuestionContent question, QuizQuestionContent[] answers, QuizQuestionType type) {
+    /**
+     * Asks the quiz question in a graphical user interface by calling the showQuizQuestion() method
+     * of the QuizQuestionUI class.
+     */
     public void askQuizQuestionWithUI() {
         QuizQuestionUI.showQuizQuestion(this);
     }
