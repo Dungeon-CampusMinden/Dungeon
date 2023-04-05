bf37c9992390532cbe9f85fafa45e88ffb93dddf
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-04-05 08:27:13 +0200

    merge QuizQuestionType enum into QuizQuestion class

--- game/src/quizquestion/QuizQuestion.java
+++ game/src/quizquestion/QuizQuestion.java
@@ -16,4 +16,18 @@ public record QuizQuestion(
     public void askQuizQuestionWithUI() {
         QuizQuestionUI.showQuizQuestion(this);
     }
+
+    /**
+     * The QuizQuestionType enum represents the different types of quiz questions that can be
+     * created. The available types are SINGLE_CHOICE, MULTIPLE_CHOICE, and FREETEXT. SINGLE_CHOICE
+     * represents a question with multiple answer choices, where the user is required to select one
+     * answer. MULTIPLE_CHOICE represents a question with multiple answer choices, where the user is
+     * allowed to select multiple answers. FREETEXT represents a question where the user is required
+     * to input their answer as text.
+     */
+    public enum QuizQuestionType {
+        SINGLE_CHOICE,
+        MULTIPLE_CHOICE,
+        FREETEXT
+    }
 }
