f282b65bd2a9320802dd5209418326d1a145b5e9
dkirshner <diana.kirzhner@fh-bielefeld.de>
2023-04-06 11:34:02 +0200

    Implementation of the dialogue for displaying contents of any size and pausing the game completed. Dialogue pauses all systems except DrawSystem after being called. fixes #481

---
+++ game/src/graphic/hud/TextDialog.java
@@ -0,0 +1,77 @@
+package graphic.hud;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+import tools.Constants;
+
+/** Contains Constructor, which immediately creates the dialogue including all its elements. */
+public final class TextDialog extends Dialog {
+
+    /** button ID (used when control is pressed) */
+    private static final String btnID = "confirm exit";
+
+    private static final String defaulMsg = "No message was load.";
+    private static final String defaultBtnMsg = "OK";
+    private boolean enable;
+    private static final int differenceMeasure = 200;
+
+    /**
+     * @param skin Skin for the dialogue (resources that can be used by UI widgets)
+     * @param outputMsg Content displayed in the scrollable label
+     * @param buttonMsg text for the button
+     * @param title Title of the dialogue
+     */
+    public TextDialog(Skin skin, String outputMsg, String buttonMsg, String title) {
+        super(title, skin);
+        enable = false;
+
+        if (outputMsg.trim().isEmpty()) outputMsg = defaulMsg;
+
+        Label labelContent = new Label(outputMsg, skin);
+        labelContent.setAlignment(Align.left);
+        labelContent.setColor(Color.WHITE);
+
+        Table scrollTable = new Table();
+        scrollTable.add(labelContent);
+        scrollTable.row();
+
+        ScrollPane scroller = new ScrollPane(scrollTable, skin);
+        scroller.setFadeScrollBars(false);
+        scroller.setScrollbarsVisible(true);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(scroller)
+                .size(
+                        Constants.WINDOW_WIDTH - differenceMeasure,
+                        Constants.WINDOW_HEIGHT - differenceMeasure);
+        this.addActor(table);
+
+        if (buttonMsg.trim().isEmpty()) buttonMsg = defaultBtnMsg;
+
+        button(buttonMsg, btnID);
+    }
+
+    /**
+     * sets identification whether the key has been pressed*
+     *
+     * @param isEnable Identification
+     */
+    public void setEnable(final boolean isEnable) {
+        enable = isEnable;
+    }
+
+    /**
+     * Provides information about the pressed Button
+     *
+     * @param object Object associated with the button
+     */
+    @Override
+    protected void result(final Object object) {
+        if (object.toString() == btnID) {
+            enable = false;
+            UITools.deleteDialogue();
+        }
+    }
+}
