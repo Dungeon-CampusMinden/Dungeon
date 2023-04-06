6fa7fcc06da7834e33863807cb7b74a9f0be2469
AHeinisch <aheinisch@fh-bielefeld.de>
2023-04-06 10:53:53 +0200

    add javadoc for WorldItemBuilder

--- game/src/ecs/items/WorldItemBuilder.java
+++ game/src/ecs/items/WorldItemBuilder.java
@@ -6,8 +6,17 @@ import ecs.components.PositionComponent;
 import ecs.entities.Entity;
 import tools.Point;
 
+/**
+ * Class which creates all needed Components for a basic WorldItem
+ */
 public class WorldItemBuilder {
 
+    /**
+     * Creates an Entity which then can be added to the game
+     *
+     * @param itemData the Data which should be given to the world Item
+     * @return the newly created Entity
+     */
     public static Entity buildWorldItem(ItemData itemData) {
         Entity droppedItem = new Entity();
         new PositionComponent(droppedItem, new Point(0, 0));
