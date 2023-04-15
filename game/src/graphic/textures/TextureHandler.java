a5bbadb89a02f232f8bcbcd1ad2c798f1e60d888
Tobias Grothe <tgrothe@users.noreply.github.com>
2023-04-15 09:27:12 +0200

    [GAME] fix TextureHandler
    - fixes #306
    - Additional firstFindFilter added, to filter all double textures

--- game/src/graphic/textures/TextureHandler.java
+++ game/src/graphic/textures/TextureHandler.java
@@ -2,12 +2,7 @@ package graphic.textures;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -46,6 +41,41 @@ public class TextureHandler {
         return pathMap.get(path).stream().map(FileHandle::path);
     }
 
+    private List<String> firstFindFilter(final List<String> pathsOriginal) {
+        List<String> pathsCopy = new ArrayList<>();
+        for (final String path1 : pathsOriginal) {
+            FileHandle fh1 = Gdx.files.absolute(path1);
+            boolean isFirst = true;
+            for (final String path2 : pathsCopy) {
+                FileHandle fh2 = Gdx.files.absolute(path2);
+                int differeces = 0;
+                FileHandle parent1 = fh1;
+                FileHandle parent2 = fh2;
+                while (true) {
+                    if (parent1.name().isEmpty() || parent2.name().isEmpty()) {
+                        if (parent1.name().isEmpty() ^ parent2.name().isEmpty()) {
+                            differeces++;
+                        }
+                        break;
+                    }
+                    if (!parent1.name().equals(parent2.name())) {
+                        differeces++;
+                    }
+                    parent1 = parent1.parent();
+                    parent2 = parent2.parent();
+                }
+                if (differeces > 1) {
+                    isFirst = false;
+                    break;
+                }
+            }
+            if (isFirst) {
+                pathsCopy.add(path1);
+            }
+        }
+        return pathsCopy;
+    }
+
     /**
      * Searches for all textures paths that matches with the given regular expression.
      *
@@ -57,9 +87,11 @@ public class TextureHandler {
      */
     public List<String> getTexturePaths(String regex) {
         Pattern pattern = Pattern.compile(regex);
-        return getAvailablePaths().stream()
-                .filter(pattern.asPredicate())
-                .flatMap(this::getTexturesForPath)
-                .collect(Collectors.toList());
+        List<String> paths =
+                getAvailablePaths().stream()
+                        .filter(pattern.asPredicate())
+                        .flatMap(this::getTexturesForPath)
+                        .collect(Collectors.toList());
+        return firstFindFilter(paths);
     }
 }
