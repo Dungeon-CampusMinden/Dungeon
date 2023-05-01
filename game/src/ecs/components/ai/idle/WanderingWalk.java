aaec425fb87d79d8b1ea534f34180b590db07210
Kerim <94744639+Skrpop@users.noreply.github.com>
2023-05-01 14:17:03 +0200

    Javadoc

--- game/src/ecs/components/ai/idle/WanderingWalk.java
+++ game/src/ecs/components/ai/idle/WanderingWalk.java
@@ -22,12 +22,23 @@ public class WanderingWalk implements IIdleAI {
     private GraphPath<Tile> currentPath;
     private int frameCounter = -1;
 
+    /**
+     * Konstruktor, der drei Argumente annimmt: radius, wanderDistance und pauseTime. Diese Variablen werden verwendet, um einen zufälligen Wanderpunkt für die Entität zu berechnen, zu dem sie sich bewegen soll.
+     * @param radius Die maximale Entfernung vom aktuellen Standort der Entität, die der Wanderpunkt haben kann.
+     * @param wanderDistance Die maximale Entfernung, die die Entität von ihrem aktuellen Standort entfernt wandern kann.
+     * @param pauseTime Die Zeit (in Millisekunden), die die Entität pausieren soll, bevor sie wieder zu wandern beginnt.
+     */
     public WanderingWalk(float radius, int wanderDistance, int pauseTime) {
         this.radius = radius;
         this.wanderDistance = wanderDistance;
         this.pauseFrames = pauseTime / (1000 / Constants.FRAME_RATE);
     }
 
+    /**
+     * Eine Methode, die einen zufälligen Wanderpunkt für die Entität berechnet.
+     * @param currentPosition Die aktuelle Position der Entität.
+     * @return Ein Point-Objekt, das den zufälligen Wanderpunkt darstellt.
+     */
     private Point getRandomWanderPoint(Point currentPosition) {
         float angle = random.nextFloat() * 2 * (float) Math.PI;
         float distance = random.nextFloat() * wanderDistance;
@@ -38,6 +49,10 @@ public class WanderingWalk implements IIdleAI {
         return new Point(newX, newY);
     }
 
+    /**
+     * Eine Methode, die aufgerufen wird, wenn die Entität keine anderen Aktionen ausführt. Sie enthält die Hauptlogik für die Wandering Walk-KI-Komponente.
+     * @param entity Die Entität, die sich bewegt.
+     */
     @Override
     public void idle(Entity entity) {
         PositionComponent position =
