0ee1b80df1af2d6d40c38bf52ef454b3266a76b5
Tobias Grothe <tgrothe@users.noreply.github.com>
2023-05-15 10:18:39 +0200

    fix GameTest.java, remove Hero

--- game/test/starter/GameTest.java
+++ game/test/starter/GameTest.java
@@ -2,7 +2,6 @@ package starter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
