b64231a15e1521b4b84cf2215a16eef977603e6f
Finn Watermann <finn.watermann@arcor.de>
2023-05-16 18:39:15 +0200

    Update JavaDoc of MissingComponentException

--- game/src/core/utils/components/MissingComponentException.java
+++ game/src/core/utils/components/MissingComponentException.java
@@ -4,8 +4,19 @@ import core.utils.logging.CustomLogLevel;
 
 import java.util.logging.Logger;
 
+/**
+ * Exception stating that a component is missing.
+ *
+ * <p>This exception is thrown by (e.g. a {@link core.System System}) when a required component is
+ * missing on an entity that is being processed.
+ */
 public class MissingComponentException extends NullPointerException {
 
+    /**
+     * Constructs a new MissingComponentException with the specified detail message.
+     *
+     * @param message Detail message. Should contain the name of the missing component.
+     */
     public MissingComponentException(String message) {
         super("Missing Component:" + message);
         Logger exceptionLogger = Logger.getLogger(this.getClass().getName());
