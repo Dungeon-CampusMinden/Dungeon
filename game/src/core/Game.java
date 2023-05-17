174c972dabcd6181db01741cc5f0b2ca59bf3282
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-05-17 13:33:24 +0200

    rearrange Game methods

--- game/src/core/Game.java
+++ game/src/core/Game.java
@@ -1,20 +1,14 @@
 package core;
 
-import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
-
-import static core.utils.logging.LoggerConfig.initBaseLogger;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 import contrib.configuration.KeyboardConfig;
 import contrib.entities.EntityFactory;
 import contrib.systems.*;
-
 import core.components.PositionComponent;
 import core.configuration.Configuration;
 import core.hud.UITools;
@@ -25,16 +19,19 @@ import core.level.generator.IGenerator;
 import core.level.generator.postGeneration.WallGenerator;
 import core.level.generator.randomwalk.RandomWalkGenerator;
 import core.level.utils.LevelSize;
-import core.systems.*;
+import core.systems.DrawSystem;
 import core.systems.PlayerSystem;
-import core.utils.*;
+import core.systems.VelocitySystem;
+import core.utils.Constants;
+import core.utils.DelayedSet;
+import core.utils.DungeonCamera;
+import core.utils.Point;
 import core.utils.components.MissingComponentException;
 import core.utils.components.draw.Painter;
 import core.utils.components.draw.TextureHandler;
 import core.utils.controller.AbstractController;
 import core.utils.controller.SystemController;
 import core.utils.logging.CustomLogLevel;
-
 import quizquestion.DummyQuizQuestionList;
 
 import java.io.IOException;
@@ -42,46 +39,59 @@ import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
-/** The heart of the framework. From here all strings are pulled. */
+import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
+import static core.utils.logging.LoggerConfig.initBaseLogger;
+
+/**
+ * The heart of the framework. From here all strings are pulled.
+ */
 public class Game extends ScreenAdapter implements IOnLevelLoader {
 
-    /** Currently used level-size configuration for generating new level */
+    /**
+     * All entities that are currently active in the dungeon
+     */
+    private static final Set<Entity> entities = new HashSet<>();
+    private static final Logger LOGGER = Logger.getLogger("Game");
+    /**
+     * Currently used level-size configuration for generating new level
+     */
     public static LevelSize LEVELSIZE = LevelSize.SMALL;
-
+    /**
+     * Contains all Controller of the Dungeon
+     */
+    public static List<AbstractController<?>> controller;
+    public static DungeonCamera camera;
+    /**
+     * List of all Systems in the ECS
+     */
+    public static SystemController systems = new SystemController();
+    public static ILevel currentLevel;
+    /**
+     * A handler for managing asset paths
+     */
+    private static TextureHandler handler;
+    private static Entity hero;
+    private static Game game;
     /**
      * The batch is necessary to draw ALL the stuff. Every object that uses draw need to know the
      * batch.
      */
     protected SpriteBatch batch;
-
-    /** Contains all Controller of the Dungeon */
-    public static List<AbstractController<?>> controller;
-
-    public static DungeonCamera camera;
-    /** Draws objects */
+    /**
+     * Draws objects
+     */
     protected Painter painter;
-
     protected LevelManager levelAPI;
-    /** Generates the level */
+    /**
+     * Generates the level
+     */
     protected IGenerator generator;
-
     private boolean doSetup = true;
-
-    /** A handler for managing asset paths */
-    private static TextureHandler handler;
-
-    /** All entities that are currently active in the dungeon */
-    private static final Set<Entity> entities = new HashSet<>();
-
-    /** List of all Systems in the ECS */
-    public static SystemController systems = new SystemController();
-
-    public static ILevel currentLevel;
-    private static Entity hero;
-    private static final Logger LOGGER = Logger.getLogger("Game");
-
     private DebuggerSystem debugger;
-    private static Game game;
+
+    // for singleton
+    private Game() {
+    }
 
     /**
      * Create a new Game instance if no instance currently exist.
@@ -93,102 +103,15 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
         return game;
     }
 
-    // for singleton
-    private Game() {}
-
     /**
      * Will inform each system that the given entity has changes in its Component-Collection.
      *
-     * @see DelayedSet
      * @param entity Entity that has changes in its Component-Collection.
+     * @see DelayedSet
      */
     public static void informAboutChanges(Entity entity) {
         LOGGER.info(entity + "was updated in Game.");
         systems.forEach(system -> system.showEntity(entity));
-    }
-
-    /**
-     * Main game loop. Redraws the dungeon and calls the own implementation (beginFrame, endFrame
-     * and onLevelLoad).
-     *
-     * @param delta Time since last loop.
-     */
-    @Override
-    public void render(float delta) {
-        if (doSetup) setup();
-        batch.setProjectionMatrix(camera.combined);
-        frame();
-        clearScreen();
-        levelAPI.update();
-        controller.forEach(AbstractController::update);
-        camera.update();
-    }
-
-    /** Called once at the beginning of the game. */
-    protected void setup() {
-        doSetup = false;
-        /*
-         * THIS EXCEPTION HANDLING IS A TEMPORARY WORKAROUND !
-         *
-         * <p>The TextureHandler can throw an exception when it is first created. This exception
-         * (IOException) must be handled somewhere. Normally we want to pass exceptions to the method
-         * caller. This approach is (atm) not possible in the libgdx render method because Java does
-         * not allow extending method signatures derived from a class. We should try to make clean
-         * code out of this workaround later.
-         *
-         * <p>Please see also discussions at:<br>
-         * - https://github.com/Programmiermethoden/Dungeon/pull/560<br>
-         * - https://github.com/Programmiermethoden/Dungeon/issues/587<br>
-         */
-        try {
-            handler = TextureHandler.getInstance();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        controller = new ArrayList<>();
-        batch = new SpriteBatch();
-        setupCameras();
-        painter = new Painter(batch, camera);
-        generator = new RandomWalkGenerator();
-        levelAPI = new LevelManager(batch, painter, generator, this);
-        initBaseLogger();
-        controller.add(systems);
-        hero = EntityFactory.getHero();
-        levelAPI =
-                new LevelManager(
-                        batch, painter, new WallGenerator(new RandomWalkGenerator()), this);
-        levelAPI.loadLevel(LEVELSIZE);
-        createSystems();
-    }
-
-    /** Called at the beginning of each frame. Before the controllers call <code>update</code>. */
-    protected void frame() {
-        getHero().ifPresent(this::loadNextLevelIfEntityIsOnEndTile);
-        setCameraFocus();
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
-            // Text Dialogue (output of information texts)
-            UITools.showInfoText(Constants.DEFAULT_MESSAGE);
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.F)) {
-            // Dialogue for quiz questions (display of quiz questions and the answer area in test
-            // mode)
-            DummyQuizQuestionList.getRandomQuestion().askQuizQuestionWithUI();
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) UITools.showInfoText();
-        if (Gdx.input.isKeyJustPressed(KeyboardConfig.DEBUG_TOGGLE_KEY.get())) {
-            debugger.toggleRun();
-            LOGGER.info("Debugger ist now " + debugger.isRunning());
-        }
-    }
-
-    @Override
-    public void onLevelLoad() {
-        currentLevel = levelAPI.getCurrentLevel();
-        removeAllEntities();
-        getHero().ifPresent(Game::addEntity);
-        getHero().ifPresent(Game::informAboutChanges);
-        EntityFactory.getChest();
-        getHero().ifPresent(this::placeOnLevelStart);
     }
 
     /**
@@ -199,46 +122,6 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
     public static void removeAllEntities() {
         systems.forEach(System::clearEntities);
         entities.clear();
-    }
-
-    private void setCameraFocus() {
-        if (getHero().isPresent()) {
-            PositionComponent pc =
-                    (PositionComponent)
-                            getHero()
-                                    .get()
-                                    .getComponent(PositionComponent.class)
-                                    .orElseThrow(
-                                            () ->
-                                                    new MissingComponentException(
-                                                            "PositionComponent"));
-            camera.setFocusPoint(pc.getPosition());
-
-        } else camera.setFocusPoint(new Point(0, 0));
-    }
-
-    private void loadNextLevelIfEntityIsOnEndTile(Entity hero) {
-        if (isOnEndTile(hero)) levelAPI.loadLevel(LEVELSIZE);
-    }
-
-    private boolean isOnEndTile(Entity entity) {
-        PositionComponent pc =
-                (PositionComponent)
-                        entity.getComponent(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
-        Tile currentTile = currentLevel.getTileAt(pc.getPosition().toCoordinate());
-        return currentTile.equals(currentLevel.getEndTile());
-    }
-
-    private void placeOnLevelStart(Entity hero) {
-        entities.add(hero);
-        PositionComponent pc =
-                (PositionComponent)
-                        hero.getComponent(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
-        pc.setPosition(currentLevel.getStartTile().getCoordinate().toPoint());
     }
 
     public static TextureHandler getHandler() {
@@ -291,6 +174,172 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
         Game.hero = hero;
     }
 
+    /**
+     * Load the configuration from the given path. If the configuration has already been loaded, the
+     * cached version will be used.
+     *
+     * @param pathAsString Path to the config-file as String
+     * @param klass        Class where the ConfigKey field are located.
+     * @throws IOException If the file could not be read
+     */
+    public static void loadConfig(String pathAsString, Class<?> klass) throws IOException {
+        Configuration.loadAndGetConfiguration(pathAsString, klass);
+    }
+
+    /**
+     * Starts the dungeon and needs a {@link Game}.
+     */
+    public static void run() {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowSizeLimits(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT, 9999, 9999);
+        // The third and fourth parameters ("maxWidth" and "maxHeight") affect the resizing
+        // behavior
+        // of the window. If the window is enlarged or maximized, then it can assume these
+        // dimensions at maximum. If you have a larger screen resolution than 9999x9999 pixels,
+        // increase these parameters.
+        config.setForegroundFPS(Constants.FRAME_RATE);
+        config.setTitle(Constants.WINDOW_TITLE);
+        config.setWindowIcon(Constants.LOGO_PATH);
+        // config.disableAudio(true);
+        // uncomment this if you wish no audio
+        new Lwjgl3Application(
+            new com.badlogic.gdx.Game() {
+                @Override
+                public void create() {
+                    setScreen(Game.newGame());
+                }
+            },
+            config);
+    }
+
+    /**
+     * Main game loop. Redraws the dungeon and calls the own implementation (beginFrame, endFrame
+     * and onLevelLoad).
+     *
+     * @param delta Time since last loop.
+     */
+    @Override
+    public void render(float delta) {
+        if (doSetup) setup();
+        batch.setProjectionMatrix(camera.combined);
+        frame();
+        clearScreen();
+        levelAPI.update();
+        controller.forEach(AbstractController::update);
+        camera.update();
+    }
+
+    /**
+     * Called once at the beginning of the game.
+     */
+    protected void setup() {
+        doSetup = false;
+        /*
+         * THIS EXCEPTION HANDLING IS A TEMPORARY WORKAROUND !
+         *
+         * <p>The TextureHandler can throw an exception when it is first created. This exception
+         * (IOException) must be handled somewhere. Normally we want to pass exceptions to the method
+         * caller. This approach is (atm) not possible in the libgdx render method because Java does
+         * not allow extending method signatures derived from a class. We should try to make clean
+         * code out of this workaround later.
+         *
+         * <p>Please see also discussions at:<br>
+         * - https://github.com/Programmiermethoden/Dungeon/pull/560<br>
+         * - https://github.com/Programmiermethoden/Dungeon/issues/587<br>
+         */
+        try {
+            handler = TextureHandler.getInstance();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        controller = new ArrayList<>();
+        batch = new SpriteBatch();
+        setupCameras();
+        painter = new Painter(batch, camera);
+        generator = new RandomWalkGenerator();
+        levelAPI = new LevelManager(batch, painter, generator, this);
+        initBaseLogger();
+        controller.add(systems);
+        hero = EntityFactory.getHero();
+        levelAPI =
+            new LevelManager(
+                batch, painter, new WallGenerator(new RandomWalkGenerator()), this);
+        levelAPI.loadLevel(LEVELSIZE);
+        createSystems();
+    }
+
+    /**
+     * Called at the beginning of each frame. Before the controllers call <code>update</code>.
+     */
+    protected void frame() {
+        getHero().ifPresent(this::loadNextLevelIfEntityIsOnEndTile);
+        setCameraFocus();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
+            // Text Dialogue (output of information texts)
+            UITools.showInfoText(Constants.DEFAULT_MESSAGE);
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.F)) {
+            // Dialogue for quiz questions (display of quiz questions and the answer area in test
+            // mode)
+            DummyQuizQuestionList.getRandomQuestion().askQuizQuestionWithUI();
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) UITools.showInfoText();
+        if (Gdx.input.isKeyJustPressed(KeyboardConfig.DEBUG_TOGGLE_KEY.get())) {
+            debugger.toggleRun();
+            LOGGER.info("Debugger ist now " + debugger.isRunning());
+        }
+    }
+
+    @Override
+    public void onLevelLoad() {
+        currentLevel = levelAPI.getCurrentLevel();
+        removeAllEntities();
+        getHero().ifPresent(Game::addEntity);
+        getHero().ifPresent(Game::informAboutChanges);
+        EntityFactory.getChest();
+        getHero().ifPresent(this::placeOnLevelStart);
+    }
+
+    private void setCameraFocus() {
+        if (getHero().isPresent()) {
+            PositionComponent pc =
+                (PositionComponent)
+                    getHero()
+                        .get()
+                        .getComponent(PositionComponent.class)
+                        .orElseThrow(
+                            () ->
+                                new MissingComponentException(
+                                    "PositionComponent"));
+            camera.setFocusPoint(pc.getPosition());
+
+        } else camera.setFocusPoint(new Point(0, 0));
+    }
+
+    private void loadNextLevelIfEntityIsOnEndTile(Entity hero) {
+        if (isOnEndTile(hero)) levelAPI.loadLevel(LEVELSIZE);
+    }
+
+    private boolean isOnEndTile(Entity entity) {
+        PositionComponent pc =
+            (PositionComponent)
+                entity.getComponent(PositionComponent.class)
+                    .orElseThrow(
+                        () -> new MissingComponentException("PositionComponent"));
+        Tile currentTile = currentLevel.getTileAt(pc.getPosition().toCoordinate());
+        return currentTile.equals(currentLevel.getEndTile());
+    }
+
+    private void placeOnLevelStart(Entity hero) {
+        entities.add(hero);
+        PositionComponent pc =
+            (PositionComponent)
+                hero.getComponent(PositionComponent.class)
+                    .orElseThrow(
+                        () -> new MissingComponentException("PositionComponent"));
+        pc.setPosition(currentLevel.getStartTile().getCoordinate().toPoint());
+    }
+
     private void clearScreen() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
@@ -315,41 +364,5 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
         new SkillSystem();
         new ProjectileSystem();
         debugger = new DebuggerSystem();
-    }
-
-    /**
-     * Load the configuration from the given path. If the configuration has already been loaded, the
-     * cached version will be used.
-     *
-     * @param pathAsString Path to the config-file as String
-     * @param klass Class where the ConfigKey field are located.
-     * @throws IOException If the file could not be read
-     */
-    public static void loadConfig(String pathAsString, Class<?> klass) throws IOException {
-        Configuration.loadAndGetConfiguration(pathAsString, klass);
-    }
-
-    /** Starts the dungeon and needs a {@link Game}. */
-    public static void run() {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowSizeLimits(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT, 9999, 9999);
-        // The third and fourth parameters ("maxWidth" and "maxHeight") affect the resizing
-        // behavior
-        // of the window. If the window is enlarged or maximized, then it can assume these
-        // dimensions at maximum. If you have a larger screen resolution than 9999x9999 pixels,
-        // increase these parameters.
-        config.setForegroundFPS(Constants.FRAME_RATE);
-        config.setTitle(Constants.WINDOW_TITLE);
-        config.setWindowIcon(Constants.LOGO_PATH);
-        // config.disableAudio(true);
-        // uncomment this if you wish no audio
-        new Lwjgl3Application(
-                new com.badlogic.gdx.Game() {
-                    @Override
-                    public void create() {
-                        setScreen(Game.newGame());
-                    }
-                },
-                config);
     }
 }
