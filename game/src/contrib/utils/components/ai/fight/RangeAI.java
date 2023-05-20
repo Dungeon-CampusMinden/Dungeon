3c3ed6812ee3d70bbd9498f3bdf6e0247c9e6b30
Cadell, Marlon <Marlon.Cadell@wago.com>
2023-05-20 02:15:47 +0200

    rangeCombatAi finished

--- game/src/contrib/utils/components/ai/fight/RangeAI.java
+++ game/src/contrib/utils/components/ai/fight/RangeAI.java
@@ -13,8 +13,12 @@ import core.utils.components.MissingComponentException;
 
 import java.util.List;
 
+<<<<<<< HEAD:game/src/contrib/utils/components/ai/fight/RangeAI.java
 import static com.badlogic.gdx.math.MathUtils.random;
 import static contrib.utils.components.ai.AITools.getAccessibleTilesInRange;
+=======
+import static ecs.components.ai.AITools.getAccessibleTilesInRange;
+>>>>>>> cb051f2 (rangeCombatAi finished):game/src/ecs/components/ai/fight/RangeAI.java
 
 
 public class RangeAI implements IFightAI {
@@ -22,7 +26,6 @@ public class RangeAI implements IFightAI {
     private final float attackRange;
     private final float distance;
     private final Skill skill;
-    boolean isNotBetweenAttackAndDistance = true;
     private GraphPath<Tile> path;
 
     /**
@@ -34,6 +37,12 @@ public class RangeAI implements IFightAI {
      */
 
     public RangeAI(float attackRange, float distance, Skill skill){
+        if(attackRange <= distance || distance < 0){
+            throw new Error("attackRange must be greater than distance and distance must be 0 or greater than 0");
+        }
+        if(Game.getHero().isEmpty()){
+            throw new Error("There must be a Hero in the Game!");
+        }
         this.attackRange = attackRange;
         this.distance = distance;
         this.skill = skill;
@@ -41,16 +50,17 @@ public class RangeAI implements IFightAI {
 
     @Override
     public void fight(Entity entity) {
-        if(attackRange > distance) {
-            if (AITools.playerInRange(entity, distance) && isNotBetweenAttackAndDistance) {
-                isNotBetweenAttackAndDistance = false;
-                Point positionHero = getPosition(Game.getHero().get());
+        boolean playerInDistanceRange = AITools.playerInRange(entity, distance);
+        boolean playerInAttackRange = AITools.playerInRange(entity, attackRange);
+
+        if (playerInAttackRange) {
+            if(playerInDistanceRange) {
+                Point positionHero = getPosition(Game.getHero().orElseThrow());
                 Point positionEntity = getPosition(entity);
                 List<Tile> tiles = getAccessibleTilesInRange(positionEntity, attackRange - distance);
-                Point newPosition;
                 boolean newPositionFound = false;
-                for (int index = 0; index < tiles.size(); index++) {
-                    newPosition = tiles.get(index).getCoordinate().toPoint();
+                for (Tile tile : tiles) {
+                    Point newPosition = tile.getCoordinate().toPoint();
                     if (!AITools.inRange(newPosition, positionHero, distance)) {
                         path = AITools.calculatePath(positionEntity, newPosition);
                         newPositionFound = true;
@@ -58,33 +68,28 @@ public class RangeAI implements IFightAI {
                     }
                 }
                 if (!newPositionFound) {
-                    tiles = getAccessibleTilesInRange(positionHero, distance);
-                    newPosition = tiles.get(random.nextInt(tiles.size())).getCoordinate().toPoint();
-                    path = AITools.calculatePath(positionEntity, newPosition);
+                    path = AITools.calculatePathToRandomTileInRange(entity, 2 * attackRange);
                 }
-            }
-
-            if (!AITools.playerInRange(entity, attackRange) && isNotBetweenAttackAndDistance) {
-                isNotBetweenAttackAndDistance = false;
-                path = AITools.calculatePathToHero(entity);
-            }
-
-            if (!(AITools.playerInRange(entity, attackRange) && !AITools.playerInRange(entity, distance))) {
                 AITools.move(entity, path);
-                isNotBetweenAttackAndDistance = true;
-            } else {
+            }
+            else{
                 skill.execute(entity);
             }
         }
+        else {
+            path = AITools.calculatePathToHero(entity);
+            AITools.move(entity, path);
+        }
     }
 
-    private Point getPosition(Entity entity){
+
+    private Point getPosition(Entity entity) {
         return ((PositionComponent)
             entity.getComponent(PositionComponent.class)
                 .orElseThrow(
                     () ->
                         new MissingComponentException(
-                            "PositionComponent")))
+                            entity.getClass().getName() + "is missing PositionComponent")))
             .getPosition();
     }
 }
