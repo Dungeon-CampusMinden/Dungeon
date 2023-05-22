a060f6c8d35ecffc70b326c7dea210d26ca8e804
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-05-22 11:25:24 +0200

    add javadoc to Component

--- game/src/core/Component.java
+++ game/src/core/Component.java
@@ -2,15 +2,24 @@ package core;
 
 import java.util.logging.Logger;
 
-/*+
- *Component is a piece of data associated with an entity
+/**
+ * Components store the data (or attributes) for an associated {@link Entity}.
+ *
+ * <p>This class is the abstract base class for each component.</p>
+ *
+ * <p>Each component is linked to exactly one entity. Use {@link #getEntity} to get the associated entity
+ * of the component.</p>
+ *
+ * <p>Each component will automatically add itself to the associated entity using {@link Entity#addComponent}.</p>
+ *
+ * <p>Components are used to describe an entity. {@link System}s will check the components of an entity
+ * and decide if they want to process the entity. The systems will then modify the values of the data
+ * stored in the components.</p>
+ *
+ * <p>Remember that an entity can only store one component of each component class.</p>
  */
 public abstract class Component {
     protected Entity entity;
-
-    /**
-     * @param entity associated with this component
-     */
 
     /**
      * Create a new component and add it to the associated entity
