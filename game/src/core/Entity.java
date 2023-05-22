d9540cc8d554afda0911206f7b6e21ca8686193d
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-05-22 11:20:28 +0200

    add javadoc to entity

--- game/src/core/Entity.java
+++ game/src/core/Entity.java
@@ -7,7 +7,29 @@ import java.util.HashMap;
 import java.util.Optional;
 import java.util.logging.Logger;
 
-/** Entity is a unique identifier for an object in the game world */
+/**
+ * An Entity is a container for {@link Component}s.
+ *
+ * <p>A new entity will automatically register itself with the {@link Game} via {@link
+ * Game#addEntity} and will be added to the game on the next frame.
+ *
+ * <p>Add different components to an entity to define it. Based on the components inside an entity,
+ * the {@link System}s will decide whether to process the entity.
+ *
+ * <p>Use {@link #addComponent} to add a Component to this entity. Normally, a component will add
+ * itself to its associated entity, so you will not have to do it manually. Remember that an entity
+ * can only store one component of each component class. For example, your entity can't have two
+ * {@link core.components.DrawComponent}s.
+ *
+ * <p>If you want to remove a component from an entity, use {@link #removeComponent} and provide the
+ * Class of the component you want to remove as a parameter.
+ *
+ * <p>With {@link #getComponent}, you can check if the entity has a component of the given class.
+ *
+ * @see Component
+ * @see System
+ * @see Optional
+ */
 @DSLType(name = "game_object")
 @DSLContextPush(name = "entity")
 public final class Entity {
@@ -18,9 +40,9 @@ public final class Entity {
     private final HashMap<Class<? extends Component>, Component> components;
 
     /**
-     * Create a new Entity and register it in {@link Game}.
+     * Create a new Entity and register it in {@link Game} using {@link Game#addEntity}.
      *
-     * @param name name of the entity, use it for better logging and debugging
+     * @param name the name of the entity, used for better logging and debugging
      */
     public Entity(String name) {
         id = nextId++;
@@ -31,18 +53,24 @@ public final class Entity {
     }
 
     /**
-     * Create a new Entity and register it in {@link Game}.
+     * Create a new Entity and register it in {@link Game} using {@link Game#addEntity}.
      *
-     * <p>The name of the entity, will be the id.
+     * <p>The name of the entity will be its id
      */
     public Entity() {
         this("" + nextId);
     }
 
     /**
-     * Add a new component to this entity
+     * Add a new component to this entity.
      *
-     * @param component The component
+     * <p>Changes in the component map of the entity will trigger a call to {@link
+     * Game#informAboutChanges}.
+     *
+     * <p>Normally, a component will add itself to its associated entity, so you will not have to do
+     * it manually. Remember that an entity can only store one component of each component class.
+     *
+     * @param component The component to add
      */
     public void addComponent(Component component) {
         components.put(component.getClass(), component);
@@ -55,14 +83,18 @@ public final class Entity {
     }
 
     /**
-     * Remove a component from this entity
+     * Remove a component from this entity.
      *
-     * @param klass Class of the component
+     * <p>Changes in the component map of the entity will trigger a call to {@link
+     * Game#informAboutChanges}.
+     *
+     * @param klass the Class of the component
      */
     public void removeComponent(Class<? extends Component> klass) {
-        components.remove(klass);
-        LOGGER.info(klass.getName() + " from " + name + " was removed.");
-        Game.informAboutChanges(this);
+        if (components.remove(klass) != null) {
+            LOGGER.info(klass.getName() + " from " + name + " was removed.");
+            Game.informAboutChanges(this);
+        }
     }
 
     /**
@@ -70,13 +102,14 @@ public final class Entity {
      *
      * @param klass Class of the component
      * @return Optional that can contain the requested component
+     * @see Optional
      */
     public Optional<Component> getComponent(Class<? extends Component> klass) {
         return Optional.ofNullable(components.get(klass));
     }
 
     /**
-     * @return The id of this Entity
+     * @return The id of this entity
      */
     public int id() {
         return id;
