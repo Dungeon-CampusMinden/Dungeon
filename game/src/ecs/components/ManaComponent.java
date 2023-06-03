31ebfb3cb99bcc5bb8eb2d5cbae07572842efd83
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2023-06-03 15:22:22 +0200

    Create ManaComponent.java
    New ManaComponent for further use of mana in combination with skills

---
+++ game/src/ecs/components/ManaComponent.java
@@ -0,0 +1,89 @@
+package ecs.components;
+
+import java.util.logging.Logger;
+
+import ecs.entities.Entity;
+
+public class ManaComponent extends Component {
+
+    private int currentMana, maxMana;
+    private transient final Logger manaLogger = Logger.getLogger(this.getClass().getName());
+
+    /**
+     * Creates a new ManaComponent
+     * 
+     * @implNote Use this one to create a fully new manaComponent
+     * 
+     * @param entity  the entity that owns this component
+     * @param maxMana the maximum amount of mana that the entity can have (also the
+     *                current amount of mana)
+     * 
+     * @see #ManaComponent(Entity, int, int)
+     */
+    public ManaComponent(Entity entity, int maxMana) {
+        this(entity, maxMana, maxMana);
+    }
+
+    /**
+     * Creates a new ManaComponent
+     * 
+     * @implNote Use this one to replace an old manaComponent
+     * 
+     * @param entity      the entity that owns this component
+     * @param maxMana     the maximum amount of mana that the entity can have
+     * @param currentMana the current amount of mana that the entity has
+     * 
+     * @see #ManaComponent(Entity, int)
+     */
+    public ManaComponent(Entity entity, int maxMana, int currentMana) {
+        super(entity);
+        this.maxMana = maxMana;
+        this.currentMana = currentMana > maxMana ? maxMana : currentMana;
+        manaLogger.info("New ManaComponent created:: maxMana = " + maxMana + ", currentMana = " + currentMana);
+    }
+
+    /**
+     * Reduces the current mana if it is greater than the mana that shall be spent
+     * 
+     * @param amount mana that shall be spent
+     * @return {@code true} if {@code amount} is equal or less than
+     *         {@code currentMana} otherwise {@code false}
+     * 
+     * @see #hasMana(int)
+     */
+    public boolean spendMana(int amount) {
+        int manaSpent = hasMana(amount) ? amount : 0;
+        currentMana -= manaSpent;
+        manaLogger.info(manaSpent + " mana used");
+        return hasMana(amount);
+    }
+
+    /**
+     * Returns wether or not there is sufficient mana
+     * 
+     * @param amount amount of mana that shall be tested for
+     * @return {@code true} if {@code amount} is equal or less than
+     *         {@code currentMana} otherwise {@code false}
+     */
+    public boolean hasMana(int amount) {
+        return currentMana >= amount;
+    }
+
+    public void setCurrentMana(int currentMana) {
+        this.currentMana = currentMana > maxMana ? maxMana : currentMana;
+    }
+
+    public void setMaxMana(int maxMana) {
+        this.maxMana = maxMana;
+        this.currentMana = maxMana > currentMana ? currentMana : maxMana;
+    }
+
+    public int getCurrentMana() {
+        return currentMana;
+    }
+
+    public int getMaxMana() {
+        return maxMana;
+    }
+
+}
