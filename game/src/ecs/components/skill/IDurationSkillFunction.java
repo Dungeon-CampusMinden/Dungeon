5b7479ef9a45cd2b000deb999bef512c11fafe52
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2023-06-03 16:19:20 +0200

    Create IDurationSkillFunction.java
    A new Interface for DurationSkillFunctions

---
+++ game/src/ecs/components/skill/IDurationSkillFunction.java
@@ -0,0 +1,17 @@
+package ecs.components.skill;
+
+public interface IDurationSkillFunction extends ISkillFunction {
+
+    /**
+     * This one for sure is to be used to reduce the duration
+     */
+    public void reduceDuration();
+
+    /**
+     * Returns {@code true} if the skill is active
+     * 
+     * @return {@code true} if the skill is active
+     */
+    public boolean isActive();
+
+}
