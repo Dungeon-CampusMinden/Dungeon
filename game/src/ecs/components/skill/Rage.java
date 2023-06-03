5c96aeb82f1f72a8144642ad2a34bdfa5b94da5d
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2023-06-03 17:02:35 +0200

    Update Rage.java
    Bound the ManaComponent to the Rage Skill

--- game/src/ecs/components/skill/Rage.java
+++ game/src/ecs/components/skill/Rage.java
@@ -1,5 +1,6 @@
 package ecs.components.skill;
 
+import ecs.components.ManaComponent;
 import ecs.components.MissingComponentException;
 import ecs.components.stats.StatsComponent;
 import ecs.damage.DamageType;
@@ -9,6 +10,7 @@ import tools.Constants;
 public class Rage implements IDurationSkillFunction {
 
     private float durationInFrames, currentDurationInFrames = 0.0f, damageMultiplier = 1.0f, originalDamageMultiplier;
+    private int manaCost;
     private StatsComponent stats;
 
     /**
@@ -19,22 +21,41 @@ public class Rage implements IDurationSkillFunction {
      * @param durationIneconds time how long the skill lasts in seconds
      * @param damageMultiplier multiplier to increase the physical damage
      * @param entity           the entity that owns this Skill
+     * @param manaCost         the amount of mana needed to activate this Skill
      * 
-     * @throws MissngComponentException if the specified entity has no
-     *                                  StatsComponent
      */
-    public Rage(float durationIneconds, float damageMultiplier, Entity entity) {
+    public Rage(float durationIneconds, float damageMultiplier, Entity entity, int manaCost) {
         if (!entity.getComponent(StatsComponent.class).isPresent())
             throw new MissingComponentException("StatsComponent");
         durationInFrames = durationIneconds * Constants.FRAME_RATE;
         this.damageMultiplier = damageMultiplier > 1.0f ? damageMultiplier : 1.0f;
         stats = (StatsComponent) entity.getComponent(StatsComponent.class).get();
         originalDamageMultiplier = stats.getDamageModifiers().getMultiplier(DamageType.PHYSICAL);
+        this.manaCost = manaCost;
+    }
+
+    /**
+     * Creates a new Rage skill
+     * <p/>
+     * The Rage skill increases the Physical damage the wielder deals
+     * <p/>
+     * The standard {@code manaCost} is {@code 10}
+     * 
+     * @param durationIneconds time how long the skill lasts in seconds
+     * @param damageMultiplier multiplier to increase the physical damage
+     * @param entity           the entity that owns this Skill
+     * 
+     */
+    public Rage(float durationIneconds, float damageMultiplier, Entity entity) {
+        this(durationIneconds, damageMultiplier, entity, 10);
     }
 
     @Override
     public void execute(Entity entity) {
-        stats.getDamageModifiers().setMultiplier(DamageType.PHYSICAL, originalDamageMultiplier * damageMultiplier);
+        if (!entity.getComponent(ManaComponent.class).isPresent())
+            throw new MissingComponentException("ManaComponent");
+        if (entity.getComponent(ManaComponent.class).map(ManaComponent.class::cast).get().spendMana(manaCost))
+            stats.getDamageModifiers().setMultiplier(DamageType.PHYSICAL, originalDamageMultiplier * damageMultiplier);
     }
 
     /**
