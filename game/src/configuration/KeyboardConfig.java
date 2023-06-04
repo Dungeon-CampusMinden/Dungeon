93b1e76d7ae0363a90a03a65c7bb77339fefd51c
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2023-06-04 16:26:53 +0200

    Added Skill Keybinds
    Added Keybinds for 4 more skills

--- game/src/configuration/KeyboardConfig.java
+++ game/src/configuration/KeyboardConfig.java
@@ -3,56 +3,64 @@ package configuration;
 import com.badlogic.gdx.Input;
 import configuration.values.ConfigIntValue;
 
-@ConfigMap(path = {"keyboard"})
+@ConfigMap(path = { "keyboard" })
 public class KeyboardConfig {
 
-    public static final ConfigKey<Integer> MOVEMENT_UP =
-            new ConfigKey<>(new String[] {"movement", "up"}, new ConfigIntValue(Input.Keys.W));
-    public static final ConfigKey<Integer> MOVEMENT_DOWN =
-            new ConfigKey<>(new String[] {"movement", "down"}, new ConfigIntValue(Input.Keys.S));
-    public static final ConfigKey<Integer> MOVEMENT_LEFT =
-            new ConfigKey<>(new String[] {"movement", "left"}, new ConfigIntValue(Input.Keys.A));
-    public static final ConfigKey<Integer> MOVEMENT_RIGHT =
-            new ConfigKey<>(new String[] {"movement", "right"}, new ConfigIntValue(Input.Keys.D));
+    public static final ConfigKey<Integer> MOVEMENT_UP = new ConfigKey<>(new String[] { "movement", "up" },
+            new ConfigIntValue(Input.Keys.W));
+    public static final ConfigKey<Integer> MOVEMENT_DOWN = new ConfigKey<>(new String[] { "movement", "down" },
+            new ConfigIntValue(Input.Keys.S));
+    public static final ConfigKey<Integer> MOVEMENT_LEFT = new ConfigKey<>(new String[] { "movement", "left" },
+            new ConfigIntValue(Input.Keys.A));
+    public static final ConfigKey<Integer> MOVEMENT_RIGHT = new ConfigKey<>(new String[] { "movement", "right" },
+            new ConfigIntValue(Input.Keys.D));
 
-    public static final ConfigKey<Integer> INVENTORY =
-        new ConfigKey<>(new String[] {"inventory", "inventory"}, new ConfigIntValue(Input.Keys.I));
-    public static final ConfigKey<Integer> INVENTORY_FIRST =
-            new ConfigKey<>(new String[] {"inventory", "first"}, new ConfigIntValue(Input.Keys.NUM_1));
+    public static final ConfigKey<Integer> INVENTORY = new ConfigKey<>(new String[] { "inventory", "inventory" },
+            new ConfigIntValue(Input.Keys.I));
+    public static final ConfigKey<Integer> INVENTORY_FIRST = new ConfigKey<>(new String[] { "inventory", "first" },
+            new ConfigIntValue(Input.Keys.NUM_1));
 
-    public static final ConfigKey<Integer> INVENTORY_SECOND =
-        new ConfigKey<>(new String[] {"inventory", "second"}, new ConfigIntValue(Input.Keys.NUM_2));
+    public static final ConfigKey<Integer> INVENTORY_SECOND = new ConfigKey<>(new String[] { "inventory", "second" },
+            new ConfigIntValue(Input.Keys.NUM_2));
 
-    public static final ConfigKey<Integer> INVENTORY_THIRD =
-        new ConfigKey<>(new String[] {"inventory", "third"}, new ConfigIntValue(Input.Keys.NUM_3));
+    public static final ConfigKey<Integer> INVENTORY_THIRD = new ConfigKey<>(new String[] { "inventory", "third" },
+            new ConfigIntValue(Input.Keys.NUM_3));
 
-    public static final ConfigKey<Integer> INVENTORY_FOURTH =
-        new ConfigKey<>(new String[] {"inventory", "fourth"}, new ConfigIntValue(Input.Keys.NUM_4));
+    public static final ConfigKey<Integer> INVENTORY_FOURTH = new ConfigKey<>(new String[] { "inventory", "fourth" },
+            new ConfigIntValue(Input.Keys.NUM_4));
 
-    public static final ConfigKey<Integer> INVENTORY_FIFTH =
-        new ConfigKey<>(new String[] {"inventory", "fifth"}, new ConfigIntValue(Input.Keys.NUM_5));
+    public static final ConfigKey<Integer> INVENTORY_FIFTH = new ConfigKey<>(new String[] { "inventory", "fifth" },
+            new ConfigIntValue(Input.Keys.NUM_5));
 
-    public static final ConfigKey<Integer> INVENTORY_SIXTH =
-        new ConfigKey<>(new String[] {"inventory", "sixth"}, new ConfigIntValue(Input.Keys.NUM_6));
+    public static final ConfigKey<Integer> INVENTORY_SIXTH = new ConfigKey<>(new String[] { "inventory", "sixth" },
+            new ConfigIntValue(Input.Keys.NUM_6));
 
-    public static final ConfigKey<Integer> INVENTORY_SEVENTH =
-        new ConfigKey<>(new String[] {"inventory", "seventh"}, new ConfigIntValue(Input.Keys.NUM_7));
+    public static final ConfigKey<Integer> INVENTORY_SEVENTH = new ConfigKey<>(new String[] { "inventory", "seventh" },
+            new ConfigIntValue(Input.Keys.NUM_7));
 
-    public static final ConfigKey<Integer> INVENTORY_EIGHTH =
-        new ConfigKey<>(new String[] {"inventory", "eighth"}, new ConfigIntValue(Input.Keys.NUM_8));
+    public static final ConfigKey<Integer> INVENTORY_EIGHTH = new ConfigKey<>(new String[] { "inventory", "eighth" },
+            new ConfigIntValue(Input.Keys.NUM_8));
 
-    public static final ConfigKey<Integer> INVENTORY_NINTH =
-        new ConfigKey<>(new String[] {"inventory", "ninth"}, new ConfigIntValue(Input.Keys.NUM_9));
+    public static final ConfigKey<Integer> INVENTORY_NINTH = new ConfigKey<>(new String[] { "inventory", "ninth" },
+            new ConfigIntValue(Input.Keys.NUM_9));
 
-    public static final ConfigKey<Integer> INVENTORY_REMOVE =
-        new ConfigKey<>(new String[] {"inventory", "remove"}, new ConfigIntValue(Input.Keys.L));
+    public static final ConfigKey<Integer> INVENTORY_REMOVE = new ConfigKey<>(new String[] { "inventory", "remove" },
+            new ConfigIntValue(Input.Keys.L));
 
-    public static final ConfigKey<Integer> INTERACT_WORLD =
-            new ConfigKey<>(new String[] {"interact", "world"}, new ConfigIntValue(Input.Keys.E));
-    public static final ConfigKey<Integer> INTERACT_WORLD_X =
-            new ConfigKey<>(new String[] {"interact", "world_x"}, new ConfigIntValue(Input.Keys.X));
-    public static final ConfigKey<Integer> FIRST_SKILL =
-            new ConfigKey<>(new String[] {"skill", "first"}, new ConfigIntValue(Input.Keys.Q));
-    public static final ConfigKey<Integer> SECOND_SKILL =
-            new ConfigKey<>(new String[] {"skill", "second"}, new ConfigIntValue(Input.Keys.R));
+    public static final ConfigKey<Integer> INTERACT_WORLD = new ConfigKey<>(new String[] { "interact", "world" },
+            new ConfigIntValue(Input.Keys.E));
+    public static final ConfigKey<Integer> INTERACT_WORLD_X = new ConfigKey<>(new String[] { "interact", "world_x" },
+            new ConfigIntValue(Input.Keys.X));
+    public static final ConfigKey<Integer> FIRST_SKILL = new ConfigKey<>(new String[] { "skill", "first" },
+            new ConfigIntValue(Input.Keys.Q));
+    public static final ConfigKey<Integer> SECOND_SKILL = new ConfigKey<>(new String[] { "skill", "second" },
+            new ConfigIntValue(Input.Keys.R));
+    public static final ConfigKey<Integer> THIRD_SKILL = new ConfigKey<>(new String[] { "skill", "third" },
+            new ConfigIntValue(Input.Keys.C));
+    public static final ConfigKey<Integer> FOURTH_SKILL = new ConfigKey<>(new String[] { "skill", "fourth" },
+            new ConfigIntValue(Input.Keys.V));
+    public static final ConfigKey<Integer> FIFTH_SKILL = new ConfigKey<>(new String[] { "skill", "fifth" },
+            new ConfigIntValue(Input.Keys.B));
+    public static final ConfigKey<Integer> SIXTH_SKILL = new ConfigKey<>(new String[] { "skill", "sixth" },
+            new ConfigIntValue(Input.Keys.N));
 }
--- game/src/ecs/components/PlayableComponent.java
+++ game/src/ecs/components/PlayableComponent.java
@@ -7,9 +7,12 @@ import java.util.logging.Logger;
 import logging.CustomLogLevel;
 
 /**
- * This component is for the player character entity only. It should only be implemented by one
- * entity and mark this entity as the player character. This component stores data that is only
- * relevant for the player character. The PlayerSystems acts on the PlayableComponent.
+ * This component is for the player character entity only. It should only be
+ * implemented by one
+ * entity and mark this entity as the player character. This component stores
+ * data that is only
+ * relevant for the player character. The PlayerSystems acts on the
+ * PlayableComponent.
  */
 public class PlayableComponent extends Component {
 
@@ -18,9 +21,31 @@ public class PlayableComponent extends Component {
 
     private Skill skillSlot1;
     private Skill skillSlot2;
+    private Skill skillSlot3;
+    private Skill skillSlot4;
+    private Skill skillSlot5;
+    private Skill skillSlot6;
 
     /**
-     * @param entity associated entity
+     * @param entity     associated entity
+     * @param skillSlot1 skill that will be on the first skillslot
+     * @param skillSlot2 skill that will be on the second skillslot
+     * @param skillSlot3 skill that will be on the third skillslot
+     * @param skillSlot4 skill that will be on the fourth skillslot
+     * @param skillSlot5 skill that will be on the fifth skillslot
+     * @param skillSlot6 skill that will be on the sixth skillslot
+     */
+    PlayableComponent(Entity entity, Skill skillSlot1, Skill skillSlot2, Skill skillSlot3, Skill skillSlot4,
+            Skill skillSlot5, Skill skillSlot6) {
+        this(entity, skillSlot1, skillSlot2);
+        this.skillSlot3 = skillSlot3;
+        this.skillSlot4 = skillSlot4;
+        this.skillSlot5 = skillSlot5;
+        this.skillSlot6 = skillSlot6;
+    }
+
+    /**
+     * @param entity     associated entity
      * @param skillSlot1 skill that will be on the first skillslot
      * @param skillSlot2 skill that will be on the second skillslot
      */
@@ -65,10 +90,38 @@ public class PlayableComponent extends Component {
     }
 
     /**
-     * @param skillSlot2 skill that will be on the first skillslot
+     * @param skillSlot2 skill that will be on the second skillslot
      */
     public void setSkillSlot2(Skill skillSlot2) {
         this.skillSlot2 = skillSlot2;
+    }
+
+    /**
+     * @param skillSlot3 skill that will be on the third skillslot
+     */
+    public void setSkillSlot3(Skill skillSlot3) {
+        this.skillSlot3 = skillSlot3;
+    }
+
+    /**
+     * @param skillSlot4 skill that will be on the fourth skillslot
+     */
+    public void setSkillSlot4(Skill skillSlot4) {
+        this.skillSlot4 = skillSlot4;
+    }
+
+    /**
+     * @param skillSlot5 skill that will be on the fifth skillslot
+     */
+    public void setSkillSlot5(Skill skillSlot5) {
+        this.skillSlot5 = skillSlot5;
+    }
+
+    /**
+     * @param skillSlot6 skill that will be on the sixth skillslot
+     */
+    public void setSkillSlot6(Skill skillSlot6) {
+        this.skillSlot6 = skillSlot6;
     }
 
     /**
@@ -83,5 +136,33 @@ public class PlayableComponent extends Component {
      */
     public Optional<Skill> getSkillSlot2() {
         return Optional.ofNullable(skillSlot2);
+    }
+
+    /**
+     * @return skill on third skill slot
+     */
+    public Optional<Skill> getSkillSlot3() {
+        return Optional.ofNullable(skillSlot3);
+    }
+
+    /**
+     * @return skill on fourth skill slot
+     */
+    public Optional<Skill> getSkillSlot4() {
+        return Optional.ofNullable(skillSlot4);
+    }
+
+    /**
+     * @return skill on fifth skill slot
+     */
+    public Optional<Skill> getSkillSlot5() {
+        return Optional.ofNullable(skillSlot5);
+    }
+
+    /**
+     * @return skill on sixth skill slot
+     */
+    public Optional<Skill> getSkillSlot6() {
+        return Optional.ofNullable(skillSlot6);
     }
 }
