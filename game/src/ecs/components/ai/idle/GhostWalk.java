19e5f7e1f045aa29eca1bd04efcd3ad092b5d9a4
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2023-06-06 22:47:08 +0200

    Update GhostWalk.java
    even more and more refactoring

--- game/src/ecs/components/ai/idle/GhostWalk.java
+++ game/src/ecs/components/ai/idle/GhostWalk.java
@@ -31,10 +31,9 @@ public class GhostWalk implements IIdleAI {
         if (path == null) {
             path = AITools.calculatePathToHero(entity);
         }
-        if (!AITools.playerInRange(entity, 2) && !AITools.pathFinishedOrLeft(entity, path)) {
-            AITools.move(entity, path);
+        AITools.move(entity, path);
+        if (!AITools.playerInRange(entity, 2) && !AITools.pathFinishedOrLeft(entity, path))
             return;
-        }
         int randomPath = (int) (Math.random() * 1000);
         final int GO_TO_PLAYER = 990;
         final int DISAPPEAR = 0;
