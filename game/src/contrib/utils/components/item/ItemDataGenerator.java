8470a969a631a442c93aba91fa3f26623db5fdb4
Edwin Dik <dik.edwin122@gmail.com>
2023-06-06 15:38:00 +0200

    add assets
    * removed old temporary assets
    
    * created new directory for item assets
    
    * added new assets

--- game/src/contrib/utils/components/item/ItemDataGenerator.java
+++ game/src/contrib/utils/components/item/ItemDataGenerator.java
@@ -7,30 +7,30 @@ import java.util.Random;
 
 /** Generator which creates a random ItemData based on the Templates prepared. */
 public class ItemDataGenerator {
-    private static final List<String> bowTexture = List.of("animation/bronzeBow.png");
-    private static final List<String> bookTexture = List.of("animation/blueBook.png");
-    private static final List<String> bagTexture = List.of("animation/bag_small.png");
+    private static final List<String> boneTexture = List.of("items/bone.png");
+    private static final List<String> scrollTexture = List.of("items/scroll.png");
+    private static final List<String> leafTexture = List.of("items/leaf.png");
 
     private final List<ItemData> templates =
             List.of(
                     new ItemData(
                             ItemType.Basic,
-                            new Animation(bowTexture, 1),
-                            new Animation(bowTexture, 1),
-                            "Buch",
-                            "Ein sehr lehrreiches Buch."),
+                            new Animation(scrollTexture, 1),
+                            new Animation(scrollTexture, 1),
+                            "Schriftrolle",
+                            "Eine sehr lehrreiche Schriftrolle."),
                     new ItemData(
                             ItemType.Basic,
-                            new Animation(bagTexture, 1),
-                            new Animation(bagTexture, 1),
-                            "Tuch",
-                            "Ein sauberes Tuch.."),
+                            new Animation(boneTexture, 1),
+                            new Animation(boneTexture, 1),
+                            "Knochen",
+                            "Ein harter Knochen.."),
                     new ItemData(
                             ItemType.Active,
-                            new Animation(bookTexture, 1),
-                            new Animation(bookTexture, 1),
-                            "Namensschild",
-                            "Ein Namensschild wo der Name nicht mehr lesbar ist.."));
+                            new Animation(leafTexture, 1),
+                            new Animation(leafTexture, 1),
+                            "Blatt",
+                            "Ein Blatt..."));
     private final Random rand = new Random();
 
     /**
