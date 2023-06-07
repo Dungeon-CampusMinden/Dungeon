e04dd2d6b907c48e967211c9a9ef44da3796274f
Lena241 <33090382+Lena241@users.noreply.github.com>
2023-06-07 14:04:25 +0200

    [GAME] Implement ProtectOnAttack (#725)
    * [Game] Implement ITransition: ProtectOnAttack (#656)
    
    * Branch for PullRequest
    
    * -Bessere JavaDocs und Typo fixed
    -Liste zum HashSet und Parameter generisch gemacht
    -Werfe Exception wenn Entity ohne HealthComponent in Liste ist
    
    * -Fix JavaDocs
    -Ignore test of Hero until solution is found
    
    * Outsource ProtectOnApproach PR
    
    * Outsource ProtectOnAttack
    
    * ProtectOnAttack for new Dungeon Structure
    
    * small refactor
    
    ---------
    
    Co-authored-by: Pascal <101928634+Pascal378@users.noreply.github.com>

---
+++ game/src/contrib/utils/components/ai/transition/ProtectOnAttack.java
@@ -0,0 +1,73 @@
+package contrib.utils.components.ai.transition;
+
+import contrib.components.HealthComponent;
+
+import core.Entity;
+import core.components.PlayerComponent;
+import core.utils.components.MissingComponentException;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+
+/**
+ * Implements an AI that protects a specific entity with a HealthComponent, if the hero dealt damage
+ * to it.
+ *
+ * <p>Entity will stay in fight mode once entered.
+ */
+public class ProtectOnAttack implements Function<Entity, Boolean> {
+
+    private boolean isInFight = false;
+
+    private final Set<Entity> toProtect = new HashSet<>();
+
+    /**
+     * Constructor for one entity to protect
+     *
+     * @param entity to protect
+     */
+    public ProtectOnAttack(Entity entity) {
+        if (!entity.isPresent(HealthComponent.class)) {
+            throw (new MissingComponentException("HealthComponent"));
+        }
+
+        toProtect.add(entity);
+    }
+
+    /**
+     * Constructor for a list of entities to protect
+     *
+     * <p>Checks if HealthComponent isPresent and adds it to the list of entities to protect
+     *
+     * @param entities - Entities that are protected
+     */
+    public ProtectOnAttack(Collection<Entity> entities) {
+        entities.stream()
+                .peek(e -> e.getComponent(HealthComponent.class).orElseThrow())
+                .forEach(this.toProtect::add);
+    }
+
+    /**
+     * If lastDamage cause of an entity to protect has a playableComponent switch to fight mode
+     *
+     * @param entity associated entity
+     * @return True if entity is in fight mode, false if entity is not
+     */
+    @Override
+    public Boolean apply(Entity entity) {
+        if (isInFight) return true;
+
+        isInFight =
+                toProtect.stream()
+                        .map(e -> (HealthComponent) e.getComponent(HealthComponent.class).get())
+                        .anyMatch(
+                                e ->
+                                        e.getLastDamageCause()
+                                                .map(t -> t.getComponent(PlayerComponent.class))
+                                                .isPresent());
+
+        return isInFight;
+    }
+}
