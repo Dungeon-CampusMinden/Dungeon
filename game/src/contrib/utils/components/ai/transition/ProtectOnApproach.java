84e1b9a82872facb6addf5c6fa1f55108cb32a4e
Lena241 <33090382+Lena241@users.noreply.github.com>
2023-06-07 14:24:28 +0200

    [GAME] Implement ProtectOnApproach (#724)
    * [Game] Implement ITransition: ProtectOnApproach (#696)
    
    Outsource ProtectOnApproach
    
    * ProtectOnApproach in new structure
    
    * small refactor
    
    ---------
    
    Co-authored-by: Pascal <101928634+Pascal378@users.noreply.github.com>

---
+++ game/src/contrib/utils/components/ai/transition/ProtectOnApproach.java
@@ -0,0 +1,47 @@
+package contrib.utils.components.ai.transition;
+
+import contrib.utils.components.ai.AITools;
+
+import core.Entity;
+
+import java.util.function.Function;
+
+/**
+ * Implements an AI that protects an entity if hero is in the given range
+ *
+ * <p>Entity will stay in fight mode once entered
+ */
+public class ProtectOnApproach implements Function<Entity, Boolean> {
+    private final float range;
+
+    private final Entity toProtect;
+
+    private boolean isInFight = false;
+
+    /**
+     * Constructor needs a range and the entity to protect.
+     *
+     * @param range - The range in which the entity should in fight mode
+     * @param toProtect - The entity which should be protected
+     */
+    public ProtectOnApproach(float range, Entity toProtect) {
+        this.range = range;
+        this.toProtect = toProtect;
+    }
+
+    /**
+     * If protecting entity isn't in fight mode yet, check if player is in range of the protected
+     * entity
+     *
+     * @param entity that protects
+     * @return boolean
+     */
+    @Override
+    public Boolean apply(Entity entity) {
+        if (isInFight) return true;
+
+        isInFight = AITools.playerInRange(toProtect, range);
+
+        return isInFight;
+    }
+}
