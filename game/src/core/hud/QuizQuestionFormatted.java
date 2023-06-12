03cbdc29b99c9a9e917007c2411ac7a8365ffcd9
AHeinisch <aheinisch@fh-bielefeld.de>
2023-06-12 12:02:43 +0200

    add QuizQuestionFormatted Jdoc

--- game/src/core/hud/QuizQuestionFormatted.java
+++ game/src/core/hud/QuizQuestionFormatted.java
@@ -1,43 +1,16 @@
 package core.hud;
 
-import core.utils.Constants;
-
-import quizquestion.QuizQuestion;
-
+/**
+ * A basic QuizQuestion String formatter which creates new linebreaks when the VirtualWindowWidth is
+ * not big large enough.
+ */
 public class QuizQuestionFormatted {
 
     /**
      * Limits the length of the string to 40 characters, after which a line break occurs
-     * automatically.
+     * automatically. BlackMagic number based on Fontsize, VirtualWindowSize and DialogPadding
      */
     private static final int MAX_ROW_LENGTH = 40;
-
-    public static QuizQuestionFormatted parse(QuizQuestion question) {
-        return new QuizQuestionFormatted();
-    }
-
-    /**
-     * All values for the variable Identifier are correctly read from the parameter arrayOfMessages
-     * and assigned to it.
-     *
-     * @param formatIdentifier Parameters that are passed to the dialogue
-     * @param arrayOfMessages Parameters defined by the user
-     */
-    private void setupMessagesForIdentifier(String[] formatIdentifier, String... arrayOfMessages) {
-        final int inputArraySize = arrayOfMessages.length;
-        final int outputArraySize = formatIdentifier.length;
-        final String[] defaultCaptions = {
-            Constants.DEFAULT_MESSAGE, Constants.DEFAULT_BUTTON_MESSAGE, Constants.DEFAULT_HEADING
-        };
-
-        for (int counter = 0; counter < outputArraySize; counter++) {
-            if (inputArraySize > counter
-                    && arrayOfMessages[counter] != null
-                    && arrayOfMessages[counter].length() > 0)
-                formatIdentifier[counter] = arrayOfMessages[counter];
-            else formatIdentifier[counter] = defaultCaptions[counter];
-        }
-    }
 
     /**
      * creates line breaks after a word once a certain char count is reached
