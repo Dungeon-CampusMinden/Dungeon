ff11ea50b40ad7a10bc20c79530fac3c3f35ade9
Lena241 <33090382+Lena241@users.noreply.github.com>
2023-06-14 14:00:48 +0200

    some tweaks

--- game/test/contrib/utils/components/skill/SkillTest.java
+++ game/test/contrib/utils/components/skill/SkillTest.java
@@ -3,60 +3,60 @@ package contrib.utils.components.skill;
 import static org.junit.Assert.*;
 
 import core.Entity;
+import core.Game;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class SkillTest {
 
     private static int value = 0;
-    private final int baseCoolDownInSeconds = 5;
 
-    private Entity entity;
-    private Skill skill;
     private Consumer<Entity> skillFunction = entity -> value++;
-
-    @Before
-    public void setup() {
-        entity = new Entity();
-        skill = new Skill(skillFunction, baseCoolDownInSeconds);
-    }
 
     @After
     public void cleanup() {
         value = 0;
+        Game.removeAllEntities();
     }
 
     @Test
     public void execute() {
-        assertTrue(skill.canBeUsedAgain());
+        Entity entity = new Entity();
+        final int baseCoolDownInSeconds = 2;
+        Skill skill = new Skill(skillFunction, baseCoolDownInSeconds);
+
+        assertTrue("Skill should be executable", skill.canBeUsedAgain());
         skill.execute(entity);
-        assertEquals(1, value);
+        assertEquals("Skill should have been executed once", 1, value);
+        assertFalse("Skill is in cool down and can not be executable", skill.canBeUsedAgain());
     }
 
     @Test
     public void executeWhenCoolDownActive() {
+        Entity entity = new Entity();
+        final int baseCoolDownInSeconds = 2;
+        Skill skill = new Skill(skillFunction, baseCoolDownInSeconds);
+
         skill.execute(entity);
-        assertFalse(skill.canBeUsedAgain());
-        assertEquals(1, value);
+        assertFalse("Skill should not be executable", skill.canBeUsedAgain());
+        assertEquals("Skill should have been executed once", 1, value);
     }
 
     @Test
     public void executeWhenCoolDownExpired() {
+        Entity entity = new Entity();
+        final long baseCoolDown = TimeUnit.MILLISECONDS.toSeconds(1);
+        Skill skill = new Skill(skillFunction, baseCoolDown);
+
         skill.execute(entity);
+        assertEquals("Skill should have been executed once", 1, value);
+        assertTrue("Skill should be usable again", skill.canBeUsedAgain());
 
-        Instant afterCoolDown = Instant.now().plusSeconds(baseCoolDownInSeconds);
-
-        while (Instant.now().isBefore(afterCoolDown)) {
-            assertEquals(1, value);
-        }
-
-        assertTrue(skill.canBeUsedAgain());
         skill.execute(entity);
-        assertEquals(2, value);
+        assertEquals("Skill should have been executed twice", 2, value);
     }
 }
