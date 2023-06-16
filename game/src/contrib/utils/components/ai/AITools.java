d9a279b0925082d6961c1d0eec7b8eb75bd05c56
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-06-16 09:57:04 +0200

    AITools: use classCast, make parameter final

--- game/src/contrib/utils/components/ai/AITools.java
+++ game/src/contrib/utils/components/ai/AITools.java
@@ -23,21 +23,23 @@ public class AITools {
      * @param entity Entity moving on the path
      * @param path Path on which the entity moves
      */
-    public static void move(Entity entity, GraphPath<Tile> path) {
+    public static void move(final Entity entity, final GraphPath<Tile> path) {
         // entity is already at the end
         if (pathFinishedOrLeft(entity, path)) {
             return;
         }
         PositionComponent pc =
-                (PositionComponent)
-                        entity.fetch(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
+                entity.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity, PositionComponent.class));
         VelocityComponent vc =
-                (VelocityComponent)
-                        entity.fetch(VelocityComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("VelocityComponent"));
+                entity.fetch(VelocityComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity, VelocityComponent.class));
         Tile currentTile = Game.tileAT(pc.position());
         int i = 0;
         Tile nextTile = null;
@@ -72,7 +74,7 @@ public class AITools {
      * @param radius Search radius
      * @return List of tiles in the given radius arround the center point
      */
-    public static List<Tile> tilesInRange(Point center, float radius) {
+    public static List<Tile> tilesInRange(final Point center, final float radius) {
         List<Tile> tiles = new ArrayList<>();
         for (float x = center.x - radius; x <= center.x + radius; x++) {
             for (float y = center.y - radius; y <= center.y + radius; y++) {
@@ -88,7 +90,7 @@ public class AITools {
      * @param radius Search radius
      * @return List of accessible tiles in the given radius arround the center point
      */
-    public static List<Tile> accessibleTilesInRange(Point center, float radius) {
+    public static List<Tile> accessibleTilesInRange(final Point center, final float radius) {
         List<Tile> tiles = tilesInRange(center, radius);
         tiles.removeIf(tile -> !tile.isAccessible());
         return tiles;
@@ -99,7 +101,8 @@ public class AITools {
      * @param radius search radius
      * @return random tile in given range
      */
-    public static Coordinate randomAccessibleTileCoordinateInRange(Point center, float radius) {
+    public static Coordinate randomAccessibleTileCoordinateInRange(
+            final Point center, final float radius) {
         List<Tile> tiles = accessibleTilesInRange(center, radius);
         Coordinate newPosition = tiles.get(random.nextInt(tiles.size())).coordinate();
         return newPosition;
@@ -110,7 +113,7 @@ public class AITools {
      * @param to end point
      * @return Path from the start point to the end point
      */
-    public static GraphPath<Tile> calculatePath(Point from, Point to) {
+    public static GraphPath<Tile> calculatePath(final Point from, final Point to) {
         return calculatePath(from.toCoordinate(), to.toCoordinate());
     }
 
@@ -119,7 +122,7 @@ public class AITools {
      * @param to end coordinate
      * @return Path from the start coordinate to the end coordinate
      */
-    public static GraphPath<Tile> calculatePath(Coordinate from, Coordinate to) {
+    public static GraphPath<Tile> calculatePath(final Coordinate from, final Coordinate to) {
         return Game.findPath(Game.tileAT(from), Game.tileAT(to));
     }
 
@@ -131,7 +134,8 @@ public class AITools {
      * @param radius Search radius
      * @return Path from the center point to the randomly selected tile
      */
-    public static GraphPath<Tile> calculatePathToRandomTileInRange(Point point, float radius) {
+    public static GraphPath<Tile> calculatePathToRandomTileInRange(
+            final Point point, final float radius) {
         Coordinate newPosition = randomAccessibleTileCoordinateInRange(point, radius);
         return calculatePath(point.toCoordinate(), newPosition);
     }
@@ -144,14 +148,14 @@ public class AITools {
      * @param radius Search radius
      * @return Path from the position of the entity to the randomly selected tile
      */
-    public static GraphPath<Tile> calculatePathToRandomTileInRange(Entity entity, float radius) {
+    public static GraphPath<Tile> calculatePathToRandomTileInRange(
+            final Entity entity, final float radius) {
         Point point =
-                ((PositionComponent)
-                                entity.fetch(PositionComponent.class)
-                                        .orElseThrow(
-                                                () ->
-                                                        new MissingComponentException(
-                                                                "PositionComponent")))
+                entity.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity, PositionComponent.class))
                         .position();
         return calculatePathToRandomTileInRange(point, radius);
     }
@@ -163,17 +167,20 @@ public class AITools {
      * @param to Entity whose position is the goal point
      * @return Path
      */
-    public static GraphPath<Tile> calculatePath(Entity from, Entity to) {
+    public static GraphPath<Tile> calculatePath(final Entity from, final Entity to) {
         PositionComponent fromPositionComponent =
-                (PositionComponent)
-                        from.fetch(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
+                from.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                from, PositionComponent.class));
         PositionComponent positionComponent =
                 (PositionComponent)
                         to.fetch(PositionComponent.class)
                                 .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
+                                        () ->
+                                                MissingComponentException.build(
+                                                        to, PositionComponent.class));
         return calculatePath(fromPositionComponent.position(), positionComponent.position());
     }
 
@@ -181,7 +188,7 @@ public class AITools {
      * @param entity
      * @return Path from the entity to the hero, if there is no hero, path from the entity to itself
      */
-    public static GraphPath<Tile> calculatePathToHero(Entity entity) {
+    public static GraphPath<Tile> calculatePathToHero(final Entity entity) {
         Optional<Entity> hero = Game.hero();
         if (hero.isPresent()) return calculatePath(entity, hero.get());
         else return calculatePath(entity, entity);
@@ -193,7 +200,7 @@ public class AITools {
      * @param range Radius
      * @return if the distance between the two points is within the radius
      */
-    public static boolean inRange(Point p1, Point p2, float range) {
+    public static boolean inRange(final Point p1, final Point p2, final float range) {
         return Point.calculateDistance(p1, p2) <= range;
     }
 
@@ -203,23 +210,22 @@ public class AITools {
      * @param range search radius
      * @return if the position of the two entities is within the given radius
      */
-    public static boolean entityInRange(Entity entity1, Entity entity2, float range) {
+    public static boolean entityInRange(
+            final Entity entity1, final Entity entity2, final float range) {
 
         Point entity1Position =
-                ((PositionComponent)
-                                entity1.fetch(PositionComponent.class)
-                                        .orElseThrow(
-                                                () ->
-                                                        new MissingComponentException(
-                                                                "PositionComponent")))
+                entity1.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity1, PositionComponent.class))
                         .position();
         Point entity2Position =
-                ((PositionComponent)
-                                entity2.fetch(PositionComponent.class)
-                                        .orElseThrow(
-                                                () ->
-                                                        new MissingComponentException(
-                                                                "PositionComponent")))
+                entity2.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity2, PositionComponent.class))
                         .position();
         return inRange(entity1Position, entity2Position, range);
     }
@@ -230,7 +236,7 @@ public class AITools {
      * @return if the position of the player is within the given radius of the position of the given
      *     entity. If there is no hero, return false.
      */
-    public static boolean playerInRange(Entity entity, float range) {
+    public static boolean playerInRange(final Entity entity, final float range) {
 
         Optional<Entity> hero = Game.hero();
         if (hero.isPresent()) return entityInRange(entity, hero.get(), range);
@@ -244,12 +250,13 @@ public class AITools {
      * @param path Path
      * @return true, if the entity is on the end of the path or has left the path
      */
-    public static boolean pathFinishedOrLeft(Entity entity, GraphPath<Tile> path) {
+    public static boolean pathFinishedOrLeft(final Entity entity, final GraphPath<Tile> path) {
         PositionComponent pc =
-                (PositionComponent)
-                        entity.fetch(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
+                entity.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity, PositionComponent.class));
         boolean finished = lastTile(path).equals(Game.tileAT(pc.position()));
 
         boolean onPath = false;
@@ -268,12 +275,13 @@ public class AITools {
      * @param path Path
      * @return true, if the entity is on the end of the path.
      */
-    public static boolean pathFinished(Entity entity, GraphPath<Tile> path) {
+    public static boolean pathFinished(final Entity entity, final GraphPath<Tile> path) {
         PositionComponent pc =
-                (PositionComponent)
-                        entity.fetch(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
+                entity.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity, PositionComponent.class));
         return lastTile(path).equals(Game.tileAT(pc.position()));
     }
 
@@ -284,12 +292,13 @@ public class AITools {
      * @param path Path
      * @return true, if the entity has left the path.
      */
-    public static boolean pathLeft(Entity entity, GraphPath<Tile> path) {
+    public static boolean pathLeft(final Entity entity, final GraphPath<Tile> path) {
         PositionComponent pc =
-                (PositionComponent)
-                        entity.fetch(PositionComponent.class)
-                                .orElseThrow(
-                                        () -> new MissingComponentException("PositionComponent"));
+                entity.fetch(PositionComponent.class)
+                        .orElseThrow(
+                                () ->
+                                        MissingComponentException.build(
+                                                entity, PositionComponent.class));
         boolean onPath = false;
         Tile currentTile = Game.tileAT(pc.position());
         for (Tile tile : path) {
@@ -305,7 +314,7 @@ public class AITools {
      * @return last Tile in the given path
      * @see GraphPath
      */
-    public static Tile lastTile(GraphPath<Tile> path) {
+    public static Tile lastTile(final GraphPath<Tile> path) {
         return path.get(path.getCount() - 1);
     }
 }
