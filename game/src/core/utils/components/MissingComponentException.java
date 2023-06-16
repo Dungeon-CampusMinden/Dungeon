5858d9865510eda0a68ed3cc3a33c27ede08ff35
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-06-16 08:39:54 +0200

    make MissingComponentException final

--- game/src/core/utils/components/MissingComponentException.java
+++ game/src/core/utils/components/MissingComponentException.java
@@ -11,14 +11,14 @@ import java.util.logging.Logger;
  * <p>This exception is thrown by (e.g. a {@link core.System System}) when a required component is
  * missing on an entity that is being processed.
  */
-public class MissingComponentException extends NullPointerException {
+public final class MissingComponentException extends NullPointerException {
 
     /**
      * Constructs a new MissingComponentException with the specified detail message.
      *
      * @param message Detail message. Should contain the name of the missing component.
      */
-    public MissingComponentException(String message) {
+    public MissingComponentException(final String message) {
         super("Missing Component:" + message);
         Logger exceptionLogger = Logger.getLogger(this.getClass().getName());
         exceptionLogger.log(CustomLogLevel.FATAL, "Missing Component: " + message);
