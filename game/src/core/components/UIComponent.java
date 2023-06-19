7ba093f6ee0bf5fdd82bd81a48fac9acd5669c2d
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-06-19 14:59:39 +0200

    update javadoc

--- game/src/core/components/UIComponent.java
+++ game/src/core/components/UIComponent.java
@@ -6,18 +6,31 @@ import core.Component;
 import core.Entity;
 
 /**
- * A simple implementation for a UI Component which allows to define a Group of
- * com.badlogic.gdx.scenes.scene2d.ui Elements. Also allows to define whether the Elements are
- * pausing the Game or not.
+ * The UIComponent stores a {@link Group} (the dialog) of {@link com.badlogic.gdx.scenes.scene2d.ui
+ * Element} and links it to the associated entity.
+ *
+ * <p>The UIComponent is used by the {@link core.systems.HudSystem} to display UI elements on the
+ * screen.
+ *
+ * <p>It also allows for defining whether the elements pause the game or not.
+ *
+ * <p>Use {@link #dialog} to get the Dialog of this Component and add or remove UI-Elements to it.
+ *
+ * @see core.systems.HudSystem
+ * @see Group
+ * @see com.badlogic.gdx.scenes.scene2d.ui
  */
 public final class UIComponent extends Component {
     private final Group dialog;
     private final boolean willPauseGame;
 
     /**
-     * @param entity where the Component should be added
-     * @param dialog a Group of Elements which should be shown
-     * @param willPauseGame if the UI should pause the Game or not
+     * Create a new UIComponent and add it to the associated entity.
+     *
+     * @param entity The associated entity.
+     * @param dialog A {@link Group} of elements which should be shown on the UI.
+     * @param willPauseGame Specifies if the UI should pause the game or not.
+     * @see Group
      */
     public UIComponent(final Entity entity, final Group dialog, final boolean willPauseGame) {
         super(entity);
@@ -26,30 +39,40 @@ public final class UIComponent extends Component {
     }
 
     /**
-     * Creates an Empty Group which can be populated with Elements and pauses the Game when visible.
+     * Create a new UIComponent and add it to the associated entity.
      *
-     * @param entity where the Component should be added
+     * <p>Creates an empty {@link Group} which can be populated with elements and pauses the game
+     * when visible.
+     *
+     * @param entity The associated entity.
+     * @see Group
      */
     public UIComponent(final Entity entity) {
         this(entity, new Group(), true);
     }
 
     /**
-     * @return true when the dialog is shown
+     * Check if the dialog for this component is currently shown.
+     *
+     * @return true if the dialog is shown, false if not.
      */
     public boolean isVisible() {
         return dialog.isVisible();
     }
 
     /**
-     * @return true if this hud should pause the Game when visible
+     * Checks if the HUD will pause the game when the dialog of this component is shown.
+     *
+     * @return true if the HUD will pause the game when visible, false if not.
      */
     public boolean willPauseGame() {
         return willPauseGame;
     }
 
     /**
-     * @return the UI Elements which should be shown
+     * Retrieves the group of UI elements comprising the dialog for this component.
+     *
+     * @return The UI elements to be shown in the dialog.
      */
     public Group dialog() {
         return dialog;
