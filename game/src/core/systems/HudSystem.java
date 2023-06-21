5dab535e90fad33ff874472c2f788fc8f872196c
AHeinisch <aheinisch@fh-bielefeld.de>
2023-06-21 13:00:41 +0200

    add new functionality

--- game/src/core/systems/HudSystem.java
+++ game/src/core/systems/HudSystem.java
@@ -1,12 +1,16 @@
 package core.systems;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import core.Entity;
 import core.Game;
 import core.System;
 import core.components.UIComponent;
 import core.utils.components.MissingComponentException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The basic handling of any UIComponent. Adds them to the Stage, updates the Stage each Frame to
@@ -16,33 +20,45 @@ import core.utils.components.MissingComponentException;
  * longer available
  */
 public final class HudSystem extends System {
+    private Map<Integer, Group> idGroupMap = new HashMap<>();
+
     /** The HudSystem needs the UIComponent to work. */
     public HudSystem() {
         super(UIComponent.class);
+        onEntityAdd = this::addListener;
+        onEntityRemove = this::removeListener;
+    }
+
+    /**
+     * once a UIComponent is removed its Dialog has to be removed from the Stage
+     *
+     * @param entity which no longer has a UIComponent
+     */
+    private void removeListener(Entity entity) {}
+
+    /**
+     * when an Entity with a UIComponent is added its dialog has to be added to the Stage for UI
+     * Representation
+     *
+     * @param entity which now has a UIComponent
+     */
+    private void addListener(Entity entity) {
+        Group dialog =
+                entity.fetch(UIComponent.class)
+                        .orElseThrow(
+                                () -> MissingComponentException.build(entity, UIComponent.class))
+                        .dialog();
+        Game.stage().ifPresent(stage -> addDialogToStage(dialog, stage));
+    }
+
+    private static void addDialogToStage(Group d, Stage stage) {
+        if (!stage.getActors().contains(d, true)) {
+            stage.addActor(d);
+        }
     }
 
     @Override
     public void execute() {
-        // Temp fix until either addEntity is available or removeEntity until dungeon Issue #727
-        // implemented
-        entityStream()
-                .forEach(
-                        x -> {
-                            Group d =
-                                    x.fetch(UIComponent.class)
-                                            .orElseThrow(
-                                                    () ->
-                                                            MissingComponentException.build(
-                                                                    x, UIComponent.class))
-                                            .dialog();
-                            Game.stage()
-                                    .ifPresent(
-                                            stage -> {
-                                                if (!stage.getActors().contains(d, true)) {
-                                                    stage.addActor(d);
-                                                }
-                                            });
-                        });
 
         if (entityStream().anyMatch(x -> pausesGame(x))) pauseGame();
         else unpauseGame();
