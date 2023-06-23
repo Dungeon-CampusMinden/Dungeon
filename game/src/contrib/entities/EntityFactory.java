6db0cdc0acb7bcc9c5dfd7f84bd77363a58eb6e6
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-06-23 09:49:42 +0200

    New items and resources added. Folder names were adapted, names of the individual items uniformly in snake_case.

--- game/src/contrib/entities/EntityFactory.java
+++ game/src/contrib/entities/EntityFactory.java
@@ -1,9 +1,7 @@
 package contrib.entities;
 
 import contrib.components.*;
-import contrib.configuration.KeyboardConfig;
 import contrib.utils.components.interaction.DropItemsInteraction;
-import contrib.utils.components.interaction.InteractionTool;
 import contrib.utils.components.item.ItemData;
 import contrib.utils.components.item.ItemDataGenerator;
 import contrib.utils.components.skill.FireballSkill;
@@ -15,13 +13,12 @@ import core.Game;
 import core.components.*;
 import core.level.utils.LevelElement;
 import core.utils.Point;
-import core.utils.components.MissingComponentException;
-import core.utils.components.draw.CoreAnimations;
+import core.utils.components.draw.Animation;
 
-import java.io.IOException;
+import dslToGame.AnimationBuilder;
+
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
 /**
@@ -29,89 +26,40 @@ import java.util.stream.IntStream;
  * static methods to construct various types of entities with different components.
  */
 public class EntityFactory {
-    private static final Logger LOGGER = Logger.getLogger(EntityFactory.class.getName());
 
     /**
      * Create a new Entity that can be used as a playable character. It will have a {@link
-     * CameraComponent}, {@link PlayerComponent}. {@link PositionComponent}, {@link
-     * VelocityComponent} {@link DrawComponent}, {@link CollideComponent}.
+     * PlayerComponent}. {@link PositionComponent}, {@link VelocityComponent} {@link DrawComponent},
+     * {@link CollideComponent}.
      *
      * @return Created Entity
      */
-    public static Entity newHero() throws IOException {
+    public static Entity getHero() {
         final int fireballCoolDown = 2;
         final float xSpeed = 0.3f;
         final float ySpeed = 0.3f;
+        final String pathToIdleLeft ="knight/idle";
+        final String pathToIdleRight ="knight/idle";
+        final String pathToRunLeft = "knight/run_left";
+        final String pathToRunRight = "knight/run_right";
 
         Entity hero = new Entity("hero");
-        new CameraComponent(hero);
         new PositionComponent(hero);
-        new VelocityComponent(hero, xSpeed, ySpeed);
-        new DrawComponent(hero, "character/knight");
+        Animation moveRight = AnimationBuilder.buildAnimation(pathToRunRight);
+        Animation moveLeft = AnimationBuilder.buildAnimation(pathToRunLeft);
+        new VelocityComponent(hero, xSpeed, ySpeed, moveLeft, moveRight);
+        Animation idleRight = AnimationBuilder.buildAnimation(pathToIdleRight);
+        Animation idleLeft = AnimationBuilder.buildAnimation(pathToIdleLeft);
+        new DrawComponent(hero, idleLeft, idleRight);
         new CollideComponent(
                 hero,
                 (you, other, direction) -> System.out.println("heroCollisionEnter"),
                 (you, other, direction) -> System.out.println("heroCollisionLeave"));
         PlayerComponent pc = new PlayerComponent(hero);
         Skill fireball =
-                new Skill(new FireballSkill(SkillTools::cursorPositionAsPoint), fireballCoolDown);
-
-        // hero movement
-        pc.registerCallback(
-                KeyboardConfig.MOVEMENT_UP.value(),
-                entity -> {
-                    VelocityComponent vc =
-                            entity.fetch(VelocityComponent.class)
-                                    .orElseThrow(
-                                            () ->
-                                                    MissingComponentException.build(
-                                                            entity, VelocityComponent.class));
-                    vc.currentYVelocity(1 * vc.yVelocity());
-                });
-        pc.registerCallback(
-                KeyboardConfig.MOVEMENT_DOWN.value(),
-                entity -> {
-                    VelocityComponent vc =
-                            entity.fetch(VelocityComponent.class)
-                                    .orElseThrow(
-                                            () ->
-                                                    MissingComponentException.build(
-                                                            entity, VelocityComponent.class));
-
-                    vc.currentYVelocity(-1 * vc.yVelocity());
-                });
-        pc.registerCallback(
-                KeyboardConfig.MOVEMENT_RIGHT.value(),
-                entity -> {
-                    VelocityComponent vc =
-                            entity.fetch(VelocityComponent.class)
-                                    .orElseThrow(
-                                            () ->
-                                                    MissingComponentException.build(
-                                                            entity, VelocityComponent.class));
-
-                    vc.currentXVelocity(1 * vc.xVelocity());
-                });
-        pc.registerCallback(
-                KeyboardConfig.MOVEMENT_LEFT.value(),
-                entity -> {
-                    VelocityComponent vc =
-                            entity.fetch(VelocityComponent.class)
-                                    .orElseThrow(
-                                            () ->
-                                                    MissingComponentException.build(
-                                                            entity, VelocityComponent.class));
-
-                    vc.currentXVelocity(-1 * vc.xVelocity());
-                });
-
-        pc.registerCallback(
-                KeyboardConfig.INTERACT_WORLD.value(),
-                InteractionTool::interactWithClosestInteractable);
-
-        // skills
-        pc.registerCallback(KeyboardConfig.FIRST_SKILL.value(), fireball::execute);
-
+                new Skill(
+                        new FireballSkill(SkillTools::getCursorPositionAsPoint), fireballCoolDown);
+        pc.setSkillSlot1(fireball);
         return hero;
     }
 
@@ -126,7 +74,7 @@ public class EntityFactory {
      *
      * @return Created Entity
      */
-    public static Entity newChest() throws IOException {
+    public static Entity getChest() {
         Random random = new Random();
         ItemDataGenerator itemDataGenerator = new ItemDataGenerator();
 
@@ -134,7 +82,9 @@ public class EntityFactory {
                 IntStream.range(0, random.nextInt(1, 3))
                         .mapToObj(i -> itemDataGenerator.generateItemData())
                         .toList();
-        return newChest(itemData, Game.randomTile(LevelElement.FLOOR).position());
+        return getChest(
+                itemData,
+                Game.currentLevel.getRandomTile(LevelElement.FLOOR).getCoordinate().toPoint());
     }
 
     /**
@@ -148,17 +98,27 @@ public class EntityFactory {
      * @param position The position of the chest.
      * @return Created Entity
      */
-    public static Entity newChest(List<ItemData> itemData, Point position) throws IOException {
+    public static Entity getChest(List<ItemData> itemData, Point position) {
         final float defaultInteractionRadius = 1f;
+        final List<String> DEFAULT_CLOSED_ANIMATION_FRAMES =
+                List.of("objects/treasurechest/chest_full_open_anim_f0.png");
+        final List<String> DEFAULT_OPENING_ANIMATION_FRAMES =
+                List.of(
+                        "objects/treasurechest/chest_full_open_anim_f0.png",
+                        "objects/treasurechest/chest_full_open_anim_f1.png",
+                        "objects/treasurechest/chest_full_open_anim_f2.png",
+                        "objects/treasurechest/chest_empty_open_anim_f2.png");
+
         Entity chest = new Entity("chest");
         new PositionComponent(chest, position);
         InventoryComponent ic = new InventoryComponent(chest, itemData.size());
         itemData.forEach(ic::addItem);
         new InteractionComponent(
                 chest, defaultInteractionRadius, false, new DropItemsInteraction());
-        DrawComponent dc = new DrawComponent(chest, "objects/treasurechest");
-        dc.getAnimation(CoreAnimations.IDLE_RIGHT).ifPresent(a -> a.setLoop(false));
-
+        new DrawComponent(
+                chest,
+                new Animation(DEFAULT_CLOSED_ANIMATION_FRAMES, 100, false),
+                new Animation(DEFAULT_OPENING_ANIMATION_FRAMES, 100, false));
         return chest;
     }
 }
