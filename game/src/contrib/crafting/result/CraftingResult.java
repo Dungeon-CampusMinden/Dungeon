706518154dd2dc86232b4cd13f233e0baf8dce33
Finn Watermann <finn.watermann@arcor.de>
2023-06-28 16:18:27 +0200

    Add javadoc to CraftingResult

--- game/src/contrib/crafting/result/CraftingResult.java
+++ game/src/contrib/crafting/result/CraftingResult.java
@@ -4,18 +4,39 @@ import com.badlogic.gdx.utils.JsonValue;
 
 import core.Entity;
 
+/**
+ * A crafting result. This is the base class for all crafting results.
+ *
+ * <p>Crafting results are used to define the result of a crafting recipe. They are executed when a
+ * recipe is crafted and can be used to add items to the player's inventory, add components to the
+ * player's entity, etc...
+ */
 public abstract class CraftingResult {
 
+    /**
+     * The type of the crafting result. This is used to determine which type of crafting result this
+     * is.
+     */
     public enum Type {
         ITEM,
     }
 
     private final Type type;
 
-    public CraftingResult(Type type) {
+    /**
+     * Creates a new crafting result.
+     *
+     * @param type The type of the crafting result.
+     */
+    protected CraftingResult(Type type) {
         this.type = type;
     }
 
+    /**
+     * Returns the type of the crafting result.
+     *
+     * @return The type of the crafting result.
+     */
     public Type type() {
         return type;
     }
@@ -23,9 +44,17 @@ public abstract class CraftingResult {
     /**
      * Execute the result on the provided entity.
      *
+     * <p>This will be called when the recipe is crafted. This can be used to add items to the
+     * entity's inventory, add components to the entity, etc...
+     *
      * @param entity Entity to execute the result on.
      */
     public abstract void execute(Entity entity);
 
+    /**
+     * Parses the crafting result from the specified JSON value.
+     *
+     * @param value The JSON value to parse.
+     */
     public abstract void parse(JsonValue value);
 }
