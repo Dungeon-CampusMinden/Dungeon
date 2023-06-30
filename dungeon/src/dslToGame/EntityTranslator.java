f1a6f6cefb22e531cc409dee3f1dfb6a0b1d8b9e
mr <malt.r@mail.de>
2023-06-30 13:43:40 +0200

    spotless apply

--- dungeon/src/dslToGame/EntityTranslator.java
+++ dungeon/src/dslToGame/EntityTranslator.java
@@ -4,23 +4,25 @@ import core.Component;
 import core.Entity;
 
 import interpreter.DSLInterpreter;
+
 import runtime.AggregateValue;
 import runtime.EncapsulatedObject;
 import runtime.IEvironment;
 import runtime.IMemorySpace;
+
 import semanticanalysis.Symbol;
 import semanticanalysis.types.AggregateType;
-import semanticanalysis.types.IType;
 import semanticanalysis.types.TypeBuilder;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class EntityTranslator implements IRuntimeObjectTranslator<Entity, AggregateValue> {
     @Override
-    public AggregateValue translate(Entity object, IEvironment env, IMemorySpace parentMemorySpace, DSLInterpreter interpreter) {
+    public AggregateValue translate(
+            Entity object,
+            IEvironment env,
+            IMemorySpace parentMemorySpace,
+            DSLInterpreter interpreter) {
         // get datatype for entity
         var entityType = env.getGlobalScope().resolve("entity");
 
@@ -28,11 +30,11 @@ public class EntityTranslator implements IRuntimeObjectTranslator<Entity, Aggreg
             throw new RuntimeException("The resolved symbol for 'entity' is not an AggregateType!");
         } else {
             // create aggregateValue
-            var value = new AggregateValue((AggregateType)entityType, parentMemorySpace, object);
+            var value = new AggregateValue((AggregateType) entityType, parentMemorySpace, object);
 
             // get components
             // TODO: use stream better
-            List<Component> componentTypes =  object.componentStream().toList();
+            List<Component> componentTypes = object.componentStream().toList();
 
             // TODO: translate components into DSL-objects
             //  this probably could be done by encapsulating objects with a previos check
@@ -46,9 +48,17 @@ public class EntityTranslator implements IRuntimeObjectTranslator<Entity, Aggreg
                     // TODO: casting to AggregateType here is probably not safe
                     //  -> was passiert, wenn das hier PODAdapted ist?
 
-                    var encapsulatedObject = new EncapsulatedObject(component, (AggregateType)componentDSLType, value.getMemorySpace(), null);
+                    var encapsulatedObject =
+                            new EncapsulatedObject(
+                                    component,
+                                    (AggregateType) componentDSLType,
+                                    value.getMemorySpace(),
+                                    null);
                     AggregateValue aggregateMemberValue =
-                        new AggregateValue((AggregateType)componentDSLType, value.getMemorySpace(), component);
+                            new AggregateValue(
+                                    (AggregateType) componentDSLType,
+                                    value.getMemorySpace(),
+                                    component);
                     aggregateMemberValue.setMemorySpace(encapsulatedObject);
 
                     value.getMemorySpace().bindValue(componentDSLName, aggregateMemberValue);
--- dungeon/src/dslToGame/IRuntimeObjectTranslator.java
+++ dungeon/src/dslToGame/IRuntimeObjectTranslator.java
@@ -1,11 +1,14 @@
 package dslToGame;
 
-import core.Entity;
 import interpreter.DSLInterpreter;
-import runtime.AggregateValue;
+
 import runtime.IEvironment;
 import runtime.IMemorySpace;
 
 public interface IRuntimeObjectTranslator<FromTy, ToTy> {
-    ToTy translate(FromTy object, IEvironment environment, IMemorySpace parentMemorySpace, DSLInterpreter interpreter);
+    ToTy translate(
+            FromTy object,
+            IEvironment environment,
+            IMemorySpace parentMemorySpace,
+            DSLInterpreter interpreter);
 }
