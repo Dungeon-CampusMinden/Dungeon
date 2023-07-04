303884379127111c32462ca3cf541cff6b433cdb
mr <malt.r@mail.de>
2023-07-04 20:55:54 +0200

    Javadoc

--- dsl/src/runtime/RuntimeObjectTranslator.java
+++ dsl/src/runtime/RuntimeObjectTranslator.java
@@ -8,10 +8,24 @@ import semanticanalysis.types.TypeBuilder;
 import java.util.HashMap;
 
 // TODO: javadoc
+
+/**
+ * This class performs conversions from "runtime objects", meaning Java-objects from the dungeon
+ * context (Entity, Components) to DSL Values
+ */
 public class RuntimeObjectTranslator {
-    private HashMap<Class<?>, IObjectToValueTranslator> translators = new HashMap<>();
+    // used to store specific IObjectToValueTranslator instances for classes, which
+    // require custom logic to translate into a DSL Value
+    private final HashMap<Class<?>, IObjectToValueTranslator> translators = new HashMap<>();
 
     // TODO: javadoc and rename
+
+    /**
+     * Register an IObjectToValueTranslator instance for a specific class
+     *
+     * @param clazz The Class to register the IObjectToValueTranslator for
+     * @param translator The translator to use for specified Class
+     */
     public void loadObjectToValueTranslator(Class<?> clazz, IObjectToValueTranslator translator) {
         if (this.translators.containsKey(clazz)) {
             throw new RuntimeException(
@@ -20,6 +34,20 @@ public class RuntimeObjectTranslator {
         this.translators.put(clazz, translator);
     }
 
+    /**
+     * Default translation for given Object to a DSL Value. This will look up the equivalent
+     * DSL-type for the Objects-Class and depending on the kind of type it will perform different
+     * operations: - if the object is of basic type (bool, int, float), it will be wrapped in a
+     * normal Value - if the object is of an aggregateType, it will be wrapped in an
+     * EncapsulatedObject, which will be used as a MemorySpace by an AggregateValue - if the object
+     * is of an adapted type (an external type, which can not be directly modified to be a
+     * DSL-Value), it is wrapped in a normal Value
+     *
+     * @param object the object to translate into an DSL Value
+     * @param parentMemorySpace the memory space in which to add the value
+     * @param environment the IEnvironment containing all runtime type information
+     * @return the translated Value
+     */
     protected Value translateRuntimeObjectDefault(
             Object object, IMemorySpace parentMemorySpace, IEvironment environment) {
         Value returnValue = Value.NONE;
@@ -55,7 +83,15 @@ public class RuntimeObjectTranslator {
         return returnValue;
     }
 
-    // TODO: javadoc
+    /**
+     * Translate a given Object into a DSL Value. Performs the translation from a registered
+     * IObjectToValueTranslator or performs the default translation
+     *
+     * @param object The Object to translate
+     * @param parentMemorySpace The IMemorySpace in which to create the Value
+     * @param environment The IEnvironment containing all type information
+     * @return The translated Value
+     */
     public Value translateRuntimeObject(
             Object object, IMemorySpace parentMemorySpace, IEvironment environment) {
 
