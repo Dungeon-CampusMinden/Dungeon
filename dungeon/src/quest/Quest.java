e13b982cd76aa5ca4c597a67b88bc6a85d03016f
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-04 21:30:17 +0200

    add Quest and QuestContent

--- dungeon/src/quest/Quest.java
+++ dungeon/src/quest/Quest.java
@@ -1,9 +1,26 @@
 package quest;
 
-import core.level.elements.ILevel;
-import core.level.generator.IGenerator;
-
-import dslToGame.QuestConfig;
+import java.util.Set;
 
 public abstract class Quest {
+    private final Set<QuestContent> content;
+    private final String questText;
+
+    protected Quest(Set<QuestContent> content, String questText) {
+        this.content = content;
+        this.questText = questText;
+    }
+
+    public Set<QuestContent> content() {
+        return content;
+    }
+
+    public String questText() {
+        return questText;
+    }
+
+    @Override
+    public String toString() {
+        return questText;
+    }
 }
--- dungeon/src/quest/QuestContent.java
+++ dungeon/src/quest/QuestContent.java
@@ -1,4 +1,18 @@
 package quest;
 
 public abstract class QuestContent {
+    private final String content;
+
+    public QuestContent(String content) {
+        this.content = content;
+    }
+
+    public String content() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
 }
