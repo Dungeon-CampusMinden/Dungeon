728444c6843e99eefdad3c28c61f1f4606215469
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-07 10:49:58 +0200

    update javadoc for cc

--- game/src/core/components/CameraComponent.java
+++ game/src/core/components/CameraComponent.java
@@ -3,15 +3,26 @@ package core.components;
 import core.Component;
 import core.Entity;
 
-/** The CameraComponent marks an entity as the point of focus for the camera. */
-public class CameraComponent extends Component {
+/**
+ * Marks an entity as the point of focus for the camera.
+ *
+ * <p>The {@link core.systems.CameraSystem} will follow the associated entity and will keep the
+ * entity in the center of the game window.
+ *
+ * <p>Note: The associated entity also needs a {@link PositionComponent} for the {@link
+ * core.systems.CameraSystem} to work.
+ *
+ * <p>Note: At the moment, there is no deterministic behavior for a game with multiple
+ * CameraComponents.
+ */
+public final class CameraComponent extends Component {
 
     /**
      * Create a new CameraComponent and add it to the associated entity.
      *
-     * @param entity entity which should be the focus point
+     * @param entity entity which should be the focus point of the camera
      */
-    public CameraComponent(Entity entity) {
+    public CameraComponent(final Entity entity) {
         super(entity);
     }
 }
