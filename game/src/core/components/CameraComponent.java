b9d0e86268f34abb818e0fbd4ffbb2995f2799ae
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-07 11:54:00 +0200

    spotless

--- game/src/core/components/CameraComponent.java
+++ game/src/core/components/CameraComponent.java
@@ -12,7 +12,8 @@ import core.Entity;
  * <p>Note: The associated entity also needs a {@link PositionComponent} for the {@link
  * core.systems.CameraSystem} to work.
  *
- * <p>Note: If there is more than one CameraComponent, i.e. if more than one entity is attached to a CameraComponent, the behaviour is undefined.
+ * <p>Note: If there is more than one CameraComponent, i.e. if more than one entity is attached to a
+ * CameraComponent, the behaviour is undefined.
  */
 public final class CameraComponent extends Component {
 
