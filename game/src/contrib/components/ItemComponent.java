84e7fc54d4675c6732e2c2ccfa15cf171567a071
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-10 12:51:25 +0200

    better class javadoc

--- game/src/contrib/components/ItemComponent.java
+++ game/src/contrib/components/ItemComponent.java
@@ -4,25 +4,33 @@ import contrib.utils.components.item.ItemData;
 
 import core.Component;
 import core.Entity;
-import core.utils.TriConsumer;
 
 /**
- * Marks an entity as an item.
+ * Marks an entity as an item in the game world.
  *
- * <p>It contains the {@link #ItemData}, which contains all information about an item.
- *
- * <p>An entity with an {@link ItemComponent} is not an item in an inventory, but an item that
- * exists in the game world. Systems such as {@link contrib.systems.CollisionSystem CollisionSystem}
- * or {@link InteractionComponent} utilize the {@link ItemData} stored in this component to place
- * the item in the player's inventory upon collision or interaction. For this to work, the
- * associated entity needs not only the corresponding components, but also needs to implement the
- * callback functions (e.g., {@link CollideComponent#collideEnter(TriConsumer)}) of the respective
- * components with the logic for collecting the item.
- *
- * <p>Some default callback functions are already implemented in {@link ItemData} and can be used.
+ * <p>In the dungeon, an item exists in two different states. The first state describes the item
+ * when it is stored in an {@link InventoryComponent inventory}. An item within an inventory
+ * consists only of the {@link ItemData} and is not an {@link Entity}. The second state is active
+ * when an item is on the ground in the level. In this state, an item is an {@link Entity} with
+ * various components. An entity with an {@link ItemComponent} is not an item in an inventory, but
+ * an item that exists in the game world.
  *
  * <p>The {@link contrib.entities.WorldItemBuilder WorldItemBuilder} demonstrates how to create an
- * item entity.
+ * item entity. You can use the {@link contrib.entities.WorldItemBuilder#buildWorldItem(ItemData)}
+ * method to create a new Item-Entity from an {@link ItemData}.
+ *
+ * <p>The {@link contrib.entities.WorldItemBuilder} will create an entity with a {@link
+ * ItemComponent}, {@link core.components.PositionComponent}, {@link core.components.DrawComponent},
+ * and {@link CollideComponent}. The {@link contrib.entities.WorldItemBuilder} will configure the
+ * collide-callback for the Item-Entity so that if it collides with the player character, the stored
+ * {@link ItemData} will be added to the {@link InventoryComponent} of the player, and the
+ * item-entity will be removed from the game.
+ *
+ * <p>By default, an {@link ItemData} that is dropped from the inventory will be created as an
+ * Item-Entity using the {@link contrib.entities.WorldItemBuilder}.
+ *
+ * <p>Note that this component does not implement this behavior. This component only marks an entity
+ * as an Item and is used to store the associated {@link ItemData}.
  *
  * @see ItemData
  * @see contrib.entities.WorldItemBuilder
