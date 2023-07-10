7ff98c450cb1fe2e8410f4482f1e6cc5a431e7fb
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-10 13:54:37 +0200

    more ctor, better javadoc

--- game/src/contrib/components/XPComponent.java
+++ game/src/contrib/components/XPComponent.java
@@ -4,13 +4,15 @@ import core.Component;
 import core.Entity;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
- * Allow associated entity to collect Experience Points (XP) and level up if enough XP is collected.
+ * Allow the associated entity to collect experience points (XP) and level up if enough XP is
+ * collected
  *
  * <p>The component stores the current character level, the amount of XP this entity has collected,
  * and the amount of XP this entity will drop if it dies. To drop XP, a {@link HealthComponent} is
- * needed.
+ * needed. The collected XP will not be set to zero after a level up was performed.
  *
  * <p>The callback {@link #callbackLevelUp} can be used to trigger specific behavior when the
  * associated entity reaches a new character level.
@@ -22,20 +24,39 @@ import java.util.function.Consumer;
  *
  * <p>The {@link contrib.systems.XPSystem} will check if the component has collected enough XP to
  * reach the next level using {@link #xpToNextCharacterLevel()} and will then update the character
- * level and trigger the {@link #levelUp()} callback function.
+ * level and trigger the {@link #triggerLevelUpCallback()} callback function.
  *
- * <p>The amount of XP needed for a level up is calculated based on the following formula: {@code
- * FORMULA_SLOPE * (currentLevel + 1)^2 + LEVEL_1_XP}. It's a quadratic function with a slope of
- * {@code FORMULA_SLOPE} and a y-intercept of {@code LEVEL_1_XP}.
+ * <p>The XP needed for the next level is calculated using a formula. You can set your own formula
+ * with {@link #levelUPFormula(Function)}. The default formula uses a quadratic function, resulting
+ * in each level requiring more XP than the previous one.
  */
 public final class XPComponent extends Component {
+
     private static final double NEEDED_XP_FOR_LEVEL_ONE = 100;
     private static final double FORMULA_SLOPE = 0.5;
+    /**
+     * XP is calculated based on the following formula: {@code FORMULA_SLOPE * (currentLevel + 1)^2
+     * + LEVEL_1_XP}.
+     *
+     * <p>It's a quadratic function with a slope of {@code FORMULA_SLOPE} and a y-intercept of
+     * {@code LEVEL_1_XP}.
+     */
+    private static final Function<Long, Long> DEFAULT_LEVEL_UP_FORMULA =
+            new Function<Long, Long>() {
+                @Override
+                public Long apply(Long level) {
+                    return Math.round(FORMULA_SLOPE * Math.pow(level, 2) + NEEDED_XP_FOR_LEVEL_ONE);
+                }
+            };
+
+    private static final float DEFAULT_LOOT_FACTOR = 0.5f;
     private static final Consumer<Entity> DEFAULT_LEVEL_UP = entity1 -> {};
     private final Consumer<Entity> callbackLevelUp;
+    private final long lootXP;
+    private final float lootFactor;
     private long characterLevel;
     private long currentXP;
-    private final long lootXP;
+    private Function<Long, Long> levelUPFormula;
 
     /**
      * Create a new XPComponent and add it to the associated entity.
@@ -49,6 +70,24 @@ public final class XPComponent extends Component {
         super(entity);
         this.callbackLevelUp = levelUp;
         this.lootXP = lootXP;
+        lootFactor = DEFAULT_LOOT_FACTOR;
+        levelUPFormula = DEFAULT_LEVEL_UP_FORMULA;
+    }
+
+    /**
+     * Create a new XPComponent and add it to the associated entity.
+     *
+     * @param entity the associated entity
+     * @param levelUp the callback for when the entity levels up
+     * @param lootFactor The {@link #lootXP} will always be {@link #currentXP} multiplied with the
+     *     given factor, dynamically adjusting as this component collects more XP.
+     */
+    public XPComponent(final Entity entity, final Consumer<Entity> levelUp, float lootFactor) {
+        super(entity);
+        this.callbackLevelUp = levelUp;
+        this.lootXP = -1;
+        this.lootFactor = lootFactor;
+        levelUPFormula = DEFAULT_LEVEL_UP_FORMULA;
     }
 
     /**
@@ -77,6 +116,19 @@ public final class XPComponent extends Component {
      */
     public XPComponent(final Entity entity, int lootXP) {
         this(entity, DEFAULT_LEVEL_UP, lootXP);
+    }
+
+    /**
+     * Create a new XPComponent with an empty level-up callback and add it to the associated entity.
+     *
+     * <p>Useful for entities that should only give XP and not gain XP themselves, such as monsters.
+     *
+     * @param entity the associated entity
+     * @param lootFactor The {@link #lootXP} will always be {@link #currentXP} multiplied with the
+     *     given factor, dynamically adjusting as this component collects more XP.
+     */
+    public XPComponent(final Entity entity, float lootFactor) {
+        this(entity, DEFAULT_LEVEL_UP, lootFactor);
     }
 
     /**
@@ -145,35 +197,42 @@ public final class XPComponent extends Component {
     }
 
     /** Trigger the level-up callback function. */
-    public void levelUp() {
+    public void triggerLevelUpCallback() {
         callbackLevelUp.accept(entity);
     }
 
     /**
      * Get the amount of XP that will be dropped when the associated entity dies.
      *
-     * <p>If no value is set, the dropped XP will be set to half of the current XP.
+     * <p>If no value is set, the dropped XP will be the current XP multiplied with the loot-factor.
+     *
+     * <p>If no factor is set, the dropped XP will be set to half of the current XP.
      *
      * <p>XP can only be dropped on death if the associated entity has a {@link HealthComponent}.
      *
      * @return the XP that will be dropped
      */
     public long lootXP() {
-        return lootXP == -1 ? currentXP / 2 : lootXP;
+        return lootXP == -1 ? (long) (currentXP * lootFactor) : lootXP;
+    }
+
+    /**
+     * Set the function to calculate the amount of XP that is missing to reach the next character
+     * level.
+     *
+     * @param formula The new formula used to calculate the missing XP. (Function<Long, Long> where
+     *     the input is the character level and the output is the XP needed for the next level)
+     */
+    public void levelUPFormula(Function<Long, Long> formula) {
+        this.levelUPFormula = formula;
     }
 
     /**
      * Calculate the amount of XP needed to reach the next character level.
      *
-     * <p>XP is calculated based on the following formula: {@code FORMULA_SLOPE * (currentLevel +
-     * 1)^2 + LEVEL_1_XP}. It's a quadratic function with a slope of {@code FORMULA_SLOPE} and a
-     * y-intercept of {@code LEVEL_1_XP}.
-     *
      * @return the amount of XP left to reach the next character level
      */
     public long xpToNextCharacterLevel() {
-        // character level 0 in Formula is character level 1 in game.
-        return Math.round(FORMULA_SLOPE * Math.pow(characterLevel, 2) + NEEDED_XP_FOR_LEVEL_ONE)
-                - currentXP;
+        return levelUPFormula.apply(characterLevel) - currentXP;
     }
 }
