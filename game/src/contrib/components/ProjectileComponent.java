0d21e325d5cceacfe0f7d811c8c1e2b3a342a91c
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-10 12:23:39 +0200

    better class javadoc

--- game/src/contrib/components/ProjectileComponent.java
+++ game/src/contrib/components/ProjectileComponent.java
@@ -9,13 +9,17 @@ import core.utils.Point;
  *
  * <p>The component stores a {@link #startPosition start} and a {@link #goalLocation goal} position.
  *
- * <p>The {@link contrib.systems.ProjectileSystem ProjectileSystem} will calculate a flight path
- * from the start to the goal and will then set the velocity of the entity accordingly. If the goal
- * location is reached, the entity will be removed from the game. A {@link
- * core.components.VelocityComponent VelocityComponent} and {@link core.components.PositionComponent
- * PositionComponent} is needed as well.
+ * <p>A projectile will need a {@link core.components.VelocityComponent VelocityComponent} and
+ * {@link core.components.PositionComponent PositionComponent} as well. See <a
+ * href="https://github.com/Programmiermethoden/Dungeon/tree/master/doc/ecs/systems">System-Overview</a>.
  *
- * <p>Examples of projectiles are fireballs or arrows.
+ * <p>The {@link contrib.systems.ProjectileSystem ProjectileSystem} will calculate a flight path
+ * from the start to the goal and will then set the velocity in the {@link
+ * core.components.VelocityComponent} of the entity accordingly. If the goal location is reached,
+ * the entity will be removed from the game.
+ *
+ * <p>Examples of projectiles are {@link contrib.utils.components.skill.FireballSkill fireballs} or
+ * arrows.
  *
  * <p>The {@link #goalLocation()} method retrieves the goal position of the projectile. The {@link
  * #startPosition()} method retrieves the start position of the projectile.
