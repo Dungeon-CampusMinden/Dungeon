24c3bf34bcd26d4a937965735da6bf99aa7a1d35
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2023-07-10 16:15:22 +0200

    [GAME] refactor ProjectileComponent (JavaDoc, Clean Code) (#830)
    * cc projectile component
    
    * missed a dot
    
    * Apply suggestions from code review
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>
    
    * better class javadoc
    
    ---------
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>

--- game/src/contrib/components/ProjectileComponent.java
+++ game/src/contrib/components/ProjectileComponent.java
@@ -5,35 +5,59 @@ import core.Entity;
 import core.utils.Point;
 
 /**
- * ProjectileComponent saves the start position and goal location of a projectile.
+ * Marks an entity as a projectile.
+ *
+ * <p>The component stores a {@link #startPosition start} and a {@link #goalLocation goal} position.
+ *
+ * <p>A projectile will need a {@link core.components.VelocityComponent VelocityComponent} and
+ * {@link core.components.PositionComponent PositionComponent} as well. See <a
+ * href="https://github.com/Programmiermethoden/Dungeon/tree/master/doc/ecs/systems">System-Overview</a>.
+ *
+ * <p>The {@link contrib.systems.ProjectileSystem ProjectileSystem} will calculate a flight path
+ * from the start to the goal and will then set the velocity in the {@link
+ * core.components.VelocityComponent} of the entity accordingly. If the goal location is reached,
+ * the entity will be removed from the game.
+ *
+ * <p>Examples of projectiles are {@link contrib.utils.components.skill.FireballSkill fireballs} or
+ * arrows.
  *
  * <p>The {@link #goalLocation()} method retrieves the goal position of the projectile. The {@link
  * #startPosition()} method retrieves the start position of the projectile.
+ *
+ * @see contrib.utils.components.skill.DamageProjectile
  */
-public class ProjectileComponent extends Component {
+public final class ProjectileComponent extends Component {
 
-    private Point goalLocation;
-    private Point startPosition;
+    private final Point goalLocation;
+    private final Point startPosition;
 
-    public ProjectileComponent(Entity entity, Point startPosition, Point goalLocation) {
+    /**
+     * Create a new ProjectileComponent and add it to the associated entity.
+     *
+     * @param entity The associated entity.
+     * @param startPosition The point from which to start the calculation to the goal location.
+     * @param goalLocation The point where the projectile should fly to.
+     */
+    public ProjectileComponent(
+            final Entity entity, final Point startPosition, final Point goalLocation) {
         super(entity);
         this.goalLocation = goalLocation;
         this.startPosition = startPosition;
     }
 
     /**
-     * gets the goal position of the projectile
+     * Get the target position of the projectile.
      *
-     * @return goal position of the projectile
+     * @return The point where the projectile should fly to.
      */
     public Point goalLocation() {
         return goalLocation;
     }
 
     /**
-     * gets the start position of the projectile
+     * Get the start position of the projectile.
      *
-     * @return start position of the projectile
+     * @return The point from which to start the calculation to the goal location
      */
     public Point startPosition() {
         return startPosition;
