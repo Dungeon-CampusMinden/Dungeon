84ce1039284dd799a1f223d9cf1ced663c4a34c1
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2023-07-10 21:20:52 +0200

    [GAME] refactor StatsComponent better api ( and JavaDoc, Clean Code) (#829)
    * better api, javadoc, cc, yagni
    
    * missed some finals
    
    * Apply suggestions from code review
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>
    
    * better javadoc
    
    * explain negativ modifier, better javadoc
    
    ---------
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>

--- game/test/contrib/systems/HealthSystemTest.java
+++ game/test/contrib/systems/HealthSystemTest.java
@@ -98,7 +98,7 @@ public class HealthSystemTest {
         Entity entity = new Entity();
         new DrawComponent(entity, ANIMATION_PATH);
         StatsComponent statsComponent = new StatsComponent(entity);
-        statsComponent.damageModifiers().setMultiplier(DamageType.PHYSICAL, 2);
+        statsComponent.multiplier(DamageType.PHYSICAL, 2);
 
         HealthComponent healthComponent = new HealthComponent(entity);
         healthComponent.maximalHealthpoints(100);
@@ -119,7 +119,7 @@ public class HealthSystemTest {
         Entity entity = new Entity();
         new DrawComponent(entity, ANIMATION_PATH);
         StatsComponent statsComponent = new StatsComponent(entity);
-        statsComponent.damageModifiers().setMultiplier(DamageType.PHYSICAL, -2);
+        statsComponent.multiplier(DamageType.PHYSICAL, -2);
 
         HealthComponent healthComponent = new HealthComponent(entity);
         healthComponent.maximalHealthpoints(200);
@@ -140,7 +140,7 @@ public class HealthSystemTest {
         Entity entity = new Entity();
         new DrawComponent(entity, ANIMATION_PATH);
         StatsComponent statsComponent = new StatsComponent(entity);
-        statsComponent.damageModifiers().setMultiplier(DamageType.PHYSICAL, 0);
+        statsComponent.multiplier(DamageType.PHYSICAL, 0);
 
         HealthComponent healthComponent = new HealthComponent(entity);
         healthComponent.maximalHealthpoints(200);
@@ -161,7 +161,7 @@ public class HealthSystemTest {
         Entity entity = new Entity();
         new DrawComponent(entity, ANIMATION_PATH);
         StatsComponent statsComponent = new StatsComponent(entity);
-        statsComponent.damageModifiers().setMultiplier(DamageType.PHYSICAL, 100);
+        statsComponent.multiplier(DamageType.PHYSICAL, 100);
 
         HealthComponent healthComponent = new HealthComponent(entity);
         healthComponent.maximalHealthpoints(200);
