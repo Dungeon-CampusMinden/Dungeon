a1fbb2e57cf79b7357d3d5945d54a16054b0ef61
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2023-07-10 21:17:57 +0200

    [GAME] refactor ItemComponent (JavaDoc, Clean Code) (#831)
    * javadoc and yagni itemcomponent
    
    * Apply suggestions from code review
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>
    
    * better class javadoc
    
    * Apply suggestions from code review
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>
    
    ---------
    
    Co-authored-by: Carsten Gips <cagix@fh-bielefeld.de>

--- game/src/contrib/components/ItemComponent.java
+++ game/src/contrib/components/ItemComponent.java
@@ -6,44 +6,55 @@ import core.Component;
 import core.Entity;
 
 /**
- * A class that marks an entity as an Item and controls its {@link ItemData}
+ * Marks an entity as an item in the game world.
  *
- * <p>It contains the {@link #itemData}, which contains all info about the Item.
+ * <p>In the dungeon, an item exists in two different states. The first state describes the item
+ * when it is stored in an {@link InventoryComponent inventory}. An item within an inventory
+ * consists only of the {@link ItemData} and is not an {@link Entity}. The second state is active
+ * when an item is on the ground in the level. In this state, an item is an {@link Entity} with
+ * various components. An entity with an {@link ItemComponent} is not an item in an inventory, but
+ * an item that exists in the game world.
+ *
+ * <p>The {@link contrib.entities.WorldItemBuilder WorldItemBuilder} demonstrates how to create an
+ * item entity. You can use the {@link contrib.entities.WorldItemBuilder#buildWorldItem(ItemData)}
+ * method to create a new item-entity from an {@link ItemData}.
+ *
+ * <p>The {@link contrib.entities.WorldItemBuilder} will create an entity with a {@link
+ * ItemComponent}, {@link core.components.PositionComponent}, {@link core.components.DrawComponent},
+ * and {@link CollideComponent}. The {@link contrib.entities.WorldItemBuilder} will configure the
+ * collide-callback for the item-entity so that if it collides with the player character, the stored
+ * {@link ItemData} will be added to the {@link InventoryComponent} of the player, and the
+ * item-entity will be removed from the game.
+ *
+ * <p>By default, an {@link ItemData} that is dropped from the inventory will be (re-) created as an
+ * item-entity using the {@link contrib.entities.WorldItemBuilder}.
+ *
+ * <p>Note that this component does not implement this behavior. This component only marks an entity
+ * as an item and is used to store the associated {@link ItemData}.
+ *
+ * @see ItemData
+ * @see contrib.entities.WorldItemBuilder
  */
-public class ItemComponent extends Component {
-    private ItemData itemData;
+public final class ItemComponent extends Component {
+    private final ItemData itemData;
 
     /**
-     * Create a new component and add it to the associated entity
+     * Creates a new {@link ItemComponent} and adds it to the associated entity.
      *
-     * @param entity entity that will be marked as an Item
+     * @param entity The associated entity.
+     * @param itemData The data of the item to store in this component.
      */
-    public ItemComponent(Entity entity) {
-        super(entity);
-    }
-
-    /**
-     * Creates a new ItemComponent and adds it to the associated entity
-     *
-     * @param entity entity that will be marked as an Item
-     * @param itemData data of the item for the component
-     */
-    public ItemComponent(Entity entity, ItemData itemData) {
+    public ItemComponent(final Entity entity, ItemData itemData) {
         super(entity);
         this.itemData = itemData;
     }
 
     /**
-     * @return the ItemData
+     * Gets the {@link ItemData} of this component.
+     *
+     * @return The {@link ItemData} stored in this component.
      */
     public ItemData itemData() {
         return itemData;
-    }
-
-    /**
-     * @param itemData data of the item for the component
-     */
-    public void itemData(ItemData itemData) {
-        this.itemData = itemData;
     }
 }
