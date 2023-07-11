8d9618098214ba961f841ecb3370abdbadb95eea
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-11 21:02:09 +0200

    make taskcontentcomponent way smaller

--- dungeon/src/task/TaskContentComponent.java
+++ dungeon/src/task/TaskContentComponent.java
@@ -15,11 +15,7 @@ import java.util.stream.Stream;
  * <p>Using this component, a connection can be made between the in-game entity and the elements
  * from the task description.
  *
- * <p>The collection can be manipulated using {@link #add(TaskContent)} and {@link
- * #remove(TaskContent)}.
- *
- * <p>The collection can be queried as a stream using {@link #stream()}, and {@link
- * #contains(TaskContent)} can be used to check if the given element is stored in the collection.
+ * <p>The collection can be queried as a stream using {@link #stream()}
  */
 public final class TaskContentComponent extends Component {
 
@@ -45,17 +41,7 @@ public final class TaskContentComponent extends Component {
     public TaskContentComponent(final Entity entity, final TaskContent content) {
         super(entity);
         this.content = new HashSet<>();
-        add(content);
-    }
-
-    /**
-     * Create a new TaskContentComponent with an empty Collection of represented {@link
-     * TaskContent}s and add it to the associated entity.
-     *
-     * @param entity associated entity
-     */
-    public TaskContentComponent(final Entity entity) {
-        this(entity, new HashSet<>());
+        this.content.add(content);
     }
 
     /**
@@ -65,33 +51,5 @@ public final class TaskContentComponent extends Component {
      */
     public Stream<TaskContent> stream() {
         return content.stream();
-    }
-
-    /**
-     * Checks if the internal Set of represented {@link TaskContent}s contains the given element.
-     *
-     * @param content TaskContent to check for
-     * @return true if the element is in the internal set, false if not
-     */
-    public boolean contains(final TaskContent content) {
-        return this.content.contains(content);
-    }
-
-    /**
-     * Add given Element to the internal set of represented {@link TaskContent}s.
-     *
-     * @param content element to add to the internal set
-     */
-    public void add(final TaskContent content) {
-        this.content.add(content);
-    }
-
-    /**
-     * Remove given Element from the internal set of represented {@link TaskContent}s.
-     *
-     * @param content element to remove from the internal set
-     */
-    public void remove(final TaskContent content) {
-        this.content.remove(content);
     }
 }
