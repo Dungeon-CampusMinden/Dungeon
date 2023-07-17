99235aa10be602d7c1792b077605dc04484947a9
mr <malt.r@mail.de>
2023-07-17 17:54:46 +0200

    Javadoc

--- dsl/src/semanticanalysis/types/CallbackAdapter/CallbackAdapterBuilder.java
+++ dsl/src/semanticanalysis/types/CallbackAdapter/CallbackAdapterBuilder.java
@@ -4,6 +4,11 @@ import interpreter.DSLInterpreter;
 
 import semanticanalysis.FunctionSymbol;
 
+/**
+ * Builder class for {@link CallbackAdapter}. Stores a reference to
+ * the DSLInterpreter, because it is needed to actually perform the
+ * execution of a callback defined in the DSL.
+ */
 public class CallbackAdapterBuilder {
     DSLInterpreter interpreter;
 
@@ -11,6 +16,13 @@ public class CallbackAdapterBuilder {
         this.interpreter = interpreter;
     }
 
+    /**
+     * Build a {@link CallbackAdapter} for a concrete DSL function for assigning
+     * to a callback-Field in a Component of the Dungeons ECS.
+     *
+     * @param functionSymbol The Symbol representing the function definition
+     * @return The created {@link CallbackAdapter}
+     */
     public CallbackAdapter buildAdapter(FunctionSymbol functionSymbol) {
         return new CallbackAdapter(
                 interpreter.getRuntimeEnvironment(), functionSymbol, interpreter);
