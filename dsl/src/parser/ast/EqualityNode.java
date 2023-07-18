c494b0fb7ace20abf2667754c314e61c4361f218
mr <malt.r@mail.de>
2023-07-18 14:18:36 +0200

    Add getter for equalityType

--- dsl/src/parser/ast/EqualityNode.java
+++ dsl/src/parser/ast/EqualityNode.java
@@ -8,6 +8,10 @@ public class EqualityNode extends BinaryNode {
 
     private final EqualityType equalityType;
 
+    public EqualityType getEqualityType() {
+        return equalityType;
+    }
+
     public EqualityNode(EqualityType type, Node lhs, Node rhs) {
         super(Type.Equality, lhs, rhs);
         this.equalityType = type;
