d54b4bb89dc24d79c337a42970a864d7ef51d656
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-18 11:56:23 +0200

    better callbacktest

--- dungeon/test/manual/quizquestion/WizardQuizTest.java
+++ dungeon/test/manual/quizquestion/WizardQuizTest.java
@@ -1,5 +1,8 @@
 package manual.quizquestion;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 import contrib.components.InteractionComponent;
 import contrib.configuration.ItemConfig;
 import contrib.entities.EntityFactory;
@@ -9,6 +12,7 @@ import core.Entity;
 import core.Game;
 import core.components.DrawComponent;
 import core.components.PositionComponent;
+import core.hud.UITools;
 import core.level.utils.LevelSize;
 import core.systems.LevelSystem;
 
@@ -17,8 +21,71 @@ import task.quizquestion.Quiz;
 import task.quizquestion.UIAnswerCallback;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 public class WizardQuizTest {
+    private static Quiz question = multipleChoiceDummy();
+
+    private static void toggleQuiz() {
+        switch (question.type()) {
+            case FREETEXT -> question = singleChoiceDummy();
+            case SINGLE_CHOICE -> question = multipleChoiceDummy();
+            case MULTIPLE_CHOICE -> question = freeTextDummy();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // start the game
+        Game.hero(EntityFactory.newHero());
+        LevelSystem.levelSize(LevelSize.SMALL);
+        Game.loadConfig(
+                "dungeon_config.json",
+                contrib.configuration.KeyboardConfig.class,
+                core.configuration.KeyboardConfig.class,
+                ItemConfig.class);
+        Game.frameRate(30);
+        Game.userOnFrame(
+                () -> {
+                    if (Gdx.input.isKeyJustPressed(Input.Keys.V)) toggleQuiz();
+                });
+        Game.userOnLevelLoad(
+                () -> {
+                    try {
+                        questWizard();
+                    } catch (IOException e) {
+                        throw new RuntimeException();
+                    }
+                });
+        Game.windowTitle("Quest Wizard");
+        Game.addSystem(new AISystem());
+        Game.addSystem(new CollisionSystem());
+        Game.addSystem(new HealthSystem());
+        Game.addSystem(new XPSystem());
+        Game.addSystem(new ProjectileSystem());
+
+        // build and start game
+        Game.run();
+    }
+
+    private static void questWizard() throws IOException {
+        Entity wizard = new Entity("Quest Wizard");
+        new PositionComponent(wizard);
+        new DrawComponent(wizard, "character/wizard");
+        new TaskComponent(wizard, question);
+        new InteractionComponent(
+                wizard, 1, false, UIAnswerCallback.askOnInteraction(question, showAnswersOnHud()));
+    }
+
+    private static Consumer<Set<Quiz.Content>> showAnswersOnHud() {
+        return taskContents -> {
+            AtomicReference<String> answers = new AtomicReference<>("");
+            taskContents.forEach(
+                    t -> answers.set(answers.get() + t.content() + System.lineSeparator()));
+            UITools.generateNewTextDialog(answers.get(), "Ok", "Given answer");
+        };
+    }
 
     public static Quiz singleChoiceDummy() {
         Quiz question =
@@ -58,53 +125,4 @@ public class WizardQuizTest {
                 Quiz.Content.Type.TEXT,
                 "Mit welchem Befehl kann man sich Dateien in der Working copy anzeigen lassen, die unversioniert sind oder in denen es Änderungen seit dem letzten Commit gab?");
     }
-
-    // todo extract
-
-    public static void main(String[] args) throws IOException {
-        // start the game
-        Game.hero(EntityFactory.newHero());
-        LevelSystem.levelSize(LevelSize.SMALL);
-        Game.loadConfig(
-                "dungeon_config.json",
-                contrib.configuration.KeyboardConfig.class,
-                core.configuration.KeyboardConfig.class,
-                ItemConfig.class);
-        Game.frameRate(30);
-        Game.userOnLevelLoad(
-                () -> {
-                    try {
-                        questWizard();
-                    } catch (IOException e) {
-                        throw new RuntimeException();
-                    }
-                });
-        Game.windowTitle("Quest Wizard");
-        Game.addSystem(new AISystem());
-        Game.addSystem(new CollisionSystem());
-        Game.addSystem(new HealthSystem());
-        Game.addSystem(new XPSystem());
-        Game.addSystem(new ProjectileSystem());
-
-        // build and start game
-        Game.run();
-    }
-
-    private static void questWizard() throws IOException {
-        Entity wizard = new Entity("Quest Wizard");
-        new PositionComponent(wizard);
-        new DrawComponent(wizard, "character/wizard");
-        new TaskComponent(wizard, TEST_QUIZ);
-        new InteractionComponent(
-                wizard,
-                1,
-                false,
-                UIAnswerCallback.askOnInteraction(
-                        TEST_QUIZ,
-                        taskContents ->
-                                taskContents.forEach(
-                                        t -> System.out.println("Answer " + t.content()))));
-    }
-
-    public static final Quiz TEST_QUIZ = multipleChoiceDummy();
 }
