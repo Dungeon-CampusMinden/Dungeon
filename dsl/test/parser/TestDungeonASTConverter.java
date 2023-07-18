a36b560ffb8abb7fb2cec093e3bc59fcab67b3c4
mr <malt.r@mail.de>
2023-07-18 13:15:06 +0200

    Add TODO

--- dsl/test/parser/TestDungeonASTConverter.java
+++ dsl/test/parser/TestDungeonASTConverter.java
@@ -578,6 +578,7 @@ public class TestDungeonASTConverter {
         Assert.assertEquals(Node.Type.Block, elseStmt.type);
     }
 
+    // TODO: tests for complex expressions
 
     @Test
     public void testUnary() {
