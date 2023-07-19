265605623914e63ecba1d04110fe54a561185077
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-07-19 10:35:05 +0200

    setup file input reader

---
+++ dungeon/src/dslToGame/DslFileLoader.java
@@ -0,0 +1,59 @@
+package dslToGame;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DslFileLoader {
+
+    private static final String DSL_FILE_ENDING = "dng";
+
+    public static Set<File> dslFiles() {
+        ClassLoader classLoader = DslFileLoader.class.getClassLoader();
+
+        // todo find correct path
+        File directory = new File(classLoader.getResource("scripts").getFile());
+        Set<File> files = new HashSet<>();
+
+        // Recursively find files with the specified file ending
+        findFilesWithEnding(directory, files);
+
+        return files;
+    }
+
+    private static void findFilesWithEnding(File currentDirectory, Set<File> files) {
+        if (currentDirectory.exists() && currentDirectory.isDirectory()) {
+            // Get all files in the current directory
+            File[] allFiles = currentDirectory.listFiles();
+
+            if (allFiles != null) {
+                // Iterate through the files
+                for (File file : allFiles) {
+                    System.out.println("FILE " + file.getPath());
+                    if (file.isFile() && file.getName().endsWith(DSL_FILE_ENDING)) {
+                        files.add(file);
+                    } else if (file.isDirectory()) {
+                        // If it's a subdirectory, recursively search for files inside it
+                        findFilesWithEnding(file, files);
+                    }
+                }
+            }
+        }
+    }
+
+    public static String fileToString(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+}
