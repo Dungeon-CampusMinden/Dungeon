8000e1501a981c94c27892787a00f5fe4b2d0775
mr <malt.r@mail.de>
2023-07-20 15:38:57 +0200

    Extend experimental test for list and set types

--- dsl/test/interpreter/CustomQuestConfig.java
+++ dsl/test/interpreter/CustomQuestConfig.java
@@ -6,7 +6,11 @@ import semanticanalysis.types.DSLType;
 import semanticanalysis.types.DSLTypeMember;
 
 import java.util.List;
+import java.util.Set;
 
 @DSLType(name = "quest_config")
-public record CustomQuestConfig(@DSLTypeMember Entity entity, @DSLTypeMember Entity secondEntity) {}
+public record CustomQuestConfig(@DSLTypeMember Entity entity,
+                                @DSLTypeMember Entity secondEntity,
+                                @DSLTypeMember List<Integer> intList,
+                                @DSLTypeMember Set<Float> floatSet) {}
 
