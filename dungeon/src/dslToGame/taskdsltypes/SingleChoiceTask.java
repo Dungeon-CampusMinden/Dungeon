cad2f0e69db371808f49a0c32fa66e00781dd3df
mr <malt.r@mail.de>
2023-07-26 09:03:30 +0200

    Add DSLType for single choice task

--- dungeon/src/dslToGame/taskdsltypes/SingleChoiceTask.java
+++ dungeon/src/dslToGame/taskdsltypes/SingleChoiceTask.java
@@ -1,2 +1,17 @@
-package dslToGame.taskdsltypes;public class SingleChoiceTask {
+package dslToGame.taskdsltypes;
+
+import semanticanalysis.types.DSLType;
+import semanticanalysis.types.DSLTypeMember;
+import task.quizquestion.Quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+@DSLType
+public class SingleChoiceTask {
+    @DSLTypeMember String description;
+    @DSLTypeMember ArrayList<Quiz.Content> answers;
+    @DSLTypeMember int correctAnswerIndex;
+    @DSLTypeMember BiFunction<SingleChoiceTask, List<Quiz.Content>, Float> scoreFunction;
 }
