87dd46ff368fa13e30d66fae595319b710fe6c91
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-08-07 15:10:17 +0200

    cleanup dsl file input reaer

--- dungeon/src/Starter.java
+++ dungeon/src/Starter.java
@@ -1,15 +1,22 @@
 import contrib.entities.EntityFactory;
 import contrib.systems.*;
 import contrib.utils.components.Debugger;
+
 import core.Game;
 import core.level.utils.LevelSize;
 
+import dslToGame.DslFileLoader;
+
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Set;
 import java.util.logging.Logger;
 
 public class Starter {
     public static void main(String[] args) throws IOException {
         setupBasicGame();
+        Set<Path> path = DslFileLoader.processArguments(args);
+        path.forEach(p -> System.out.println(p.getFileName()));
         Game.run();
     }
 
@@ -20,24 +27,24 @@ public class Starter {
         // start the game
         Game.hero(EntityFactory.newHero());
         Game.loadConfig(
-            "dungeon_config.json",
-            contrib.configuration.KeyboardConfig.class,
-            core.configuration.KeyboardConfig.class);
+                "dungeon_config.json",
+                contrib.configuration.KeyboardConfig.class,
+                core.configuration.KeyboardConfig.class);
         Game.frameRate(30);
         Game.disableAudio(true);
         Game.userOnLevelLoad(
-            () -> {
-                try {
-                    EntityFactory.newChest();
-                    for (int i = 0; i < 5; i++) {
-                        EntityFactory.randomMonster();
+                () -> {
+                    try {
+                        EntityFactory.newChest();
+                        for (int i = 0; i < 5; i++) {
+                            EntityFactory.randomMonster();
+                        }
+                    } catch (IOException e) {
+                        LOGGER.warning("Could not create new Chest: " + e.getMessage());
+                        throw new RuntimeException();
                     }
-                } catch (IOException e) {
-                    LOGGER.warning("Could not create new Chest: " + e.getMessage());
-                    throw new RuntimeException();
-                }
-                Game.levelSize(LevelSize.randomSize());
-            });
+                    Game.levelSize(LevelSize.randomSize());
+                });
         Game.userOnFrame(debugger::execute);
         Game.windowTitle("DSL Dungeon");
         Game.addSystem(new AISystem());
--- dungeon/src/dslToGame/DslFileLoader.java
+++ dungeon/src/dslToGame/DslFileLoader.java
@@ -1,57 +1,43 @@
 package dslToGame;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
 public class DslFileLoader {
 
     private static final String DSL_FILE_ENDING = "dng";
+    private static final String JAR_FILE_ENDING = "jar";
     private static final String SCRIPT_FOLDER = "scripts";
 
-    public static Set<File> dslFiles() {
-        ClassLoader classLoader = DslFileLoader.class.getClassLoader();
-
-        File directory = new File(classLoader.getResource(SCRIPT_FOLDER).getFile());
-        Set<File> files = new HashSet<>();
-
-        // Recursively find files with the specified file ending
-        findFilesWithEnding(directory, files);
-
-        return files;
+    public static Set<Path> processArguments(String[] args) {
+        Set<Path> paths = new HashSet<>();
+        for (String arg : args) {
+            Path path = Paths.get(arg);
+            if (DslFileLoader.is(path, JAR_FILE_ENDING)) {
+                // get skripts out of jar
+            } else if (DslFileLoader.is(path, DSL_FILE_ENDING)) paths.add(path);
+        }
+        return paths;
     }
 
-    private static void findFilesWithEnding(File currentDirectory, Set<File> files) {
-        if (currentDirectory.exists() && currentDirectory.isDirectory()) {
-            // Get all files in the current directory
-            File[] allFiles = currentDirectory.listFiles();
-
-            if (allFiles != null) {
-                // Iterate through the files
-                for (File file : allFiles) {
-                    if (file.isFile() && file.getName().endsWith(DSL_FILE_ENDING)) {
-                        files.add(file);
-                    } else if (file.isDirectory()) {
-                        findFilesWithEnding(file, files);
-                    }
-                }
+    private static boolean is(Path path, String ending) {
+        if (Files.exists(path))
+            if (Files.isRegularFile(path)) {
+                String fileExtension = getFileExtension(path);
+                if (fileExtension != null) return fileExtension.equalsIgnoreCase(ending);
             }
-        }
+        return false;
     }
 
-    public static String fileToString(File file) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line).append(System.lineSeparator());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static String getFileExtension(Path path) {
+        String fileName = path.getFileName().toString();
+        int dotIndex = fileName.lastIndexOf('.');
+        if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
+            return fileName.substring(dotIndex + 1);
         }
-        return stringBuilder.toString();
+        return null;
     }
 }
