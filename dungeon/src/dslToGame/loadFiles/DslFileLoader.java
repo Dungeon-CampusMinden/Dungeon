0d42b7a50c73890851d4a4157b0466cefd9cf35f
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-08-07 15:54:38 +0200

    add javadoc

--- dungeon/src/dslToGame/loadFiles/DslFileLoader.java
+++ dungeon/src/dslToGame/loadFiles/DslFileLoader.java
@@ -6,12 +6,30 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Provides functions for loading DSL files.
+ *
+ * <p>Use {@link #processArguments(String[])} to read in all DSL files in the given paths.
+ * Basically, use this function to parse the command line arguments and extract all DSL file paths.
+ */
 public class DslFileLoader {
 
     private static final String DSL_FILE_ENDING = "dng";
     private static final String JAR_FILE_ENDING = "jar";
     private static final String SCRIPT_FOLDER = "/scripts";
 
+    /**
+     * Load DSL files from the given paths.
+     *
+     * <p>This function will try to parse each given String to a Path and then check if it's a DSL
+     * file or a jar.
+     *
+     * <p>If it's a jar, it will load each DSL file from inside the jar's "/script" directory.
+     *
+     * @param args Strings that could be paths, basically use the command line arguments.
+     * @return Set containing all paths to DSL files.
+     * @throws IOException if an I/O error occurs while reading the files.
+     */
     public static Set<Path> processArguments(String[] args) throws IOException {
         Set<Path> paths = new HashSet<>();
         for (String arg : args) {
@@ -23,7 +41,7 @@ public class DslFileLoader {
         return paths;
     }
 
-    public static Set<Path> findDSLFilesInJar(Path jarPath) throws IOException {
+    private static Set<Path> findDSLFilesInJar(Path jarPath) throws IOException {
         Set<Path> dngFiles = new HashSet<>();
         ClassLoader classLoader = DslFileLoader.class.getClassLoader();
         try (FileSystem jarFileSystem = FileSystems.newFileSystem(jarPath, classLoader)) {
@@ -32,10 +50,10 @@ public class DslFileLoader {
             if (Files.exists(scriptsFolderPath) && Files.isDirectory(scriptsFolderPath)) {
                 Files.walkFileTree(
                         scriptsFolderPath,
-                        new SimpleFileVisitor<Path>() {
+                        new SimpleFileVisitor<>() {
                             @Override
                             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                                if (file.toString().toLowerCase().endsWith(".dng")) {
+                                if (file.toString().toLowerCase().endsWith(DSL_FILE_ENDING)) {
                                     dngFiles.add(file);
                                 }
                                 return FileVisitResult.CONTINUE;
@@ -53,6 +71,13 @@ public class DslFileLoader {
         return dngFiles;
     }
 
+    /**
+     * Check if the given Path is a File with the specified file ending.
+     *
+     * @param path The Path to check.
+     * @param ending The expected file ending.
+     * @return true if the file ends with the given ending (is of that type), otherwise false.
+     */
     private static boolean is(Path path, String ending) {
         if (Files.exists(path))
             if (Files.isRegularFile(path)) {
