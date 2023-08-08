20a6e5131ed8ad225ab9e2a57ed78c91e5004c06
Finn Watermann <finn.watermann@arcor.de>
2023-08-08 23:01:33 +0200

    Add DragAndDrop support to GUICombination

--- game/src/contrib/hud/GUICombination.java
+++ game/src/contrib/hud/GUICombination.java
@@ -1,8 +1,8 @@
 package contrib.hud;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.utils.Align;
 
 import core.Game;
@@ -11,8 +11,16 @@ import java.util.Arrays;
 
 public class GUICombination extends HorizontalGroup {
 
-    public GUICombination(Actor... guis) {
-        Arrays.stream(guis).forEach(this::addActor);
+    private final DragAndDrop dragAndDrop;
+
+    public GUICombination(GUI... guis) {
+        this.dragAndDrop = new DragAndDrop();
+        Arrays.stream(guis)
+                .forEach(
+                        a -> {
+                            this.addActor(a);
+                            a.dragAndDrop(this.dragAndDrop);
+                        });
         this.space(10.0f);
         this.validate();
         this.center();
@@ -42,5 +50,14 @@ public class GUICombination extends HorizontalGroup {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
+    }
+
+    /**
+     * Get the drag and drop object
+     *
+     * @return the drag and drop object
+     */
+    public DragAndDrop dragAndDrop() {
+        return this.dragAndDrop;
     }
 }
