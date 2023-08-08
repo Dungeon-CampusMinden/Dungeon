8d4d3b91e6b5aa40c3e6426a040a3d4e2513cbcb
mr <malt.r@mail.de>
2023-08-08 15:13:51 +0200

    Modify QuestConfig to include displayname property

--- dungeon/src/dslToGame/QuestConfig.java
+++ dungeon/src/dslToGame/QuestConfig.java
@@ -19,4 +19,5 @@ public record QuestConfig(
         @DSLTypeMember int questPoints,
         @DSLTypeMember String password,
         @DSLTypeMember Entity entity,
+        @DSLTypeMember(name="name") String displayName,
         @DSLTypeMember List<Task> tasks) {}
