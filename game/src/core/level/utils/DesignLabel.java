fba6d15a4db0fcb90a492c9302b4512106337294
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-08-09 10:31:56 +0200

    add chance for DesignLabel to get picked

--- game/src/core/level/utils/DesignLabel.java
+++ game/src/core/level/utils/DesignLabel.java
@@ -1,29 +1,56 @@
 package core.level.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 /**
  * Specifies which textures and layouts should be used for the room.
  *
- * @author Andre Matutat
+ * <p>Modified by [Your Name]
  */
 public enum DesignLabel {
-    DEFAULT,
-    FIRE,
-    FOREST,
-    ICE,
-    RAINBOW,
-    TEMPLE,
-    DARK;
-    private static final List<DesignLabel> VALUES = List.of(values());
-    private static final int SIZE = VALUES.size();
+    DEFAULT(50), // 50% chance
+    FIRE(10), // 10% chance
+    FOREST(10), // 10% chance
+    ICE(10), // 10% chance
+    TEMPLE(10), // 10% chance
+    DARK(10), // 10% chance
+    RAINBOW(0); // 0% chance, will not be returned
+
+    private final int chance;
     private static final Random RANDOM = new Random();
+    private static final List<DesignLabel> VALUES = new ArrayList<>();
+
+    static {
+        VALUES.addAll(Arrays.asList(values()));
+    }
 
     /**
-     * @return A random enum-value
+     * Create a new label.
+     *
+     * @param chance chance in % that this label will be returned by the {@link #randomDesign()}
+     *     function.
+     */
+    DesignLabel(int chance) {
+        this.chance = chance;
+    }
+
+    /**
+     * @return A random enum-value based on chances
      */
     public static DesignLabel randomDesign() {
-        return VALUES.get(RANDOM.nextInt(SIZE));
+        int totalChances = VALUES.stream().mapToInt(label -> label.chance).sum();
+        int randomValue = RANDOM.nextInt(totalChances);
+        int cumulativeChances = 0;
+        for (DesignLabel label : VALUES) {
+            cumulativeChances += label.chance;
+            if (randomValue < cumulativeChances) {
+                return label;
+            }
+        }
+        // error case
+        return DEFAULT;
     }
 }
