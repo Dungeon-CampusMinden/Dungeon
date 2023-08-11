5a1574cc0ff89980c8e1ab68c6c016bdd3c58246
mr <malt.r@mail.de>
2023-08-11 16:27:40 +0200

    Add isSettable and isGettable methods to IDSLTypeProperty

--- dsl/src/semanticanalysis/types/IDSLTypeProperty.java
+++ dsl/src/semanticanalysis/types/IDSLTypeProperty.java
@@ -3,4 +3,6 @@ package semanticanalysis.types;
 public interface IDSLTypeProperty<T,V> {
     void set(T instance, V valueToSet);
     V get(T instance);
+    boolean isSettable();
+    boolean isGettable();
 }
