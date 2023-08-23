914f5132e13fb56f6989322786d87cd3e0a06681
larnold <praisethesun@firelink-shrine.de>
2023-08-23 09:36:11 +0200

    change existing currentAnimation(...) to nextAnimation(...)

--- game/src/contrib/utils/components/interaction/DropItemsInteraction.java
+++ game/src/contrib/utils/components/interaction/DropItemsInteraction.java
@@ -92,6 +92,6 @@ public class DropItemsInteraction implements BiConsumer<Entity, Entity> {
                                                 index.getAndIncrement() / count)));
 
         entity.fetch(DrawComponent.class)
-                .ifPresent(x -> x.currentAnimation(CoreAnimations.IDLE_RIGHT));
+                .ifPresent(x -> x.nextAnimation(10, CoreAnimations.IDLE_RIGHT));
     }
 }
