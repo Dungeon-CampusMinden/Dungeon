43e87e72c29690a863245691a69656e433d3d826
mr <malt.r@mail.de>
2023-08-31 13:33:42 +0200

    Add native methods for set type

--- dsl/src/runtime/SetValue.java
+++ dsl/src/runtime/SetValue.java
@@ -1,8 +1,12 @@
 package runtime;
 
+import interpreter.DSLInterpreter;
+import parser.ast.Node;
+import semanticanalysis.IInstanceCallable;
 import semanticanalysis.types.SetType;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /** Implements a set value */
@@ -25,6 +29,7 @@ public class SetValue extends Value {
         return (SetType) this.dataType;
     }
 
+    protected HashSet<Value> set() { return ((HashSet<Value>) this.object); }
     /**
      * Add a Value to the set. The Value will only be added to the set, if no other Value with the
      * same internal value of the passed Value is already stored in this set.
@@ -38,7 +43,7 @@ public class SetValue extends Value {
             return false;
         }
         internalValueSet.add(object);
-        ((HashSet<Value>) this.object).add(value);
+        set().add(value);
         return true;
     }
 
@@ -46,10 +51,61 @@ public class SetValue extends Value {
      * @return all stored values
      */
     public Set<Value> getValues() {
-        return (Set<Value>) this.object;
+        return set();
     }
 
     public void clearSet() {
-        ((Set<Value>) this.object).clear();
+        set().clear();
     }
+
+
+    // region native_methods
+    public static class AddMethod implements IInstanceCallable {
+
+        public static SetValue.AddMethod instance = new SetValue.AddMethod();
+
+        private AddMethod() {}
+
+        @Override
+        public Object call(DSLInterpreter interpreter, Object instance, List<Node> parameters) {
+            SetValue setValue = (SetValue) instance;
+            Node paramNode = parameters.get(0);
+            Value paramValue = (Value) paramNode.accept(interpreter);
+
+            return setValue.addValue(paramValue);
+        }
+    }
+
+    public static class SizeMethod implements IInstanceCallable {
+
+        public static SetValue.SizeMethod instance = new SetValue.SizeMethod();
+
+        private SizeMethod() {}
+
+        @Override
+        public Object call(DSLInterpreter interpreter, Object instance, List<Node> parameters) {
+            SetValue setValue = (SetValue) instance;
+
+            return setValue.set().size();
+        }
+    }
+
+    public static class ContainsMethod implements IInstanceCallable {
+
+        public static SetValue.ContainsMethod instance = new SetValue.ContainsMethod();
+
+        private ContainsMethod() {}
+
+        @Override
+        public Object call(DSLInterpreter interpreter, Object instance, List<Node> parameters) {
+            SetValue setValue = (SetValue) instance;
+
+            Node valueToCheckNode = parameters.get(0);
+            Value valueToCheck = (Value) valueToCheckNode.accept(interpreter);
+
+            return setValue.internalValueSet.contains(valueToCheck.getInternalValue());
+        }
+    }
+    // endregion
+
 }
--- dsl/src/semanticanalysis/types/SetType.java
+++ dsl/src/semanticanalysis/types/SetType.java
@@ -1,9 +1,11 @@
 package semanticanalysis.types;
 
+import runtime.SetValue;
+import runtime.nativefunctions.NativeMethod;
 import semanticanalysis.IScope;
-import semanticanalysis.Symbol;
+import semanticanalysis.ScopedSymbol;
 
-public class SetType extends Symbol implements IType {
+public class SetType extends ScopedSymbol implements IType {
     // private final IType elementType;
     public IType getElementType() {
         return this.dataType;
@@ -14,9 +16,31 @@ public class SetType extends Symbol implements IType {
     }
 
     public SetType(IType elementType, IScope parentScope) {
-        this.name = getSetTypeName(elementType);
-        this.dataType = elementType;
-        this.scope = parentScope;
+        super(getSetTypeName(elementType), parentScope, elementType);
+
+        NativeMethod addMethod =
+            new NativeMethod(
+                "add",
+                this,
+                new FunctionType(BuiltInType.boolType, elementType),
+                SetValue.AddMethod.instance);
+        this.bind(addMethod);
+
+        NativeMethod sizeMethod =
+            new NativeMethod(
+                "size",
+                this,
+                new FunctionType(BuiltInType.intType, BuiltInType.noType),
+                SetValue.SizeMethod.instance);
+        this.bind(sizeMethod);
+
+        NativeMethod getMethod =
+            new NativeMethod(
+                "contains",
+                this,
+                new FunctionType(BuiltInType.boolType, elementType),
+                SetValue.ContainsMethod.instance);
+        this.bind(getMethod);
     }
 
     @Override
