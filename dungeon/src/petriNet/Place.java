6b849d059e15f5cd8364c2a8d1401fc70aff8134
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-08-31 12:52:03 +0200

    impl Transition and observer pattern

--- dungeon/src/petriNet/Place.java
+++ dungeon/src/petriNet/Place.java
@@ -1,13 +1,22 @@
 package petriNet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Place {
     private int tokenCount = 0;
+    private Set<Transition> observer = new HashSet<>();
 
     public void placeToken() {
         tokenCount++;
+        observer.forEach(Transition::notify);
     }
 
     public int tokenCount() {
         return tokenCount;
+    }
+
+    public void register(Transition observer) {
+        this.observer.add(observer);
     }
 }
--- dungeon/src/petriNet/Transition.java
+++ dungeon/src/petriNet/Transition.java
@@ -1,3 +1,35 @@
 package petriNet;
 
-public class Transition {}
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Transition {
+
+    private final Map<Place, Boolean> dependencyPlaces;
+    private final Set<Place> addTokenIfTriggerd;
+
+    private final Set<Place> addTokenOnFire;
+
+    public Transition(
+            Set<Place> dependencyPlaces, Set<Place> addTokenIfTriggerd, Set<Place> addTokenOnFire) {
+        this.addTokenOnFire = addTokenOnFire;
+        this.dependencyPlaces = new HashMap<>();
+        for (Place place : dependencyPlaces) {
+            this.dependencyPlaces.put(place, place.tokenCount() > 0);
+            place.register(this);
+        }
+        this.addTokenIfTriggerd = addTokenIfTriggerd;
+    }
+
+    public void notify(Place place) {
+        dependencyPlaces.replace(place, true);
+
+        // if all places have a token fire
+        if (dependencyPlaces.values().stream().allMatch(v -> v == true)) fire();
+    }
+
+    private void fire() {
+        addTokenOnFire.forEach(Place::placeToken);
+    }
+}
