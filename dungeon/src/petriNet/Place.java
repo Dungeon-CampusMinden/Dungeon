6a70fc629d3126f66fe68c6665c0994530989fdb
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-08-31 12:40:16 +0200

    impl place

--- dungeon/src/petriNet/Place.java
+++ dungeon/src/petriNet/Place.java
@@ -1,13 +1,13 @@
 package petriNet;
 
 public class Place {
+    private int tokenCount = 0;
 
+    public void placeToken() {
+        tokenCount++;
+    }
 
-
-
+    public int tokenCount() {
+        return tokenCount;
+    }
 }
-
-
-
-
-
