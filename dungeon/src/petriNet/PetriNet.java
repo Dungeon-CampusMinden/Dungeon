960a277bf8fa2d90ce6665f694a0f75219bbe872
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-08-31 12:36:04 +0200

    petri net is not a task anymore

---
+++ dungeon/src/petriNet/PetriNet.java
@@ -0,0 +1,11 @@
+package petriNet;
+
+// TODO teil von Milestone https://github.com/Programmiermethoden/Dungeon/milestone/59
+
+import task.Task;
+
+public final class PetriNet {
+    public PetriNet() {
+        super();
+    }
+}
