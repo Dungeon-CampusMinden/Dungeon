914fa409f95d53ab345315531bea3ca267b5b6ec
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-09-04 13:09:11 +0200

    add tests

--- dungeon/test/petriNet/PlaceTest.java
+++ dungeon/test/petriNet/PlaceTest.java
@@ -1,9 +1,12 @@
 package petriNet;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import task.Task;
 
 public class PlaceTest {
 
@@ -18,7 +21,94 @@ public class PlaceTest {
     }
 
     @Test
-    public void register_transition() {
+    public void removeToken() {
+        Place place = new Place();
+        place.placeToken();
+        place.placeToken();
+        assertEquals(2, place.tokenCount());
+        place.removeToken();
+        assertEquals(1, place.tokenCount());
+        place.removeToken();
+        assertEquals(0, place.tokenCount());
+        place.removeToken();
+        assertEquals("Token-count should not be negative", 0, place.tokenCount());
+    }
+
+    @Test
+    public void changeStateOnTokenAdd() {
+        Task taskA = new DummyTask();
+        Task taskB = new DummyTask();
+        Task.TaskState changeATo = Task.TaskState.ACTIVE;
+        Task.TaskState changeBTo = Task.TaskState.FINISHED_BAD;
+        Place place = new Place();
+        place.changeStateOnTokenAdd(taskA, changeATo);
+        place.changeStateOnTokenAdd(taskB, changeBTo);
+        assertEquals(Task.TaskState.INACTIVE, taskA.state());
+        assertEquals(Task.TaskState.INACTIVE, taskB.state());
+        place.placeToken();
+        assertEquals(changeATo, taskA.state());
+        assertEquals(changeBTo, taskB.state());
+    }
+
+    @Test
+    public void observe_and_notify() {
+        Task taskA = new DummyTask();
+        Task taskB = new DummyTask();
+        Task.TaskState aAt = Task.TaskState.ACTIVE;
+        Task.TaskState bAt = Task.TaskState.FINISHED_BAD;
+        Place place = new Place();
+        place.observe(taskA, aAt);
+        place.observe(taskB, bAt);
+        assertEquals(0, place.tokenCount());
+        taskA.state(aAt);
+        assertEquals(1, place.tokenCount());
+        taskB.state(aAt);
+        assertEquals(1, place.tokenCount());
+        taskB.state(bAt);
+        assertEquals(2, place.tokenCount());
+    }
+
+    @Test
+    public void cant_observe_and_change() {
+        Place place = new Place();
+        place.observe(new DummyTask(), Task.TaskState.FINISHED_PERFECT);
+
+        // Use assertThrows to check if RuntimeException is thrown
+        RuntimeException exception =
+                assertThrows(
+                        RuntimeException.class,
+                        () -> {
+                            place.changeStateOnTokenAdd(
+                                    new DummyTask(), Task.TaskState.FINISHED_PERFECT);
+                        });
+
+        // Optionally, you can check the exception message if needed
+        String expectedMessage = "A Place cannot observe and activate Tasks at the same time.";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void cant_change_and_observe() {
+        Place place = new Place();
+        place.changeStateOnTokenAdd(new DummyTask(), Task.TaskState.FINISHED_PERFECT);
+
+        // Use assertThrows to check if RuntimeException is thrown
+        RuntimeException exception =
+                assertThrows(
+                        RuntimeException.class,
+                        () -> {
+                            place.observe(new DummyTask(), Task.TaskState.FINISHED_PERFECT);
+                        });
+
+        // Optionally, you can check the exception message if needed
+        String expectedMessage = "A Place cannot observe and activate Tasks at the same time.";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void register_and_notify_transition() {
         Place place = new Place();
         Transition transitionA = Mockito.mock(Transition.class);
         Transition transitionB = Mockito.mock(Transition.class);
@@ -28,4 +118,6 @@ public class PlaceTest {
         Mockito.verify(transitionA).notify(place);
         Mockito.verify(transitionB).notify(place);
     }
+
+    private static class DummyTask extends Task {}
 }
---
+++ dungeon/test/task/TaskTest.java
@@ -0,0 +1,131 @@
+package task;
+
+import static junit.framework.TestCase.assertEquals;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import petriNet.Place;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class TaskTest {
+
+    private Task task;
+
+    @Before
+    public void setup() {
+        task = new DummyTask();
+    }
+
+    @Test
+    public void register_and_notify_place() {
+        Place onActive = new Place();
+        Place onPerfect = new Place();
+        Place onBad = new Place();
+        onActive.observe(task, Task.TaskState.ACTIVE);
+        onPerfect.observe(task, Task.TaskState.FINISHED_PERFECT);
+        onBad.observe(task, Task.TaskState.FINISHED_BAD);
+
+        assertEquals(0, onActive.tokenCount());
+        assertEquals(0, onBad.tokenCount());
+        assertEquals(0, onPerfect.tokenCount());
+
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals(1, onActive.tokenCount());
+        assertEquals(0, onBad.tokenCount());
+        assertEquals(0, onPerfect.tokenCount());
+
+        task.state(Task.TaskState.FINISHED_BAD);
+        assertEquals(1, onActive.tokenCount());
+        assertEquals(1, onBad.tokenCount());
+        assertEquals(0, onPerfect.tokenCount());
+    }
+
+    @Test
+    public void state_perfect() {
+        assertEquals(Task.TaskState.INACTIVE, task.state());
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals(Task.TaskState.ACTIVE, task.state());
+        task.state(Task.TaskState.INACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.ACTIVE, task.state());
+        task.state(Task.TaskState.FINISHED_PERFECT);
+        assertEquals(Task.TaskState.FINISHED_PERFECT, task.state());
+        task.state(Task.TaskState.INACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.FINISHED_PERFECT, task.state());
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.FINISHED_PERFECT, task.state());
+    }
+
+    @Test
+    public void state_okay() {
+        assertEquals(Task.TaskState.INACTIVE, task.state());
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals(Task.TaskState.ACTIVE, task.state());
+        task.state(Task.TaskState.INACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.ACTIVE, task.state());
+        task.state(Task.TaskState.FINISHED_OKAY);
+        assertEquals(Task.TaskState.FINISHED_OKAY, task.state());
+        task.state(Task.TaskState.INACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.FINISHED_OKAY, task.state());
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.FINISHED_OKAY, task.state());
+    }
+
+    @Test
+    public void state_bad() {
+        assertEquals(Task.TaskState.INACTIVE, task.state());
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals(Task.TaskState.ACTIVE, task.state());
+        task.state(Task.TaskState.INACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.ACTIVE, task.state());
+        task.state(Task.TaskState.FINISHED_BAD);
+        assertEquals(Task.TaskState.FINISHED_BAD, task.state());
+        task.state(Task.TaskState.INACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.FINISHED_BAD, task.state());
+        task.state(Task.TaskState.ACTIVE);
+        assertEquals("Can not change state back.", Task.TaskState.FINISHED_BAD, task.state());
+    }
+
+    @Test
+    public void taskText() {
+        String newTest = "Test 1";
+        task.taskText(newTest);
+        assertEquals(newTest, task.taskText());
+    }
+
+    @Test
+    public void managerComponent() {
+        TaskComponent component = new TaskComponent(task);
+        task.managerComponent(component);
+        assertEquals(component, task.managerComponent());
+    }
+
+    @Test
+    public void addContent() {
+        TaskContent a = Mockito.mock(TaskContent.class);
+        TaskContent b = Mockito.mock(TaskContent.class);
+        TaskContent c = Mockito.mock(TaskContent.class);
+        task.addContent(a);
+        task.addContent(b);
+        task.addContent(c);
+        List<TaskContent> contents = task.contentStream().toList();
+        assertTrue(contents.contains(a));
+        assertTrue(contents.contains(b));
+        assertTrue(contents.contains(c));
+    }
+
+    @Test
+    public void scoringFunction() {
+        BiFunction scoring = Mockito.mock(BiFunction.class);
+        task.scoringFunction(scoring);
+        Mockito.verifyNoMoreInteractions(scoring);
+        assertEquals(scoring, task.scoringFunction());
+    }
+
+    private static class DummyTask extends Task {}
+}
