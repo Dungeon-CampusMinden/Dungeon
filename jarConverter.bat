94a1e7c811689cb170ae1a0e34ee14bfae070420
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-09-13 11:15:19 +0200

    add converter bat for windows

---
+++ jarConverter.bat
@@ -0,0 +1,24 @@
+@echo off
+rem Create the "scripts" folder if it doesn't exist
+mkdir scripts
+
+rem Iterate over all passed arguments
+for %%A in (%*) do (
+    rem Check if the argument is a file or directory
+    if exist %%A (
+        rem Extract the filename from the path
+        setlocal enabledelayedexpansion
+        for %%F in ("%%A") do set "filename=%%~nxF"
+        endlocal
+
+        rem Copy the file or directory to the "scripts" folder
+        xcopy /s /i "%%A" "scripts\!filename!"
+    ) else (
+        echo Das Argument '%%A' ist keine gültige Datei oder Verzeichnis.
+    )
+)
+
+rem Create the JAR file
+jar -cvf scripts.jar scripts
+
+echo Die Dateien und Verzeichnisse wurden in den 'scripts'-Ordner kopiert und als 'scripts.jar' exportiert.
