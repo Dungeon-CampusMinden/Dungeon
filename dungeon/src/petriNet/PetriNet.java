44c318014a0cee3b333461f170a26bb3422f3ed9
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-09-25 15:23:46 +0200

    PetriNet record ctor

--- dungeon/src/petriNet/PetriNet.java
+++ dungeon/src/petriNet/PetriNet.java
@@ -1,3 +1,17 @@
 package petriNet;
 
-public record PetriNet(Transition activateTask) {}
+public record PetriNet(
+        Place taskNotActivated,
+        Transition activateTask,
+        Place taskActivated,
+        Transition afterActivated,
+        Transition activateprocessing,
+        Place processingActivated,
+        Place finishedFalse,
+        Place finishedCorrect,
+        Transition correct,
+        Transition wrong,
+        Place end_correct,
+        Place end_false,
+        Place end,
+        Transition finisehd) {}
