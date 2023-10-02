d5aa1de6f3b22c94ab84a3d0d842f6888991507d
mr <malt.r@mail.de>
2023-10-02 16:04:35 +0200

    Add javadoc for ScenarioBuilderStorage

--- dsl/src/interpreter/ScenarioBuilderStorage.java
+++ dsl/src/interpreter/ScenarioBuilderStorage.java
@@ -1,6 +1,7 @@
 package interpreter;
 
 import runtime.IEvironment;
+import runtime.Value;
 import semanticanalysis.FunctionSymbol;
 import semanticanalysis.types.AggregateType;
 import semanticanalysis.types.IType;
@@ -8,14 +9,28 @@ import task.Task;
 
 import java.util.*;
 
-// TODO: javadoc
+/**
+ * This class stores {@link FunctionSymbol}s, which refer to scenario builder
+ * methods (which take a `task` definition as an argument and return `entity<><>`)
+ * in a Map, in which the concrete task type is the key (as {@link IType}).
+ */
 public class ScenarioBuilderStorage {
     HashMap<IType, List<FunctionSymbol>> storedScenarioBuilders;
 
+    /**
+     * Constructor.
+     */
     public ScenarioBuilderStorage() {
         storedScenarioBuilders = new HashMap<>();
     }
 
+    /**
+     * Initialize this {@link ScenarioBuilderStorage} from an existing {@link IEvironment}.
+     * The environment's global symbols will be scanned for {@link AggregateType} instances,
+     * which map to a {@link Task} implementation. For each such {@link AggregateType} a
+     * new key is inserted into the internal HashMap.
+     * @param environment The {@link IEvironment} to scan for {@link Task} related types.
+     */
     public void initializeScenarioBuilderStorage(IEvironment environment) {
         var symbols = environment.getGlobalScope().getSymbols();
 
@@ -35,17 +50,32 @@ public class ScenarioBuilderStorage {
             .forEach(this::initializeStorageForType);
     }
 
-    public void initializeStorageForType(IType type) {
+    protected void initializeStorageForType(IType type) {
         if (storedScenarioBuilders.containsKey(type)) {
             throw new RuntimeException("Storage for type " + type + " is already initialized");
         }
         storedScenarioBuilders.put(type, new ArrayList<>());
     }
 
+    /**
+     * Get all {@link IType}s, for which this {@link ScenarioBuilderStorage} currently
+     * can store scenario builder methods as {@link FunctionSymbol}s.
+     * @return a {@link Set} of {@link IType}s.
+     */
     public Set<IType> getTypesWithStorage() {
         return storedScenarioBuilders.keySet();
     }
 
+    /**
+     * Store a {@link FunctionSymbol} as a scenario builder method. The
+     * task type will be determined from the {@link semanticanalysis.types.FunctionType}
+     * of the passed functionSymbol. The passed {@link FunctionSymbol} should have
+     * a {@link semanticanalysis.types.FunctionType} which accepts a single parameter
+     * of a {@link IType}, which maps to {@link Task} and return an `entity<><>` {@link Value}.
+     * The client code is responsible to ensure this!
+     *
+     * @param functionSymbol the {@link FunctionSymbol} to store as a scenario builder method.
+     */
     public void storeScenarioBuilder(FunctionSymbol functionSymbol) {
         // retrieve list for task type
         // the first parametertype denotes the task type
@@ -57,6 +87,15 @@ public class ScenarioBuilderStorage {
         }
     }
 
+    /**
+     * Get a random {@link Optional} of a {@link FunctionSymbol} for a scenario builder method for a given
+     * {@link IType}, which maps to {@link Task}.
+     *
+     * @param type The {@link IType}, to retrieve a random scenario builder method for.
+     * @return An {@link Optional} containing a scenario builder method as {@link FunctionSymbol}
+     * for the given {@link IType}. If this {@link ScenarioBuilderStorage} does not store
+     * such a scenario builder, an empty {@link Optional} is returned.
+     */
     public Optional<FunctionSymbol> retrieveRandomScenarioBuilderForType(IType type) {
         Optional<FunctionSymbol> returnSymbol = Optional.empty();
         if (!storedScenarioBuilders.containsKey(type)) {
