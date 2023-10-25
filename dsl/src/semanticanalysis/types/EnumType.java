bbed22cdf9282070269c6da14aa0e55e6e5d16da
mr <malt.r@mail.de>
2023-10-25 11:37:49 +0200

    Add javadoc to EnumType

--- dsl/src/semanticanalysis/types/EnumType.java
+++ dsl/src/semanticanalysis/types/EnumType.java
@@ -2,15 +2,30 @@ package semanticanalysis.types;
 
 import semanticanalysis.IScope;
 import semanticanalysis.ScopedSymbol;
+import semanticanalysis.Symbol;
 
+/**
+ * Represents an {@link Enum} in the dsl type system. The variants of the enum are stored as {@link
+ * Symbol} in the scope of this type.
+ */
 public class EnumType extends ScopedSymbol implements IType {
     protected Class<? extends Enum<?>> originType;
 
+    /**
+     * Constructor.
+     *
+     * @param name The name of this {@link EnumType}.
+     * @param parentScope The scope in which this type is declared.
+     * @param originType The corresponding Java {@link Enum}-type of this {@link EnumType}
+     */
     public EnumType(String name, IScope parentScope, Class<? extends Enum<?>> originType) {
         super(name, parentScope, null);
         this.originType = originType;
     }
 
+    /**
+     * @return The corresponding Java {@link Enum}-type of this {@link EnumType}.
+     */
     public Class<? extends Enum<?>> getOriginType() {
         return originType;
     }
