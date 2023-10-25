0c800bbf83eaad52e20323b3c5822a3a20ef95f2
mr <malt.r@mail.de>
2023-10-25 13:04:16 +0200

    Add get_content method to multiple choice tasks

--- dungeon/src/task/taskdsltypes/MultipleChoiceTask.java
+++ dungeon/src/task/taskdsltypes/MultipleChoiceTask.java
@@ -1,13 +1,18 @@
 package task.taskdsltypes;
 
+import semanticanalysis.types.DSLExtensionMethod;
 import semanticanalysis.types.DSLTypeAdapter;
 import semanticanalysis.types.DSLTypeMember;
 
+import semanticanalysis.types.IDSLExtensionMethod;
 import task.Quiz;
 import task.Task;
 import task.TaskContent;
 import task.quizquestion.MultipleChoice;
+import task.quizquestion.SingleChoice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -52,5 +57,27 @@ public class MultipleChoiceTask {
             }
         }
         return (float) givenCorrectAnswers / (float) totalCorrectAnswers;
+    }
+
+
+    @DSLExtensionMethod(name = "get_content", extendedType = MultipleChoice.class)
+    public static class GetContentMethod implements IDSLExtensionMethod<MultipleChoice, List<TaskContent>> {
+        public static MultipleChoiceTask.GetContentMethod instance = new MultipleChoiceTask.GetContentMethod();
+
+        @Override
+        public List<TaskContent> call(MultipleChoice instance, List<Object> params) {
+            // This has to return an ArrayList. Calling the `.toList()`-Method on the result of
+            // the `map()`-call bellow will create an `java.util.ImmutableCollections$ListN`-instance,
+            // for which the TypeBuilder cannot create a corresponding dsl-type.
+            List<TaskContent> returnList = new ArrayList<>();
+            instance.contentStream().forEach(returnList::add);
+            return returnList;
+        }
+
+        @Override
+        public List<Class<?>> getParameterTypes() {
+            var arr = new Class<?>[] {};
+            return Arrays.stream(arr).toList();
+        }
     }
 }
--- dungeon/src/task/taskdsltypes/SingleChoiceTask.java
+++ dungeon/src/task/taskdsltypes/SingleChoiceTask.java
@@ -77,5 +77,4 @@ public class SingleChoiceTask {
             return Arrays.stream(arr).toList();
         }
     }
-
 }
