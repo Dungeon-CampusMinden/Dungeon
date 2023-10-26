497df38c8bcb3987c112b14506e3b7c661d6de58
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-10-26 11:27:18 +0200

    add MC Tests

--- dungeon/test/reporting/GradingFunctionsTest.java
+++ dungeon/test/reporting/GradingFunctionsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import task.Quiz;
+import task.quizquestion.MultipleChoice;
 import task.quizquestion.SingleChoice;
 
 import java.util.Set;
@@ -18,6 +19,18 @@ public class GradingFunctionsTest {
         sc.addCorrectAnswerIndex(1);
         sc.scoringFunction(GradingFunctions.singleChoiceGrading());
         return sc;
+    }
+
+    private MultipleChoice setupMultipleChoiceTask() {
+        MultipleChoice mc = new MultipleChoice("Dummy");
+        mc.addAnswer(new Quiz.Content("A"));
+        mc.addAnswer(new Quiz.Content("B"));
+        mc.addAnswer(new Quiz.Content("C"));
+        mc.addAnswer(new Quiz.Content("C"));
+        mc.addCorrectAnswerIndex(0);
+        mc.addCorrectAnswerIndex(1);
+        mc.scoringFunction(GradingFunctions.multipeChoiceGrading());
+        return mc;
     }
 
     @Test
@@ -34,5 +47,47 @@ public class GradingFunctionsTest {
         float points = 5f;
         sc.points(points);
         assertEquals(0, sc.executeScoringFunction(Set.of(sc.contentByIndex(0))), 0.00001f);
+    }
+
+    @Test
+    public void multipechoice_correctAnswer() {
+        MultipleChoice mc = setupMultipleChoiceTask();
+        float points = 4f;
+        mc.points(points);
+        assertEquals(
+                points,
+                mc.executeScoringFunction(Set.of(mc.contentByIndex(0), mc.contentByIndex(1))),
+                0.00001f);
+    }
+
+    @Test
+    public void multipechoice_wrongAnswer() {
+        MultipleChoice mc = setupMultipleChoiceTask();
+        float points = 4f;
+        mc.points(points);
+        assertEquals(
+                0,
+                mc.executeScoringFunction(Set.of(mc.contentByIndex(2), mc.contentByIndex(3))),
+                0.00001f);
+    }
+
+    @Test
+    public void multipechoice_someRightSomeWrongAnswer() {
+        MultipleChoice mc = setupMultipleChoiceTask();
+        float points = 4f;
+        mc.points(points);
+        assertEquals(
+                2,
+                mc.executeScoringFunction(
+                        Set.of(mc.contentByIndex(0), mc.contentByIndex(1), mc.contentByIndex(2))),
+                0.00001f);
+    }
+
+    @Test
+    public void multipechoice_oneRightAnswer() {
+        MultipleChoice mc = setupMultipleChoiceTask();
+        float points = 4f;
+        mc.points(points);
+        assertEquals(2, mc.executeScoringFunction(Set.of(mc.contentByIndex(0))), 0.00001f);
     }
 }
