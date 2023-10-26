70b4c3dbbc9d63296d86b784d080977676bbb0c4
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-10-26 10:18:59 +0200

    add grading function for multiplechoice

--- dungeon/src/reporting/GradingFunctions.java
+++ dungeon/src/reporting/GradingFunctions.java
@@ -2,8 +2,10 @@ package reporting;
 
 import task.Task;
 import task.TaskContent;
+import task.quizquestion.MultipleChoice;
 import task.quizquestion.SingleChoice;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.BiFunction;
 
@@ -19,20 +21,48 @@ public class GradingFunctions {
      * @return BiFunction that can be used for {@link Task#scoringFunction(BiFunction)}
      */
     public static BiFunction<Task, Set<TaskContent>, Float> singleChoiceGrading() {
-        return new BiFunction<Task, Set<TaskContent>, Float>() {
-            @Override
-            public Float apply(Task task, Set<TaskContent> contents) {
-                SingleChoice singleChoice = (SingleChoice) task;
-                int index = singleChoice.correctAnswerIndices().get(0);
-                TaskContent correctAnswer = singleChoice.contentByIndex(index);
-                TaskContent givenAnswer =
-                        contents.stream()
-                                .findFirst()
-                                .orElseThrow(
-                                        () -> new IllegalArgumentException("No answer was given"));
-                if (correctAnswer == givenAnswer) return singleChoice.points();
-                else return 0f;
-            }
+        return (task, answer) -> {
+            SingleChoice singleChoice = (SingleChoice) task;
+            int index = singleChoice.correctAnswerIndices().get(0);
+            TaskContent correctAnswer = singleChoice.contentByIndex(index);
+            TaskContent givenAnswer =
+                    answer.stream()
+                            .findFirst()
+                            .orElseThrow(() -> new IllegalArgumentException("No answer was given"));
+            if (correctAnswer == givenAnswer) return singleChoice.points();
+            else return 0f;
         };
+    }
+
+    /**
+     * A simple grading function for the {@link MultipleChoice} Task.
+     *
+     * <p>For each correct answer, points will be added; for each wrong answer, points will be
+     * removed.
+     *
+     * <p>The amount of points given/removed per answer is calculated by the number of points the
+     * Task is worth divided by the count of correct answers.
+     *
+     * <p>The return value cannot be lower than 0.
+     *
+     * @return a BiFunction that can be used for {@link Task#scoringFunction(BiFunction)}
+     */
+    public static BiFunction<Task, Set<TaskContent>, Float> multipeChoiceGrading() {
+        return (BiFunction<Task, Set<TaskContent>, Float>)
+                (task, givenAnswers) -> {
+                    MultipleChoice multipleChoice = (MultipleChoice) task;
+
+                    Set<TaskContent> correctAnswers = new HashSet<>();
+                    for (int index : multipleChoice.correctAnswerIndices())
+                        correctAnswers.add(multipleChoice.contentByIndex(index));
+
+                    float pointPerAnswer = multipleChoice.points() / correctAnswers.size();
+                    float reachedPoints = 0f;
+                    for (TaskContent answer : givenAnswers) {
+                        if (correctAnswers.contains(answer)) reachedPoints += pointPerAnswer;
+                        else reachedPoints -= pointPerAnswer;
+                    }
+                    return Math.max(0, reachedPoints);
+                };
     }
 }
--- dungeon/src/task/quizquestion/MultipleChoice.java
+++ dungeon/src/task/quizquestion/MultipleChoice.java
@@ -2,6 +2,7 @@ package task.quizquestion;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
+import reporting.GradingFunctions;
 import task.Quiz;
 
 /**
@@ -11,6 +12,7 @@ import task.Quiz;
 public class MultipleChoice extends Quiz {
     public MultipleChoice(String questionText, Image image) {
         super(questionText, image);
+        scoringFunction(GradingFunctions.multipeChoiceGrading());
     }
 
     public MultipleChoice(String questionText) {
