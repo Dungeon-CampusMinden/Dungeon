d5535ddf98c49e4f3318f60211c6437bf9d06346
mr <malt.r@mail.de>
2023-10-27 14:20:26 +0200

    Test for loop and counting for loop interpretation

--- dsl/test/interpreter/TestDSLInterpreter.java
+++ dsl/test/interpreter/TestDSLInterpreter.java
@@ -3189,4 +3189,136 @@ public class TestDSLInterpreter {
         String output = outputStream.toString();
         Assert.assertTrue(output.contains("N"));
     }
+
+    @Test
+    public void testForLoop() {
+        String program =
+            """
+            entity_type my_type {
+                test_component1 {},
+                test_component_with_callback {
+                    consumer: func
+                }
+            }
+
+            fn func(entity ent) {
+                var my_list : int[];
+                my_list.add(1);
+                my_list.add(2);
+                my_list.add(3);
+                for int entry in my_list {
+                    print(entry);
+                }
+            }
+
+            quest_config c {
+                entity: instantiate(my_type)
+            }
+            """;
+
+        // print currently just prints to system.out, so we need to
+        // check the contents for the printed string
+        var outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        TestEnvironment env = new TestEnvironment();
+        DSLInterpreter interpreter = new DSLInterpreter();
+        env.getTypeBuilder().createDSLTypeForJavaTypeInScope(env.getGlobalScope(), Entity.class);
+        env.getTypeBuilder()
+            .createDSLTypeForJavaTypeInScope(
+                env.getGlobalScope(), TestComponentEntityConsumerCallback.class);
+        env.getTypeBuilder()
+            .createDSLTypeForJavaTypeInScope(env.getGlobalScope(), TestComponent1.class);
+
+        var config =
+            (CustomQuestConfig)
+                Helpers.generateQuestConfigWithCustomTypes(program, env, interpreter);
+
+        var entity = config.entity();
+
+        TestComponentEntityConsumerCallback componentWithConsumer =
+            (TestComponentEntityConsumerCallback)
+                entity.components.stream()
+                    .filter(c -> c instanceof TestComponentEntityConsumerCallback)
+                    .toList()
+                    .get(0);
+
+        componentWithConsumer.consumer.accept(entity);
+
+        String output = outputStream.toString();
+        assertEquals("1" + System.lineSeparator() + "2" + System.lineSeparator() + "3" + System.lineSeparator(), output);
+    }
+
+    @Test
+    public void testCountingForLoop() {
+        String program =
+            """
+            entity_type my_type {
+                test_component1 {},
+                test_component_with_callback {
+                    consumer: func
+                }
+            }
+
+            fn func(entity ent) {
+                var my_list : string[];
+                my_list.add("Hello");
+                my_list.add("World");
+                my_list.add("!");
+                for int entry in my_list count i {
+                    print(i);
+                    print(entry);
+                }
+            }
+
+            quest_config c {
+                entity: instantiate(my_type)
+            }
+            """;
+
+        // print currently just prints to system.out, so we need to
+        // check the contents for the printed string
+        var outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        TestEnvironment env = new TestEnvironment();
+        DSLInterpreter interpreter = new DSLInterpreter();
+        env.getTypeBuilder().createDSLTypeForJavaTypeInScope(env.getGlobalScope(), Entity.class);
+        env.getTypeBuilder()
+            .createDSLTypeForJavaTypeInScope(
+                env.getGlobalScope(), TestComponentEntityConsumerCallback.class);
+        env.getTypeBuilder()
+            .createDSLTypeForJavaTypeInScope(env.getGlobalScope(), TestComponent1.class);
+
+        var config =
+            (CustomQuestConfig)
+                Helpers.generateQuestConfigWithCustomTypes(program, env, interpreter);
+
+        var entity = config.entity();
+
+        TestComponentEntityConsumerCallback componentWithConsumer =
+            (TestComponentEntityConsumerCallback)
+                entity.components.stream()
+                    .filter(c -> c instanceof TestComponentEntityConsumerCallback)
+                    .toList()
+                    .get(0);
+
+        componentWithConsumer.consumer.accept(entity);
+
+        String output = outputStream.toString();
+        assertEquals(
+            "0"
+                + System.lineSeparator()
+                + "Hello"
+                + System.lineSeparator()
+                + "1"
+                + System.lineSeparator()
+                + "World"
+                + System.lineSeparator()
+                + "2"
+                + System.lineSeparator()
+                + "!"
+                + System.lineSeparator(),
+            output);
+    }
 }
