5f3f8e5db6472d5538d818c055f64800387eca0b
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2023-10-29 14:48:46 +0100

    [GAME] change default camera zoom to 0.35 (#1100)
    * change default zoom to .35
    
    * remove outdated doc

--- game/src/core/utils/Constants.java
+++ game/src/core/utils/Constants.java
@@ -28,8 +28,7 @@ public final class Constants {
         return Game.windowHeight() / FIELD_WIDTH_AND_HEIGHT_IN_PIXEL;
     }
 
-    /** 200% zoom. */
-    public static final float DEFAULT_ZOOM_FACTOR = 0.5f;
+    public static final float DEFAULT_ZOOM_FACTOR = 0.35f;
 
     /** Value for LevelElements that are accessible */
     public static final boolean LEVELELEMENT_IS_ACCESSIBLE = true;
