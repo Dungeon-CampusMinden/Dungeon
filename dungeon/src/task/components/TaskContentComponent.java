30bf1f36da7daab7a7096851b581435bed6f8630
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-10-30 12:56:07 +0100

    TaskContentComponent only stores one TaskContent

--- dungeon/src/task/components/TaskContentComponent.java
+++ dungeon/src/task/components/TaskContentComponent.java
@@ -4,32 +4,19 @@ import core.Component;
 
 import task.TaskContent;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Stream;
-
 /**
  * Marks an entity as a representation of one or more {@link TaskContent}.
  *
- * <p>Stores a collection of {@link TaskContent}s that are represented by the associated entity.
+ * <p>Stores a {@link TaskContent}s that is represented by the associated entity.
  *
  * <p>Using this component, a connection can be made between the in-game entity and the elements
  * from the task description.
  *
- * <p>The collection can be queried as a stream using {@link #stream()}
+ * <p>The collection can be queried as a stream using {@link #content()}
  */
 public final class TaskContentComponent implements Component {
 
-    private final Set<TaskContent> content;
-
-    /**
-     * Create a new TaskContentComponent and add it to the associated entity.
-     *
-     * @param content Collection of {@link TaskContent} that this Component represent
-     */
-    public TaskContentComponent(final Set<TaskContent> content) {
-        this.content = new HashSet<>(content);
-    }
+    private final TaskContent content;
 
     /**
      * Create a new TaskContentComponent and add it to the associated entity.
@@ -37,8 +24,7 @@ public final class TaskContentComponent implements Component {
      * @param content Single {@link TaskContent} that this Component represent
      */
     public TaskContentComponent(final TaskContent content) {
-        this.content = new HashSet<>();
-        this.content.add(content);
+        this.content = content;
     }
 
     /**
@@ -46,7 +32,7 @@ public final class TaskContentComponent implements Component {
      *
      * @return internal Set as stream
      */
-    public Stream<TaskContent> stream() {
-        return content.stream();
+    public TaskContent content() {
+        return content;
     }
 }
