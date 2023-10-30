e2ab7a613dcd96ad99425b4b074c78a1493c6c1e
mr <malt.r@mail.de>
2023-10-30 16:56:50 +0100

    Add javadoc for LoopBottomMark

--- dsl/src/parser/ast/LoopBottomMark.java
+++ dsl/src/parser/ast/LoopBottomMark.java
@@ -5,12 +5,22 @@ import semanticanalysis.Symbol;
 
 import java.util.Iterator;
 
+/**
+ * This AST Node is used in the {@link interpreter.DSLInterpreter} to mark the end
+ * of the execution of one iteration of an {@link LoopStmtNode}. The {@link interpreter.DSLInterpreter}
+ * checks, if another iteration of the loop statement should be executed. This class stores the
+ * state of the loop (the iterator and the symbols corresponding to the loop and counter variable).
+ */
 public class LoopBottomMark extends Node {
     private final LoopStmtNode loopStmtNode;
     private final Iterator<Value> internalIterator;
     private final Symbol loopVariableSymbol;
     private final Symbol counterVariableSymbol;
 
+    /**
+     * Constructor for bottom mark of {@link LoopStmtNode}s, which have no internal iterator and no loop variables.
+     * @param loopStmtNode The {@link LoopStmtNode} of which the new instance will be the bottom mark.
+     */
     public LoopBottomMark(LoopStmtNode loopStmtNode) {
         super(Type.LoopBottomMark);
         this.loopStmtNode = loopStmtNode;
@@ -19,6 +29,13 @@ public class LoopBottomMark extends Node {
         counterVariableSymbol = Symbol.NULL;
     }
 
+    /**
+     * Constructor for bottom mark of {@link LoopStmtNode}s, which have an internal iterator and loop and/or counter variables.
+     * @param loopStmtNode The {@link LoopStmtNode} of which the new instance will be the bottom mark.
+     * @param internalIterator The {@link Iterator<Value>} for the iterable expression of the {@link LoopStmtNode}
+     * @param loopVariableSymbol The {@link Symbol} for the loop variable, which is used to iterate over the iterable expression
+     * @param counterVariableSymbol The {@link Symbol} for the counter variable, which will be used to count the iterations of the loop
+     */
     public LoopBottomMark(LoopStmtNode loopStmtNode, Iterator<Value> internalIterator, Symbol loopVariableSymbol, Symbol counterVariableSymbol) {
         super(Type.LoopBottomMark);
         this.loopStmtNode = loopStmtNode;
