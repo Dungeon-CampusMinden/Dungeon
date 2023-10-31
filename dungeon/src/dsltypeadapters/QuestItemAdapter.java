c46f98b8c0e44ac870898371086507e4cc8336c1
mr <malt.r@mail.de>
2023-10-31 10:56:28 +0100

    Add javadoc to QuestItemAdapter

--- dungeon/src/dsltypeadapters/QuestItemAdapter.java
+++ dungeon/src/dsltypeadapters/QuestItemAdapter.java
@@ -8,7 +8,19 @@ import semanticanalysis.types.DSLTypeMember;
 import task.QuestItem;
 import task.components.TaskContentComponent;
 
+/**
+ * Typeadatper for creation of {@link QuestItem}s
+ */
 public class QuestItemAdapter {
+    /**
+     * Buildermethod for creating a {@link QuestItem}. Will create a new {@link TaskContentComponent}
+     * and pass it to the ctor of {@link QuestItem}.
+     *
+     * @param displayName The displayName of the new {@link QuestItem}.
+     * @param description The description of the new {@link QuestItem}.
+     * @param texturePath The path to a texture, which will be used for the new {@link QuestItem}.
+     * @return the newly created {@link QuestItem}.
+     */
     @DSLTypeAdapter(name = "quest_item")
     public static QuestItem buildQuestItem(
             @DSLTypeMember(name = "display_name") String displayName,
