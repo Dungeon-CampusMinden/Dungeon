cda8413375717dae2dd5e479391c26f64017dd30
mr <malt.r@mail.de>
2023-10-31 10:59:05 +0100

    Add javadoc to QuestItemExtension

--- dungeon/src/dsltypeproperties/QuestItemExtension.java
+++ dungeon/src/dsltypeproperties/QuestItemExtension.java
@@ -6,10 +6,18 @@ import semanticanalysis.types.IDSLTypeProperty;
 import task.QuestItem;
 import task.components.TaskContentComponent;
 
+/**
+ * Class, which stores dsl-extensions (e.g. {@link IDSLTypeProperty} or {@link semanticanalysis.types.IDSLExtensionMethod})
+ * for {@link QuestItem}s
+ */
 public class QuestItemExtension {
     // private ctor, because this class should not be instantiated
     private QuestItemExtension() {}
 
+    /**
+     * {@link IDSLTypeProperty} extension to access the {@link TaskContentComponent} of a
+     * {@link QuestItem} instance. Not settable.
+     */
     @DSLTypeProperty(
             name = "task_content_component",
             extendedType = QuestItem.class,
