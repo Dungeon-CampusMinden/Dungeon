d5cd43669ab0eb59d70cf9412cfb7af98cb3bf45
mr <malt.r@mail.de>
2023-11-01 16:50:26 +0100

    Add convenience methods to NativeFunction for evaluating parameter nodes and values

--- dsl/src/runtime/nativefunctions/NativeFunction.java
+++ dsl/src/runtime/nativefunctions/NativeFunction.java
@@ -1,11 +1,18 @@
 package runtime.nativefunctions;
 
+import interpreter.DSLInterpreter;
+import parser.ast.Node;
+import runtime.SetValue;
+import runtime.Value;
 import semanticanalysis.ICallable;
 import semanticanalysis.IScope;
 import semanticanalysis.ScopedSymbol;
 import semanticanalysis.Symbol;
 import semanticanalysis.types.FunctionType;
 import semanticanalysis.types.IType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class NativeFunction extends ScopedSymbol implements ICallable {
     protected NativeFunction(String name, IScope parentScope, FunctionType type) {
@@ -22,6 +29,25 @@ public abstract class NativeFunction extends ScopedSymbol implements ICallable {
 
     public void overwriteFunctionType(FunctionType type) {
         this.dataType = type;
+    }
+
+    protected List<Value> evaluateParameters(DSLInterpreter interpreter, List<Node> parameterNodes) {
+        ArrayList<Value> parameterValues = new ArrayList<>(parameterNodes.size());
+        for (Node node : parameterNodes) {
+            Value taskValue = (Value) node.accept(interpreter);
+            parameterValues.add(taskValue);
+        }
+        return parameterValues;
+    }
+
+    protected List<Object> translateValuesToObjects(DSLInterpreter interpreter, List<Value> values) {
+        ArrayList<Object> objects = new ArrayList<>(values.size());
+        var instantiator = interpreter.getRuntimeEnvironment().getTypeInstantiator();
+        for (Value value : values) {
+            Object object = instantiator.instantiate(value);
+            objects.add(object);
+        }
+        return objects;
     }
 
     @Override
