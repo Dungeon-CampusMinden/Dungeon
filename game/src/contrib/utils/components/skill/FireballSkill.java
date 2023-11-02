9fb71e00954c5b3cf0c7be5730f7a19c6b4c8b87
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2023-11-02 13:29:52 +0100

    [GAME] make the game a little bit more challenging  (#1133)
    make the game a little bit more challanging

--- game/src/contrib/utils/components/skill/FireballSkill.java
+++ game/src/contrib/utils/components/skill/FireballSkill.java
@@ -21,10 +21,10 @@ public class FireballSkill extends DamageProjectile {
     private static final String PROJECTILE_TEXTURES = "skills/fireball";
     private static final String PROJECTILE_SOUND = "sounds/fireball.wav";
     private static final float PROJECTILE_SPEED = 15.0f;
-    private static final int DAMAGE_AMOUNT = 1;
+    private static final int DAMAGE_AMOUNT = 5;
     private static final DamageType DAMAGE_TYPE = DamageType.FIRE;
     private static final Point HITBOX_SIZE = new Point(1, 1);
-    private static final float PROJECTILE_RANGE = 5f;
+    private static final float PROJECTILE_RANGE = 7f;
 
     /**
      * Create a {@link DamageProjectile} that looks like a fireball and will cause fire damage.
