e52a9f85134bbc2aaa6040d5876395c81260369c
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-11-03 11:24:24 +0100

    add javadoc

--- dungeon/src/task/YesNoDialog.java
+++ dungeon/src/task/YesNoDialog.java
@@ -16,22 +16,64 @@ import task.quizquestion.QuizUI;
 import java.util.Objects;
 import java.util.function.BiFunction;
 
+/**
+ * A Dialog with a "yes" and "no" Button on the Bottom.
+ *
+ * <p>Use {@link #showYesNoDialog(String, String, IVoidFunction, IVoidFunction)} to create a simple
+ * dialog.
+ *
+ * <p>Use {@link #showYesNoDialog(Task)} to create a dialog that will execute the grading function
+ * of the given task.
+ */
 public class YesNoDialog {
 
-    public static Entity showYesNoDialog(Task t) {
-        return showYesNoDialog();
+    /**
+     * Open a dialog window that shows the given task's text and will execute the {@link
+     * Task#gradeTask()} if "yes" is pressed.
+     *
+     * @param task task that should be graded if the player presses "yes" on the dialog HUD.
+     * @return The Entity that stores the HUD components.
+     */
+    public static Entity showYesNoDialog(final Task task) {
+        return showYesNoDialog(task.taskText(), task.taskName(), gradeOn(task), () -> {});
     }
 
+    /**
+     * Show a Yes or No Dialog.
+     *
+     * @param text text to show in the dialog
+     * @param title title of the dialog window
+     * @param onYes function to execute if "yes" is pressed
+     * @param onNo function to execute if "no" is pressed
+     * @return Entity that stores the HUD components.
+     */
     public static Entity showYesNoDialog(
-            String text, String title, IVoidFunction onYes, IVoidFunction onNo) {
+            final String text,
+            final String title,
+            final IVoidFunction onYes,
+            final IVoidFunction onNo) {
 
         Entity entity = showYesNoDialog(UITools.DEFAULT_SKIN, text, title, onYes, onNo);
         Game.add(entity);
         return entity;
     }
 
+    /**
+     * Show a Yes or No Dialog.
+     *
+     * @param skin UI skin to use
+     * @param text text to show in the dialog
+     * @param title title of the dialog window
+     * @param onYes function to execute if "yes" is pressed
+     * @param onNo function to execute if "no" is pressed
+     * @return Entity that stores the HUD components.
+     */
     public static Entity showYesNoDialog(
-            Skin skin, String text, String title, IVoidFunction onYes, IVoidFunction onNo) {
+            final Skin skin,
+            final String text,
+            final String title,
+            final IVoidFunction onYes,
+            final IVoidFunction onNo) {
         Entity entity = new Entity();
 
         UITools.show(
@@ -56,10 +98,10 @@ public class YesNoDialog {
     }
 
     private static Dialog createYesNoDialog(
-            Skin skin,
-            String text,
-            String title,
-            BiFunction<TextDialog, String, Boolean> resultHandler) {
+            final Skin skin,
+            final String text,
+            final String title,
+            final BiFunction<TextDialog, String, Boolean> resultHandler) {
         Dialog textDialog = new TextDialog(title, skin, "Letter", resultHandler);
         textDialog
                 .getContentTable()
@@ -72,12 +114,12 @@ public class YesNoDialog {
         return textDialog;
     }
 
-    public static BiFunction<TextDialog, String, Boolean> createResultHandlerYesNo(
+    private static BiFunction<TextDialog, String, Boolean> createResultHandlerYesNo(
             final Entity entity,
             final String yesButtonID,
-            String noButtonID,
-            IVoidFunction onYes,
-            IVoidFunction onNo) {
+            final String noButtonID,
+            final IVoidFunction onYes,
+            final IVoidFunction onNo) {
         return (d, id) -> {
             if (Objects.equals(id, yesButtonID)) {
                 onYes.execute();
@@ -93,7 +135,7 @@ public class YesNoDialog {
         };
     }
 
-    public static IVoidFunction gradeOn(Task t) {
+    private static IVoidFunction gradeOn(final Task t) {
         return t::gradeTask;
     }
 }
