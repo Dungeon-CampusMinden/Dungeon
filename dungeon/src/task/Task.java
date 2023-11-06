e74f0b66415d523881def0b62b06af371f010fd9
Andre Matutat <andre.matutat@fh-bielefeld.de>
2023-11-06 11:46:26 +0100

    remove questItem from game

--- dungeon/src/task/Task.java
+++ dungeon/src/task/Task.java
@@ -447,7 +447,7 @@ public abstract class Task {
                                         .content()
                                         .task()
                                         .equals(t)) {
-                                    item.drop(hero, pc.position());
+                                    //item.drop(hero, pc.position());
                                     ic.remove(item);
                                 }
                             }
