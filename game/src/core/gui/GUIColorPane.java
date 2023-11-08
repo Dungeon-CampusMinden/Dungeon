3160be0ebc42fd5f8c9f47f28da84a0e4af6c042
Finn Watermann <finn.watermann@hsbi.de>
2023-11-08 22:48:17 +0100

    Add GUIColorPane

---
+++ game/src/core/gui/GUIColorPane.java
@@ -0,0 +1,16 @@
+package core.gui;
+
+import core.utils.math.Vector2f;
+import core.utils.math.Vector4f;
+
+public class GUIColorPane extends GUIElement {
+
+    public GUIColorPane(Vector4f color) {
+        this.backgroundColor = color;
+    }
+
+    @Override
+    public Vector2f preferredSize() {
+        return this.size.xy();
+    }
+}
