53100dccd2db1da2902d1e18a91a61193f458566
Finn Watermann <finn.watermann@hsbi.de>
2023-11-08 22:54:56 +0100

    Add render help methods to render context

--- game/src/core/gui/backend/opengl/OpenGLRenderContext.java
+++ game/src/core/gui/backend/opengl/OpenGLRenderContext.java
@@ -6,14 +6,14 @@ import org.lwjgl.opengl.GL33;
 
 import java.util.HashMap;
 
-public class OpenGLRenderStructure {
+public class OpenGLRenderContext {
 
     private final HashMap<String, Integer> uniformLocations = new HashMap<>();
     private boolean begun = false;
 
     public int vao, vbo, ebo, frameBuffer, texture, shader;
 
-    public OpenGLRenderStructure() {
+    public OpenGLRenderContext() {
         this.vao = 0;
         this.vbo = 0;
         this.ebo = 0;
@@ -23,7 +23,7 @@ public class OpenGLRenderStructure {
     }
 
     public void begin() {
-        if(this.begun) {
+        if (this.begun) {
             OpenGLUtil.log(CustomLogLevel.WARNING, "OpenGLRenderStructure.begin() called twice!");
         }
         GL33.glBindVertexArray(this.vao);
@@ -32,20 +32,23 @@ public class OpenGLRenderStructure {
     }
 
     public void end() {
-        if(this.begun) {
+        if (this.begun) {
             GL33.glUseProgram(0);
             GL33.glBindVertexArray(0);
             this.begun = false;
         } else {
-            OpenGLUtil.log(CustomLogLevel.WARNING, "OpenGLRenderStructure.end() called before .begin()!");
+            OpenGLUtil.log(
+                    CustomLogLevel.WARNING, "OpenGLRenderStructure.end() called before .begin()!");
         }
     }
 
     public void draw() {
-        if(this.begun) {
+        if (this.begun) {
             GL33.glDrawElements(GL33.GL_TRIANGLE_STRIP, 4, GL33.GL_UNSIGNED_SHORT, 0);
         } else {
-            OpenGLUtil.log(CustomLogLevel.WARNING, "OpenGLRenderStructure.draw() called before .begin()! Not drawing.");
+            OpenGLUtil.log(
+                    CustomLogLevel.WARNING,
+                    "OpenGLRenderStructure.draw() called before .begin()! Not drawing.");
         }
     }
 
