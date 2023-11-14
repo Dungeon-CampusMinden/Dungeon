ca85b43756ed339033a3e44c6de037323604be6a
Finn Watermann <finn.watermann@hsbi.de>
2023-11-14 15:48:07 +0100

    GUIRoot Event handling & rendering only on update

--- game/src/core/gui/GUIRoot.java
+++ game/src/core/gui/GUIRoot.java
@@ -1,59 +1,73 @@
 package core.gui;
 
+import core.gui.events.GUIElementListUpdateEvent;
 import core.gui.events.GUIResizeEvent;
 import core.gui.layouts.BorderLayout;
 import core.utils.math.Vector2f;
-import core.utils.math.Vector4f;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GUIRoot {
 
-    private IGUIBackend backend;
-    private GUIContainer rootContainer;
+    private final IGUIBackend backend;
+    private final GUIContainer rootContainer;
+    private final List<GUIElement> elementList = new ArrayList<>();
+    private boolean updateNextFrame = true;
 
     public GUIRoot(IGUIBackend backend) {
         this.backend = backend;
         this.rootContainer =
-                new GUIContainer(new BorderLayout(BorderLayout.BorderLayoutMode.VERTICAL));
+                new GUIContainer(new BorderLayout(BorderLayout.BorderLayoutMode.VERTICAL)) {
+                    @Override
+                    public void event(GUIEvent event) {
+                        if (event instanceof GUIElementListUpdateEvent) {
+                            GUIRoot.this.update();
+                        }
+                    }
+                };
         this.rootContainer.position = Vector2f.zero();
-
-        GUIColorPane pane1 = new GUIColorPane(new Vector4f(1, 0, 0, 1.0f));
-        GUIColorPane pane2 = new GUIColorPane(new Vector4f(0.99f, 0.6f, 0, 1f));
-        GUIColorPane pane3 = new GUIColorPane(new Vector4f(1f, 1f, 0f, 1f));
-        GUIColorPane pane4 = new GUIColorPane(new Vector4f(0f, 0.6f, 0f, 1f));
-
-        pane1.size(new Vector2f(100, 100));
-        pane2.size(new Vector2f(100, 100));
-        pane3.size(new Vector2f(100, 100));
-        pane4.size(new Vector2f(100, 100));
-
-        this.rootContainer.add(pane1);
-        this.rootContainer.add(pane2);
-        this.rootContainer.add(pane3);
-        this.rootContainer.add(pane4);
-
-        this.rootContainer.layout.layout(this.rootContainer, this.rootContainer.elements);
     }
 
     public void render(float delta) {
-        this.backend.render(this.rootContainer.elements);
+        this.backend.render(this.elementList, updateNextFrame);
+        this.updateNextFrame = false;
     }
 
     public void event(GUIEvent event) {
         // Handle events
-
         if (event instanceof GUIResizeEvent resizeEvent) {
             this.backend.resize(resizeEvent.width(), resizeEvent.height());
             this.rootContainer.size = new Vector2f(resizeEvent.width(), resizeEvent.height());
-            this.rootContainer.layout.layout(this.rootContainer, this.rootContainer.elements);
+            this.update();
             return;
         }
-
-        this.rootContainer.elements.forEach(element -> element.event(event));
+        if (event instanceof GUIElementListUpdateEvent) {
+            this.update();
+        }
+        this.rootContainer.fireEvent(event, GUIEvent.TraverseMode.DOWN);
     }
 
     /** Invalidates the current layout and redraws & updates it. */
     public void update() {
-        // TODO
+        this.elementList.clear();
+        // Breitendurchlauf durch elemente
+        List<GUIContainer> queue = new ArrayList<>();
+        queue.add(this.rootContainer);
+        while (!queue.isEmpty()) {
+            GUIContainer container = queue.remove(0);
+            container.layout.layout(container, container.elements);
+            container.elements.forEach(
+                    element -> {
+                        if (element instanceof GUIContainer container1) {
+                            queue.add(container1);
+                        } else {
+                            this.elementList.add(element);
+                        }
+                        element.update();
+                    });
+        }
+        this.updateNextFrame = true;
     }
 
     /**
