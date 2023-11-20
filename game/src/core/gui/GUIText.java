1b4593207aeb70aaca2ce734b51dcf7d1c5b3a2e
Finn Watermann <finn.watermann@hsbi.de>
2023-11-20 15:57:29 +0100

    Add GUIText class

--- game/src/core/gui/GUIText.java
+++ game/src/core/gui/GUIText.java
@@ -1,3 +1,39 @@
 package core.gui;
 
-public class GUIText {}
+import core.gui.util.Font;
+import core.utils.math.Vector2f;
+
+public class GUIText extends GUIElement {
+
+    private Font font;
+    private String text;
+
+    public GUIText(String text, Font font) {
+        this.text = text;
+        this.font = font;
+    }
+
+    public Font font() {
+        return font;
+    }
+
+    public GUIText font(Font font) {
+        this.font = font;
+        return this;
+    }
+
+    public String text() {
+        return text;
+    }
+
+    public GUIText text(String text) {
+        this.text = text;
+        return this;
+    }
+
+    @Override
+    public Vector2f preferredSize() {
+        // TODO: Implement
+        return new Vector2f(500, 200);
+    }
+}
