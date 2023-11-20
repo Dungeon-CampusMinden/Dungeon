f11bcfbe50f2854f9fda7fd454de98084060b129
Lena241 <33090382+Lena241@users.noreply.github.com>
2023-11-20 15:20:31 +0100

    [GAME] `EntityFactory` in `HeroFactory`, `MonsterFactory` und `MiscFactory` aufgeteilt (#1231)
    split EntityFactory into different Factories

---
+++ game/src/contrib/entities/MiscFactory.java
@@ -0,0 +1,194 @@
+package contrib.entities;
+
+import contrib.components.CollideComponent;
+import contrib.components.InteractionComponent;
+import contrib.components.InventoryComponent;
+import contrib.components.UIComponent;
+import contrib.hud.GUICombination;
+import contrib.hud.crafting.CraftingGUI;
+import contrib.hud.inventory.InventoryGUI;
+import contrib.item.Item;
+import contrib.utils.components.draw.ChestAnimations;
+import contrib.utils.components.interaction.DropItemsInteraction;
+import contrib.utils.components.item.ItemDataGenerator;
+
+import core.Entity;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
+import core.utils.Point;
+import core.utils.components.draw.CoreAnimations;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/** A utility class for building different miscellaneous entities in the game world. */
+public class MiscFactory {
+
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Create a new Entity that can be used as a chest.
+     *
+     * <p>It will have a {@link InteractionComponent}. {@link PositionComponent}, {@link
+     * DrawComponent}, {@link CollideComponent} and {@link InventoryComponent}. It will use the
+     * {@link DropItemsInteraction} on interaction.
+     *
+     * <p>{@link ItemDataGenerator} is used to generate random items
+     *
+     * @return Created Entity
+     */
+    public static Entity newChest() throws IOException {
+        ItemDataGenerator itemDataGenerator = new ItemDataGenerator();
+
+        Set<Item> items =
+                IntStream.range(0, RANDOM.nextInt(1, 3))
+                        .mapToObj(i -> itemDataGenerator.generateItemData())
+                        .collect(Collectors.toSet());
+        return newChest(items, PositionComponent.ILLEGAL_POSITION);
+    }
+
+    /**
+     * Create a new Entity that can be used as a chest.
+     *
+     * <p>It will have a {@link InteractionComponent}. {@link PositionComponent}, {@link
+     * DrawComponent}, {@link CollideComponent} and {@link InventoryComponent}. It will use the
+     * {@link DropItemsInteraction} on interaction.
+     *
+     * @param item The {@link Item} for the Items inside the chest.
+     * @param position The position of the chest.
+     * @return Created Entity
+     */
+    public static Entity newChest(Set<Item> item, Point position) throws IOException {
+        final float defaultInteractionRadius = 1f;
+        Entity chest = new Entity("chest");
+
+        if (position == null) chest.addComponent(new PositionComponent());
+        else chest.addComponent(new PositionComponent(position));
+        InventoryComponent ic = new InventoryComponent(item.size());
+        chest.addComponent(ic);
+        item.forEach(ic::add);
+        chest.addComponent(
+                new InteractionComponent(
+                        defaultInteractionRadius,
+                        true,
+                        (interacted, interactor) -> {
+                            interactor
+                                    .fetch(InventoryComponent.class)
+                                    .ifPresent(
+                                            whoIc -> {
+                                                UIComponent uiComponent =
+                                                        new UIComponent(
+                                                                new GUICombination(
+                                                                        new InventoryGUI(whoIc),
+                                                                        new InventoryGUI(ic)),
+                                                                true);
+                                                uiComponent.onClose(
+                                                        () ->
+                                                                interacted
+                                                                        .fetch(DrawComponent.class)
+                                                                        .ifPresent(
+                                                                                interactedDC -> {
+                                                                                    // remove all
+                                                                                    // prior
+                                                                                    // opened
+                                                                                    // animations
+                                                                                    interactedDC
+                                                                                            .deQueueByPriority(
+                                                                                                    ChestAnimations
+                                                                                                            .OPEN_FULL
+                                                                                                            .priority());
+                                                                                    if (ic.count()
+                                                                                            > 0) {
+                                                                                        // as long
+                                                                                        // as
+                                                                                        // there is
+                                                                                        // an
+                                                                                        // item
+                                                                                        // inside
+                                                                                        // the chest
+                                                                                        // show a
+                                                                                        // full
+                                                                                        // chest
+                                                                                        interactedDC
+                                                                                                .queueAnimation(
+                                                                                                        ChestAnimations
+                                                                                                                .OPEN_FULL);
+                                                                                    } else {
+                                                                                        // empty
+                                                                                        // chest
+                                                                                        // show the
+                                                                                        // empty
+                                                                                        // animation
+                                                                                        interactedDC
+                                                                                                .queueAnimation(
+                                                                                                        ChestAnimations
+                                                                                                                .OPEN_EMPTY);
+                                                                                    }
+                                                                                }));
+                                                interactor.addComponent(uiComponent);
+                                            });
+                            interacted
+                                    .fetch(DrawComponent.class)
+                                    .ifPresent(
+                                            interactedDC -> {
+                                                // only add opening animation when it is not
+                                                // finished
+                                                if (interactedDC
+                                                        .getAnimation(ChestAnimations.OPENING)
+                                                        .map(animation -> !animation.isFinished())
+                                                        .orElse(true)) {
+                                                    interactedDC.queueAnimation(
+                                                            ChestAnimations.OPENING);
+                                                }
+                                            });
+                        }));
+        DrawComponent dc = new DrawComponent("objects/treasurechest");
+        var mapping = dc.animationMap();
+        // set the closed chest as default idle
+        mapping.put(
+                CoreAnimations.IDLE.pathString(), mapping.get(ChestAnimations.CLOSED.pathString()));
+        // opening animation should not loop
+        mapping.get(ChestAnimations.OPENING.pathString()).setLoop(false);
+        // reset Idle Animation
+        dc.deQueueByPriority(CoreAnimations.IDLE.priority());
+        dc.currentAnimation(CoreAnimations.IDLE);
+        chest.addComponent(dc);
+
+        return chest;
+    }
+
+    /**
+     * Create a new Entity that can be used as a crafting cauldron.
+     *
+     * @return Created Entity
+     * @throws IOException if the textures do not exist
+     */
+    public static Entity newCraftingCauldron() throws IOException {
+        Entity cauldron = new Entity("cauldron");
+        cauldron.addComponent(new PositionComponent());
+        cauldron.addComponent(new DrawComponent("objects/cauldron"));
+        cauldron.addComponent(new CollideComponent());
+        cauldron.addComponent(
+                new InteractionComponent(
+                        1f,
+                        true,
+                        (entity, who) ->
+                                who.fetch(InventoryComponent.class)
+                                        .ifPresent(
+                                                ic -> {
+                                                    CraftingGUI craftingGUI = new CraftingGUI(ic);
+                                                    UIComponent component =
+                                                            new UIComponent(
+                                                                    new GUICombination(
+                                                                            new InventoryGUI(ic),
+                                                                            craftingGUI),
+                                                                    true);
+                                                    component.onClose(craftingGUI::cancel);
+                                                    who.addComponent(component);
+                                                })));
+        return cauldron;
+    }
+}
---
+++ game/src/contrib/entities/MonsterFactory.java
@@ -0,0 +1,150 @@
+package contrib.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+import contrib.components.*;
+import contrib.item.Item;
+import contrib.utils.components.health.DamageType;
+import contrib.utils.components.interaction.DropItemsInteraction;
+import contrib.utils.components.item.ItemDataGenerator;
+
+import core.Entity;
+import core.Game;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
+import core.components.VelocityComponent;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.function.BiConsumer;
+
+/** A utility class for building monster entities in the game world. */
+public class MonsterFactory {
+
+    private static final Random RANDOM = new Random();
+
+    private static final String[] MONSTER_FILE_PATHS = {
+        "character/monster/chort",
+        "character/monster/imp",
+        "character/monster/big_deamon",
+        "character/monster/big_zombie",
+        "character/monster/doc",
+        "character/monster/goblin",
+        "character/monster/ice_zombie",
+        "character/monster/ogre",
+        "character/monster/orc_shaman",
+        "character/monster/orc_warrior",
+        "character/monster/pumpkin_dude",
+        "character/monster/zombie"
+    };
+
+    private static final int MIN_MONSTER_HEALTH = 10;
+
+    // NOTE: +1 for health as nextInt() is exclusive
+    private static final int MAX_MONSTER_HEALTH = 50 + 1;
+    private static final float MIN_MONSTER_SPEED = 5.0f;
+    private static final float MAX_MONSTER_SPEED = 8.5f;
+    private static final DamageType MONSTER_COLLIDE_DAMAGE_TYPE = DamageType.PHYSICAL;
+    private static final int MONSTER_COLLIDE_DAMAGE = 10;
+    private static final int MONSTER_COLLIDE_COOL_DOWN = 2 * Game.frameRate();
+
+    /**
+     * Create a new Entity that can be used as a Monster.
+     *
+     * <p>It will have a {@link PositionComponent}, {@link HealthComponent}, {@link AIComponent}
+     * with random AIs from the {@link AIFactory} class, {@link DrawComponent} with a randomly set
+     * Animation, {@link VelocityComponent}, {@link CollideComponent} and a 10% chance for an {@link
+     * InventoryComponent}. If it has an Inventory it will use the {@link DropItemsInteraction} on
+     * death.
+     *
+     * @return The generated "Monster".
+     */
+    public static Entity randomMonster() throws IOException {
+        return randomMonster(MONSTER_FILE_PATHS[RANDOM.nextInt(0, MONSTER_FILE_PATHS.length)]);
+    }
+
+    /**
+     * Create a new Entity that can be used as a Monster.
+     *
+     * <p>It will have a {@link PositionComponent}, {@link HealthComponent}, {@link AIComponent}
+     * with random AIs from the {@link AIFactory} class, {@link DrawComponent} with the Animations
+     * in the given path, {@link VelocityComponent}, {@link CollideComponent} and a 10% chance for
+     * an {@link InventoryComponent}. If it has an Inventory it will use the {@link
+     * DropItemsInteraction} on death.
+     *
+     * @param pathToTexture Path to the directory that contains the texture that should be used for
+     *     the created monster
+     * @return The generated "Monster".
+     * @see DrawComponent
+     */
+    public static Entity randomMonster(String pathToTexture) throws IOException {
+        int health = RANDOM.nextInt(MIN_MONSTER_HEALTH, MAX_MONSTER_HEALTH);
+        float speed = RANDOM.nextFloat(MIN_MONSTER_SPEED, MAX_MONSTER_SPEED);
+
+        Entity monster = new Entity("monster");
+        int itemRoll = RANDOM.nextInt(0, 10);
+        BiConsumer<Entity, Entity> onDeath;
+        if (itemRoll == 0) {
+            ItemDataGenerator itemDataGenerator = new ItemDataGenerator();
+            Item item = itemDataGenerator.generateItemData();
+            InventoryComponent ic = new InventoryComponent(1);
+            monster.addComponent(ic);
+            ic.add(item);
+            onDeath =
+                    (e, who) -> {
+                        playMonsterDieSound();
+                        new DropItemsInteraction().accept(e, who);
+                    };
+        } else {
+            onDeath = (e, who) -> playMonsterDieSound();
+        }
+        monster.addComponent(new HealthComponent(health, (e) -> onDeath.accept(e, null)));
+        monster.addComponent(new PositionComponent());
+        monster.addComponent(
+                new AIComponent(
+                        AIFactory.generateRandomFightAI(),
+                        AIFactory.generateRandomIdleAI(),
+                        AIFactory.generateRandomTransitionAI(monster)));
+        monster.addComponent(new DrawComponent(pathToTexture));
+        monster.addComponent(new VelocityComponent(speed, speed));
+        monster.addComponent(new CollideComponent());
+        monster.addComponent(
+                new SpikyComponent(
+                        MONSTER_COLLIDE_DAMAGE,
+                        MONSTER_COLLIDE_DAMAGE_TYPE,
+                        MONSTER_COLLIDE_COOL_DOWN));
+        monster.addComponent(new IdleSoundComponent(randomMonsterIdleSound()));
+        return monster;
+    }
+
+    private static void playMonsterDieSound() {
+        Sound dieSoundEffect;
+        switch (RANDOM.nextInt(4)) {
+            case 0 -> dieSoundEffect = Gdx.audio.newSound(Gdx.files.internal("sounds/die_01.wav"));
+            case 1 -> dieSoundEffect = Gdx.audio.newSound(Gdx.files.internal("sounds/die_02.wav"));
+            case 2 -> dieSoundEffect = Gdx.audio.newSound(Gdx.files.internal("sounds/die_03.wav"));
+            default -> dieSoundEffect = Gdx.audio.newSound(Gdx.files.internal("sounds/die_04.wav"));
+        }
+        long soundid = dieSoundEffect.play();
+        dieSoundEffect.setLooping(soundid, false);
+        dieSoundEffect.setVolume(soundid, 0.35f);
+    }
+
+    private static String randomMonsterIdleSound() {
+        switch (RANDOM.nextInt(4)) {
+            case 0 -> {
+                return "sounds/monster1.wav";
+            }
+            case 1 -> {
+                return "sounds/monster2.wav";
+            }
+            case 2 -> {
+                return "sounds/monster3.wav";
+            }
+            default -> {
+                return "sounds/monster4.wav";
+            }
+        }
+    }
+}
