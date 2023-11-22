734cd6736486d030f4aaa57e172c2c6e6c1288c5
Finn Watermann <finn.watermann@hsbi.de>
2023-11-22 12:45:18 +0100

    Add absolute layout hint

---
+++ game/src/core/gui/layouts/hints/AbsolutLayoutHint.java
@@ -0,0 +1,23 @@
+package core.gui.layouts.hints;
+
+import core.gui.ILayoutHint;
+import core.utils.math.Vector2i;
+
+public class AbsolutLayoutHint implements ILayoutHint {
+
+    public final int x;
+    public final int y;
+    public final int width;
+    public final int height;
+
+    public AbsolutLayoutHint(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public AbsolutLayoutHint(Vector2i position, Vector2i size) {
+        this(position.x(), position.y(), size.x(), size.y());
+    }
+}
