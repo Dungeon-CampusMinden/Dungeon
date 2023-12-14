cf05eb7b19d6fac7db44b003c748ca79376cefc2
Finn Watermann <finn.watermann@hsbi.de>
2023-12-14 01:33:07 +0100

    Add hover callbacks

--- game/src/core/gui/GUIButton.java
+++ game/src/core/gui/GUIButton.java
@@ -2,24 +2,45 @@ package core.gui;
 
 import core.gui.events.GUIMouseClickEvent;
 import core.gui.events.GUIMouseMoveEvent;
-import core.gui.math.Vector4f;
+
+import org.lwjgl.glfw.GLFW;
 
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class GUIButton extends GUIContainer {
 
-    private BiConsumer<GUIButton, GUIMouseClickEvent> onClick;
+    private BiConsumer<GUIButton, GUIMouseClickEvent> onClick = (a, b) -> {};
+    private Consumer<GUIButton> onHoverEnter = (a) -> {};
+    private Consumer<GUIButton> onHoverLeave = (a) -> {};
+    protected boolean hovering = false;
 
-    public GUIButton() {
-        this.backgroundColor = new Vector4f(0.0f, 0.0f, 0.0f, 1.0f);
-    }
+    public GUIButton() {}
 
     public BiConsumer<GUIButton, GUIMouseClickEvent> onClick() {
-        return onClick;
+        return this.onClick;
     }
 
-    public GUIButton onClick(BiConsumer<GUIButton, GUIMouseClickEvent> onClick) {
-        this.onClick = onClick;
+    public Consumer<GUIButton> onHoverEnter() {
+        return this.onHoverEnter;
+    }
+
+    public Consumer<GUIButton> onHoverLeave() {
+        return this.onHoverLeave;
+    }
+
+    public GUIButton onClick(BiConsumer<GUIButton, GUIMouseClickEvent> callback) {
+        this.onClick = callback;
+        return this;
+    }
+
+    public GUIButton onHoverEnter(Consumer<GUIButton> callback) {
+        this.onHoverEnter = callback;
+        return this;
+    }
+
+    public GUIButton onHoverLeave(Consumer<GUIButton> callback) {
+        this.onHoverLeave = callback;
         return this;
     }
 
@@ -28,20 +49,25 @@ public class GUIButton extends GUIContainer {
         super.event(event);
 
         if (event instanceof GUIMouseMoveEvent mouseMoveEvent) {
-            // TODO: Make background color configurable
-            if (this.isPointIn(mouseMoveEvent.to)) {
-                this.backgroundColor = new Vector4f(0.0f, 0.0f, 1.0f, 1.0f);
-            } else {
-                this.backgroundColor = new Vector4f(0.0f, 1.0f, 0.0f, 1.0f);
+            boolean isIn = this.isPointIn(mouseMoveEvent.to);
+            if (isIn && !this.hovering) {
+                this.hovering = true;
+                this.onHoverEnter.accept(this);
+                return;
+            }
+            if (!isIn && this.hovering) {
+                this.hovering = false;
+                this.onHoverLeave.accept(this);
+                return;
             }
             return;
         }
 
         if (event instanceof GUIMouseClickEvent mouseClickEvent) {
-            if (this.isPointIn(mouseClickEvent.mousePos)) {
+            if (mouseClickEvent.action == GLFW.GLFW_PRESS
+                    && this.isPointIn(mouseClickEvent.mousePos)) {
                 if (this.onClick != null) this.onClick.accept(this, mouseClickEvent);
             }
-            return;
         }
     }
 }
