c04e8b07d378a8277c1df00d40b78952345d0add
Tobias Grothe <tgrothe@users.noreply.github.com>
2024-01-17 15:07:34 +0100

    DngJFileChooser added

---
+++ dungeon/src/starter/DngJFileChooser.java
@@ -0,0 +1,24 @@
+package starter;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class DngJFileChooser {
+  /**
+   * Selects a single DNG file using a file chooser dialog.
+   *
+   * @return the absolute path of the selected DNG file, or null if no file was selected
+   */
+  public static String selectSingleDngFile() {
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.setMultiSelectionEnabled(false);
+    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    fileChooser.setFileFilter(new FileNameExtensionFilter("Nur DNG Dateien", "dng"));
+    fileChooser.setAcceptAllFileFilterUsed(false);
+    int result = fileChooser.showOpenDialog(null);
+    if (result == JFileChooser.APPROVE_OPTION) {
+      return fileChooser.getSelectedFile().getAbsolutePath();
+    }
+    return null;
+  }
+}
