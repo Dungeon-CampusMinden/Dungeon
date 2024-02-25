0fa851d2b85a6c65e38c683171eb13481dc6823b
Tobias Grothe <tgrothe@hsbi.de>
2024-02-25 15:41:33 +0100

    Update SimpleFilePathVisitorI.java
    Update java doc strings

--- game/src/core/utils/files/SimpleFilePathVisitorI.java
+++ game/src/core/utils/files/SimpleFilePathVisitorI.java
@@ -7,9 +7,22 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
+/**
+ * Class that extends {@link FileVisitor} and implements {@link SimpleFileVisitor} for use in {@link
+ * core.utils.files.FileSystemUtil}. You only have to implement {@link #visitFile(Path,
+ * BasicFileAttributes)}
+ */
 public interface SimpleFilePathVisitorI extends FileVisitor<Path> {
   SimpleFileVisitor<Path> INSTANCE = new SimpleFileVisitor<>() {};
 
+  /**
+   * Decide what to do with a file found in the file system.
+   *
+   * @param file a reference to the file
+   * @param attrs the file's basic attributes
+   * @return see {@link FileVisitor#visitFile(Object, BasicFileAttributes)}
+   * @throws IOException see {@link FileVisitor#visitFile(Object, BasicFileAttributes)}
+   */
   @Override
   FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException;
 
