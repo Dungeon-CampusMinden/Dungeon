d55dcbb0d2c56757f9ef1d12590140abb57a4e2c
Tobias Grothe <tobias.grothe@gmail.com>
2024-02-26 15:24:27 +0100

    Update blockly/src/client/Client.java
    loadRecipes() doppelt

--- blockly/src/client/Client.java
+++ blockly/src/client/Client.java
@@ -67,7 +67,6 @@ public class Client {
             createHero();
             Crafting.loadRecipes();
             startServer();
-            // Crafting.loadRecipes();
           } catch (IOException e) {
             throw new RuntimeException(e);
           }
