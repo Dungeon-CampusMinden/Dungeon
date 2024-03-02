199d14853aba1808e2bae0edded61dd54e93e2c4
Tobias Grothe <tgrothe@hsbi.de>
2024-03-02 10:10:14 +0100

    Update FileSystemUtil.java
    Add debug logger

--- game/src/core/utils/files/FileSystemUtil.java
+++ game/src/core/utils/files/FileSystemUtil.java
@@ -3,7 +3,10 @@ package core.utils.files;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class contains a search (visit) method to recursively visit resources, also called assets.
@@ -29,12 +32,22 @@ public class FileSystemUtil {
     }
   }
 
+  private static final HashSet<String> paths = new HashSet<>();
+  private static int max_paths = 0;
+  private static final Logger LOGGER = Logger.getLogger(FileSystemUtil.class.getSimpleName());
+
   public static void visitResources(final String path, final FileSystemUtilVisitor visitor)
       throws IOException {
     // Try first to visit test resources, cause some tests need it in Crafting and DrawComponent
     // class
     try {
       visitJUnitResourcesViaWalkFileTree(path, visitor);
+      paths.add(path);
+      if (paths.size() > max_paths) {
+        max_paths = paths.size();
+        LOGGER.log(Level.WARNING, "Warnung: Paths: Max: " + max_paths);
+        LOGGER.log(Level.WARNING, "Warnung: Paths: " + paths);
+      }
       return;
     } catch (IOException ignore) {
     }
