69603e2f981129a2c9dc31c82e7579f83d7224ba
Flamtky <niklas.schumann+github@proton.me>
2024-03-07 19:13:31 +0100

    removed wip hud

--- devDungeon/src/entities/DevHeroFactory.java
+++ devDungeon/src/entities/DevHeroFactory.java
@@ -36,19 +36,6 @@ public class DevHeroFactory extends HeroFactory {
         hero.fetch(PlayerComponent.class)
             .orElseThrow(() -> MissingComponentException.build(hero, PlayerComponent.class));
 
-    // hud inventory
-    // TODO: WIP - Goal is a Small Inventory Hud in the bottom right corner
-    InventoryComponent ic =
-        hero.fetch(InventoryComponent.class)
-            .orElseThrow(() -> MissingComponentException.build(hero, InventoryComponent.class));
-    InventoryGUI invGUI = new InventoryGUI("", ic, 3, false);
-    GUICombination guiComb = new GUICombination(invGUI);
-    guiComb.setSize(10, 10); // TODO: Not working anymore?
-    guiComb.fullScreen(false);
-    UIComponent uiComponent = new UIComponent(guiComb, false, false);
-
-    hero.add(uiComponent);
-
     // hero movement again to allow for mouse movement
     registerMovement(pc, core.configuration.KeyboardConfig.MOVEMENT_UP.value(), new Vector2(0, 1));
     registerMovement(
