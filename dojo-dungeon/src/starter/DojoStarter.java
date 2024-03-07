cfd62b3df5aad9c0f5773395e5a17f377fb38a9f
Carsten Gips <cagix@hsbi.de>
2024-03-07 19:02:55 +0100

    fix dojo-dungeon

--- dojo-dungeon/src/starter/DojoStarter.java
+++ dojo-dungeon/src/starter/DojoStarter.java
@@ -5,7 +5,16 @@ import core.utils.components.path.SimpleIPath;
 import java.io.IOException;
 import java.util.logging.Level;
 
+/**
+ * WTF? .
+ */
 public class DojoStarter {
+    /**
+     * WTF? .
+     *
+     * @param args foo
+     * @throws IOException foo
+     */
   public static void main(String[] args) throws IOException {
     Game.initBaseLogger(Level.WARNING);
     Game.loadConfig(new SimpleIPath("dungeon_config.json"), KeyboardConfig.class);
