e44a5087dc7faa59e9262acac9f94d8aa8747539
Denniso <wisniadenniso@gmail.com>
2024-03-11 13:18:49 +0100

    fix dungeon.src.entrypoint

--- dungeon/src/entrypoint/DSLEntryPoint.java
+++ dungeon/src/entrypoint/DSLEntryPoint.java
@@ -2,5 +2,6 @@ package entrypoint;
 
 import dsl.parser.ast.ObjectDefNode;
 
+/** Represents an entry point. */
 public record DSLEntryPoint(
     ParsedFile file, String displayName, ObjectDefNode configDefinitionNode) {}
--- dungeon/src/entrypoint/ParsedFile.java
+++ dungeon/src/entrypoint/ParsedFile.java
@@ -3,4 +3,5 @@ package entrypoint;
 import dsl.parser.ast.Node;
 import java.nio.file.Path;
 
+/** Represents a parsed file. */
 public record ParsedFile(Path filePath, Node rootASTNode) {}
