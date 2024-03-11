bc24b86eb0f1fbb38d5d002446aee043eb621f86
Carsten Gips <cagix@hsbi.de>
2024-03-11 18:54:18 +0100

    fix contrib.utils

--- dungeon/src/contrib/utils/level/NoTileFoundException.java
+++ dungeon/src/contrib/utils/level/NoTileFoundException.java
@@ -1,6 +1,12 @@
 package contrib.utils.level;
 
+/** WTF? . */
 public class NoTileFoundException extends NullPointerException {
+  /**
+   * WTF? .
+   *
+   * @param message foo
+   */
   public NoTileFoundException(final String message) {
     super(message);
   }
