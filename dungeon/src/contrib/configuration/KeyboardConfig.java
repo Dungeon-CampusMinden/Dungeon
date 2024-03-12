208dfe145d2ce29b85665646637e79f12d0fc782
Tobias Grothe <tgrothe@hsbi.de>
2024-03-12 09:15:59 +0100

    Fix docs in: modified:   dungeon/src/contrib/configuration/KeyboardConfig.java modified:   game/src/core/level/Tile.java modified:   game/src/core/level/TileLevel.java modified:   game/src/core/level/elements/ILevel.java modified:   game/src/core/level/elements/astar/TileConnection.java modified:   game/src/core/level/elements/astar/TileHeuristic.java modified:   game/src/core/level/elements/tile/TileFactory.java modified:   game/src/core/level/generator/IGenerator.java modified:   game/src/core/level/generator/postGeneration/WallGenerator.java modified:   game/src/core/level/generator/randomwalk/RandomWalkGenerator.java modified:   game/src/core/level/utils/Coordinate.java

--- dungeon/src/contrib/configuration/KeyboardConfig.java
+++ dungeon/src/contrib/configuration/KeyboardConfig.java
@@ -5,62 +5,62 @@ import core.configuration.ConfigKey;
 import core.configuration.ConfigMap;
 import core.configuration.values.ConfigIntValue;
 
-/** Keyboard-configuration for the core-package. */
+/** WTF? . */
 @ConfigMap(path = {"keyboard"})
 public class KeyboardConfig {
-  /** Keys for the core-package for open inventory. */
+  /** WTF? . */
   public static final ConfigKey<Integer> INVENTORY_OPEN =
       new ConfigKey<>(new String[] {"inventory", "open"}, new ConfigIntValue(Input.Keys.I));
 
-  /** Keys for the core-package for close inventory. */
+  /** WTF? . */
   public static final ConfigKey<Integer> CLOSE_UI =
       new ConfigKey<>(new String[] {"ui", "close"}, new ConfigIntValue(Input.Keys.ESCAPE));
 
-  /** Keys for the core-package for world interaction. */
+  /** WTF? . */
   public static final ConfigKey<Integer> INTERACT_WORLD =
       new ConfigKey<>(new String[] {"interact", "world"}, new ConfigIntValue(Input.Keys.E));
 
-  /** Keys for the core-package for mouse world interaction. */
+  /** WTF? . */
   public static final ConfigKey<Integer> MOUSE_INTERACT_WORLD =
       new ConfigKey<>(new String[] {"interact", "mouse"}, new ConfigIntValue(Input.Buttons.LEFT));
 
-  /** Keys for the core-package for use item. */
+  /** WTF? . */
   public static final ConfigKey<Integer> USE_ITEM =
       new ConfigKey<>(new String[] {"item", "use"}, new ConfigIntValue(Input.Keys.E));
 
-  /** Keys for the core-package for first skill. */
+  /** WTF? . */
   public static final ConfigKey<Integer> FIRST_SKILL =
       new ConfigKey<>(new String[] {"skill", "first"}, new ConfigIntValue(Input.Keys.Q));
 
-  /** Keys for the core-package for debug zoom in. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_ZOOM_IN =
       new ConfigKey<>(new String[] {"debug", "zoom_in"}, new ConfigIntValue(Input.Keys.K));
 
-  /** Keys for the core-package for debug zoom out. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_ZOOM_OUT =
       new ConfigKey<>(new String[] {"debug", "zoom_out"}, new ConfigIntValue(Input.Keys.L));
 
-  /** Keys for the core-package for debug toggle level size. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_TOGGLE_LEVELSIZE =
       new ConfigKey<>(new String[] {"debug", "toggle_levelsize"}, new ConfigIntValue(Input.Keys.Z));
 
-  /** Keys for the core-package for debug spawn monster. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_SPAWN_MONSTER =
       new ConfigKey<>(new String[] {"debug", "spawn_monster"}, new ConfigIntValue(Input.Keys.X));
 
-  /** Keys for the core-package for debug teleport to start. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_TELEPORT_TO_START =
       new ConfigKey<>(new String[] {"debug", "teleport_Start"}, new ConfigIntValue(Input.Keys.J));
 
-  /** Keys for the core-package for debug teleport to end. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_TELEPORT_TO_END =
       new ConfigKey<>(new String[] {"debug", "teleport_end"}, new ConfigIntValue(Input.Keys.H));
 
-  /** Keys for the core-package for debug teleport on end. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_TELEPORT_ON_END =
       new ConfigKey<>(new String[] {"debug", "teleport_onEnd"}, new ConfigIntValue(Input.Keys.G));
 
-  /** Keys for the core-package for debug teleport to cursor. */
+  /** WTF? . */
   public static final ConfigKey<Integer> DEBUG_TELEPORT_TO_CURSOR =
       new ConfigKey<>(new String[] {"debug", "teleport_cursor"}, new ConfigIntValue(Input.Keys.O));
 }
