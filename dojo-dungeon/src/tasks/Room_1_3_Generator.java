47ba04b87ababb11952cd73062d4d96ada5e393c
larnold <praisethesun@firelink-shrine.de>
2024-03-18 12:09:12 +0100

    removed unused Question Types and toggle from Room_1_3_Generator Changed Result Dialog to OkDialog

--- dojo-dungeon/src/tasks/Room_1_3_Generator.java
+++ dojo-dungeon/src/tasks/Room_1_3_Generator.java
@@ -3,6 +3,7 @@ package tasks;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import contrib.components.InteractionComponent;
+import contrib.hud.dialogs.OkDialog;
 import contrib.hud.dialogs.TextDialog;
 import contrib.level.generator.graphBased.RoomGenerator;
 import contrib.level.generator.graphBased.levelGraph.LevelNode;
@@ -20,6 +21,8 @@ import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
+import java.util.regex.Pattern;
+
 import task.Task;
 import task.TaskContent;
 import task.game.components.TaskComponent;
@@ -30,14 +33,10 @@ import task.tasktype.quizquestion.MultipleChoice;
 import task.tasktype.quizquestion.SingleChoice;
 
 public class Room_1_3_Generator extends TaskRoomGenerator {
-  private static Quiz question = multipleChoice2();
+    private static final String[] regexes = {"Wort", "AnderesWort"};
+    private static final int chosenRegex = new Random().nextInt(regexes.length);
+    private static Quiz question = freeText();
 
-  // to toggle between question types
-  private static int toggle = 0;
-
-  private static final String[] regexes = {"Wort", "AnderesWort"};
-
-  private static int chosenRegex;
 
   public Room_1_3_Generator(RoomGenerator gen, LevelNode room, LevelNode nextNeighbour) {
     super(gen, room, nextNeighbour);
@@ -66,7 +65,7 @@ public class Room_1_3_Generator extends TaskRoomGenerator {
 
   private static Entity questBoss() throws IOException {
     // choose random regex for question
-    chosenRegex = new Random().nextInt(regexes.length);
+
 
     Entity bossOgrex = new Entity("OgreX");
     bossOgrex.add(new PositionComponent());
@@ -77,8 +76,6 @@ public class Room_1_3_Generator extends TaskRoomGenerator {
             1,
             true,
             (e, who) -> {
-              if (Gdx.input.isKeyJustPressed(Input.Keys.E))
-                toggleQuiz(); // toggle between question types
               UIAnswerCallback.askOnInteraction(question, showAnswersOnHud()).accept(e, who);
             }));
     return bossOgrex;
@@ -91,64 +88,19 @@ public class Room_1_3_Generator extends TaskRoomGenerator {
           .map(t -> (Quiz.Content) t)
           .forEach(t -> answers.set(answers.get() + t.content() + System.lineSeparator()));
 
-      if (answers.get().trim().matches(regexes[chosenRegex])) {
-        TextDialog.textDialog("Ihre Antwort ist korrekt!", "Ok", "Ihre Antwort ist korrekt!");
+      Pattern pattern = Pattern.compile(regexes[chosenRegex]);
+
+      // remove the automatically added \n from the answer string
+      String answer = answers.get();
+      String cleanedAnswer = answer.substring(0, answer.length() - 1);
+
+      if (cleanedAnswer.matches(regexes[chosenRegex])) {
+        OkDialog.showOkDialog("Ihre Antwort ist korrekt!", "Antwort", () -> {});
       } else {
-        TextDialog.textDialog(
-            "Ihre Antwort ist nicht korrekt!", "Ok", "Ihre Antwort ist nicht korrekt!");
+        OkDialog.showOkDialog(
+            "Ihre Antwort ist nicht korrekt!", "Ok", () -> {});
       }
     };
-  }
-
-  private static void toggleQuiz() {
-    toggle = (toggle + 1) % 4;
-    switch (toggle) {
-      case 0 -> question = singleChoice();
-      case 1 -> question = multipleChoice();
-      case 2 -> question = multipleChoice2();
-      case 3 -> question = freeText();
-    }
-  }
-
-  public static Quiz singleChoice() {
-    SingleChoice question = new SingleChoice("Wofür werden reguläre Ausdrücke nicht genutzt?");
-    question.addAnswer(new Quiz.Content("Zum Finden von Bestandteilen in Zeichenketten"));
-    question.addAnswer(new Quiz.Content("Zum Validieren textueller Eingaben"));
-    question.addAnswer(new Quiz.Content("Zum Aufteilen von Strings in Tokens"));
-    question.addAnswer(new Quiz.Content("Zum Konkatenieren von Strings"));
-    return question;
-  }
-
-  public static Quiz multipleChoice() {
-    Quiz question =
-        new MultipleChoice(
-            "Welche der folgenden regulären Ausdrücke sind valide, wenn es darum geht,"
-                + " eine dreistellige Zahl zu beschreiben?");
-    question.addAnswer(new Quiz.Content("\\D[0-9]\\D"));
-    question.addAnswer(new Quiz.Content("\t\\d\\d\\d"));
-    question.addAnswer(new Quiz.Content("\\d\\D[0-9]"));
-    question.addAnswer(new Quiz.Content("3[0-9]"));
-    question.addAnswer(new Quiz.Content("3*\\d"));
-    question.addAnswer(new Quiz.Content("[0-9]\\d\\d"));
-    question.addAnswer(new Quiz.Content("\t\\d[0-9](3)"));
-    return question;
-  }
-
-  public static Quiz multipleChoice2() {
-    Quiz question =
-        new MultipleChoice(
-            "Welche der Strings entsprechen dem folgenden regulären Ausdruck: \"B[A-Z2-5a-e]\\\\.N\"");
-    question.addAnswer(new Quiz.Content("BAN"));
-    question.addAnswer(new Quiz.Content("B4\\NN"));
-    question.addAnswer(new Quiz.Content("Aa\\rN"));
-    question.addAnswer(new Quiz.Content("B3\\JN"));
-    question.addAnswer(new Quiz.Content("BX\\en"));
-    question.addAnswer(new Quiz.Content("Bl\\\\mathbb{N}"));
-    question.addAnswer(new Quiz.Content("Be\\mathbb{N}"));
-    question.addAnswer(new Quiz.Content("\"B6\\rN\""));
-    question.addAnswer(new Quiz.Content("Be\\J"));
-    question.addAnswer(new Quiz.Content("AA\\AN"));
-    return question;
   }
 
   public static Quiz freeText() {
