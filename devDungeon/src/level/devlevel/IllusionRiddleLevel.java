c57e8688017a99b74f84bffad2300717ccc67ee9
Flamtky <niklas.schumann+github@proton.me>
2024-03-19 18:00:54 +0100

    created placeholder classes for level 3

---
+++ devDungeon/src/level/devlevel/IllusionRiddleLevel.java
@@ -0,0 +1,96 @@
+package level.devlevel;
+
+import contrib.components.InventoryComponent;
+import contrib.entities.MiscFactory;
+import contrib.item.HealthPotionType;
+import contrib.item.concreteItem.ItemPotionHealth;
+import core.Entity;
+import core.Game;
+import core.components.PositionComponent;
+import core.level.elements.tile.DoorTile;
+import core.level.elements.tile.ExitTile;
+import core.level.utils.Coordinate;
+import core.level.utils.DesignLabel;
+import core.level.utils.LevelElement;
+import core.utils.components.MissingComponentException;
+import entities.EntityUtils;
+import entities.MonsterType;
+import java.util.*;
+import level.DevDungeonLevel;
+import level.devlevel.riddleHandler.DamagedBridgeRiddleHandler;
+import level.utils.ITickable;
+
+/** The Damaged Bridge Riddle Level */
+public class IllusionRiddleLevel extends DevDungeonLevel implements ITickable {
+
+  // Difficulty (Mob Count, Mob Types)
+  private static final int MOB_COUNT = 7;
+  private static final MonsterType[] MONSTER_TYPES =
+      new MonsterType[] {MonsterType.ORC_WARRIOR, MonsterType.ORC_SHAMAN};
+  private static final MonsterType BOSS_TYPE = MonsterType.CHORT;
+
+  // Spawn Points / Locations
+  private final Coordinate[] mobSpawns;
+  private final Coordinate levelBossSpawn;
+
+  private final DamagedBridgeRiddleHandler riddleHandler;
+
+  public IllusionRiddleLevel(
+      LevelElement[][] layout, DesignLabel designLabel, List<Coordinate> customPoints) {
+    super(layout, designLabel, customPoints);
+    this.riddleHandler = new DamagedBridgeRiddleHandler(customPoints, this);
+    this.mobSpawns = new Coordinate[0];
+    this.levelBossSpawn = new Coordinate(0, 0);
+  }
+
+  @Override
+  public void onTick(boolean isFirstTick) {
+    if (isFirstTick) {
+      ((ExitTile) this.endTile()).close(); // close exit at start (to force defeating the boss)
+      this.doorTiles().forEach(DoorTile::close);
+      this.pitTiles()
+          .forEach(
+              pit -> {
+                pit.timeToOpen(50);
+                pit.close();
+              });
+      this.handleFirstTick();
+    }
+
+    this.riddleHandler.onTick(isFirstTick);
+  }
+
+  private void handleFirstTick() {
+
+    // Spawn all entities and it's content
+    this.spawnChestsAndCauldrons();
+
+    EntityUtils.spawnMobs(MOB_COUNT, MONSTER_TYPES, this.mobSpawns, BOSS_TYPE, this.levelBossSpawn);
+  }
+
+  /**
+   * Spawns the chests and cauldrons in the riddle room.
+   *
+   * @throws RuntimeException if any of the entities could not be created
+   */
+  private void spawnChestsAndCauldrons() {
+    Entity chest;
+    try {
+      chest = MiscFactory.newChest(MiscFactory.FILL_CHEST.EMPTY);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create speed potion chest");
+    }
+    PositionComponent pc =
+        chest
+            .fetch(PositionComponent.class)
+            .orElseThrow(() -> MissingComponentException.build(chest, PositionComponent.class));
+    // pc.position();
+    InventoryComponent ic =
+        chest
+            .fetch(InventoryComponent.class)
+            .orElseThrow(() -> MissingComponentException.build(chest, InventoryComponent.class));
+    ic.add(new ItemPotionHealth(HealthPotionType.GREATER));
+
+    Game.add(chest);
+  }
+}
---
+++ devDungeon/src/level/devlevel/riddleHandler/IllusionRiddleHandler.java
@@ -0,0 +1,39 @@
+package level.devlevel.riddleHandler;
+
+import core.level.TileLevel;
+import core.level.utils.Coordinate;
+import java.util.List;
+import level.utils.ITickable;
+
+public class IllusionRiddleHandler implements ITickable {
+
+  private final TileLevel level;
+  private final Coordinate[] riddleRoomBounds; // TopLeft, BottomRight
+  private final Coordinate
+      riddleRewardSpawn; // The spawn point of the reward for solving the riddle
+  private boolean rewardGiven = false;
+
+  public IllusionRiddleHandler(List<Coordinate> customPoints, TileLevel level) {
+    this.riddleRoomBounds = new Coordinate[] {new Coordinate(0, 0), new Coordinate(0, 0)};
+    this.riddleRewardSpawn = new Coordinate(0, 0);
+
+    this.level = level;
+  }
+
+  @Override
+  public void onTick(boolean isFirstTick) {
+    if (isFirstTick) {
+      this.handleFirstTick();
+    }
+  }
+
+  private void giveReward() {
+
+    this.rewardGiven = true;
+  }
+
+  private void handleFirstTick() {
+    if (this.level.tileAt(this.riddleRewardSpawn) != null)
+      this.level.tileAt(this.riddleRewardSpawn).tintColor(0x22FF22FF);
+  }
+}
