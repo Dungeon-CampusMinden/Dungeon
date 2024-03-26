c2f58f1d52384c5c7ba139c6a8bd694efca9ba49
Flamtky <niklas.schumann+github@proton.me>
2024-03-26 18:41:25 +0100

    added a fill method to make my life easier

--- devDungeon/src/systems/LevelEditorSystem.java
+++ devDungeon/src/systems/LevelEditorSystem.java
@@ -6,14 +6,15 @@ import contrib.utils.components.skill.SkillTools;
 import core.Game;
 import core.System;
 import core.level.Tile;
+import core.level.utils.Coordinate;
 import core.level.utils.LevelElement;
 import core.systems.LevelSystem;
 import core.utils.Point;
+import java.util.Queue;
 import level.DevDungeonLevel;
 import level.utils.DungeonSaver;
 
 public class LevelEditorSystem extends System {
-  private static boolean active = true;
   private static final int SKIP_BUTTON = Input.Keys.NUM_1;
   private static final int PIT_BUTTON = Input.Keys.NUM_2;
   private static final int FLOOR_BUTTON = Input.Keys.NUM_3;
@@ -22,7 +23,10 @@ public class LevelEditorSystem extends System {
   private static final int EXIT_BUTTON = Input.Keys.NUM_6;
   private static final int DOOR_BUTTON = Input.Keys.NUM_7;
   private static final int CUSTOM_POINT = Input.Keys.NUM_8;
+  private static final int FILL_WITH_FLOOR = Input.Keys.NUM_9;
   private static final int SAVE_BUTTON = Input.Keys.Y;
+  private static final int maxFillRange = 100;
+  private static boolean active = true;
 
   public static boolean active() {
     return active;
@@ -67,6 +71,52 @@ public class LevelEditorSystem extends System {
       } else {
         java.lang.System.out.println(Game.currentLevel().printLevel());
       }
+    }
+    if (Gdx.input.isKeyJustPressed(FILL_WITH_FLOOR)) {
+      this.fillWithFloor();
+    }
+  }
+
+  /**
+   * Floods area with floor tiles. Only affects tiles within a certain range of the cursor and only
+   * skip tiles. It uses a queue to flood the area.
+   */
+  private void fillWithFloor() {
+    Point mosPos = SkillTools.cursorPositionAsPoint();
+    mosPos = new Point(mosPos.x - 0.5f, mosPos.y - 0.25f);
+    Tile startTile = LevelSystem.level().tileAt(mosPos);
+    if (startTile == null) {
+      return;
+    }
+    Queue<Tile> queue = new java.util.LinkedList<>();
+    queue.add(startTile);
+    int range = 0;
+    while (!queue.isEmpty() && range < maxFillRange) {
+      Tile currentTile = queue.poll();
+      if (currentTile == null) {
+        continue;
+      }
+      if (currentTile.levelElement() == LevelElement.SKIP
+          || currentTile.levelElement() == LevelElement.FLOOR) {
+        LevelSystem.level().changeTileElementType(currentTile, LevelElement.FLOOR);
+        queue.add(
+            LevelSystem.level()
+                .tileAt(
+                    new Coordinate(currentTile.coordinate().x + 1, currentTile.coordinate().y)));
+        queue.add(
+            LevelSystem.level()
+                .tileAt(
+                    new Coordinate(currentTile.coordinate().x - 1, currentTile.coordinate().y)));
+        queue.add(
+            LevelSystem.level()
+                .tileAt(
+                    new Coordinate(currentTile.coordinate().x, currentTile.coordinate().y + 1)));
+        queue.add(
+            LevelSystem.level()
+                .tileAt(
+                    new Coordinate(currentTile.coordinate().x, currentTile.coordinate().y - 1)));
+      }
+      range++;
     }
   }
 
