fc560e22e94cce68c3eb9cefef0189c85e755fbf
Tobias Grothe <tgrothe@hsbi.de>
2024-04-01 17:23:25 +0200

    spotlessApply: modified:   dungeon/src/contrib/hud/UIUtils.java

--- dungeon/src/contrib/hud/UIUtils.java
+++ dungeon/src/contrib/hud/UIUtils.java
@@ -69,10 +69,9 @@ public final class UIUtils {
    * <p>Characteristics:
    *
    * <ul>
-   *   <li>Every whitespace character (i.e. {@code " "} or {@code "\n"}, etc.) that occurs at least once
-   *       should be replaced by exactly one space character.
-   *   <li>A line of text should not be longer than {@code MAX_ROW_LENGTH}
-   *       characters.
+   *   <li>Every whitespace character (i.e. {@code " "} or {@code "\n"}, etc.) that occurs at least
+   *       once should be replaced by exactly one space character.
+   *   <li>A line of text should not be longer than {@code MAX_ROW_LENGTH} characters.
    *   <li>Long words at the end of a line should be inserted into the next line (soft word wrap).
    *   <li>The {@code '\n'} character should be used as the line separator.
    *   <li>Lines should not be filled up entirely with spaces.
