c512228704c114949047b82383ed53a45a11f917
mr <malt.r@mail.de>
2024-04-04 00:37:18 +0200

    Add profiliing timer utility class

---
+++ dungeon/src/dsl/helper/ProfilingTimer.java
@@ -0,0 +1,43 @@
+package dsl.helper;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ProfilingTimer implements AutoCloseable {
+  public enum Unit {
+    nano,
+    micro,
+    milli
+  }
+
+  private final String message;
+  private final long startTime;
+  private final HashMap<String, Long> times;
+  private final Unit unit;
+
+  public ProfilingTimer(String message, HashMap<String, Long> times) {
+    this.message = message;
+    this.times = times;
+    this.unit = Unit.nano;
+    this.startTime = System.nanoTime();
+  }
+
+  public ProfilingTimer(String message, HashMap<String, Long> times, Unit unit) {
+    this.message = message;
+    this.times = times;
+    this.unit = unit;
+    this.startTime = System.nanoTime();
+  }
+
+  @Override
+  public void close() {
+    long elapsedTime = System.nanoTime() - startTime;
+    long divided = switch(this.unit) {
+      case nano -> elapsedTime;
+      case micro -> elapsedTime / 1000;
+      case milli -> elapsedTime / 1000000;
+    };
+
+    this.times.put(message, divided);
+  }
+}
