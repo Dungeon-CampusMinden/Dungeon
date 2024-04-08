059231fdbd5ed2716f6194db92c2b7ffafb85128
Tobias Grothe <tgrothe@hsbi.de>
2024-04-08 09:55:17 +0200

    Implementation for room 9 (#39)
    closes #35
    
    ---------
    
    Co-authored-by: Lukas Arnold <59529322+JudiTeller@users.noreply.github.com>
    Co-authored-by: Denniso <wisniadenniso@gmail.com>

--- dojo-dungeon/src/level/rooms/Room9.java
+++ dojo-dungeon/src/level/rooms/Room9.java
@@ -1,10 +1,33 @@
 package level.rooms;
 
+import contrib.components.InteractionComponent;
+import contrib.hud.dialogs.OkDialog;
 import contrib.level.generator.graphBased.RoomGenerator;
+import core.Entity;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelSize;
+import core.utils.components.path.SimpleIPath;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiConsumer;
+import task.Task;
+import task.TaskContent;
+import task.game.components.TaskComponent;
+import task.game.hud.UIAnswerCallback;
+import task.tasktype.Quiz;
+import task.tasktype.quizquestion.FreeText;
 
-public class Room9 extends TaskRoom {
+public class Room9 extends Room {
+  private final String[] expectedPatterns = {"Observer", "Visitor"};
+  private int currentPatternIndex = 0;
+
+  private Entity zauberer;
+  private int correctAnswerCount = 0;
+
   Room9(
       LevelRoom levelRoom,
       RoomGenerator gen,
@@ -12,6 +35,81 @@ public class Room9 extends TaskRoom {
       LevelSize levelSize,
       DesignLabel designLabel) {
     super(levelRoom, gen, nextRoom, levelSize, designLabel);
-    // TODO: generate ...
+
+    try {
+      generate();
+    } catch (IOException e) {
+      throw new RuntimeException(
+          "Failed to generate: " + getClass().getName() + ": " + e.getMessage(), e);
+    }
+  }
+
+  public void generate() throws IOException {
+    // add entities to room
+    Set<Entity> roomEntities = new HashSet<>();
+
+    // add boss
+    roomEntities.add(questBoss());
+
+    addRoomEntities(roomEntities);
+  }
+
+  private Entity questBoss() throws IOException {
+    // add boss
+    zauberer = new Entity("Zauberer von Patternson");
+    zauberer.add(new PositionComponent());
+    zauberer.add(new DrawComponent(new SimpleIPath("character/wizard")));
+
+    setNextTask();
+
+    return zauberer;
+  }
+
+  private void setNextTask() {
+    final Quiz question = newFreeText();
+
+    zauberer.add(new TaskComponent(question, zauberer));
+    zauberer.add(
+        new InteractionComponent(
+            1,
+            true,
+            (e, who) -> {
+              UIAnswerCallback.askOnInteraction(question, showAnswersOnHud()).accept(e, who);
+            }));
+  }
+
+  private BiConsumer<Task, Set<TaskContent>> showAnswersOnHud() {
+    return (task, taskContents) -> {
+      AtomicReference<String> answers = new AtomicReference<>("");
+      taskContents.stream()
+          .map(t -> (Quiz.Content) t)
+          .forEach(t -> answers.set(answers.get() + t.content() + System.lineSeparator()));
+
+      // remove the automatically added \n from the answer string
+      String answer = answers.get();
+      String cleanedAnswer = answer.trim();
+
+      if (cleanedAnswer.equals(expectedPatterns[currentPatternIndex])) {
+        OkDialog.showOkDialog("Ihre Antwort ist korrekt!", "Antwort", () -> {});
+        correctAnswerCount++;
+        if (correctAnswerCount >= 2) {
+          openDoors();
+        }
+        currentPatternIndex++;
+        if (currentPatternIndex < expectedPatterns.length) {
+          setNextTask();
+        }
+      } else {
+        OkDialog.showOkDialog("Ihre Antwort ist nicht korrekt!", "Ok", () -> {});
+      }
+    };
+  }
+
+  private Quiz newFreeText() {
+    String questionText =
+        "Welches Design-Pattern wird in dem UML-Klasseniagramm unter \"dojo-dungeon/todo-assets/r9/UML_Klassendiagramm"
+            + (currentPatternIndex + 1)
+            + ".png\" dargestellt? Es reicht das Wort ohne den Zusatz Pattern!";
+    return new FreeText(questionText);
   }
 }
