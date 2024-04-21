fb155341f2ad37164cda3b35a48e211a4ed5caa4
Tobias Grothe <tgrothe@hsbi.de>
2024-04-21 05:44:32 +0200

    modified:   dojo-dungeon/todo-assets/Fehler_Quader/Cuboid.java (java docs added)

--- dojo-dungeon/todo-assets/Fehler_Quader/Cuboid.java
+++ dojo-dungeon/todo-assets/Fehler_Quader/Cuboid.java
@@ -9,14 +9,35 @@ public class Cuboid {
     this.sideC = sideC;
   }
 
+  /**
+   * Calculate the area of the Cuboid.
+   *
+   * <p>Area refers to the measurement of the surface enclosed by a shape.
+   *
+   * @return the area
+   */
   public float calculateArea() {
     return 2 * (sideA * sideB + sideA * sideB + sideB * sideC);
   }
 
+  /**
+   * Calculate the perimeter of the Cuboid.
+   *
+   * <p>Perimeter refers to the measurement of the boundary of a shape.
+   *
+   * @return the perimeter
+   */
   public float calculatePerimeter() {
     return 6 * (sideA + sideB + sideC);
   }
 
+  /**
+   * Calculate the volume of the Cuboid.
+   *
+   * <p>Volume refers to the measurement of the space enclosed by a three-dimensional object.
+   *
+   * @return the volume
+   */
   public float calculateVolume() {
     return sideA * sideB;
   }
