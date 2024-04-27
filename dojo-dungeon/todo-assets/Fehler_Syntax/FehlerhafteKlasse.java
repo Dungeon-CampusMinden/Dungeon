37478277d6fd4426d02b6ded554b6edb6809524e
Tobias Grothe <tgrothe@hsbi.de>
2024-04-27 21:00:45 +0200

    Apply suggestions from code review
    Co-authored-by: Carsten Gips <cagix@hsbi.de>

--- dojo-dungeon/todo-assets/Fehler_Syntax/FehlerhafteKlasse.java
+++ dojo-dungeon/todo-assets/Fehler_Syntax/FehlerhafteKlasse.java
@@ -7,12 +7,12 @@ public class FehlerhafteKlasse {
   private final int addend2 = 2;
 
   /**
-   * Diese Methode soll 2 zu {@code in} addieren und das Ergebnis in einen String umwandeln und
+   * Diese Methode soll 2 zum Zahlenwert, der im Parameter {@code in} als String übergeben wird, addieren und das Ergebnis wieder in einen String umwandeln und
    * zurückgeben.
    *
    * <p>Wenn {@code in} {@code null} oder keine Zahl ist, soll "NaN" zurückgegeben werden.
    *
-   * <p>Wenn das Ergebnis größer als {@link Integer#MAX_VALUE} ist, soll "Integer overflow"
+   * <p>Wenn die Operation einen Integer-Overflow verursachen würde, soll "Integer overflow"
    * zurückgegeben werden.
    *
    * @param in Eingabe, zu der 2 addiert werden soll
@@ -27,10 +27,10 @@ public class FehlerhafteKlasse {
    *
    * <p>Passen Sie dazu nur diese Methode an.
    *
-   * @return Die Summe beider Addenden als String.
+   * @return Die Summe beider Summanden als String.
    */
   public static String getSum() {
-    FehlerhafteKlasse fk = null;
+    FehlerhafteKlasse fk;
     return "" + fk.addend1 + fk.addend2;
   }
 }
