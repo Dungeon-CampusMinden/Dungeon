17288f9ca2ebad423a8fcc0c83dca74b2debfdf3
Tobias Grothe <tgrothe@hsbi.de>
2024-04-27 09:57:16 +0200

    modified:   dojo-dungeon/todo-assets/Fehler_Syntax/FehlerhafteKlasse.java (Fix Java doc string typos)

--- dojo-dungeon/todo-assets/Fehler_Syntax/FehlerhafteKlasse.java
+++ dojo-dungeon/todo-assets/Fehler_Syntax/FehlerhafteKlasse.java
@@ -1,6 +1,6 @@
 /**
- * In dieser Klasse sollen unterschiedliche Methoden implementiert werden, die fehlerhaft sind. Das
- * Verhalten der Methoden wird dann überprüft werden.
+ * In dieser Klasse sollen unterschiedliche Methoden implementiert werden, die noch keine
+ * Implementierung haben. Das Verhalten dieser Methoden wird dann im Spiel überprüft werden.
  */
 public class FehlerhafteKlasse {
 
@@ -8,30 +8,26 @@ public class FehlerhafteKlasse {
    * Diese Methode soll 2 zu {@code in} addieren und das Ergebnis in einen String umwandeln und
    * zurückgeben.
    *
-   * <p>Wenn {@code in} {@code null} oder keine Zahl ist, soll "NaN" zurückgeben werden.
+   * <p>Wenn {@code in} {@code null} oder keine Zahl ist, soll "NaN" zurückgegeben werden.
    *
    * <p>Wenn das Ergebnis größer als {@link Integer#MAX_VALUE} ist, soll "Integer overflow"
-   * zurückgeben werden.
+   * zurückgegeben werden.
    *
-   * @param in Eingabe, die geparst werden soll
+   * @param in Eingabe, zu der 2 addiert werden soll
    * @return Die Summe der Addition als String
    * @see Integer
    */
-  public static String incrementByTwo(String in) {
-    return null;
-  }
+  public static String incrementByTwo(String in) {}
 
   /**
    * Diese Methode soll die Fibonacci-Zahl für {@code n} berechnen und zurückgeben.
    *
-   * <p>Wenn {@code in} {@code null} oder keine Zahl ist, soll "NaN" zurückgeben werden.
+   * <p>Wenn {@code in} {@code null} oder keine Zahl ist, soll "NaN" zurückgegeben werden.
    *
-   * <p>Wenn {@code in} kleiner 0 oder größer als 10 ist, soll "Too large" zurückgeben werden.
+   * <p>Wenn {@code in} kleiner 0 oder größer als 10 ist, soll "Too large" zurückgegeben werden.
    *
-   * @param n Eingabe, die geparst werden soll
+   * @param n Eingabe, für die die Fibonacci-Zahl berechnet werden soll
    * @return Die Fibonacci-Zahl als String
    */
-  public static String calculateFibonacci(String n) {
-    return null;
-  }
+  public static String calculateFibonacci(String n) {}
 }
