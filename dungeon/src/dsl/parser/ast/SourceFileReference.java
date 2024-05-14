0d2fe888ca884aa6bbb189c63bafec575eec8d0d
mr <malt.r@mail.de>
2024-05-14 14:15:14 +0200

    Remove unused combineWith in SourceFileReference

--- dungeon/src/dsl/parser/ast/SourceFileReference.java
+++ dungeon/src/dsl/parser/ast/SourceFileReference.java
@@ -5,7 +5,6 @@ import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
 
-// TODO: substitute this for more sophisticated method of source reference...
 @NodeEntity
 public class SourceFileReference {
   @Id @GeneratedValue private Long id;
@@ -54,49 +53,6 @@ public class SourceFileReference {
     this.startColumn = sourceColumn;
     this.endLine = endLine;
     this.endColumn = endColumn;
-  }
-
-  public SourceFileReference combineWith(SourceFileReference other) {
-    // smaller start
-    int startLine;
-    int startColumn;
-
-    if (this == SourceFileReference.NULL) {
-      return other;
-    }
-    if (other == SourceFileReference.NULL) {
-      return this;
-    }
-
-    if (this.startLine == other.startLine) {
-      startLine = this.startLine;
-      startColumn = Math.min(this.endColumn, other.startColumn);
-    } else {
-      if (this.startLine < other.startLine) {
-        startLine = this.startLine;
-        startColumn = this.startColumn;
-      } else {
-        startLine = other.startLine;
-        startColumn = other.startColumn;
-      }
-    }
-
-    // bigger end
-    int endLine;
-    int endColumn;
-    if (this.endLine == other.endLine) {
-      endLine = this.endLine;
-      endColumn = Math.max(this.endColumn, other.endColumn);
-    } else {
-      if (this.endLine > other.endLine) {
-        endLine = this.endLine;
-        endColumn = this.endColumn;
-      } else {
-        endLine = other.endLine;
-        endColumn = other.endColumn;
-      }
-    }
-    return new SourceFileReference(startLine, startColumn, endLine, endColumn);
   }
 
   public static SourceFileReference NULL = new SourceFileReference(-1, -1);
