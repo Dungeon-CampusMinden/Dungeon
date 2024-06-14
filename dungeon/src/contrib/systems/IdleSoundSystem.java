51be863da07904c3cf5032b64e51f9caec1b4ea6
Niklas Schumann <68606032+Flamtky@users.noreply.github.com>
2024-06-14 22:23:11 +0200

    Dungeon: mute distant entities via proximity filter (IdleSoundSystem) (#1552)

--- dungeon/src/contrib/systems/IdleSoundSystem.java
+++ dungeon/src/contrib/systems/IdleSoundSystem.java
@@ -3,7 +3,11 @@ package contrib.systems;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import contrib.components.IdleSoundComponent;
+import core.Entity;
+import core.Game;
 import core.System;
+import core.components.PositionComponent;
+import core.utils.Point;
 import core.utils.components.MissingComponentException;
 import java.util.Random;
 
@@ -18,15 +22,34 @@ import java.util.Random;
 public final class IdleSoundSystem extends System {
 
   private static final Random RANDOM = new Random();
+  private static final float DISTANCE_THRESHOLD = 10.0f;
 
   /** Create a new {@link IdleSoundSystem}. */
   public IdleSoundSystem() {
     super(IdleSoundComponent.class);
   }
 
+  private static boolean isEntityNearby(Point heroPos, Entity entity) {
+    Point entityPosition =
+        entity.fetch(PositionComponent.class).map(PositionComponent::position).orElse(null);
+
+    if (heroPos == null || entityPosition == null) {
+      return false;
+    }
+
+    double distance = heroPos.distance(entityPosition);
+
+    return distance < DISTANCE_THRESHOLD;
+  }
+
   @Override
   public void execute() {
+    Point heroPos =
+        Game.hero()
+            .flatMap(e -> e.fetch(PositionComponent.class).map(PositionComponent::position))
+            .orElse(null);
     entityStream()
+        .filter(e -> isEntityNearby(heroPos, e))
         .forEach(
             e ->
                 playSound(
