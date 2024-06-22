bb70524fe552baee5cac3e009d02ad992a63cf61
Finn Watermann <finn.watermann@hsbi.de>
2024-06-22 14:58:43 +0200

    Add GLFWException (used for Errors thrown by GLFW)

---
+++ engine/src/main/java/de/fwatermann/engine/exception/GLFWException.java
@@ -0,0 +1,34 @@
+package de.fwatermann.engine.exception;
+
+/**
+ * Represents a GLFW exception.
+ * This class extends the RuntimeException class and is used to signal that an error has occurred in the GLFW library.
+ */
+public class GLFWException extends RuntimeException {
+
+    /**
+     * Constructs a new GLFWException with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    public GLFWException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new GLFWException with the specified detail message and cause.
+     *
+     * @param message the detail message
+     * @param cause the cause of the exception
+     */
+    public GLFWException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new GLFWException with no detail message.
+     */
+    public GLFWException() {
+        super();
+    }
+}
