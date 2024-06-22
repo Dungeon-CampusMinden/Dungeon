0b4045788573cb71a1b1b2158102f519c1df45f1
Finn Watermann <finn.watermann@hsbi.de>
2024-06-22 14:58:57 +0200

    Add GameWindow class

---
+++ engine/src/main/java/de/fwatermann/engine/window/GameWindow.java
@@ -0,0 +1,494 @@
+package de.fwatermann.engine.window;
+
+import de.fwatermann.engine.event.input.KeyboardEvent;
+import de.fwatermann.engine.event.input.MouseButtonEvent;
+import de.fwatermann.engine.event.input.MouseMoveEvent;
+import de.fwatermann.engine.event.input.MouseScrollEvent;
+import de.fwatermann.engine.event.window.WindowCloseEvent;
+import de.fwatermann.engine.event.window.WindowFocusChangedEvent;
+import de.fwatermann.engine.event.window.WindowMoveEvent;
+import de.fwatermann.engine.event.window.WindowResizeEvent;
+import de.fwatermann.engine.exception.GLFWException;
+import org.joml.Vector2d;
+import org.joml.Vector2i;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL33;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public abstract class GameWindow {
+
+    private Thread updateThread;
+    private String title;
+    private Vector2i size;
+    private Vector2i position;
+    private boolean debug = false;
+    private boolean visible = false;
+    private boolean resizable = true;
+    private boolean rawMouseInput = false;
+    private boolean vsync = false;
+    private long frameRate = -1;
+    private long tickRate = 50;
+
+    private Vector2d mousePosition = new Vector2d(0, 0);
+    private long glfwWindow;
+
+    /**
+     * Constructs a new GameWindow.
+     *
+     * @param title   the title of the game window
+     * @param size    the size of the game window as a Vector2i object
+     * @param visible the visibility state of the game window
+     * @param debug   the debug state of the game window
+     */
+    public GameWindow(String title, Vector2i size, boolean visible, boolean debug) {
+        this.title = title;
+        this.size = size;
+        this.visible = visible;
+        this.debug = debug;
+    }
+
+    /**
+     * Render the game.
+     * This method is called every frame.
+     * It should <b>ONLY</b> be used to render the game.
+     * Attention: This method is called asynchronously to the update method!
+     *
+     * @param deltaTime The time since the last render in seconds.
+     */
+    public abstract void render(float deltaTime);
+
+    /**
+     * Update the game logic.
+     * Attention: This method is called asynchronously to the render method!
+     *
+     * @param deltaTime The time since the last update in seconds.
+     */
+    public abstract void update(float deltaTime);
+
+    /**
+     * Initialize the game.
+     * This method is called once before the render loop starts.
+     */
+    public abstract void init();
+
+    /**
+     * Clean up the game.
+     * This method is called once after the game loop ends.
+     */
+    public abstract void cleanup();
+
+    private void _init() {
+        this._initGLFW();
+        this._initOpenGL();
+
+        if (this.visible) {
+            glfwShowWindow(this.glfwWindow);
+        }
+
+        this.updateThread = new Thread(this::_updateLoop, "Update Thread");
+        this.updateThread.start();
+        this._renderLoop();
+    }
+
+    private void _initGLFW() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, this.visible ? GLFW_TRUE : GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, this.resizable ? GLFW_TRUE : GLFW_FALSE);
+        if (this.debug) {
+            glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+        }
+        this.glfwWindow = glfwCreateWindow(this.size.x, this.size.y, this.title, 0L, 0L);
+        if (this.glfwWindow == 0L) {
+            throw new GLFWException("Unable to create GLFW window");
+        }
+
+        glfwMakeContextCurrent(this.glfwWindow);
+        glfwSwapInterval(this.vsync ? 1 : 0);
+
+        //Register Callbacks
+        glfwSetWindowSizeCallback(this.glfwWindow, (window, width, height) -> {
+            WindowResizeEvent event = new WindowResizeEvent(this.size, new Vector2i(width, height), this);
+            event.fire();
+            if (event.isCanceled()) {
+                glfwSetWindowSize(window, event.from.x, event.from.y);
+            }
+            this.size = new Vector2i(width, height);
+        });
+
+        glfwSetFramebufferSizeCallback(this.glfwWindow, (window, width, height) -> {
+            GL33.glViewport(0, 0, width, height);
+        });
+
+        glfwSetWindowPosCallback(this.glfwWindow, (window, x, y) -> {
+            WindowMoveEvent event = new WindowMoveEvent(this.position, new Vector2i(x, y));
+            event.fire();
+            if (event.isCanceled()) {
+                glfwSetWindowPos(window, event.from.x, event.from.y);
+            }
+            this.position = event.to;
+        });
+
+        glfwSetWindowCloseCallback(this.glfwWindow, window -> {
+            WindowCloseEvent event = new WindowCloseEvent(this);
+            event.fire();
+            if (event.isCanceled()) {
+                glfwSetWindowShouldClose(window, false);
+                return;
+            }
+            glfwSetWindowShouldClose(window, true);
+        });
+
+
+        glfwSetWindowFocusCallback(this.glfwWindow, (window, focused) -> {
+            WindowFocusChangedEvent event = new WindowFocusChangedEvent(focused, this);
+            event.fire();
+        });
+
+        glfwSetMouseButtonCallback(this.glfwWindow, (window, button, action, mods) -> {
+            MouseButtonEvent.MouseButtonAction mouseButtonAction = switch (action) {
+                case GLFW_PRESS -> MouseButtonEvent.MouseButtonAction.PRESS;
+                case GLFW_RELEASE -> MouseButtonEvent.MouseButtonAction.RELEASE;
+                case GLFW_REPEAT -> MouseButtonEvent.MouseButtonAction.REPEAT;
+                default -> throw new IllegalStateException("Unexpected value: " + action);
+            };
+            MouseButtonEvent event = new MouseButtonEvent(button, mouseButtonAction);
+            event.fire();
+        });
+
+        glfwSetScrollCallback(this.glfwWindow, (window, xoffset, yoffset) -> {
+            MouseScrollEvent event = new MouseScrollEvent((int) xoffset, (int) yoffset);
+            event.fire();
+        });
+
+        glfwSetCursorPosCallback(this.glfwWindow, (window, xpos, ypos) -> {
+            Vector2i from = new Vector2i((int) this.mousePosition.x, (int) this.mousePosition.y);
+            Vector2i to = new Vector2i((int) xpos, (int) ypos);
+            MouseMoveEvent event = new MouseMoveEvent(from, to);
+            event.fire();
+            if (event.isCanceled()) {
+                glfwSetCursorPos(window, from.x, from.y);
+            } else {
+                this.mousePosition = new Vector2d(xpos, ypos);
+            }
+        });
+
+        glfwSetKeyCallback(this.glfwWindow, (window, key, scancode, action, mods) -> {
+            KeyboardEvent.KeyAction keyAction = switch (action) {
+                case GLFW_PRESS -> KeyboardEvent.KeyAction.PRESS;
+                case GLFW_RELEASE -> KeyboardEvent.KeyAction.RELEASE;
+                case GLFW_REPEAT -> KeyboardEvent.KeyAction.REPEAT;
+                default -> throw new IllegalStateException("Unexpected value: " + action);
+            };
+            KeyboardEvent event = new KeyboardEvent(key, keyAction);
+            event.fire();
+        });
+
+
+    }
+
+    private void _initOpenGL() {
+        GL.createCapabilities();
+        GL33.glClearColor(0.51f, 0.78f, 0.89f, 1f);
+        GL33.glEnable(GL33.GL_DEPTH_TEST);
+        GL33.glEnable(GL33.GL_CULL_FACE);
+        GL33.glCullFace(GL33.GL_BACK);
+    }
+
+    private void _updateLoop() {
+        long lastTime = 0;
+        while (!glfwWindowShouldClose(this.glfwWindow)) {
+            long currentTime = System.nanoTime();
+            float deltaTime = (currentTime - lastTime) / 1_000_000_000f;
+            lastTime = currentTime;
+
+            long start = System.nanoTime();
+            this.update(deltaTime);
+            long end = System.nanoTime();
+            long execution = end - start;
+
+            try {
+                long sleepTime = ((1_000_000_000L / this.tickRate) - (execution)) / 1_000_000L;
+                if (sleepTime > 0) {
+                    TimeUnit.MILLISECONDS.sleep(sleepTime);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void _renderLoop() {
+        this.init();
+        long lastTime = System.nanoTime();
+        while (!glfwWindowShouldClose(this.glfwWindow)) {
+            long currentTime = System.nanoTime();
+            float deltaTime = (currentTime - lastTime) / 1_000_000_000f;
+            lastTime = currentTime;
+
+            long start = System.nanoTime();
+            GL33.glClear(GL33.GL_COLOR_BUFFER_BIT | GL33.GL_DEPTH_BUFFER_BIT);
+            this.render(deltaTime);
+            glfwSwapBuffers(this.glfwWindow);
+            glfwPollEvents();
+            long end = System.nanoTime();
+            long execution = end - start;
+
+            try {
+                long sleepTime = ((1_000_000_000L / 60) - (execution)) / 1_000_000L;
+                if (sleepTime > 0 && this.frameRate > 0) {
+                    TimeUnit.MILLISECONDS.sleep(sleepTime);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Returns the title of the game window.
+     *
+     * @return the title of the game window
+     */
+    public String title() {
+        return this.title;
+    }
+
+    /**
+     * Sets the title of the game window.
+     *
+     * @param title the new title of the game window
+     * @return the game window
+     */
+    public GameWindow title(String title) {
+        glfwSetWindowTitle(this.glfwWindow, title);
+        this.title = title;
+        return this;
+    }
+
+    /**
+     * Returns the size of the game window.
+     *
+     * @return the size of the game window
+     */
+    public Vector2i size() {
+        return this.size;
+    }
+
+    /**
+     * Sets the size of the game window.
+     *
+     * @param size the new size of the game window
+     * @return the game window
+     */
+    public GameWindow size(Vector2i size) {
+        glfwSetWindowSize(this.glfwWindow, size.x, size.y);
+        this.size = size;
+        return this;
+    }
+
+    /**
+     * Returns the position of the game window.
+     *
+     * @return the position of the game window
+     */
+    public Vector2i position() {
+        return this.position;
+    }
+
+    /**
+     * Sets the position of the game window.
+     *
+     * @param position the new position of the game window
+     * @return the game window
+     */
+    public GameWindow position(Vector2i position) {
+        glfwSetWindowPos(this.glfwWindow, position.x, position.y);
+        this.position = position;
+        return this;
+    }
+
+    /**
+     * Returns the debug state of the game window.
+     *
+     * @return the debug state of the game window
+     */
+    public boolean debug() {
+        return this.debug;
+    }
+
+    /**
+     * Sets the debug state of the game window.
+     *
+     * @param debug the new debug state of the game window
+     * @return the game window
+     */
+    public GameWindow debug(boolean debug) {
+        if (!this.debug && debug) {
+            glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+        } else if (this.debug && !debug) {
+            glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_FALSE);
+        }
+        this.debug = debug;
+        return this;
+    }
+
+    /**
+     * Returns the visibility state of the game window.
+     *
+     * @return the visibility state of the game window
+     */
+    public boolean visible() {
+        return this.visible;
+    }
+
+    /**
+     * Sets the visibility state of the game window.
+     *
+     * @param visible the new visibility state of the game window
+     * @return the game window
+     */
+    public GameWindow visible(boolean visible) {
+        if (this.visible && !visible) {
+            glfwHideWindow(this.glfwWindow);
+        } else if (!this.visible && visible) {
+            glfwShowWindow(this.glfwWindow);
+        }
+        this.visible = visible;
+        return this;
+    }
+
+    /**
+     * Returns the VSync state of the game window.
+     *
+     * @return the VSync state of the game window
+     */
+    public boolean vsync() {
+        return this.vsync;
+    }
+
+    /**
+     * Sets the VSync state of the game window.
+     *
+     * @param vsync the new VSync state of the game window
+     * @return the game window
+     */
+    public GameWindow vsync(boolean vsync) {
+        if (this.vsync && !vsync) {
+            glfwSwapInterval(0);
+        } else if (!this.vsync && vsync) {
+            glfwSwapInterval(1);
+        }
+        this.vsync = vsync;
+        return this;
+    }
+
+    /**
+     * Returns the frame rate of the game window.
+     *
+     * @return the frame rate of the game window
+     */
+    public long frameRate() {
+        return this.frameRate;
+    }
+
+    /**
+     * Sets the frame rate of the game window.
+     *
+     * @param frameRate the new frame rate of the game window
+     * @return the game window
+     */
+    public GameWindow frameRate(long frameRate) {
+        this.frameRate = frameRate;
+        return this;
+    }
+
+    /**
+     * Returns the tick rate of the game window.
+     *
+     * @return the tick rate of the game window
+     */
+    public long tickRate() {
+        return this.tickRate;
+    }
+
+    /**
+     * Sets the tick rate of the game window.
+     *
+     * @param tickRate the new tick rate of the game window
+     * @return the game window
+     */
+    public GameWindow tickRate(long tickRate) {
+        this.tickRate = tickRate;
+        return this;
+    }
+
+    /**
+     * Starts the game window.
+     */
+    public void start() {
+        this._init();
+    }
+
+    /**
+     * Returns the raw mouse input state of the game window.
+     *
+     * @return the raw mouse input state of the game window
+     */
+    public boolean rawMouseInput() {
+        return this.rawMouseInput;
+    }
+
+    /**
+     * Sets the raw mouse input state of the game window.
+     *
+     * @param rawMouseInput the new raw mouse input state of the game window
+     * @return the game window
+     */
+    public GameWindow rawMouseInput(boolean rawMouseInput) {
+        if (this.rawMouseInput && !rawMouseInput) {
+            glfwSetInputMode(this.glfwWindow, GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
+        } else if (!this.rawMouseInput && rawMouseInput) {
+            if (glfwRawMouseMotionSupported()) {
+                glfwSetInputMode(this.glfwWindow, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+            } else {
+                throw new GLFWException("Raw mouse input is not supported on this system");
+            }
+        }
+        this.rawMouseInput = rawMouseInput;
+        return this;
+    }
+
+    /**
+     * Returns the resizable state of the game window.
+     *
+     * @return the resizable state of the game window
+     */
+    public boolean resizable() {
+        return this.resizable;
+    }
+
+    /**
+     * Sets the resizable state of the game window.
+     *
+     * @param resizable the new resizable state of the game window
+     * @return the game window
+     */
+    public GameWindow resizable(boolean resizable) {
+        if (this.resizable && !resizable) {
+            glfwSetWindowAttrib(this.glfwWindow, GLFW_RESIZABLE, GLFW_FALSE);
+        } else if (!this.resizable && resizable) {
+            glfwSetWindowAttrib(this.glfwWindow, GLFW_RESIZABLE, GLFW_TRUE);
+        }
+        this.resizable = resizable;
+        return this;
+    }
+}
