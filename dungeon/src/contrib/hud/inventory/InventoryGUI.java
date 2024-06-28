0b20d210f4aad0038aea6619463d1be34ff303b2
Flamtky <niklas.schumann+github@proton.me>
2024-06-28 14:54:10 +0200

    fixed tooltips | add quick item transfer | hide empty titles

--- dungeon/src/contrib/hud/inventory/InventoryGUI.java
+++ dungeon/src/contrib/hud/inventory/InventoryGUI.java
@@ -1,7 +1,6 @@
 package contrib.hud.inventory;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -15,7 +14,10 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import contrib.components.InventoryComponent;
+import contrib.components.UIComponent;
 import contrib.configuration.KeyboardConfig;
+import contrib.hud.UIUtils;
+import contrib.hud.crafting.CraftingGUI;
 import contrib.hud.elements.CombinableGUI;
 import contrib.hud.elements.GUICombination;
 import contrib.item.Item;
@@ -232,23 +234,30 @@ public class InventoryGUI extends CombinableGUI {
     Item item = InventoryGUI.this.inventoryComponent.get(hoveredSlot);
     if (item == null) return;
 
-    GlyphLayout layoutName = new GlyphLayout(bitmapFont, item.displayName());
-    GlyphLayout layoutDesc = new GlyphLayout(bitmapFont, item.description());
+    String title = item.displayName();
+    String description = UIUtils.formatString(item.description());
+    GlyphLayout layoutName = new GlyphLayout(bitmapFont, title);
+    GlyphLayout layoutDesc = new GlyphLayout(bitmapFont, description);
 
     float x = mousePos.x + HOVER_OFFSET.x;
     float y = mousePos.y + HOVER_OFFSET.y;
     float width = Math.max(layoutName.width, layoutDesc.width) + HOVER_OFFSET.x;
     float height = layoutName.height + layoutDesc.height + HOVER_OFFSET.y + LINE_GAP;
+
+    // if out of bounds, move to the left of cursor
+    if (x + width > Gdx.graphics.getWidth()) {
+      x = mousePos.x - width - HOVER_OFFSET.x;
+    }
+
     batch.draw(hoverBackground, x, y, width, height);
     bitmapFont.setColor(Color.BLACK);
     bitmapFont.draw(
         batch,
-        item.displayName(),
+        title,
         x + BORDER_PADDING,
-        y + layoutName.height + LINE_GAP + layoutDesc.height + 5);
+        y + layoutDesc.height + LINE_GAP + layoutName.height + LINE_GAP);
     bitmapFont.setColor(new Color(0x000000b0));
-    bitmapFont.draw(
-        batch, item.description(), x + BORDER_PADDING, y + layoutName.height + LINE_GAP);
+    bitmapFont.draw(batch, description, x + BORDER_PADDING, y + layoutDesc.height + LINE_GAP);
   }
 
   @Override
@@ -347,14 +356,49 @@ public class InventoryGUI extends CombinableGUI {
         .addListener(
             new InputListener() {
               @Override
-              public boolean keyTyped(InputEvent event, char character) {
-                if (inHeroInventory
-                    && (Character.toLowerCase(character)
-                        == Input.Keys.toString(KeyboardConfig.USE_ITEM.value())
-                            .toLowerCase()
-                            .toCharArray()[0])) {
-                  useItem(InventoryGUI.this.inventoryComponent.get(getSlotByMousePosition()));
-                  return true;
+              public boolean keyDown(InputEvent event, int keycode) {
+                if (inHeroInventory) {
+                  if (KeyboardConfig.USE_ITEM.value() == keycode) {
+                    InventoryGUI.this.useItem(
+                        InventoryGUI.this.inventoryComponent.get(
+                            InventoryGUI.this.getSlotByMousePosition()));
+                    return true;
+                  }
+                }
+                return false;
+              }
+
+              @Override
+              public boolean touchDown(
+                  InputEvent event, float x, float y, int pointer, int button) {
+                if (inHeroInventory) return false;
+
+                UIComponent uiComponent =
+                    Game.hero().flatMap(e -> e.fetch(UIComponent.class)).orElse(null);
+                if (uiComponent != null
+                    && uiComponent.dialog() instanceof GUICombination guiCombination) {
+                  // if two inventories are open, transfer items between them if key is pressed
+                  if (KeyboardConfig.TRANSFER_ITEM.value() == button) {
+                    int slot = InventoryGUI.this.getSlotByMousePosition();
+                    Item item = InventoryGUI.this.inventoryComponent.get(slot);
+                    if (item != null) {
+                      guiCombination
+                          .combinableGuis()
+                          .forEach(
+                              gui -> {
+                                if (gui instanceof InventoryGUI inventoryGui) {
+                                  if (inventoryGui != InventoryGUI.this) {
+                                    InventoryGUI.this.inventoryComponent.transfer(
+                                        item, inventoryGui.inventoryComponent);
+                                  }
+                                } else if (gui instanceof CraftingGUI craftingGui) {
+                                  craftingGui.addItem(item);
+                                  InventoryGUI.this.inventoryComponent.remove(item);
+                                }
+                              });
+                    }
+                    return true;
+                  }
                 }
                 return false;
               }
