1fa4b78325ac63e2577bbcbd55ac2dbbf6bd32b1
Finn Watermann <finn.watermann@hsbi.de>
2024-07-03 02:39:20 +0200

    Reformat

--- engine/src/main/java/de/fwatermann/dungine/graphics/mesh/InstancedIndexedMesh.java
+++ engine/src/main/java/de/fwatermann/dungine/graphics/mesh/InstancedIndexedMesh.java
@@ -1,3 +1,38 @@
 package de.fwatermann.dungine.graphics.mesh;
 
-public class InstancedIndexedMesh extends Mesh  {}
+import de.fwatermann.dungine.graphics.GLUsageHint;
+import de.fwatermann.dungine.graphics.shader.ShaderProgram;
+import java.nio.FloatBuffer;
+
+public class InstancedIndexedMesh extends Mesh {
+
+  public InstancedIndexedMesh(GLUsageHint usageHint, VertexAttribute... attributes) {
+    super(usageHint, attributes);
+  }
+
+  @Override
+  public int getVertexCount() {
+    return 0;
+  }
+
+  @Override
+  public FloatBuffer getVertexBuffer() {
+    return null;
+  }
+
+  @Override
+  public void setVertexBuffer(FloatBuffer buffer) {}
+
+  @Override
+  protected void calcBoundingBox() {}
+
+  @Override
+  protected void calcTransformMatrix() {}
+
+  @Override
+  public void render(
+      ShaderProgram shaderProgram, int primitiveType, int offset, int count, boolean bindShader) {}
+
+  @Override
+  public void dispose() {}
+}
--- engine/src/main/java/de/fwatermann/dungine/utils/MonitorUtils.java
+++ engine/src/main/java/de/fwatermann/dungine/utils/MonitorUtils.java
@@ -9,24 +9,24 @@ import org.lwjgl.glfw.GLFWVidMode;
 
 public class MonitorUtils {
 
-    /**
-     * Retrieves the resolution of the default monitor.
-     *
-     * <p>This method first gets the primary monitor using the glfwGetPrimaryMonitor() function. Then,
-     * it retrieves the video mode of the primary monitor using the glfwGetVideoMode() function. If
-     * the video mode is null, it throws a GLFWException indicating that the video mode of the primary
-     * monitor could not be retrieved. If the video mode is not null, it creates a new Vector2i
-     * instance with the width and height of the video mode and returns it.
-     *
-     * @return a Vector2i instance representing the resolution of the default monitor.
-     * @throws GLFWException if the video mode of the primary monitor could not be retrieved.
-     */
-    public static Vector2i getDefaultMonitorResolution() {
-        long monitor = glfwGetPrimaryMonitor();
-        GLFWVidMode mode = glfwGetVideoMode(monitor);
-        if (mode == null) {
-            throw new GLFWException("Failed to get video mode of primary monitor");
-        }
-        return new Vector2i(mode.width(), mode.height());
+  /**
+   * Retrieves the resolution of the default monitor.
+   *
+   * <p>This method first gets the primary monitor using the glfwGetPrimaryMonitor() function. Then,
+   * it retrieves the video mode of the primary monitor using the glfwGetVideoMode() function. If
+   * the video mode is null, it throws a GLFWException indicating that the video mode of the primary
+   * monitor could not be retrieved. If the video mode is not null, it creates a new Vector2i
+   * instance with the width and height of the video mode and returns it.
+   *
+   * @return a Vector2i instance representing the resolution of the default monitor.
+   * @throws GLFWException if the video mode of the primary monitor could not be retrieved.
+   */
+  public static Vector2i getDefaultMonitorResolution() {
+    long monitor = glfwGetPrimaryMonitor();
+    GLFWVidMode mode = glfwGetVideoMode(monitor);
+    if (mode == null) {
+      throw new GLFWException("Failed to get video mode of primary monitor");
     }
+    return new Vector2i(mode.width(), mode.height());
+  }
 }
