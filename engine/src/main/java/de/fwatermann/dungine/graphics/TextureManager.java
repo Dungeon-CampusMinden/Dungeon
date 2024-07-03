4c9da5e1d9b5691c8f60e07f91af777781e7a972
Finn Watermann <finn.watermann@hsbi.de>
2024-07-03 13:30:11 +0200

    Add load Texture from Resource + mark other methods as deprecated.

--- engine/src/main/java/de/fwatermann/dungine/graphics/TextureManager.java
+++ engine/src/main/java/de/fwatermann/dungine/graphics/TextureManager.java
@@ -1,5 +1,6 @@
 package de.fwatermann.dungine.graphics;
 
+import de.fwatermann.dungine.utils.resource.Resource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -29,11 +30,37 @@ public class TextureManager {
     return instance;
   }
 
-  private final HashMap<String, Texture> resourceCache = new HashMap<>();
-
+  private final HashMap<Resource, Texture> resourceCache = new HashMap<>();
+  private final HashMap<String, Texture> classPathCache = new HashMap<>();
   private final HashMap<String, Texture> fileCache = new HashMap<>();
 
   private TextureManager() {}
+
+  /**
+   * Loads a texture from a resource and caches it. If the texture has already been loaded, the
+   * cached texture is returned.
+   *
+   * @param resource the Resource that contains the texture data
+   * @return the loaded texture
+   * @throws RuntimeException if the image fails to load or if an I/O error occurs
+   */
+  public Texture load(Resource resource) {
+    return this.resourceCache.computeIfAbsent(resource, k -> {
+      try {
+        ByteBuffer bytes = resource.readBytes();
+        int[] width = new int[1];
+        int[] height = new int[1];
+        int[] channels = new int[1];
+        ByteBuffer pixels = STBImage.stbi_load_from_memory(bytes, width, height, channels, 4);
+        if(pixels == null) {
+          throw new RuntimeException("Failed to load image: " + STBImage.stbi_failure_reason());
+        }
+        return new Texture(width[0], height[0], pixels);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
 
   /**
    * Loads a texture from a resource and caches it. If the texture has already been loaded, the
@@ -44,8 +71,9 @@ public class TextureManager {
    * @throws RuntimeException if the resource is not found, if the image fails to load, or if an I/O
    *     error occurs
    */
-  public Texture textureFromResource(String resourcePath) {
-    return this.resourceCache.computeIfAbsent(
+  @Deprecated
+  public Texture loadFromClassPath(String resourcePath) {
+    return this.classPathCache.computeIfAbsent(
         resourcePath,
         p -> {
           try {
@@ -84,7 +112,8 @@ public class TextureManager {
    * @return the loaded texture
    * @throws RuntimeException if the image fails to load or if an I/O error occurs
    */
-  public Texture textureFromFile(String path) {
+  @Deprecated
+  public Texture loadFromFile(String path) {
     return this.fileCache.computeIfAbsent(
         path,
         p -> {
@@ -117,6 +146,7 @@ public class TextureManager {
    * @param path the path of the texture
    * @return the removed texture, or null if the texture was not in the cache
    */
+  @Deprecated
   public Texture removeFromFileCache(String path) {
     return this.fileCache.remove(path);
   }
@@ -127,7 +157,18 @@ public class TextureManager {
    * @param path the path of the texture
    * @return the removed texture, or null if the texture was not in the cache
    */
-  public Texture removeFromResourceCache(String path) {
-    return this.resourceCache.remove(path);
+  @Deprecated
+  public Texture removeFromClassPathCache(String path) {
+    return this.classPathCache.remove(path);
   }
+
+  /**
+   * Removes the texture from the resource cache.
+   * @param resource the resource to remove
+   * @return the removed texture, or null if the texture was not in the cache
+   */
+  public Texture removeFromResourceCache(Resource resource) {
+    return this.resourceCache.remove(resource);
+  }
+
 }
