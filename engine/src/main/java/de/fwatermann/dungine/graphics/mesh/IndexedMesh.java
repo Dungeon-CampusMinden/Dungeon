f4f5bc36a404fdc15d5a64af349907b353461eab
Finn Watermann <finn.watermann@hsbi.de>
2024-07-03 02:38:11 +0200

    Implement ArrayMesh & IndexedMesh

--- engine/src/main/java/de/fwatermann/dungine/graphics/mesh/IndexedMesh.java
+++ engine/src/main/java/de/fwatermann/dungine/graphics/mesh/IndexedMesh.java
@@ -1,3 +1,296 @@
 package de.fwatermann.dungine.graphics.mesh;
 
-public class IndexedMesh extends Mesh {}
+import de.fwatermann.dungine.graphics.GLUsageHint;
+import de.fwatermann.dungine.graphics.shader.ShaderProgram;
+import de.fwatermann.dungine.utils.BoundingBox;
+import de.fwatermann.dungine.utils.ThreadUtils;
+import de.fwatermann.dungine.utils.annotations.Nullable;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL33;
+
+/**
+ * Represents an indexed mesh, which is a collection of vertices and indices defining the geometry
+ * of a 3D object. This class extends the Mesh class, providing functionality specific to indexed
+ * meshes, such as managing vertex and index buffers, and rendering the mesh using OpenGL.
+ */
+public class IndexedMesh extends Mesh {
+
+  private int glVAO;
+  private int glVBO;
+  private int glEBO;
+
+  @Nullable private FloatBuffer vertices;
+
+  @Nullable private IntBuffer indices;
+
+  private boolean verticesDirty = false;
+  private boolean indicesDirty = false;
+
+  private ShaderProgram lastShaderProgarm = null;
+
+  /**
+   * Constructs an IndexedMesh with specified vertices, indices, usage hint, and vertex attributes.
+   *
+   * @param vertices The buffer containing the mesh's vertices.
+   * @param indices The buffer containing the mesh's indices.
+   * @param usageHint Hint indicating how the mesh will be used, which affects performance
+   *     optimizations.
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(
+      FloatBuffer vertices,
+      IntBuffer indices,
+      GLUsageHint usageHint,
+      VertexAttributeList attributes) {
+    super(usageHint, attributes);
+    if (vertices.order() != ByteOrder.nativeOrder()) {
+      throw new IllegalArgumentException("VertexBuffer must be in native byte order!");
+    }
+    if (indices.order() != ByteOrder.nativeOrder()) {
+      throw new IllegalArgumentException("IndexBuffer must be in native byte order!");
+    }
+    this.vertices = vertices;
+    this.indices = indices;
+    this.indicesDirty = true;
+    this.verticesDirty = true;
+    this.initGL();
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified vertices, indices, usage hint, and vertex attributes.
+   *
+   * @param vertices The buffer containing the mesh's vertices.
+   * @param indices The buffer containing the mesh's indices.
+   * @param usageHint Hint indicating how the mesh will be used, which affects performance
+   *     optimizations.
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(
+      FloatBuffer vertices,
+      IntBuffer indices,
+      GLUsageHint usageHint,
+      VertexAttribute... attributes) {
+    this(vertices, indices, usageHint, new VertexAttributeList(attributes));
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified vertices, indices, and vertex attributes.
+   *
+   * @param vertices The buffer containing the mesh's vertices.
+   * @param indices The buffer containing the mesh's indices.
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(FloatBuffer vertices, IntBuffer indices, VertexAttributeList attributes) {
+    this(vertices, indices, GLUsageHint.DRAW_STATIC, attributes);
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified vertices, indices, and vertex attributes.
+   *
+   * @param vertices The buffer containing the mesh's vertices.
+   * @param indices The buffer containing the mesh's indices.
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(FloatBuffer vertices, IntBuffer indices, VertexAttribute... attributes) {
+    this(vertices, indices, GLUsageHint.DRAW_STATIC, new VertexAttributeList(attributes));
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified usage hint and vertex attributes.
+   *
+   * @param usageHint Hint indicating how the mesh will be used, which affects performance
+   *     optimizations.
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(GLUsageHint usageHint, VertexAttributeList attributes) {
+    this(null, null, usageHint, attributes);
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified usage hint and vertex attributes.
+   *
+   * @param usageHint Hint indicating how the mesh will be used, which affects performance
+   *     optimizations.
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(GLUsageHint usageHint, VertexAttribute... attributes) {
+    this(null, null, usageHint, new VertexAttributeList(attributes));
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified vertex attributes.
+   *
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(VertexAttributeList attributes) {
+    this(null, null, GLUsageHint.DRAW_STATIC, attributes);
+  }
+
+  /**
+   * Constructs an IndexedMesh with specified vertex attributes.
+   *
+   * @param attributes The vertex attributes of the mesh.
+   */
+  public IndexedMesh(VertexAttribute... attributes) {
+    this(null, null, GLUsageHint.DRAW_STATIC, new VertexAttributeList(attributes));
+  }
+
+  private void initGL() {
+    ThreadUtils.checkMainThread();
+
+    this.glVAO = GL33.glGenVertexArrays();
+    this.glVBO = GL33.glGenBuffers();
+    this.glEBO = GL33.glGenBuffers();
+
+    GL33.glBindVertexArray(this.glVAO);
+    GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, this.glVBO);
+    GL33.glBindBuffer(GL33.GL_ELEMENT_ARRAY_BUFFER, this.glEBO);
+    GL33.glBindVertexArray(0);
+  }
+
+  /**
+   * Disposes of OpenGL resources associated with this mesh. This should be called when the mesh is
+   * no longer needed to free up resources.
+   */
+  @Override
+  public void dispose() {
+    GL33.glDeleteBuffers(this.glVBO);
+    GL33.glDeleteBuffers(this.glEBO);
+    GL33.glDeleteVertexArrays(this.glVAO);
+  }
+
+  @Override
+  public int getVertexCount() {
+    if (this.vertices == null) {
+      return 0;
+    }
+    return this.vertices.capacity() / this.attributes.sizeInBytes();
+  }
+
+  @Override
+  @Nullable
+  public FloatBuffer getVertexBuffer() {
+    return this.vertices;
+  }
+
+  @Override
+  public void setVertexBuffer(FloatBuffer buffer) {
+    if (buffer.order() != ByteOrder.nativeOrder()) {
+      throw new IllegalArgumentException("Buffer must be in native byte order!");
+    }
+    this.vertices = buffer;
+    this.verticesDirty = true;
+    this.calcBoundingBox();
+  }
+
+  /**
+   * Sets the indices of this mesh.
+   *
+   * @param buffer the new indices of this mesh
+   */
+  public void setIndexBuffer(IntBuffer buffer) {
+    if (buffer.order() != ByteOrder.nativeOrder()) {
+      throw new IllegalArgumentException("Buffer must be in native byte order!");
+    }
+    this.indices = buffer;
+    this.indicesDirty = true;
+  }
+
+  /**
+   * Returns the indices of this mesh.
+   *
+   * @return the indices of this mesh
+   */
+  public IntBuffer getIndexBuffer() {
+    return this.indices;
+  }
+
+  @Override
+  protected void calcBoundingBox() {
+    float minX = Float.MAX_VALUE;
+    float minY = Float.MAX_VALUE;
+    float minZ = Float.MAX_VALUE;
+    float maxX = Float.MIN_VALUE;
+    float maxY = Float.MIN_VALUE;
+    float maxZ = Float.MIN_VALUE;
+    this.vertices.position(0);
+    int vertexCount = this.vertices.capacity() / this.attributes.sizeInBytes();
+    for (int i = 0; i < vertexCount; i++) {
+      float x = this.vertices.get();
+      float y = this.vertices.get();
+      float z = this.vertices.get();
+      this.vertices.position(
+          this.vertices.position() + this.attributes.sizeInBytes() - 3 * Float.BYTES);
+      minX = Math.min(minX, x);
+      minY = Math.min(minY, y);
+      minZ = Math.min(minZ, z);
+      maxX = Math.max(maxX, x);
+      maxY = Math.max(maxY, y);
+      maxZ = Math.max(maxZ, z);
+    }
+    this.boundingBox = new BoundingBox(minX, minY, minZ, maxX, maxY, maxZ);
+  }
+
+  @Override
+  protected void calcTransformMatrix() {
+    this.transformMatrix =
+        new Matrix4f().translationRotateScale(this.translation, this.rotation, this.scale);
+  }
+
+  private void updateBuffers() {
+    GL33.glBindVertexArray(this.glVAO);
+    if (this.verticesDirty && this.vertices != null) {
+      GL33.glBufferData(GL33.GL_ARRAY_BUFFER, this.vertices, this.usageHint.getGLConstant());
+      this.verticesDirty = false;
+    }
+    if (this.indicesDirty && this.indices != null) {
+      this.indices.position(0);
+      GL33.glBufferData(GL33.GL_ELEMENT_ARRAY_BUFFER, this.indices, this.usageHint.getGLConstant());
+      this.indicesDirty = false;
+    }
+    GL33.glBindVertexArray(0);
+  }
+
+  @Override
+  public void render(
+      ShaderProgram shaderProgram, int primitiveType, int offset, int count, boolean bindShader) {
+    ThreadUtils.checkMainThread();
+
+    if (this.vertices == null || this.indices == null) {
+      return;
+    }
+
+    this.updateBuffers();
+
+    if (this.lastShaderProgarm != shaderProgram) {
+      GL33.glBindVertexArray(this.glVAO);
+      this.attributes.forEach(
+          attrib -> {
+            int loc = shaderProgram.getAttributeLocation(attrib.name);
+            if (loc != -1) {
+              GL33.glEnableVertexAttribArray(loc);
+              GL33.glVertexAttribPointer(
+                  loc,
+                  attrib.numComponents,
+                  attrib.glType,
+                  false,
+                  this.attributes.sizeInBytes(),
+                  attrib.offset);
+            }
+          });
+      GL33.glBindVertexArray(0);
+      this.lastShaderProgarm = shaderProgram;
+    }
+
+    if (bindShader) shaderProgram.bind();
+
+    GL33.glBindVertexArray(this.glVAO);
+    GL33.glDrawElements(primitiveType, count, GL33.GL_UNSIGNED_INT, offset);
+    GL33.glBindVertexArray(0);
+
+    if (bindShader) shaderProgram.unbind();
+  }
+}
--- engine/src/main/java/de/fwatermann/dungine/graphics/mesh/Mesh.java
+++ engine/src/main/java/de/fwatermann/dungine/graphics/mesh/Mesh.java
@@ -1,19 +1,41 @@
 package de.fwatermann.dungine.graphics.mesh;
 
-import de.fwatermann.dungine.graphics.ShaderProgram;
+import de.fwatermann.dungine.graphics.GLUsageHint;
+import de.fwatermann.dungine.graphics.shader.ShaderProgram;
 import de.fwatermann.dungine.utils.BoundingBox;
 import de.fwatermann.dungine.utils.Disposable;
+import de.fwatermann.dungine.utils.annotations.Nullable;
+import java.nio.FloatBuffer;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
-import java.nio.FloatBuffer;
-
 /**
- * The Mesh class represents a 3D mesh object in the game engine.
- * It provides methods for manipulating the mesh's position, rotation, and scale, as well as methods for rendering the mesh.
+ * The Mesh class represents a 3D mesh object in the game engine. It provides methods for
+ * manipulating the mesh's position, rotation, and scale, as well as methods for rendering the mesh.
  * This class is abstract, and should be extended by specific types of meshes.
  */
 public abstract class Mesh implements Disposable {
+
+  protected VertexAttributeList attributes;
+  protected Vector3f translation = new Vector3f(0, 0, 0);
+  protected Vector3f scale = new Vector3f(1, 1, 1);
+  protected Quaternionf rotation = new Quaternionf();
+  protected Matrix4f transformMatrix = new Matrix4f();
+
+  @Nullable protected BoundingBox boundingBox;
+
+  protected GLUsageHint usageHint = GLUsageHint.DRAW_STATIC;
+
+  public Mesh(GLUsageHint usageHint, VertexAttribute... attributes) {
+    this.attributes = new VertexAttributeList(attributes);
+    this.usageHint = usageHint;
+  }
+
+  public Mesh(GLUsageHint usageHint, VertexAttributeList attributes) {
+    this.attributes = attributes;
+    this.usageHint = usageHint;
+  }
 
   /**
    * Returns the number of vertices in the mesh.
@@ -30,11 +52,24 @@ public abstract class Mesh implements Disposable {
   public abstract FloatBuffer getVertexBuffer();
 
   /**
+   * Sets the vertex buffer of the mesh to the specified buffer.
+   *
+   * @param buffer the new vertex buffer of the mesh
+   */
+  public abstract void setVertexBuffer(FloatBuffer buffer);
+
+  /**
    * Returns the bounding box of the mesh.
    *
    * @return the bounding box of the mesh
    */
-  public abstract BoundingBox getBoundingBox();
+  @Nullable
+  public BoundingBox getBoundingBox() {
+    return this.boundingBox;
+  }
+
+  /** Calculates the bounding box of the mesh. */
+  protected abstract void calcBoundingBox();
 
   /**
    * Translates the mesh by the specified amounts along the x, y, and z axes.
@@ -43,7 +78,10 @@ public abstract class Mesh implements Disposable {
    * @param y the amount to translate along the y axis
    * @param z the amount to translate along the z axis
    */
-  public abstract void translate(float x, float y, float z);
+  public void translate(float x, float y, float z) {
+    this.translation.add(x, y, z);
+    this.calcTransformMatrix();
+  }
 
   /**
    * Sets the position of the mesh to the specified coordinates.
@@ -52,7 +90,10 @@ public abstract class Mesh implements Disposable {
    * @param y the y coordinate of the new position
    * @param z the z coordinate of the new position
    */
-  public abstract void setTranslation(float x, float y, float z);
+  public void setTranslation(float x, float y, float z) {
+    this.translation = new Vector3f(x, y, z);
+    this.calcTransformMatrix();
+  }
 
   /**
    * Rotates the mesh around the specified axis by the specified angle.
@@ -62,7 +103,10 @@ public abstract class Mesh implements Disposable {
    * @param z the z coordinate of the axis of rotation
    * @param angle the angle of rotation, in degrees
    */
-  public abstract void rotate(float x, float y, float z, float angle);
+  public void rotate(float x, float y, float z, float angle) {
+    this.rotation.rotateAxis(x, y, z, angle);
+    this.calcTransformMatrix();
+  }
 
   /**
    * Scales the mesh by the specified factors along the x, y, and z axes.
@@ -71,7 +115,10 @@ public abstract class Mesh implements Disposable {
    * @param y the scaling factor along the y axis
    * @param z the scaling factor along the z axis
    */
-  public abstract void scale(float x, float y, float z);
+  public void scale(float x, float y, float z) {
+    this.scale.mul(x, y, z);
+    this.calcTransformMatrix();
+  }
 
   /**
    * Sets the scale of the mesh to the specified values.
@@ -80,7 +127,10 @@ public abstract class Mesh implements Disposable {
    * @param y the new scale along the y axis
    * @param z the new scale along the z axis
    */
-  public abstract void setScale(float x, float y, float z);
+  public void setScale(float x, float y, float z) {
+    this.scale = new Vector3f(x, y, z);
+    this.calcTransformMatrix();
+  }
 
   /**
    * Translates the mesh by the specified vector.
@@ -89,6 +139,7 @@ public abstract class Mesh implements Disposable {
    */
   public void translate(Vector3f translation) {
     this.translate(translation.x, translation.y, translation.z);
+    this.calcTransformMatrix();
   }
 
   /**
@@ -98,6 +149,7 @@ public abstract class Mesh implements Disposable {
    */
   public void setTranslation(Vector3f translation) {
     this.setTranslation(translation.x, translation.y, translation.z);
+    this.calcTransformMatrix();
   }
 
   /**
@@ -108,6 +160,7 @@ public abstract class Mesh implements Disposable {
    */
   public void rotate(Vector3f axis, float angle) {
     this.rotate(axis.x, axis.y, axis.z, angle);
+    this.calcTransformMatrix();
   }
 
   /**
@@ -117,6 +170,7 @@ public abstract class Mesh implements Disposable {
    */
   public void scale(Vector3f scale) {
     this.scale(scale.x, scale.y, scale.z);
+    this.calcTransformMatrix();
   }
 
   /**
@@ -126,6 +180,7 @@ public abstract class Mesh implements Disposable {
    */
   public void setScale(Vector3f scale) {
     this.setScale(scale.x, scale.y, scale.z);
+    this.calcTransformMatrix();
   }
 
   /**
@@ -133,14 +188,35 @@ public abstract class Mesh implements Disposable {
    *
    * @param matrix the new transformation matrix of the mesh
    */
-  public abstract void setTransformMatrix(Matrix4f matrix);
+  public void transformMatrix(Matrix4f matrix) {
+    this.transformMatrix = matrix;
+  }
 
   /**
    * Returns the transformation matrix of the mesh.
    *
    * @return the transformation matrix of the mesh
    */
-  public abstract Matrix4f getTransformMatrix();
+  public Matrix4f transformMatrix() {
+    return this.transformMatrix;
+  }
+
+  /**
+   * Calculates the transformation matrix of the mesh. This method is abstract and must be
+   * implemented by subclasses to define how the transformation matrix is updated. The
+   * transformation matrix is typically used to apply transformations such as translation, rotation,
+   * and scaling to the mesh.
+   */
+  protected abstract void calcTransformMatrix();
+
+  /**
+   * Returns the usage hint of the mesh.
+   *
+   * @return the usage hint of the mesh
+   */
+  public GLUsageHint usageHint() {
+    return this.usageHint;
+  }
 
   /**
    * Renders the mesh using the specified shader program and primitive type.
@@ -165,7 +241,8 @@ public abstract class Mesh implements Disposable {
   }
 
   /**
-   * Renders a portion of the mesh using the specified shader program and primitive type, with an option to bind the shader.
+   * Renders a portion of the mesh using the specified shader program and primitive type, with an
+   * option to bind the shader.
    *
    * @param shaderProgram the shader program to use for rendering
    * @param primitiveType the primitive type to use for rendering
