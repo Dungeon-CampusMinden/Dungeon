63efcf30c8b072a9e178a99c1962c61e0321bee2
Finn Watermann <finn.watermann@hsbi.de>
2024-07-03 13:18:45 +0200

    Add Dungine Class for global Variables & Functions

---
+++ engine/src/main/java/de/fwatermann/dungine/Dungine.java
@@ -0,0 +1,12 @@
+package de.fwatermann.dungine;
+
+import de.fwatermann.dungine.window.GameWindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Dungine {
+
+  public static Set<GameWindow> WINDOWS = new HashSet<>();
+
+}
