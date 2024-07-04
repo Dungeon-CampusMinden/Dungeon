47fe47508e2fb2b4a0e0d5e755058dfcc4db1324
Finn Watermann <finn.watermann@hsbi.de>
2024-07-04 11:31:44 +0200

    Use Then-Obj on runOnMainThreadQueue

---
+++ engine/src/test/java/LoadingScreenTransition.java
@@ -0,0 +1,23 @@
+import de.fwatermann.dungine.state.GameStateTransition;
+import de.fwatermann.dungine.window.GameWindow;
+
+public class LoadingScreenTransition extends GameStateTransition {
+
+  public LoadingScreenTransition(GameWindow window) {
+    super(window);
+  }
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void render(float deltaTime) {
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+}
