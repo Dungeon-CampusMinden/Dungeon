ac86f5623f9f66188653189f020fbcb55945b646
Finn Watermann <finn.watermann@hsbi.de>
2024-07-04 21:49:35 +0200

    Make Camera class abstract + add CameraOrthographic & CameraPerspective

---
+++ engine/src/main/java/de/fwatermann/dungine/graphics/camera/CameraOrthographic.java
@@ -0,0 +1,167 @@
+package de.fwatermann.dungine.graphics.camera;
+
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
+public class CameraOrthographic extends Camera<CameraOrthographic> {
+
+  private float viewportWidth;
+  private float viewportHeight;
+  private float zoom = 1.0f;
+
+  private final CameraFrustum frustumIntersection;
+
+  /**
+   * Constructs a new Camera instance.
+   *
+   * @param viewportWidth The width of the camera's viewport.
+   * @param viewportHeight The height of the camera's viewport.
+   * @param zoom The zoom level of the camera.
+   * @param position The initial position of the camera.
+   * @param front The initial front vector of the camera, indicating the direction it is facing.
+   * @param up The initial up vector of the camera.
+   * @param updateOnChange If true, the camera will automatically update its view matrix when its
+   *     state changes.
+   */
+  public CameraOrthographic(
+      float viewportWidth,
+      float viewportHeight,
+      float zoom,
+      Vector3f position,
+      Vector3f front,
+      Vector3f up,
+      boolean updateOnChange) {
+    super(position, front, up, updateOnChange);
+    this.viewportWidth = viewportWidth;
+    this.viewportHeight = viewportHeight;
+    this.zoom = zoom;
+    this.frustumIntersection = new CameraFrustum();
+    this._update(true);
+  }
+
+  /**
+   * Constructs a new Camera instance.
+   *
+   * @param viewportWidth The width of the camera's viewport.
+   * @param viewportHeight The height of the camera's viewport.
+   * @param zoom The zoom level of the camera.
+   * @param updateOnChange If true, the camera will automatically update its view matrix when its
+   *     state changes.
+   */
+  public CameraOrthographic(
+      float viewportWidth, float viewportHeight, float zoom, boolean updateOnChange) {
+    this(
+        viewportWidth,
+        viewportHeight,
+        zoom,
+        new Vector3f(),
+        new Vector3f(0, 0, -1),
+        new Vector3f(0, 1, 0),
+        updateOnChange);
+  }
+
+  /**
+   * Constructs a new Camera instance.
+   *
+   * @param viewportWidth The width of the camera's viewport.
+   * @param viewportHeight The height of the camera's viewport.
+   * @param zoom The zoom level of the camera.
+   */
+  public CameraOrthographic(float viewportWidth, float viewportHeight, float zoom) {
+    this(viewportWidth, viewportHeight, zoom, true);
+  }
+
+  /**
+   * Constructs a new Camera instance.
+   *
+   * @param viewportWidth The width of the camera's viewport.
+   * @param viewportHeight The height of the camera's viewport.
+   */
+  public CameraOrthographic(float viewportWidth, float viewportHeight) {
+    this(viewportWidth, viewportHeight, 1.0f, true);
+  }
+
+  @Override
+  protected Matrix4f calcProjectionMatrix(Matrix4f projectionMatrix) {
+    return projectionMatrix.setOrtho(
+        this.zoom * -this.viewportWidth / 2,
+        this.zoom * this.viewportWidth / 2,
+        this.zoom * -this.viewportHeight / 2,
+        this.zoom * this.viewportHeight / 2,
+        -1,
+        1);
+  }
+
+  @Override
+  protected void onUpdate() {
+    if (this.frustumIntersection != null) {
+      this.frustumIntersection.set(this.viewMatrix.mul(this.projectionMatrix, new Matrix4f()));
+    }
+  }
+
+  /**
+   * Returns the current width of the viewport.
+   *
+   * @return The width of the viewport.
+   */
+  public float viewportWidth() {
+    return this.viewportWidth;
+  }
+
+  /**
+   * Sets the width of the viewport and returns the updated camera instance. This method allows for
+   * chaining.
+   *
+   * @param viewportWidth The new width to set for the viewport.
+   * @return The updated OrthographicCamera instance.
+   */
+  public CameraOrthographic viewportWidth(float viewportWidth) {
+    this.viewportWidth = viewportWidth;
+    this._update(false);
+    return this;
+  }
+
+  /**
+   * Returns the current height of the viewport.
+   *
+   * @return The height of the viewport.
+   */
+  public float viewportHeight() {
+    return this.viewportHeight;
+  }
+
+  /**
+   * Sets the height of the viewport and returns the updated camera instance. This method allows for
+   * chaining.
+   *
+   * @param viewportHeight The new height to set for the viewport.
+   * @return The updated OrthographicCamera instance.
+   */
+  public CameraOrthographic viewportHeight(float viewportHeight) {
+    this.viewportHeight = viewportHeight;
+    this._update(false);
+    return this;
+  }
+
+  /**
+   * Returns the current zoom level of the camera.
+   *
+   * @return The zoom level.
+   */
+  public float zoom() {
+    return this.zoom;
+  }
+
+  /**
+   * Sets the zoom level of the camera and returns the updated camera instance. This method allows
+   * for chaining.
+   *
+   * @param zoom The new zoom level to set.
+   * @return The updated OrthographicCamera instance.
+   */
+  public CameraOrthographic zoom(float zoom) {
+    this.zoom = zoom;
+    this._update(false);
+    return this;
+  }
+}
---
+++ engine/src/main/java/de/fwatermann/dungine/graphics/camera/CameraPerspective.java
@@ -0,0 +1,213 @@
+package de.fwatermann.dungine.graphics.camera;
+
+import org.joml.FrustumIntersection;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.joml.Math;
+
+/**
+ * Represents a camera in a 3D space, allowing for movement and orientation adjustments. The
+ * camera's view and projection matrices can be updated based on its position, orientation, and
+ * perspective settings.
+ */
+public class CameraPerspective extends Camera<CameraPerspective> {
+
+  private float fov;
+  private float aspectRatio;
+  private float near;
+  private float far;
+
+  private FrustumIntersection frustumIntersection;
+
+  /**
+   * Constructs a Camera with specified perspective settings.
+   *
+   * @param position The position of the camera.
+   * @param front The direction the camera is facing.
+   * @param up The up vector of the camera.
+   * @param fov The field of view angle in degrees.
+   * @param aspectRatio The aspect ratio (width / height) of the camera.
+   * @param near The distance to the near clipping plane.
+   * @param far The distance to the far clipping plane.
+   * @param updateOnChange Whether the camera should update its view and projection matrices when
+   */
+  public CameraPerspective(
+      Vector3f position,
+      Vector3f front,
+      Vector3f up,
+      float fov,
+      float aspectRatio,
+      float near,
+      float far,
+      boolean updateOnChange) {
+    super(position, front, up, updateOnChange);
+    this.fov = fov;
+    this.aspectRatio = aspectRatio;
+    this.near = near;
+    this.far = far;
+    this._update(true);
+  }
+
+  /**
+   * Constructs a Camera with specified perspective settings.
+   *
+   * @param position The position of the camera.
+   * @param front The direction the camera is facing.
+   * @param up The up vector of the camera.
+   * @param fov The field of view angle in degrees.
+   * @param aspectRatio The aspect ratio (width / height) of the camera.
+   * @param near The distance to the near clipping plane.
+   * @param far The distance to the far clipping plane.
+   */
+  public CameraPerspective(
+      Vector3f position,
+      Vector3f front,
+      Vector3f up,
+      float fov,
+      float aspectRatio,
+      float near,
+      float far) {
+    this(position, front, up, fov, aspectRatio, near, far, false);
+  }
+
+  /**
+   * Constructs a Camera with specified perspective settings.
+   *
+   * @param position The position of the camera.
+   * @param fov The field of view angle in degrees.
+   * @param aspectRatio The aspect ratio (width / height) of the camera.
+   * @param near The distance to the near clipping plane.
+   * @param far The distance to the far clipping plane.
+   */
+  public CameraPerspective(Vector3f position, float fov, float aspectRatio, float near, float far) {
+    this(
+        position,
+        new Vector3f(0.0f, 0.0f, -1.0f),
+        new Vector3f(0.0f, 1.0f, 0.0f),
+        fov,
+        aspectRatio,
+        near,
+        far);
+  }
+
+  /**
+   * Constructs a Camera with specified perspective settings.
+   *
+   * @param fov The field of view angle in degrees.
+   * @param aspectRatio The aspect ratio (width / height) of the camera.
+   * @param near The distance to the near clipping plane.
+   * @param far The distance to the far clipping plane.
+   */
+  public CameraPerspective(float fov, float aspectRatio, float near, float far) {
+    this(new Vector3f(0.0f, 0.0f, 0.0f), fov, aspectRatio, near, far);
+  }
+
+  /**
+   * Constructs a Camera with default perspective settings.
+   *
+   * <p>The default settings are as follows:
+   *
+   * <ul>
+   *   <li>Position: (0.0, 0.0, 0.0)
+   *   <li>Front: (0.0, 0.0, -1.0)
+   *   <li>Up: (0.0, 1.0, 0.0)
+   *   <li>Field of View: 80.0 degrees
+   *   <li>Aspect Ratio: 16:9
+   *   <li>Near Clipping Plane: 0.1
+   *   <li>Far Clipping Plane: 100.0
+   *   <li>Update on Change: false
+   * </ul>
+   */
+  public CameraPerspective() {
+    this(80.0f, 16.0f / 9.0f, 0.1f, 100.0f);
+  }
+
+  @Override
+  protected Matrix4f calcProjectionMatrix(Matrix4f projectionMatrix) {
+    return projectionMatrix.setPerspective(Math.toRadians(this.fov), this.aspectRatio, this.near, this.far);
+  }
+
+  /**
+   * Get the field of view angle of the camera.
+   *
+   * @return the field of view angle
+   */
+  public float fov() {
+    return this.fov;
+  }
+
+  /**
+   * Set the field of view angle of the camera.
+   *
+   * @param fov the new field of view angle
+   * @return this camera
+   */
+  public CameraPerspective fov(float fov) {
+    this.fov = fov;
+    this._update(false);
+    return this;
+  }
+
+  /**
+   * Get the aspect ratio of the camera.
+   *
+   * @return the aspect ratio
+   */
+  public float aspectRatio() {
+    return this.aspectRatio;
+  }
+
+  /**
+   * Set the aspect ratio of the camera.
+   *
+   * @param aspectRatio the new aspect ratio
+   * @return this camera
+   */
+  public CameraPerspective aspectRatio(float aspectRatio) {
+    this.aspectRatio = aspectRatio;
+    this._update(false);
+    return this;
+  }
+
+  /**
+   * Get the distance to the near clipping plane of the camera.
+   *
+   * @return the distance to the near clipping plane
+   */
+  public float nearPlane() {
+    return this.near;
+  }
+
+  /**
+   * Set the distance to the near clipping plane of the camera.
+   *
+   * @param nearPlane the new distance to the near clipping plane
+   * @return this camera
+   */
+  public CameraPerspective nearPlane(float nearPlane) {
+    this.near = nearPlane;
+    this._update(false);
+    return this;
+  }
+
+  /**
+   * Get the distance to the far clipping plane of the camera.
+   *
+   * @return the distance to the far clipping plane
+   */
+  public float farPlane() {
+    return this.far;
+  }
+
+  /**
+   * Set the distance to the far clipping plane of the camera.
+   *
+   * @param farPlane the new distance to the far clipping plane
+   * @return this camera
+   */
+  public CameraPerspective farPlane(float farPlane) {
+    this.far = farPlane;
+    this._update(false);
+    return this;
+  }
+}
