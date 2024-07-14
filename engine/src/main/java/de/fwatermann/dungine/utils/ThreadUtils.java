5f47dc29ec49662f34c296362c53d651928ee08c
Finn Watermann <finn.watermann@hsbi.de>
2024-07-14 14:02:10 +0200

    Update ThreadUtils class

--- engine/src/main/java/de/fwatermann/dungine/utils/ThreadUtils.java
+++ engine/src/main/java/de/fwatermann/dungine/utils/ThreadUtils.java
@@ -5,9 +5,14 @@ import de.fwatermann.dungine.window.GameWindow;
 public class ThreadUtils {
 
   public static void checkMainThread() {
-    if (Thread.currentThread() != GameWindow.MAIN_THREAD) {
+    if (!isMainThread()) {
       throw new IllegalStateException(
           "This method can only be called from the main thread (aka. Render-/Input-Thread)");
     }
   }
+
+  public static boolean isMainThread() {
+    return Thread.currentThread() == GameWindow.MAIN_THREAD;
+  }
+
 }
