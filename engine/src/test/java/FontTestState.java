2857895679216af03a65af6b88629405fb552c8a
Finn Watermann <finn.watermann@hsbi.de>
2024-08-06 17:32:52 +0200

    Add testing classes for font

---
+++ engine/src/test/java/FontTestState.java
@@ -0,0 +1,39 @@
+import de.fwatermann.dungine.graphics.text.Font;
+import de.fwatermann.dungine.resource.Resource;
+import de.fwatermann.dungine.state.GameState;
+import de.fwatermann.dungine.window.GameWindow;
+
+import java.io.IOException;
+
+public class FontTestState extends GameState {
+
+
+  /**
+   * Create a new game state.
+   *
+   * @param window the game window
+   */
+  protected FontTestState(GameWindow window) {
+    super(window);
+  }
+
+  @Override
+  public void init() {
+    try {
+      Font font = Font.load(Resource.load("/fonts/opensans.ttf"), 24);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    this.window.close();
+  }
+
+  @Override
+  public boolean loaded() {
+    return true;
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+}
