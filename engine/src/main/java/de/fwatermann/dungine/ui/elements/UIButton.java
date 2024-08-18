77e00c16b555d043f40bbb35ca0009b7d411dbcb
Finn Watermann <finn.watermann@hsbi.de>
2024-08-19 00:58:01 +0200

    Add UIButton class

---
+++ engine/src/main/java/de/fwatermann/dungine/ui/elements/UIButton.java
@@ -0,0 +1,23 @@
+package de.fwatermann.dungine.ui.elements;
+
+import de.fwatermann.dungine.event.input.MouseButtonEvent;
+import de.fwatermann.dungine.graphics.camera.Camera;
+import de.fwatermann.dungine.ui.IUIClickable;
+import de.fwatermann.dungine.ui.UIElement;
+
+public class UIButton extends UIElement<UIButton> implements IUIClickable {
+
+  @Override
+  protected void render(Camera<?> camera) {
+
+
+
+  }
+
+  @Override
+  public void click(int button, MouseButtonEvent.MouseButtonAction action) {
+
+
+
+  }
+}
