4429d88f87173b07195d012a3fa04f3a53fe5035
Finn Watermann <finn.watermann@hsbi.de>
2024-08-23 14:04:05 +0200

    Add Colliders

---
+++ engine/src/main/java/de/fwatermann/dungine/physics/BoxCollider.java
@@ -0,0 +1,9 @@
+package de.fwatermann.dungine.physics;
+
+public class BoxCollider extends Collider {
+
+  @Override
+  public boolean collide(Collider other) {
+    return false;
+  }
+}
---
+++ engine/src/main/java/de/fwatermann/dungine/physics/Collider.java
@@ -0,0 +1,17 @@
+package de.fwatermann.dungine.physics;
+
+/**
+ * The Collider class is an abstract base class for all collider objects in the physics engine.
+ * It defines a method for detecting collisions with other colliders.
+ */
+public abstract class Collider {
+
+  /**
+   * Determines if this collider collides with another collider.
+   *
+   * @param other the other collider to check for collision with
+   * @return true if this collider collides with the other collider, false otherwise
+   */
+  public abstract boolean collide(Collider other);
+
+}
