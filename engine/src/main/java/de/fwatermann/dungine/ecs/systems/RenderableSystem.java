b5700d25b0387555114c5003a39d65ee5ef96bdc
Finn Watermann <finn.watermann@hsbi.de>
2024-08-29 00:27:24 +0200

    Fix Frustum Check

--- engine/src/main/java/de/fwatermann/dungine/ecs/systems/RenderableSystem.java
+++ engine/src/main/java/de/fwatermann/dungine/ecs/systems/RenderableSystem.java
@@ -4,10 +4,19 @@ import de.fwatermann.dungine.ecs.ECS;
 import de.fwatermann.dungine.ecs.System;
 import de.fwatermann.dungine.ecs.components.RenderableComponent;
 import de.fwatermann.dungine.graphics.camera.Camera;
+import de.fwatermann.dungine.graphics.camera.CameraPerspective;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.joml.FrustumIntersection;
+import org.joml.Vector3f;
 
 public class RenderableSystem extends System<RenderableSystem> {
 
+  private static final Logger LOGGER = LogManager.getLogger(RenderableSystem.class);
+
   private Camera<?> camera;
+  private int latestRenderCount = 0;
+  private int renderCount = 0;
 
   public RenderableSystem(Camera<?> camera) {
     super(1, true, RenderableComponent.class);
@@ -18,10 +27,19 @@ public class RenderableSystem extends System<RenderableSystem> {
   public void update(ECS ecs) {
     ecs.entities(s -> s.forEach(e -> {
       e.components(RenderableComponent.class).forEach(c -> {
+        if(this.camera instanceof CameraPerspective pCam) {
+          int intersect = pCam.frustum().intersectAab(e.position(), e.position().add(e.size(), new Vector3f()));
+          if(intersect != FrustumIntersection.INTERSECT && intersect != FrustumIntersection.INSIDE) {
+            return;
+          }
+        }
         c.renderable.transformation(e.position(), e.rotation(), e.size());
         c.renderable.render(this.camera);
+        this.renderCount ++;
       });
     }), RenderableComponent.class);
+    this.latestRenderCount = this.renderCount;
+    this.renderCount = 0;
   }
 
   public Camera<?> camera() {
@@ -31,5 +49,9 @@ public class RenderableSystem extends System<RenderableSystem> {
   public RenderableSystem camera(Camera<?> camera) {
     this.camera = camera;
     return this;
+  }
+
+  public int latestRenderCount() {
+    return this.latestRenderCount;
   }
 }
