57fb0d9e1719efb7118d706a9dc04c29c9cbf708
Finn Watermann <finn.watermann@hsbi.de>
2024-09-18 02:49:14 +0200

    Fix CuboidTextured

--- engine/src/main/java/de/fwatermann/dungine/graphics/simple/Cuboid.java
+++ engine/src/main/java/de/fwatermann/dungine/graphics/simple/Cuboid.java
@@ -1,6 +1,7 @@
 package de.fwatermann.dungine.graphics.simple;
 
 import de.fwatermann.dungine.graphics.GLUsageHint;
+import de.fwatermann.dungine.graphics.camera.Camera;
 import de.fwatermann.dungine.graphics.camera.CameraFrustum;
 import de.fwatermann.dungine.graphics.mesh.DataType;
 import de.fwatermann.dungine.graphics.mesh.IndexDataType;
@@ -9,6 +10,7 @@ import de.fwatermann.dungine.graphics.mesh.PrimitiveType;
 import de.fwatermann.dungine.graphics.mesh.VertexAttribute;
 import de.fwatermann.dungine.graphics.scene.model.Material;
 import de.fwatermann.dungine.graphics.scene.model.Model;
+import de.fwatermann.dungine.graphics.shader.ShaderProgram;
 import de.fwatermann.dungine.utils.BoundingBox;
 import java.nio.ByteBuffer;
 import org.joml.Vector4f;
@@ -26,12 +28,8 @@ public class Cuboid extends Model {
    * @param rgb the color of the cuboid in RGBA format
    */
   public Cuboid(int rgb) {
-    initMesh();
     this.color = rgb;
-    Material material = new Material();
-    material.diffuseColor.set(0.8f);
-    material.meshes.add(MESH);
-    this.materials.add(material);
+    initMesh();
   }
 
   public Cuboid() {
@@ -40,72 +38,87 @@ public class Cuboid extends Model {
 
   private static void initMesh() {
     if(MESH != null) return;
-    ByteBuffer vertices = BufferUtils.createByteBuffer(4 * 6 * 6 * 4);
+    ByteBuffer vertices = BufferUtils.createByteBuffer(4 * 4 * 8 * 6);
     vertices
-        .asFloatBuffer()
-        .position(0)
-        .put(
-            new float[] {
-              //Format: x, y, z, nx, ny, nz, tx, ty, tz, bix, biy, biz
+            .asFloatBuffer()
+            .position(0)
+            .put(
+                    new float[] {
+                            //Format: x, y, z, nx, ny, nz, u, v
 
-              //Top (y+)
-              -0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f,
-               0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f,
-               0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-              -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+                            //Top (y+)
+                            -0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+                            0.5f, 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+                            0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+                            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 
-              //Bottom (y-)
-              -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-               0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-               0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,
-              -0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,
+                            //Bottom (y-)
+                            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+                            0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+                            0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+                            -0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
 
-              //Front (z+)
-              -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-               0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-               0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-              -0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+                            //Front (z+)
+                            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                            0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+                            0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+                            -0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
 
-              //Back (z-)
-               0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-              -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-              -0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-               0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+                            //Back (z-)
+                            0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+                            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+                            -0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+                            0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
 
-              //Right (x+)
-              0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-              0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-              0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-              0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+                            //Right (x+)
+                            0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                            0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+                            0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+                            0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
 
-              //Left (x-)
-              -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-              -0.5f, -0.5f,  0.5f, -1.0f, 0.0f, 0.0f,
-              -0.5f,  0.5f,  0.5f, -1.0f, 0.0f, 0.0f,
-              -0.5f,  0.5f, -0.5f, -1.0f, 0.0f, 0.0f
-            });
-    ByteBuffer indices = BufferUtils.createByteBuffer(36 * 2);
+                            //Left (x-)
+                            -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                            -0.5f, -0.5f,  0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+                            -0.5f,  0.5f,  0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+                            -0.5f,  0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f
+                    });
+    ByteBuffer indices = BufferUtils.createByteBuffer(36 * 4);
     indices
-        .asShortBuffer()
-        .position(0)
-        .put(
-            new short[] {
-              0, 1, 2, 2, 3, 0,
-              4, 5, 6, 6, 7, 4,
-              8, 9, 10, 10, 11, 8,
-              12, 13, 14, 14, 15, 12,
-              16, 17, 18, 18, 19, 16,
-              20, 21, 22, 22, 23, 20
-            });
+            .asShortBuffer()
+            .position(0)
+            .put(
+                    new short[] {
+                            0, 1, 2, 2, 3, 0,
+                            4, 5, 6, 6, 7, 4,
+                            8, 9, 10, 10, 11, 8,
+                            12, 13, 14, 14, 15, 12,
+                            16, 17, 18, 18, 19, 16,
+                            20, 21, 22, 22, 23, 20
+                    });
 
     MESH = new IndexedMesh(
-        vertices,
-        PrimitiveType.TRIANGLES,
-        indices,
-        IndexDataType.UNSIGNED_SHORT,
-        GLUsageHint.DRAW_STATIC,
-        new VertexAttribute(3, DataType.FLOAT, "aPosition"),
-        new VertexAttribute(3, DataType.FLOAT, "aNormal"));
+            vertices,
+            PrimitiveType.TRIANGLES,
+            indices,
+            IndexDataType.UNSIGNED_SHORT,
+            GLUsageHint.DRAW_STATIC,
+            new VertexAttribute(3, DataType.FLOAT, "aPosition"),
+            new VertexAttribute(3, DataType.FLOAT, "aNormal"),
+            new VertexAttribute(2, DataType.FLOAT, "aTexCoord"));
+  }
+
+  protected void initMaterials() {
+    Material material = new Material();
+    material.diffuseColor.set(0.8f);
+    material.meshes.add(new Material.MeshEntry(MESH, 0, 0));
+    this.materials.add(material);
+  }
+
+  @Override
+  public void render(Camera<?> camera, ShaderProgram shader) {
+    if(this.materials.isEmpty())
+      this.initMaterials();
+    super.render(camera, shader);
   }
 
   /**
--- engine/src/main/java/de/fwatermann/dungine/graphics/simple/CuboidTextured.java
+++ engine/src/main/java/de/fwatermann/dungine/graphics/simple/CuboidTextured.java
@@ -1,31 +1,10 @@
 package de.fwatermann.dungine.graphics.simple;
 
-import de.fwatermann.dungine.graphics.GLUsageHint;
-import de.fwatermann.dungine.graphics.Renderable;
-import de.fwatermann.dungine.graphics.camera.Camera;
-import de.fwatermann.dungine.graphics.camera.CameraFrustum;
-import de.fwatermann.dungine.graphics.mesh.DataType;
-import de.fwatermann.dungine.graphics.mesh.IndexDataType;
-import de.fwatermann.dungine.graphics.mesh.IndexedMesh;
-import de.fwatermann.dungine.graphics.mesh.PrimitiveType;
-import de.fwatermann.dungine.graphics.mesh.VertexAttribute;
-import de.fwatermann.dungine.graphics.shader.Shader;
-import de.fwatermann.dungine.graphics.shader.ShaderProgram;
+import de.fwatermann.dungine.graphics.scene.model.Material;
 import de.fwatermann.dungine.graphics.texture.animation.Animation;
-import de.fwatermann.dungine.resource.Resource;
-import de.fwatermann.dungine.utils.BoundingBox;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL33;
 
-public class CuboidTextured extends Renderable<CuboidTextured> {
+public class CuboidTextured extends Cuboid {
 
-  private static ShaderProgram DEFAULT_SHADER;
-  private static IndexedMesh MESH;
-
-  private ShaderProgram shader;
-  private BoundingBox boundingBox = new BoundingBox(0, 0, 0, 0, 0, 0);
   private final Animation[] animations = new Animation[6];
 
   /**
@@ -56,113 +35,14 @@ public class CuboidTextured extends Renderable<CuboidTextured> {
     this.animations[5] = bottom;
   }
 
-  private static void initShader() {
-    if(DEFAULT_SHADER != null) return;
-    try {
-      Shader vertexShader = Shader.loadShader(Resource.load("/shaders/3d/CuboidTextured.vsh"), Shader.ShaderType.VERTEX_SHADER);
-      Shader fragmentShader = Shader.loadShader(Resource.load("/shaders/3d/CuboidTextured.fsh"), Shader.ShaderType.FRAGMENT_SHADER);
-      DEFAULT_SHADER = new ShaderProgram(vertexShader, fragmentShader);
-    } catch (IOException e) {
-      throw new RuntimeException("Failed to load default shader for CuboidTextured", e);
+  @Override
+  protected void initMaterials() {
+    for(int i = 0; i < 6; i ++) {
+      Material material = new Material();
+      material.diffuseTexture = this.animations[i];
+      material.meshes.add(new Material.MeshEntry(MESH, i * 6 * 2, 6));
+      material.flags = Material.MATERIAL_FLAG_HAS_DIFFUSE_TEXTURE;
+      this.materials.add(material);
     }
-  }
-
-  private static void initMesh() {
-    if(MESH != null) return;
-
-    ByteBuffer vertices = BufferUtils.createByteBuffer(4 * 6 * 6 * 4);
-    vertices.asFloatBuffer().position(0).put(new float[] {
-      //FRONT
-      -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0,
-       0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0,
-      -0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 0,
-       0.5f,  0.5f, 0.5f, 1.0f, 0.0f, 0,
-
-      //BACK
-       0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1,
-      -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1,
-       0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 1,
-      -0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 1,
-
-      //LEFT
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 2,
-      -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 2,
-      -0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 2,
-      -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 2,
-
-      //RIGHT
-       0.5f, -0.5f,  0.5f, 0.0f, 1.0f, 3,
-       0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 3,
-       0.5f,  0.5f,  0.5f, 0.0f, 0.0f, 3,
-       0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 3,
-
-      //TOP
-      -0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 4,
-       0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 4,
-      -0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 4,
-       0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 4,
-
-      //BOTTOM
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 5,
-       0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 5,
-      -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 5,
-       0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 5
-    });
-
-    ByteBuffer indices = BufferUtils.createByteBuffer(6 * 6 * 2);
-    indices.asShortBuffer().position(0).put(new short[] {
-      0, 1, 2, 1, 3, 2,
-      4, 5, 6, 5, 7, 6,
-      8, 9, 10, 9, 11, 10,
-      12, 13, 14, 13, 15, 14,
-      16, 17, 18, 17, 19, 18,
-      20, 21, 22, 21, 23, 22
-    });
-
-    MESH =
-        new IndexedMesh(
-            vertices,
-            PrimitiveType.TRIANGLES,
-            indices,
-            IndexDataType.UNSIGNED_SHORT,
-            GLUsageHint.DRAW_STATIC,
-            new VertexAttribute(3, DataType.FLOAT, "aPosition"),
-            new VertexAttribute(2, DataType.FLOAT, "aTexCoord"),
-            new VertexAttribute(1, DataType.FLOAT, "aAnimationIndex"));
-  }
-
-  @Override
-  public void render(Camera<?> camera) {
-    if(this.shader == null) {
-      initShader();
-      this.render(camera, DEFAULT_SHADER);
-    } else {
-      this.render(camera, this.shader);
-    }
-  }
-
-  @Override
-  public void render(Camera<?> camera, ShaderProgram shader) {
-    initMesh();
-    shader.bind();
-    for(int i = 0; i < this.animations.length; i ++) {
-      this.animations[i].bind(shader, Animation.AnimationSlot.fromIndex(i), GL33.GL_TEXTURE10 + i);
-    }
-    MESH.transformation(this.position(),this.rotation(), this.scaling());
-    MESH.render(camera, shader);
-    shader.unbind();
-  }
-
-  @Override
-  public boolean shouldRender(CameraFrustum frustum) {
-    int frustumResult = frustum.intersectAab(this.boundingBox.getMin(), this.boundingBox.getMax());
-    return frustumResult == CameraFrustum.INTERSECT || frustumResult == CameraFrustum.INSIDE;
-  }
-
-  @Override
-  protected void transformationChanged() {
-    super.transformationChanged();
-    if(MESH == null) return;
-    this.boundingBox = BoundingBox.fromVertices(MESH.vertexBuffer().asFloatBuffer(), 0, 5, 24, this.transformationMatrix());
   }
 }
