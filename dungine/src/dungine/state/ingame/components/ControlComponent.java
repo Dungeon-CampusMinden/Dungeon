507b0f1b6a349c0e892e59f0d21e21aa9e74aaec
Finn Watermann <finn.watermann@hsbi.de>
2024-09-19 17:42:40 +0200

    Add ControlComponent & -System

---
+++ dungine/src/dungine/state/ingame/components/ControlComponent.java
@@ -0,0 +1,12 @@
+package dungine.state.ingame.components;
+
+import de.fwatermann.dungine.ecs.Component;
+
+public class ControlComponent extends Component {
+
+  public ControlComponent() {
+    super(false);
+  }
+
+
+}
