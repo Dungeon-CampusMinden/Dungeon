50e67c5b19856948e98a5ae4b3ed9ce0ab9b0f1f
Finn Watermann <finn.watermann@hsbi.de>
2024-10-23 03:22:34 +0200

    Disable OpenGL debug

--- dungine-demo/src/dungine/Main.java
+++ dungine-demo/src/dungine/Main.java
@@ -6,7 +6,7 @@ import dungine.state.StateTransition;
 public class Main {
 
   public static void main(String[] args) {
-    Dungine dungine = new Dungine(true);
+    Dungine dungine = new Dungine(false);
     dungine.setState(new StateMainMenu(dungine));
     dungine.setStateTransition(new StateTransition(dungine));
     dungine.start();
