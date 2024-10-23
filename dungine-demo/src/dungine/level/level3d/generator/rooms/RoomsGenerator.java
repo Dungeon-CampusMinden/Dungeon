dc3f18fd27ee841f41c65b5eab97dc418619e818
Finn Watermann <finn.watermann@hsbi.de>
2024-10-23 03:22:11 +0200

    Implement Room Generator

--- dungine-demo/src/dungine/level/level3d/generator/rooms/RoomsGenerator.java
+++ dungine-demo/src/dungine/level/level3d/generator/rooms/RoomsGenerator.java
@@ -1,8 +1,3 @@
-/*
-Implementation des Algorithmus zur Generierung von Räumen in einem 3D-Level. Inspiriert
-von https://www.gamedeveloper.com/programming/procedural-dungeon-generation-algorithm
-*/
-
 package dungine.level.level3d.generator.rooms;
 
 import dungine.level.level3d.Chunk;
@@ -11,37 +6,74 @@ import dungine.level.level3d.block.FloorBlock;
 import dungine.level.level3d.block.WallBlock;
 import dungine.level.level3d.generator.IGenerator;
 import dungine.level.level3d.utils.ChunkUtils;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joml.Random;
+import org.joml.Vector2f;
 import org.joml.Vector2i;
+import org.joml.Vector3f;
 import org.joml.Vector3i;
 
+/**
+ * The `RoomsGenerator` class is responsible for generating rooms and hallways in a 3D level.
+ * It uses a random seed to ensure reproducibility of the generated structures.
+ * The class provides methods to generate rooms, separate overlapping rooms, connect rooms with hallways,
+ * and build the rooms and hallways within the level.
+ *
+ * <p>Key functionalities include:</p>
+ * <ul>
+ *   <li>Generating a specified number of rooms with random sizes and positions within a given radius.</li>
+ *   <li>Separating overlapping rooms to ensure no two rooms occupy the same space.</li>
+ *   <li>Connecting rooms with hallways to create a navigable dungeon layout.</li>
+ *   <li>Building the rooms and hallways by placing floor and wall blocks in the level.</li>
+ * </ul>
+ *
+ * <p>Usage example:</p>
+ * <pre>
+ * {@code
+ * Level3D level = new Level3D();
+ * RoomsGenerator generator = new RoomsGenerator(level, 3467589736L);
+ * generator.generate();
+ * }
+ * </pre>
+ *
+ *
+ * @see Level3D
+ * @see Chunk
+ * @see FloorBlock
+ * @see WallBlock
+ * @see ChunkUtils
+ * @see Logger
+ */
 public class RoomsGenerator implements IGenerator {
 
   private static final Logger LOGGER = LogManager.getLogger(RoomsGenerator.class);
 
   private Set<Room> rooms = new HashSet<>();
+  private Vector2f meanSize = new Vector2f();
 
   private Level3D level;
+  private Random random;
 
-  public RoomsGenerator(Level3D level) {
+  public RoomsGenerator(Level3D level, long seed) {
     this.level = level;
+    this.random = new Random(seed);
   }
 
-  private void generateRooms(int numRooms, Vector2i minSize, Vector2i maxSize, int radius, long seed) {
-
-    Random random = new Random(seed);
-
+  private void generateRooms(int numRooms, Vector2i minSize, Vector2i maxSize, int radius) {
 
     for(int i = 0; i < numRooms; i++) {
-        Vector2i size = new Vector2i((int) (random.nextFloat() * (maxSize.x - minSize.x) + minSize.x),
-            (int) (random.nextFloat() * (maxSize.y - minSize.y) + minSize.y));
+        Vector2i size = new Vector2i((int) (this.random.nextFloat() * (maxSize.x - minSize.x) + minSize.x),
+            (int) (this.random.nextFloat() * (maxSize.y - minSize.y) + minSize.y));
 
-        float t = 2 * (float) Math.PI * random.nextFloat();
-        float u = random.nextFloat() + random.nextFloat();
+        this.meanSize.add(size.x, size.y);
+
+        float t = 2 * (float) Math.PI * this.random.nextFloat();
+        float u = this.random.nextFloat() + this.random.nextFloat();
         float r = u > 1 ? 2 - u : u;
 
         Vector3i position = new Vector3i((int) (radius * r * Math.cos(t)), 0, (int) (radius * r * Math.sin(t)));
@@ -55,6 +87,15 @@ public class RoomsGenerator implements IGenerator {
 
         LOGGER.debug("Generated room at {} with size {}", room.position, room.size);
     }
+
+    this.meanSize.div(numRooms);
+
+    for(Room room : this.rooms) {
+      if(room.size.x >= this.meanSize.x && room.size.y >= this.meanSize.y) {
+        room.mainRoom = true;
+      }
+    }
+
   }
 
   private void separateRooms() {
@@ -83,6 +124,28 @@ public class RoomsGenerator implements IGenerator {
       }
       iterations ++;
     } while(overlapping && iterations < maxIterations);
+  }
+
+  public void connectRooms() {
+    for(Room room : this.rooms) {
+      for(int i = 0; i < 2; i ++) {
+        Room closestRoom = null;
+        float closestDistance = Float.MAX_VALUE;
+        for(Room otherRoom : this.rooms) {
+          if(room == otherRoom) continue;
+          float distance = room.position.distanceSquared(otherRoom.position);
+          if(distance < closestDistance && !room.connectedRooms.contains(otherRoom)) {
+            closestDistance = distance;
+            closestRoom = otherRoom;
+          }
+        }
+        if(closestRoom == null) continue;
+
+        room.connectedRooms.add(closestRoom);
+        closestRoom.connectedRooms.add(room);
+      }
+    }
+
   }
 
   private boolean isOverlapping(Room roomA, Room roomB) {
@@ -117,10 +180,16 @@ public class RoomsGenerator implements IGenerator {
     return new Vector2i(overlapX, overlapZ);
   }
 
-  public void generate(int rooms, long seed) {
-    this.generateRooms(rooms, new Vector2i(5, 5), new Vector2i(20, 20), 50, seed);
+  public void generate() {
+    this.generateRooms(this.random.nextInt(40) + 10, new Vector2i(8, 8), new Vector2i(20, 20), 50);
     this.separateRooms();
+    this.connectRooms();
 
+    this.buildRooms();
+    this.buildHallways();
+  }
+
+  private void buildRooms() {
     for(Room room : this.rooms) {
       for(int x = 0; x < room.size.x; x++) {
         for(int z = 0; z < room.size.y; z++) {
@@ -140,14 +209,113 @@ public class RoomsGenerator implements IGenerator {
     }
   }
 
+  public void buildHallways() {
+
+    List<Vector3i> hallwayFloor = new ArrayList<>();
+
+    for(Room room : this.rooms) {
+      for(Room connectedRoom : room.connectedRooms) {
+        Vector3i start = new Vector3i(room.position.x + room.size.x / 2, 0, room.position.z + room.size.y / 2);
+        Vector3i end = new Vector3i(connectedRoom.position.x + connectedRoom.size.x / 2, 0, connectedRoom.position.z + connectedRoom.size.y / 2);
+        Vector3i current = new Vector3i(start);
+
+        while(current.x != end.x || current.z != end.z) {
+          if(current.x != end.x) {
+            current.x += current.x < end.x ? 1 : -1;
+          } else {
+            current.z += current.z < end.z ? 1 : -1;
+          }
+          this.makeFloor(current.x, current.y, current.z);
+          this.makeFloor(current.x+1, current.y, current.z);
+          this.makeFloor(current.x, current.y, current.z+1);
+          this.makeFloor(current.x+1, current.y, current.z+1);
+
+          //Clear air above floor
+          for(int i = 1; i < 3; i ++) {
+            this.makeNull(current.x, current.y+i, current.z);
+            this.makeNull(current.x+1, current.y+i, current.z);
+            this.makeNull(current.x, current.y+i, current.z+1);
+            this.makeNull(current.x+1, current.y+i, current.z+1);
+          }
+
+          hallwayFloor.add(new Vector3i(current));
+          hallwayFloor.add(new Vector3i(current.x + 1, current.y, current.z));
+          hallwayFloor.add(new Vector3i(current.x, current.y, current.z + 1));
+          hallwayFloor.add(new Vector3i(current.x + 1, current.y, current.z + 1));
+        }
+      }
+    }
+
+    for(Vector3i pos : hallwayFloor) {
+      if(!this.isFloor(pos.x + 1, pos.y, pos.z)) {
+        this.makeWall(pos.x + 1, pos.y + 1, pos.z);
+        this.makeWall(pos.x + 1, pos.y + 2, pos.z);
+        this.makeWall(pos.x + 1, pos.y + 3, pos.z);
+      }
+      if(!this.isFloor(pos.x - 1, pos.y, pos.z)) {
+        this.makeWall(pos.x - 1, pos.y + 1, pos.z);
+        this.makeWall(pos.x - 1, pos.y + 2, pos.z);
+        this.makeWall(pos.x - 1, pos.y + 3, pos.z);
+      }
+      if(!this.isFloor(pos.x, pos.y, pos.z + 1)) {
+        this.makeWall(pos.x, pos.y + 1, pos.z + 1);
+        this.makeWall(pos.x, pos.y + 2, pos.z + 1);
+        this.makeWall(pos.x, pos.y + 3, pos.z + 1);
+      }
+      if(!this.isFloor(pos.x, pos.y, pos.z - 1)) {
+        this.makeWall(pos.x, pos.y + 1, pos.z - 1);
+        this.makeWall(pos.x, pos.y + 2, pos.z - 1);
+        this.makeWall(pos.x, pos.y + 3, pos.z - 1);
+      }
+    }
+
+
+
+  }
+
+  private void makeFloor(int x, int y, int z) {
+    Chunk chunk = this.level.chunkByWorldCoordinates(x, y, z, true);
+    if(chunk == null) return;
+    chunk.setBlock(new FloorBlock(chunk, ChunkUtils.worldToChunkRelative(x, y, z)));
+  }
+
+  private void makeWall(int x, int y, int z) {
+    Chunk chunk = this.level.chunkByWorldCoordinates(x, y, z, true);
+    if(chunk == null) return;
+    chunk.setBlock(new WallBlock(chunk, ChunkUtils.worldToChunkRelative(x, y, z)));
+  }
+
+  private void makeNull(int x, int y, int z) {
+    Chunk chunk = this.level.chunkByWorldCoordinates(x, y, z, false);
+    if(chunk == null) return;
+    chunk.removeBlock(ChunkUtils.worldToChunkRelative(x, y, z));
+  }
+
+  private boolean isFloor(int x, int y, int z) {
+    Chunk chunk = this.level.chunkByWorldCoordinates(x, y, z, false);
+    if(chunk == null) return false;
+    return chunk.getBlockAt(ChunkUtils.worldToChunkRelative(x, y, z)) instanceof FloorBlock;
+  }
+
   @Override
   public Chunk generateChunk(Vector3i chunkCoordinates) {
     return new Chunk(this.level, chunkCoordinates);
   }
 
+  public Vector3f getStartPosition() {
+    for(Room room : this.rooms) {
+      if(room.mainRoom) {
+        return new Vector3f(room.position.x + room.size.x / 2.0f, 1.5f, room.position.z + room.size.y / 2.0f);
+      }
+    }
+    return new Vector3f(0, 1.5f, 0);
+  }
+
   public static class Room {
     public Vector3i position;
     public Vector2i size;
+    public List<Room> connectedRooms = new ArrayList<>();
+    boolean mainRoom = false;
   }
 
 
