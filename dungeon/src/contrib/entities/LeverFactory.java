ead21e3e19ad4e453ec70a76b7f9fb45e4132734
Andre Matutat <andre.matutat@hsbi.de>
2025-03-18 12:17:02 +0100

    add torches as leavers

--- dungeon/src/contrib/entities/LeverFactory.java
+++ dungeon/src/contrib/entities/LeverFactory.java
@@ -11,6 +11,7 @@ import core.utils.components.MissingComponentException;
 import core.utils.components.draw.Animation;
 import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
+import java.util.List;
 import java.util.Map;
 
 /** The LeverFactory class is responsible for creating lever entities. */
@@ -20,23 +21,46 @@ public class LeverFactory {
   private static final IPath LEVER_TEXTURE_ON = new SimpleIPath("objects/lever/on/lever_0.png");
   private static final IPath LEVER_TEXTURE_OFF = new SimpleIPath("objects/lever/off/lever_0.png");
 
+  private static final IPath TORCH_TEXTURE_OFF = new SimpleIPath("objects/torch/off/torch_0.png");
+  private static final List<IPath> TORCH_TEXTURE_ON =
+      List.of(
+          new SimpleIPath("objects/torch/on/torch_1.png"),
+          new SimpleIPath("objects/torch/on/torch_2.png"),
+          new SimpleIPath("objects/torch/on/torch_3.png"),
+          new SimpleIPath("objects/torch/on/torch_4.png"),
+          new SimpleIPath("objects/torch/on/torch_5.png"),
+          new SimpleIPath("objects/torch/on/torch_6.png"),
+          new SimpleIPath("objects/torch/on/torch_7.png"),
+          new SimpleIPath("objects/torch/on/torch_8.png"));
+
   /**
    * Creates a lever entity at a given position, with a specified behavior when interacted with. The
    * lever is initially off. The lever is interactable and can be toggled on and off.
    *
    * @param pos The position where the lever will be created.
    * @param onInteract The behavior when the lever is interacted with. (isOn, lever, who)
+   * @param torchDesign True if the lever should look like a torch, false if it should look like a
+   *     lever
    * @return The created lever entity.
    * @see LeverComponent LeverComponent
    * @see contrib.systems.LeverSystem LeverSystem
    */
-  public static Entity createLever(Point pos, ICommand onInteract) {
+  public static Entity createLever(Point pos, ICommand onInteract, boolean torchDesign) {
     Entity lever = new Entity("lever");
 
     lever.add(new PositionComponent(pos));
-    DrawComponent dc = new DrawComponent(Animation.fromSingleImage(LEVER_TEXTURE_OFF));
-    Map<String, Animation> animationMap =
-        Map.of("off", dc.currentAnimation(), "on", Animation.fromSingleImage(LEVER_TEXTURE_ON));
+    DrawComponent dc;
+    Map<String, Animation> animationMap;
+    if (torchDesign) {
+      dc = new DrawComponent(Animation.fromSingleImage(TORCH_TEXTURE_OFF));
+      animationMap =
+          Map.of("off", dc.currentAnimation(), "on", Animation.fromCollection(TORCH_TEXTURE_ON));
+
+    } else {
+      dc = new DrawComponent(Animation.fromSingleImage(LEVER_TEXTURE_OFF));
+      animationMap =
+          Map.of("off", dc.currentAnimation(), "on", Animation.fromSingleImage(LEVER_TEXTURE_ON));
+    }
     dc.animationMap(animationMap);
     dc.currentAnimation("off");
     lever.add(dc);
@@ -55,11 +79,49 @@ public class LeverFactory {
               entity
                   .fetch(DrawComponent.class)
                   .ifPresent(
-                      drawComponent -> {
-                        drawComponent.currentAnimation(lc.isOn() ? "on" : "off");
-                      });
+                      drawComponent -> drawComponent.currentAnimation(lc.isOn() ? "on" : "off"));
             }));
     return lever;
+  }
+
+  /**
+   * Creates a lever entity at a given position, with a specified behavior when interacted with. The
+   * lever is initially off. The lever is interactable and can be toggled on and off.
+   *
+   * @param pos The position where the lever will be created.
+   * @param onInteract The behavior when the lever is interacted with. (isOn, lever, who)
+   * @return The created lever entity.
+   * @see LeverComponent LeverComponent
+   * @see contrib.systems.LeverSystem LeverSystem
+   */
+  public static Entity createLever(Point pos, ICommand onInteract) {
+    return createLever(pos, onInteract, false);
+  }
+
+  /**
+   * Creates a lever entity at a given position with a default interaction behavior.
+   *
+   * <p>The lever is initially off. This method provides a default behavior where no action is
+   * performed when the lever is interacted with (empty command).
+   *
+   * @param pos The position where the lever will be created.
+   * @param torchDesign True if the lever should look like a torch, false if it should look like a
+   *     lever
+   * @return The created lever entity.
+   * @see LeverComponent LeverComponent
+   * @see contrib.systems.LeverSystem LeverSystem
+   */
+  public static Entity createLever(Point pos, boolean torchDesign) {
+    return createLever(
+        pos,
+        new ICommand() {
+          @Override
+          public void execute() {}
+
+          @Override
+          public void undo() {}
+        },
+        torchDesign);
   }
 
   /**
@@ -78,14 +140,10 @@ public class LeverFactory {
         pos,
         new ICommand() {
           @Override
-          public void execute() {
-            return;
-          }
+          public void execute() {}
 
           @Override
-          public void undo() {
-            return;
-          }
+          public void undo() {}
         });
   }
 }
