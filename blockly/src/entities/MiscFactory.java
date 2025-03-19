6d13c965e90ddb238324a8cfbba6771e4005052d
Andre Matutat <andre.matutat@hsbi.de>
2025-03-19 10:56:42 +0100

    add preasure plate

--- blockly/src/entities/MiscFactory.java
+++ blockly/src/entities/MiscFactory.java
@@ -3,26 +3,28 @@ package entities;
 import components.BlockComponent;
 import components.PushableComponent;
 import contrib.components.CollideComponent;
-import contrib.components.InteractionComponent;
 import contrib.components.LeverComponent;
 import contrib.utils.ICommand;
 import core.Entity;
 import core.components.DrawComponent;
 import core.components.PositionComponent;
 import core.components.VelocityComponent;
+import core.level.Tile;
 import core.utils.Point;
+import core.utils.TriConsumer;
 import core.utils.components.draw.Animation;
 import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
-
 import java.util.Map;
 
 /** Factory class for creating miscellaneous game entities. */
 public class MiscFactory {
 
   private static final IPath STONE = new SimpleIPath("objects/stone/stone.png");
-  private static final IPath PRESSURE_PLATE_ON = new SimpleIPath("objects/pressureplate/on/pressureplate_0.png");
-  private static final IPath PRESSURE_PLATE_OFF = new SimpleIPath("objects/pressureplate/off/pressureplate_0.png");
+  private static final IPath PRESSURE_PLATE_ON =
+      new SimpleIPath("objects/pressureplate/on/pressureplate_0.png");
+  private static final IPath PRESSURE_PLATE_OFF =
+      new SimpleIPath("objects/pressureplate/off/pressureplate_0.png");
   private static final float STONE_SPEED = 7.5f;
 
   /**
@@ -45,17 +47,32 @@ public class MiscFactory {
     return stone;
   }
 
-  public static Entity pressurePlate(Point position){
+  /**
+   * Creates a pressure plate entity at the given position.
+   *
+   * <p>The pressure plate is an entity that reacts to collisions by toggling its lever state.
+   *
+   * @param position The initial position of the pressure plate.
+   * @return A new pressure plate entity lever and collision behavior.
+   */
+  public static Entity pressurePlate(Point position) {
     Entity pressurePlate = new Entity("pressureplate");
     pressurePlate.add(new PositionComponent(position.toCoordinate().toCenteredPoint()));
     DrawComponent dc = new DrawComponent(Animation.fromSingleImage(PRESSURE_PLATE_OFF));
     Map<String, Animation> animationMap =
-            Map.of("off", dc.currentAnimation(), "on", Animation.fromSingleImage(PRESSURE_PLATE_ON));
+        Map.of("off", dc.currentAnimation(), "on", Animation.fromSingleImage(PRESSURE_PLATE_ON));
     dc.animationMap(animationMap);
     dc.currentAnimation("off");
     pressurePlate.add(dc);
-    pressurePlate.add(new LeverComponent(false, null));
-
+    LeverComponent lc = new LeverComponent(false, ICommand.EMPTY_COMMAND);
+    pressurePlate.add(lc);
+    TriConsumer<Entity, Entity, Tile.Direction> collide =
+        (entity, entity2, direction) -> {
+          lc.toggle();
+          if (lc.isOn()) dc.currentAnimation("on");
+          else dc.currentAnimation("off");
+        };
+    pressurePlate.add(new CollideComponent(collide, collide));
     return pressurePlate;
   }
 }
