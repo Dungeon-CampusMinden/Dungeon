b3b658e0f0dc66afee5d690c8534777a2c236370
Andre Matutat <andre.matutat@hsbi.de>
2025-03-25 13:48:36 +0100

    finish lvl 1.5

--- blockly/src/client/Client.java
+++ blockly/src/client/Client.java
@@ -73,7 +73,7 @@ public class Client {
           // DevDungeonLoader.addLevel(Tuple.of("chapter11", Chapter11Level.class));
           // DevDungeonLoader.addLevel(Tuple.of("chapter12", Chapter12Level.class));
           // DevDungeonLoader.addLevel(Tuple.of("chapter13", Chapter13Level.class));
-          DevDungeonLoader.addLevel(Tuple.of("chapter14", Chapter14Level.class));
+          // DevDungeonLoader.addLevel(Tuple.of("chapter14", Chapter14Level.class));
           DevDungeonLoader.addLevel(Tuple.of("chapter15", Chapter15Level.class));
           DevDungeonLoader.addLevel(Tuple.of("chapter16", Chapter16Level.class));
           // DevDungeonLoader.addLevel(Tuple.of("chapter17", Chapter17Level.class));
