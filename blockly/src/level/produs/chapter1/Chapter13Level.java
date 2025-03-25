751b2d44a912fd779222ca1489ce167f8f356eb4
Andre Matutat <andre.matutat@hsbi.de>
2025-03-25 13:10:27 +0100

    spotless

--- blockly/src/level/produs/chapter1/Chapter13Level.java
+++ blockly/src/level/produs/chapter1/Chapter13Level.java
@@ -1,24 +1,21 @@
 package level.produs.chapter1;
 
 import contrib.components.LeverComponent;
-import contrib.utils.EntityUtils;
 import core.Entity;
 import core.Game;
-import core.components.PositionComponent;
 import core.level.elements.tile.DoorTile;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
-import java.util.List;
-
 import entities.MiscFactory;
+import java.util.List;
 import level.BlocklyLevel;
 
 public class Chapter13Level extends BlocklyLevel {
 
-
   private DoorTile door;
-  private LeverComponent switch1,switch2;
+  private LeverComponent switch1, switch2;
+
   /**
    * Call the parent constructor of a tile level with the given layout and design label. Set the
    * start tile of the hero to the given heroPos.
@@ -40,23 +37,19 @@ public class Chapter13Level extends BlocklyLevel {
     Coordinate switch2C = customPoints().get(3);
     Game.add(MiscFactory.stone(stone1C.toCenteredPoint()));
     Game.add(MiscFactory.stone(stone2C.toCenteredPoint()));
-    Entity s1=MiscFactory.pressurePlate(switch1C.toCenteredPoint());
-    Entity s2=MiscFactory.pressurePlate(switch2C.toCenteredPoint());
+    Entity s1 = MiscFactory.pressurePlate(switch1C.toCenteredPoint());
+    Entity s2 = MiscFactory.pressurePlate(switch2C.toCenteredPoint());
     Game.add(s1);
     Game.add(s2);
-    switch1=s1.fetch(LeverComponent.class).get();
-    switch2=s2.fetch(LeverComponent.class).get();
-    door = (DoorTile) Game.tileAT(new Coordinate(5,12));
+    switch1 = s1.fetch(LeverComponent.class).get();
+    switch2 = s2.fetch(LeverComponent.class).get();
+    door = (DoorTile) Game.tileAT(new Coordinate(5, 12));
     door.close();
-
   }
 
   @Override
   protected void onTick() {
-    if(switch1.isOn() && switch2.isOn())
-      door.open();
-    else
-      door.close();
-
+    if (switch1.isOn() && switch2.isOn()) door.open();
+    else door.close();
   }
 }
--- blockly/src/level/produs/chapter1/Chapter14Level.java
+++ blockly/src/level/produs/chapter1/Chapter14Level.java
@@ -7,11 +7,9 @@ import core.components.PositionComponent;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
-import java.util.List;
-
-import core.systems.CameraSystem;
 import entities.BlocklyMonsterFactory;
 import entities.MiscFactory;
+import java.util.List;
 import level.BlocklyLevel;
 
 public class Chapter14Level extends BlocklyLevel {
@@ -33,12 +31,12 @@ public class Chapter14Level extends BlocklyLevel {
   protected void onFirstTick() {
     Game.hero().get().remove(CameraComponent.class);
     Entity focusPoint = new Entity();
-    focusPoint.add(new PositionComponent(7,6));
+    focusPoint.add(new PositionComponent(7, 6));
     focusPoint.add(new CameraComponent());
     Game.add(focusPoint);
 
-    Coordinate stone1C= customPoints().get(1);
-    Coordinate stone2C= customPoints().get(5);
+    Coordinate stone1C = customPoints().get(1);
+    Coordinate stone2C = customPoints().get(5);
     Coordinate m1C = customPoints().get(0);
     Coordinate m2C = customPoints().get(2);
     Coordinate m3C = customPoints().get(3);
@@ -46,13 +44,11 @@ public class Chapter14Level extends BlocklyLevel {
 
     Game.add(MiscFactory.stone(stone1C.toCenteredPoint()));
     Game.add(MiscFactory.stone(stone2C.toCenteredPoint()));
-    //TODO replace with guards
-    Game.add(BlocklyMonsterFactory.guard(m1C,"Monster 1",PositionComponent.Direction.LEFT));
-    Game.add(BlocklyMonsterFactory.guard(m2C,"Monster 2",PositionComponent.Direction.RIGHT));
-    Game.add(BlocklyMonsterFactory.guard(m3C,"Monster 3",PositionComponent.Direction.UP));
-    Game.add(BlocklyMonsterFactory.guard(m4C,"Monster 4",PositionComponent.Direction.UP));
-
-
+    // TODO replace with guards
+    Game.add(BlocklyMonsterFactory.guard(m1C, "Monster 1", PositionComponent.Direction.LEFT));
+    Game.add(BlocklyMonsterFactory.guard(m2C, "Monster 2", PositionComponent.Direction.RIGHT));
+    Game.add(BlocklyMonsterFactory.guard(m3C, "Monster 3", PositionComponent.Direction.UP));
+    Game.add(BlocklyMonsterFactory.guard(m4C, "Monster 4", PositionComponent.Direction.UP));
   }
 
   @Override
