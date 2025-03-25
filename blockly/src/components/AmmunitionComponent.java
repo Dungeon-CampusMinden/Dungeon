317ddffbbf4bd02140e2b9b2b5f87325c6bd8a3b
Tobi511 <130839159+Tobi511@users.noreply.github.com>
2025-03-25 16:25:05 +0100

    Update blockly/src/components/AmmunitionComponent.java
    Co-authored-by: Carsten Gips <cagix@hsbi.de>

--- blockly/src/components/AmmunitionComponent.java
+++ blockly/src/components/AmmunitionComponent.java
@@ -56,8 +56,10 @@ public final class AmmunitionComponent implements Component {
    * Set the desired amount of ammunition.
    *
    * @param ammunitionAmount New amount of current ammunition
+   * @return the new ammunition value
    */
-  public void setCurrentAmmunition(int ammunitionAmount) {
-    this.currentAmmunition = ammunitionAmount;
+  public int setCurrentAmmunition(int ammunitionAmount) {
+    currentAmmunition = ammunitionAmount;
+    return currentAmmunition;
   }
 }
