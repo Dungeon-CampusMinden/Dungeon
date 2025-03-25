c96996c105e727a3d958e24cdd44cbfec06dd8ea
Andre Matutat <andre.matutat@hsbi.de>
2025-03-25 12:56:13 +0100

    finish lvl 3

--- blockly/src/level/produs/chapter1/Chapter13Level.java
+++ blockly/src/level/produs/chapter1/Chapter13Level.java
@@ -1,13 +1,24 @@
 package level.produs.chapter1;
 
+import contrib.components.LeverComponent;
+import contrib.utils.EntityUtils;
+import core.Entity;
+import core.Game;
+import core.components.PositionComponent;
+import core.level.elements.tile.DoorTile;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
 import java.util.List;
+
+import entities.MiscFactory;
 import level.BlocklyLevel;
 
 public class Chapter13Level extends BlocklyLevel {
 
+
+  private DoorTile door;
+  private LeverComponent switch1,switch2;
   /**
    * Call the parent constructor of a tile level with the given layout and design label. Set the
    * start tile of the hero to the given heroPos.
@@ -22,8 +33,30 @@ public class Chapter13Level extends BlocklyLevel {
   }
 
   @Override
-  protected void onFirstTick() {}
+  protected void onFirstTick() {
+    Coordinate stone1C = customPoints().get(0);
+    Coordinate stone2C = customPoints().get(1);
+    Coordinate switch1C = customPoints().get(2);
+    Coordinate switch2C = customPoints().get(3);
+    Game.add(MiscFactory.stone(stone1C.toCenteredPoint()));
+    Game.add(MiscFactory.stone(stone2C.toCenteredPoint()));
+    Entity s1=MiscFactory.pressurePlate(switch1C.toCenteredPoint());
+    Entity s2=MiscFactory.pressurePlate(switch2C.toCenteredPoint());
+    Game.add(s1);
+    Game.add(s2);
+    switch1=s1.fetch(LeverComponent.class).get();
+    switch2=s2.fetch(LeverComponent.class).get();
+    door = (DoorTile) Game.tileAT(new Coordinate(5,12));
+    door.close();
+
+  }
 
   @Override
-  protected void onTick() {}
+  protected void onTick() {
+    if(switch1.isOn() && switch2.isOn())
+      door.open();
+    else
+      door.close();
+
+  }
 }
