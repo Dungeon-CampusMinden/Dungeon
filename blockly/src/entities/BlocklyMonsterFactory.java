c165cd753aaec58beb08f5f7a66b1df7d7b1ae16
Andre Matutat <andre.matutat@hsbi.de>
2025-03-25 12:38:54 +0100

    spotless

--- blockly/src/entities/BlocklyMonsterFactory.java
+++ blockly/src/entities/BlocklyMonsterFactory.java
@@ -10,24 +10,23 @@ import core.components.PositionComponent;
 import core.level.utils.Coordinate;
 import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
-
 import java.io.IOException;
 
 public class BlocklyMonsterFactory {
-    private static final IPath HEDGEHOG = new SimpleIPath("character/monster/big_daemon");
+  private static final IPath HEDGEHOG = new SimpleIPath("character/monster/big_daemon");
 
-    public static Entity hedgehog(Coordinate coordinate, String name){
-        Entity entity = new Entity(name);
-        PositionComponent pc = new PositionComponent();
-        pc.position(Game.tileAT(coordinate));
-        entity.add(pc);
-        entity.add(new CollideComponent());
-        entity.add(new SpikyComponent(100, DamageType.PHYSICAL, 0));
-        try {
-            entity.add(new DrawComponent(HEDGEHOG));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return entity;
+  public static Entity hedgehog(Coordinate coordinate, String name) {
+    Entity entity = new Entity(name);
+    PositionComponent pc = new PositionComponent();
+    pc.position(Game.tileAT(coordinate));
+    entity.add(pc);
+    entity.add(new CollideComponent());
+    entity.add(new SpikyComponent(100, DamageType.PHYSICAL, 0));
+    try {
+      entity.add(new DrawComponent(HEDGEHOG));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+    return entity;
+  }
 }
