8bb240ba5c658060e3fe0af98ac76f96a20f188b
Andre Matutat <andre.matutat@hsbi.de>
2025-03-25 12:38:54 +0100

    spotless

--- blockly/src/level/produs/chapter1/Chapter13Level.java
+++ blockly/src/level/produs/chapter1/Chapter13Level.java
@@ -1,35 +1,29 @@
 package level.produs.chapter1;
 
-import contrib.hud.DialogUtils;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
-import level.BlocklyLevel;
-
 import java.util.List;
+import level.BlocklyLevel;
 
 public class Chapter13Level extends BlocklyLevel {
 
+  /**
+   * Call the parent constructor of a tile level with the given layout and design label. Set the
+   * start tile of the hero to the given heroPos.
+   *
+   * @param layout 2D array containing the tile layout.
+   * @param designLabel The design label for the level.
+   * @param customPoints The custom points of the level.
+   */
+  public Chapter13Level(
+      LevelElement[][] layout, DesignLabel designLabel, List<Coordinate> customPoints) {
+    super(layout, designLabel, customPoints, "Chapter13");
+  }
 
-    /**
-     * Call the parent constructor of a tile level with the given layout and design label. Set the
-     * start tile of the hero to the given heroPos.
-     *
-     * @param layout       2D array containing the tile layout.
-     * @param designLabel  The design label for the level.
-     * @param customPoints The custom points of the level.
-     */
-    public Chapter13Level(LevelElement[][] layout, DesignLabel designLabel, List<Coordinate> customPoints) {
-        super(layout, designLabel, customPoints, "Chapter13");
-    }
+  @Override
+  protected void onFirstTick() {}
 
-    @Override
-    protected void onFirstTick() {
-
-    }
-
-    @Override
-    protected void onTick() {
-
-    }
+  @Override
+  protected void onTick() {}
 }
