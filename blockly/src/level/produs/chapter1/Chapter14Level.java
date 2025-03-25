e70b27d496904ef07613bf919a76c220c6786e28
Andre Matutat <andre.matutat@hsbi.de>
2025-03-25 13:10:13 +0100

    level 1.4 without guards

--- blockly/src/level/produs/chapter1/Chapter14Level.java
+++ blockly/src/level/produs/chapter1/Chapter14Level.java
@@ -1,9 +1,17 @@
 package level.produs.chapter1;
 
+import core.Entity;
+import core.Game;
+import core.components.CameraComponent;
+import core.components.PositionComponent;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
 import java.util.List;
+
+import core.systems.CameraSystem;
+import entities.BlocklyMonsterFactory;
+import entities.MiscFactory;
 import level.BlocklyLevel;
 
 public class Chapter14Level extends BlocklyLevel {
@@ -22,7 +30,30 @@ public class Chapter14Level extends BlocklyLevel {
   }
 
   @Override
-  protected void onFirstTick() {}
+  protected void onFirstTick() {
+    Game.hero().get().remove(CameraComponent.class);
+    Entity focusPoint = new Entity();
+    focusPoint.add(new PositionComponent(7,6));
+    focusPoint.add(new CameraComponent());
+    Game.add(focusPoint);
+
+    Coordinate stone1C= customPoints().get(1);
+    Coordinate stone2C= customPoints().get(5);
+    Coordinate m1C = customPoints().get(0);
+    Coordinate m2C = customPoints().get(2);
+    Coordinate m3C = customPoints().get(3);
+    Coordinate m4C = customPoints().get(4);
+
+    Game.add(MiscFactory.stone(stone1C.toCenteredPoint()));
+    Game.add(MiscFactory.stone(stone2C.toCenteredPoint()));
+    //TODO replace with guards
+    Game.add(BlocklyMonsterFactory.guard(m1C,"Monster 1",PositionComponent.Direction.LEFT));
+    Game.add(BlocklyMonsterFactory.guard(m2C,"Monster 2",PositionComponent.Direction.RIGHT));
+    Game.add(BlocklyMonsterFactory.guard(m3C,"Monster 3",PositionComponent.Direction.UP));
+    Game.add(BlocklyMonsterFactory.guard(m4C,"Monster 4",PositionComponent.Direction.UP));
+
+
+  }
 
   @Override
   protected void onTick() {}
