2b156e29143c73637cf2e6fe9713a603f96a01ea
Andre Matutat <andre.matutat@hsbi.de>
2025-03-26 10:29:23 +0100

    finish lvl 19

--- blockly/src/level/produs/chapter1/Chapter19Level.java
+++ blockly/src/level/produs/chapter1/Chapter19Level.java
@@ -1,9 +1,13 @@
 package level.produs.chapter1;
 
+import core.Game;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
 import java.util.List;
+
+import entities.BlocklyMonsterFactory;
+import entities.MiscFactory;
 import level.BlocklyLevel;
 
 public class Chapter19Level extends BlocklyLevel {
@@ -22,7 +26,14 @@ public class Chapter19Level extends BlocklyLevel {
   }
 
   @Override
-  protected void onFirstTick() {}
+  protected void onFirstTick() {
+    Game.add(MiscFactory.fireballScroll(customPoints().get(0).toCenteredPoint()));
+    Game.add(MiscFactory.fireballScroll(customPoints().get(1).toCenteredPoint()));
+    Game.add(BlocklyMonsterFactory.hedgehog(customPoints().get(2),"guard 1"));
+    Game.add(BlocklyMonsterFactory.hedgehog(customPoints().get(3),"guard 2"));
+    Game.add(BlocklyMonsterFactory.hedgehog(customPoints().get(4),"guard 3"));
+
+  }
 
   @Override
   protected void onTick() {}
