58d02693b0b56268e7ebb21db2d40d8fb72a5ddd
Andre Matutat <andre.matutat@hsbi.de>
2025-03-26 10:26:06 +0100

    lvl 18 wihout guards

--- blockly/src/level/produs/chapter1/Chapter18Level.java
+++ blockly/src/level/produs/chapter1/Chapter18Level.java
@@ -1,10 +1,15 @@
 package level.produs.chapter1;
 
+import core.Game;
+import core.components.PositionComponent;
 import core.level.utils.Coordinate;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
+import entities.BlocklyMonsterFactory;
+import entities.MiscFactory;
 import java.util.List;
 import level.BlocklyLevel;
+import level.Utils;
 
 public class Chapter18Level extends BlocklyLevel {
 
@@ -22,7 +27,30 @@ public class Chapter18Level extends BlocklyLevel {
   }
 
   @Override
-  protected void onFirstTick() {}
+  protected void onFirstTick() {
+    Utils.cameraFocusOn(new Coordinate(8, 6));
+    Game.add(MiscFactory.fireballScroll(customPoints().get(0).toCenteredPoint()));
+    Game.add(MiscFactory.fireballScroll(customPoints().get(1).toCenteredPoint()));
+    Game.add(MiscFactory.fireballScroll(customPoints().get(2).toCenteredPoint()));
+    Game.add(MiscFactory.fireballScroll(customPoints().get(3).toCenteredPoint()));
+    Game.add(MiscFactory.fireballScroll(customPoints().get(4).toCenteredPoint()));
+
+    Game.add(
+        BlocklyMonsterFactory.guard(
+            customPoints().get(5), "guard 1", PositionComponent.Direction.DOWN));
+    Game.add(
+        BlocklyMonsterFactory.guard(
+            customPoints().get(6), "guard 2", PositionComponent.Direction.RIGHT));
+    Game.add(
+        BlocklyMonsterFactory.guard(
+            customPoints().get(7), "guard 3", PositionComponent.Direction.RIGHT));
+    Game.add(
+        BlocklyMonsterFactory.guard(
+            customPoints().get(8), "guard 4", PositionComponent.Direction.DOWN));
+    Game.add(
+        BlocklyMonsterFactory.guard(
+            customPoints().get(9), "guard 5", PositionComponent.Direction.DOWN));
+  }
 
   @Override
   protected void onTick() {}
