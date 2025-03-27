e191e0a41c9024f772909073d15db028d327b8ac
Andre Matutat <andre.matutat@hsbi.de>
2025-03-27 07:22:26 +0100

    lvl 110 without guards

--- blockly/src/entities/BlocklyMonsterFactory.java
+++ blockly/src/entities/BlocklyMonsterFactory.java
@@ -1,6 +1,7 @@
 package entities;
 
 import contrib.components.CollideComponent;
+import contrib.components.HealthComponent;
 import contrib.components.SpikyComponent;
 import contrib.utils.components.health.DamageType;
 import core.Entity;
@@ -8,12 +9,15 @@ import core.Game;
 import core.components.DrawComponent;
 import core.components.PositionComponent;
 import core.level.utils.Coordinate;
+import core.utils.components.draw.CoreAnimations;
 import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class BlocklyMonsterFactory {
   private static final IPath HEDGEHOG = new SimpleIPath("character/monster/big_daemon");
+  private static final IPath KNIGHT = new SimpleIPath("character/knight");
 
   public static Entity hedgehog(Coordinate coordinate, String name) {
     Entity entity = new Entity(name);
@@ -34,5 +38,28 @@ public class BlocklyMonsterFactory {
       Coordinate coordinate, String name, PositionComponent.Direction viewDirection) {
     // TODO REPLACE WITH GUARD
     return hedgehog(coordinate, name);
+  }
+
+  public static Entity knight(
+      Coordinate coordinate,
+      String name,
+      PositionComponent.Direction viewDirection,
+      int hp,
+      Consumer<Entity> onDeath) {
+    Entity entity = new Entity(name);
+    PositionComponent pc = new PositionComponent();
+    pc.position(Game.tileAT(coordinate));
+    entity.add(pc);
+    entity.add(new CollideComponent());
+    entity.add(new SpikyComponent(100, DamageType.PHYSICAL, 0));
+    entity.add(new HealthComponent(hp, onDeath));
+    try {
+      DrawComponent dc = new DrawComponent(KNIGHT);
+      dc.currentAnimation(CoreAnimations.IDLE_UP);
+      entity.add(new DrawComponent(KNIGHT));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return entity;
   }
 }
