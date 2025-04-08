1c89a26eaa977ef9c818debacec47f390e29d956
Andre Matutat <andre.matutat@hsbi.de>
2025-04-08 16:38:16 +0200

    extract knight to blocklymonster

--- blockly/src/entities/BlocklyMonsterFactory.java
+++ blockly/src/entities/BlocklyMonsterFactory.java
@@ -1,22 +1,14 @@
 package entities;
 
-import contrib.components.CollideComponent;
 import contrib.components.HealthComponent;
-import contrib.components.SpikyComponent;
-import contrib.utils.components.health.DamageType;
 import core.Entity;
-import core.Game;
-import core.components.DrawComponent;
 import core.components.PositionComponent;
 import core.level.utils.Coordinate;
-import core.utils.components.draw.CoreAnimations;
 import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
-import java.io.IOException;
 import java.util.function.Consumer;
 
 public class BlocklyMonsterFactory {
-  private static final IPath HEDGEHOG = new SimpleIPath("character/monster/big_daemon");
   private static final IPath KNIGHT = new SimpleIPath("character/knight");
 
   public static Entity hedgehog(Coordinate coordinate) {
@@ -36,25 +28,13 @@ public class BlocklyMonsterFactory {
   }
 
   public static Entity knight(
-      Coordinate coordinate,
-      String name,
-      PositionComponent.Direction viewDirection,
-      int hp,
-      Consumer<Entity> onDeath) {
-    Entity entity = new Entity(name);
-    PositionComponent pc = new PositionComponent();
-    pc.position(Game.tileAT(coordinate));
-    entity.add(pc);
-    entity.add(new CollideComponent());
-    entity.add(new SpikyComponent(100, DamageType.PHYSICAL, 0));
-    entity.add(new HealthComponent(hp, onDeath));
-    try {
-      DrawComponent dc = new DrawComponent(KNIGHT);
-      dc.currentAnimation(CoreAnimations.IDLE_UP);
-      entity.add(new DrawComponent(KNIGHT));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    return entity;
+      Coordinate coordinate, PositionComponent.Direction viewDirection, Consumer<Entity> onDeath) {
+    BlocklyMonster.BlocklyMonsterBuilder builder = BlocklyMonster.BLACK_KNIGHT.builder();
+    builder.spawnPoint(coordinate.toCenteredPoint());
+    builder.viewDirection(viewDirection);
+    builder.range(0);
+    Entity knight = builder.build().orElseThrow();
+    knight.fetch(HealthComponent.class).orElseThrow().onDeath(onDeath);
+    return knight;
   }
 }
