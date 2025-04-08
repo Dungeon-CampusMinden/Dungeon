631802c3f0419d0fa9749ca9ac5b948a512ce1fc
Andre Matutat <andre.matutat@hsbi.de>
2025-04-08 16:22:53 +0200

    add level files

---
+++ blockly/src/level/produs/Chapter22Level.java
@@ -0,0 +1,29 @@
+package level.produs;
+
+import core.level.utils.Coordinate;
+import core.level.utils.DesignLabel;
+import core.level.utils.LevelElement;
+import java.util.List;
+import level.BlocklyLevel;
+
+public class Chapter22Level extends BlocklyLevel {
+
+  /**
+   * Call the parent constructor of a tile level with the given layout and design label. Set the
+   * start tile of the hero to the given heroPos.
+   *
+   * @param layout 2D array containing the tile layout.
+   * @param designLabel The design label for the level.
+   * @param customPoints The custom points of the level.
+   */
+  public Chapter22Level(
+      LevelElement[][] layout, DesignLabel designLabel, List<Coordinate> customPoints) {
+    super(layout, designLabel, customPoints, "Kapitel 2: Level 2");
+  }
+
+  @Override
+  protected void onFirstTick() {}
+
+  @Override
+  protected void onTick() {}
+}
---
+++ blockly/src/level/produs/Chapter34Level.java
@@ -0,0 +1,29 @@
+package level.produs;
+
+import core.level.utils.Coordinate;
+import core.level.utils.DesignLabel;
+import core.level.utils.LevelElement;
+import java.util.List;
+import level.BlocklyLevel;
+
+public class Chapter34Level extends BlocklyLevel {
+
+  /**
+   * Call the parent constructor of a tile level with the given layout and design label. Set the
+   * start tile of the hero to the given heroPos.
+   *
+   * @param layout 2D array containing the tile layout.
+   * @param designLabel The design label for the level.
+   * @param customPoints The custom points of the level.
+   */
+  public Chapter34Level(
+      LevelElement[][] layout, DesignLabel designLabel, List<Coordinate> customPoints) {
+    super(layout, designLabel, customPoints, "Kapitel 3: Level 4");
+  }
+
+  @Override
+  protected void onFirstTick() {}
+
+  @Override
+  protected void onTick() {}
+}
