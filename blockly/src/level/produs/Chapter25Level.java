9ff6c02286c8a589e7c730314ed04c5d2cc231a8
Andre Matutat <andre.matutat@hsbi.de>
2025-04-08 16:22:53 +0200

    add level files

---
+++ blockly/src/level/produs/Chapter25Level.java
@@ -0,0 +1,29 @@
+package level.produs;
+
+import core.level.utils.Coordinate;
+import core.level.utils.DesignLabel;
+import core.level.utils.LevelElement;
+import java.util.List;
+import level.BlocklyLevel;
+
+public class Chapter25Level extends BlocklyLevel {
+
+  /**
+   * Call the parent constructor of a tile level with the given layout and design label. Set the
+   * start tile of the hero to the given heroPos.
+   *
+   * @param layout 2D array containing the tile layout.
+   * @param designLabel The design label for the level.
+   * @param customPoints The custom points of the level.
+   */
+  public Chapter25Level(
+      LevelElement[][] layout, DesignLabel designLabel, List<Coordinate> customPoints) {
+    super(layout, designLabel, customPoints, "Kapitel 2: Level 5");
+  }
+
+  @Override
+  protected void onFirstTick() {}
+
+  @Override
+  protected void onTick() {}
+}
