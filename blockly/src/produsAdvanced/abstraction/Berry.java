06eedb65f179a7faa529f9ecf58567ddc607326b
Andre Matutat <andre.matutat@hsbi.de>
2025-05-13 12:37:43 +0200

    javadoc + cleanup

--- blockly/src/produsAdvanced/abstraction/Berry.java
+++ blockly/src/produsAdvanced/abstraction/Berry.java
@@ -12,27 +12,46 @@ import core.utils.components.path.SimpleIPath;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Eine Beere, die als Item im Spiel verwendet werden kann.
+ *
+ * <p>Je nach Konstruktorparameter kann die Beere giftig sein. Die Beere besitzt außerdem eine
+ * Textur und kann im Spiel visuell angepasst werden (z. B. durch Änderung der Farbe oder des
+ * Bildes).
+ */
 public class Berry extends Item {
 
+  /** Pfad zur Textur eines Donuts (optional verwendbar). */
   public static String DONUT_TEXTURE = "items/resource/donut.png";
+
+  /** Pfad zur Standardtextur der Beere. */
   public static String BERRY_TEXTURE = "items/resource/berry.png";
 
+  /** Standard-Texturpfad, der bei Initialisierung verwendet wird. */
   private static final IPath DEFAULT_TEXTURE = new SimpleIPath(BERRY_TEXTURE);
+
+  /** Gibt an, ob die Beere giftig ist. */
   private final boolean toxic;
 
+  // Registrierung dieser Item-Klasse im Spielsystem
   static {
     Item.registerItem(Berry.class);
   }
 
+  /**
+   * Erzeugt eine neue Beere mit einer optionalen Giftigkeit.
+   *
+   * @param toxic true, wenn die Beere giftig sein soll; false sonst
+   */
   public Berry(boolean toxic) {
     super("Eine Beere.", " Könnte sie giftig sein?", Animation.fromSingleImage(DEFAULT_TEXTURE));
     this.toxic = toxic;
   }
 
   /**
-   * 0xffffffcc
+   * Verändert die Tönung (Farbe) der Beeren-Textur im Spiel.
    *
-   * @param color
+   * @param color Farbwert als Integer (ARGB), z. B. 0xffffffcc für gelblich
    */
   public void tintColor(int color) {
     Entity berry = getEntity();
@@ -40,6 +59,11 @@ public class Berry extends Item {
     dc.tintColor(color);
   }
 
+  /**
+   * Ändert die aktuell verwendete Textur der Beere sowohl im Inventar als auch in der Spielwelt.
+   *
+   * @param texture Pfad zur neuen Texturdatei
+   */
   public void changeTexture(String texture) {
     this.worldAnimation(Animation.fromSingleImage(new SimpleIPath(texture)));
     this.inventoryAnimation(Animation.fromSingleImage(new SimpleIPath(texture)));
@@ -52,13 +76,30 @@ public class Berry extends Item {
     return;
   }
 
+  /**
+   * Definiert die Wirkung der Beere beim Gebrauch.
+   *
+   * <p>Aktuell nicht implementiert.
+   *
+   * @param user Die Entity, die die Beere verwendet
+   */
   @Override
   public void use(final Entity user) {}
 
+  /**
+   * Gibt an, ob diese Beere giftig ist.
+   *
+   * @return true, wenn die Beere giftig ist; false sonst
+   */
   public boolean isToxic() {
     return toxic;
   }
 
+  /**
+   * Ermittelt die zu dieser Item-Instanz gehörende Entity im Spiel.
+   *
+   * @return Die zugehörige {@link Entity}, oder null, wenn keine gefunden wurde
+   */
   private Entity getEntity() {
     Set<Entity> berrys =
         Game.allEntities()
