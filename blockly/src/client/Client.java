9c7ef5fce0ba8a5c7c2122a7438c94436f78f7c6
Flamtky <niklas.schumann+github@proton.me>
2025-06-03 13:08:19 +0200

    fix levels

--- blockly/src/client/Client.java
+++ blockly/src/client/Client.java
@@ -74,7 +74,33 @@ public class Client {
   private static void onSetup() {
     Game.userOnSetup(
         () -> {
+          // chapter 1
+          DevDungeonLoader.addLevel(Tuple.of("level1", Chapter11Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level2", Chapter12Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level3", Chapter13Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level4", Chapter14Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level5", Chapter15Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level6", Chapter16Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level7", Chapter17Level.class));
           DevDungeonLoader.addLevel(Tuple.of("level8", Chapter18Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level9", Chapter19Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level10", Chapter110Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level11", Chapter111Level.class));
+
+          // chapter 2
+          DevDungeonLoader.addLevel(Tuple.of("level12", Chapter21Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level13", Chapter22Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level14", Chapter23Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level15", Chapter24Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level16", Chapter25Level.class));
+
+          // chapter 3
+          DevDungeonLoader.addLevel(Tuple.of("level17", Chapter31Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level18", Chapter32Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level19", Chapter33Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level20", Chapter34Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level21", Chapter35Level.class));
+          DevDungeonLoader.addLevel(Tuple.of("level22", Chapter36Level.class));
 
           createSystems();
 
