0042677f304a50de8e402355232c196073905a77
Carsten Gips <cagix@hsbi.de>
2025-06-11 16:57:26 +0200

    Clean-up of unmaintained projects: remove Dungine (#2105)

--- dungine/src/main/java/de/fwatermann/dungine/event/EventListener.java
+++
@@ -1,7 +0,0 @@
-package de.fwatermann.dungine.event;
-
-/**
- * Represents an event listener. This interface serves as a marker for all classes that are intended
- * to listen and respond to events.
- */
-public interface EventListener {}
