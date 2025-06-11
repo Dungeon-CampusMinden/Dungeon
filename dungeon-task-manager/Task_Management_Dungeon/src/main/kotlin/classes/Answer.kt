97e05001d1522736f3859f35f254f8b6c0bd76db
Carsten Gips <cagix@hsbi.de>
2025-06-11 16:57:00 +0200

    Clean-up of unmaintained projects: remove Task-Manager (#2104)

--- dungeon-task-manager/Task_Management_Dungeon/src/main/kotlin/classes/Answer.kt
+++
@@ -1,6 +0,0 @@
-package classes
-
-data class Answer(
-    val id : Int,
-    val answer: String
-)
--- dungeon-task-manager/Task_Management_Dungeon/src/main/kotlin/classes/DependencyType.kt
+++
@@ -1,5 +0,0 @@
-package classes
-
-enum class DependencyType {
-    SUBTASK_MANDATORY, SUBTASK_OPTIONAL, SEQUENCE, CONDITONAL_FALSE, CONDITIONAL_CORRECT
-}
