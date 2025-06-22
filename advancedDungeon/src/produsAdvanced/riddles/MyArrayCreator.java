1c4dfbb6df472b09a7b25892f2bb63e5238ec115
wefeke <kennet.wefelmeier@hsbi.de>
2025-06-22 14:42:48 +0200

    Fixed Texts for Array Level

--- advancedDungeon/src/produsAdvanced/riddles/MyArrayCreator.java
+++ advancedDungeon/src/produsAdvanced/riddles/MyArrayCreator.java
@@ -12,8 +12,10 @@ public class MyArrayCreator extends ArrayCreator {
    * Implementiere die Methode countMonstersInRooms(), die ein Array mit der korrekten Anzahl von
    * Monstern zurückgibt.
    *
-   * <p>Die Aufgabe: - Erstelle ein Array der Länge 5 - Trage an der jeweiligen Position (Index) die
+   * Die Aufgabe: - Erstelle ein Array der Länge 5 - Trage an der jeweiligen Position (Index) die
    * Anzahl der Monster ein
+   *
+   * Hinweise: Index und Raumnummer könnten etwas miteinander zu tun haben.
    *
    * @return Ein Integer-Array der Länge 5, wobei jeder Index die Anzahl der Monster im
    *     entsprechenden Raum enthält
--- advancedDungeon/src/produsAdvanced/riddles/MyArrayRemover.java
+++ advancedDungeon/src/produsAdvanced/riddles/MyArrayRemover.java
@@ -24,14 +24,14 @@ public class MyArrayRemover extends ArrayRemover {
    * Implementiere die Methode entfernePositionen(), die ein neues Array ohne leere Monsterräume
    * erstellt.
    *
-   * <p>Die Aufgabe: - Du erhältst das Array 'monsterArray' mit Monstern in verschiedenen Räumen -
+   * Die Aufgabe: - Du erhältst das Array 'monsterArray' mit Monstern in verschiedenen Räumen -
    * Ändere die Monsteranzahl in den Räumen, in denen jetzt neue Monster stehen - Räume ohne Monster
-   * sollen nicht im neuen Array vorkommen
+   * sollen NICHT im neuen Array vorkommen
    *
-   * <p>Beispiel für das Array: monsterArray = [5, 0, 3, 0, 2] // 5 Monster in Raum 0, keine in Raum
+   * Beispiel für das Array: monsterArray = [5, 0, 3, 0, 2] // 5 Monster in Raum 0, keine in Raum
    * 1, 3 in Raum 2, usw.
    *
-   * <p>Hinweise: - Arrays haben in Java eine feste Größe - Wenn du einen Eintrag in einem Array
+   * Hinweise: - Arrays haben in Java eine feste Größe - Wenn du einen Eintrag in einem Array
    * löschen möchtest, muss sich das Array also verkleinern - Das geht nicht direkt, aber du kannst
    * ein neues Array erstellen und die Werte übertragen
    *
