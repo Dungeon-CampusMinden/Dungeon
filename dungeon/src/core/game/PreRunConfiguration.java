9012bfe113a7fab2d3a7e13f968cbab3204f1af1
Flamtky <niklas.schumann+github@proton.me>
2025-07-28 14:09:34 +0200

    remove local logic to instead always use an server

--- dungeon/src/core/game/PreRunConfiguration.java
+++ dungeon/src/core/game/PreRunConfiguration.java
@@ -277,12 +277,4 @@ public final class PreRunConfiguration {
   public static void loadConfig(final IPath path, Class<?>... klass) throws IOException {
     Configuration.loadAndGetConfiguration(path, klass);
   }
-
-  /**
-   * Checks if the game is running on a server.
-   * @return True if the game is running on a server, false otherwise.
-   */
-  public static boolean isServer() {
-    return false; // TODO: implement me
-  }
 }
