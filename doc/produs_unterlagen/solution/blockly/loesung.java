56996afcda2620f4d48f1aab856d0facc8348a09
erenuez <84851615+erenuez@users.noreply.github.com>
2025-07-28 16:24:15 +0200

    Update loesung.java

--- doc/produs_unterlagen/solution/blockly/loesung.java
+++ doc/produs_unterlagen/solution/blockly/loesung.java
@@ -1,20 +1,20 @@
-hero.move();
-hero.move();
-hero.move();
-hero.rotate(Direction.LEFT);
 while(true){
-    if(hero.isNearTile(LevelElement.WALL, Direction.UP)){
-        if(hero.isNearTile(LevelElement.WALL, Direction.RIGHT)){
-            hero.rotate(Direction.LEFT);
-            hero.move();
+    if(hero.checkBossViewDirection(Direction.RIGHT)){
+        if(hero.isNearTile(LevelElement.WALL, Direction.UP) || hero.isNearTile(LevelElement.HOLE, Direction.UP) || hero.isNearTile(LevelElement.PIT, Direction.UP)){
+            if((!hero.isNearTile(LevelElement.WALL, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.HOLE, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.PIT, Direction.RIGHT))){
+                hero.rotate(Direction.RIGHT);
+            }else{
+                hero.rotate(Direction.LEFT);
+            }
         }else{
-            hero.rotate(Direction.RIGHT);
+            if((!hero.isNearTile(LevelElement.WALL, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.HOLE, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.PIT, Direction.RIGHT))){
+                hero.rotate(Direction.RIGHT);
+            }
+        }
+        if(!hero.isNearTile(LevelElement.PIT, Direction.UP)){
             hero.move();
         }
     }else{
-        if(!hero.isNearTile(LevelElement.WALL, Direction.RIGHT)){
-            hero.rotate(Direction.RIGHT);
-        }
-        hero.move();
+        hero.rest();
     }
 }
