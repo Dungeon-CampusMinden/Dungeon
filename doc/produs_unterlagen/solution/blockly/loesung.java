982a8d882186745e65c3e61653c4c30ca234471c
erenuez <84851615+erenuez@users.noreply.github.com>
2025-07-30 00:03:25 +0200

    Aktualisierung der Lösungen

--- doc/produs_unterlagen/solution/blockly/loesung.java
+++ doc/produs_unterlagen/solution/blockly/loesung.java
@@ -1,20 +1,38 @@
-while(true){
+hero.rotate(Direction.RIGHT);
+while(!hero.isNearTile(LevelElement.WALL, Direction.UP)){
     if(hero.checkBossViewDirection(Direction.RIGHT)){
-        if(hero.isNearTile(LevelElement.WALL, Direction.UP) || hero.isNearTile(LevelElement.HOLE, Direction.UP) || hero.isNearTile(LevelElement.PIT, Direction.UP)){
-            if((!hero.isNearTile(LevelElement.WALL, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.HOLE, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.PIT, Direction.RIGHT))){
-                hero.rotate(Direction.RIGHT);
-            }else{
-                hero.rotate(Direction.LEFT);
-            }
-        }else{
-            if((!hero.isNearTile(LevelElement.WALL, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.HOLE, Direction.RIGHT)) && (!hero.isNearTile(LevelElement.PIT, Direction.RIGHT))){
-                hero.rotate(Direction.RIGHT);
-            }
-        }
-        if(!hero.isNearTile(LevelElement.PIT, Direction.UP)){
-            hero.move();
-        }
-    }else{
         hero.rest();
+    }else{
+        hero.move();
     }
 }
+hero.rotate(Direction.LEFT);
+while(!hero.isNearTile(LevelElement.WALL, Direction.UP)){
+    if(hero.checkBossViewDirection(Direction.RIGHT)){
+        hero.rest();
+    }else{
+        hero.move();
+    }
+}
+hero.rotate(Direction.LEFT);
+while(hero.isNearTile(LevelElement.WALL, Direction.RIGHT)){
+    if(hero.checkBossViewDirection(Direction.RIGHT)){
+        hero.rest();
+    }else{
+        hero.move();
+    }
+}
+if(hero.checkBossViewDirection(Direction.RIGHT)){
+        hero.rest();
+    }else{
+        hero.move();
+    }
+hero.rotate(Direction.RIGHT);
+while(!hero.isNearTile(LevelElement.PIT, Direction.RIGHT)){
+    if(hero.checkBossViewDirection(Direction.RIGHT)){
+        hero.rest();
+    }else{
+        hero.move();
+    }
+}
+hero.rotate(Direction.RIGHT);
