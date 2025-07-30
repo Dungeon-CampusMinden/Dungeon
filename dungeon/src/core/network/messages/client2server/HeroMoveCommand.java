9d094b5fa6823263ed5ff47fe0c06cc3f8da5248
Flamtky <niklas.schumann+github@proton.me>
2025-07-30 18:49:44 +0200

    refactor code to use own records

--- dungeon/src/core/network/messages/client2server/HeroMoveCommand.java
+++ dungeon/src/core/network/messages/client2server/HeroMoveCommand.java
@@ -1,11 +1,43 @@
 package core.network.messages.client2server;
 
-import core.network.messages.NetworkMessage;
+import static contrib.entities.HeroFactory.ENABLE_MOUSE_MOVEMENT;
+
+import contrib.components.PathComponent;
+import core.Game;
+import core.components.VelocityComponent;
 import core.utils.Direction;
+import core.utils.Vector2;
+import core.utils.components.MissingComponentException;
 
 /**
  * Record representing a hero movement command.
  *
  * @param direction The direction of movement.
  */
-public record HeroMoveCommand(Direction direction) implements NetworkMessage {}
+public record HeroMoveCommand(Direction direction) implements ClientMessage {
+  @Override
+  public void process() {
+    Game.hero()
+        .ifPresent(
+            hero -> {
+              VelocityComponent vc =
+                  hero.fetch(VelocityComponent.class)
+                      .orElseThrow(
+                          () -> MissingComponentException.build(hero, VelocityComponent.class));
+
+              Vector2 newVelocity = vc.currentVelocity();
+              if (direction.x() != 0) {
+                newVelocity = Vector2.of(direction.scale(vc.velocity()).x(), newVelocity.y());
+              }
+              if (direction.y() != 0) {
+                newVelocity = Vector2.of(newVelocity.x(), direction.scale(vc.velocity()).y());
+              }
+              vc.currentVelocity(newVelocity);
+
+              // Abort any path finding on own movement
+              if (ENABLE_MOUSE_MOVEMENT) {
+                hero.fetch(PathComponent.class).ifPresent(PathComponent::clear);
+              }
+            });
+  }
+}
