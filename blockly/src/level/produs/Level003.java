51f09d1db09b30a170e629ffa6b433207a4a5aba
Mohamed Tarek Dawalibi <tarek.dawalibi@hotmail.com>
2025-08-07 12:54:26 +0200

    Level003.java: Refactored onFirstTick to close DoorTile via Optional<Tile> without null

--- blockly/src/level/produs/Level003.java
+++ blockly/src/level/produs/Level003.java
@@ -65,8 +65,11 @@ public class Level003 extends BlocklyLevel {
     Coordinate stone2C = customPoints().get(1);
     Game.add(MiscFactory.stone(stone1C.toCenteredPoint()));
     Game.add(MiscFactory.stone(stone2C.toCenteredPoint()));
-    DoorTile door = (DoorTile) Game.tileAT(new Coordinate(0, 5));
-    door.close();
+
+    Game.tileAT(new Coordinate(0, 5))
+      .filter(t -> t instanceof DoorTile)
+      .map(t -> (DoorTile) t)
+      .ifPresent(DoorTile::close);
   }
 
   @Override
