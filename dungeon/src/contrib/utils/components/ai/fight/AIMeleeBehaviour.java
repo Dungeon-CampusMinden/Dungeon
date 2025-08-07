616520b61b84245583cb216e1407c03d56297def
Kevin Zuber <130918494+kzuber93@users.noreply.github.com>
2025-08-07 14:32:28 +0200

    Dungeon: refactor `MeleeAI` (#2281)
    Die Klasse hatte folgendes Problem:
    
    - Die Verfolgungslogik war bereits in der selben Art und Weise in der
    Klasse `AIChaseBehaviour` (alter Name: `CollideAI`) vorhanden.
    
    Deswegen habe ich die Logik an `AIChaseBehaviour` delegiert, um
    Code-Duplizierung zu vermeiden.
    Zusätzlich habe ich den Klassennamen `MeleeAI` in `AIMeleeBehaviour`
    umbenannt, um eine Namensvereinheitlichung zu gewährleisten und die
    Funktionalität der Klasse besser zu beschreiben.

--- dungeon/src/contrib/utils/components/ai/fight/AIMeleeBehaviour.java
+++ dungeon/src/contrib/utils/components/ai/fight/AIMeleeBehaviour.java
@@ -1,50 +1,40 @@
 package contrib.utils.components.ai.fight;
 
-import com.badlogic.gdx.ai.pfa.GraphPath;
-import contrib.utils.components.ai.AIUtils;
 import contrib.utils.components.skill.ISkillUser;
 import contrib.utils.components.skill.Skill;
 import core.Entity;
-import core.Game;
-import core.level.Tile;
 import core.level.utils.LevelUtils;
 import java.util.function.Consumer;
 
 /**
  * Implements a fight AI. The entity attacks the player if he is in a given range. When the entity
- * is not in range but in fight mode, the entity will be moving to ward the player.
+ * is not in range but in fight mode, the entity will be moving towards the player.
  *
  * @see ISkillUser
  */
-public class MeleeAI implements Consumer<Entity>, ISkillUser {
+public class AIMeleeBehaviour extends AIChaseBehaviour implements Consumer<Entity>, ISkillUser {
   private final float attackRange;
-  private final int delay = Game.frameRate();
   private Skill fightSkill;
-  private int timeSinceLastUpdate = 0;
-  private GraphPath<Tile> path;
 
   /**
    * Attacks the player if he is within the given range. Otherwise, it will move towards the player.
    *
+   * @param chaseRange Range in which the entity will chase the player.
    * @param attackRange Range in which the attack skill should be executed.
    * @param fightSkill Skill to be used when an attack is performed.
    */
-  public MeleeAI(final float attackRange, final Skill fightSkill) {
+  public AIMeleeBehaviour(float chaseRange, float attackRange, Skill fightSkill) {
+    super(chaseRange);
     this.attackRange = attackRange;
     this.fightSkill = fightSkill;
   }
 
   @Override
-  public void accept(final Entity entity) {
+  public void accept(Entity entity) {
     if (LevelUtils.playerInRange(entity, attackRange)) {
       useSkill(fightSkill, entity);
     } else {
-      if (path == null || timeSinceLastUpdate >= delay) {
-        path = LevelUtils.calculatePathToHero(entity);
-        timeSinceLastUpdate = -1;
-      }
-      timeSinceLastUpdate++;
-      AIUtils.move(entity, path);
+      super.accept(entity);
     }
   }
 
