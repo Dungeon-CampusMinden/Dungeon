f6199a9feba30486babae567b593f7ff7210d08e
Flamtky <niklas.schumann+github@proton.me>
2025-08-09 15:09:13 +0200

    fix rebase

--- dungeon/src/core/game/GameLoop.java
+++ dungeon/src/core/game/GameLoop.java
@@ -246,13 +246,6 @@ public final class GameLoop extends ScreenAdapter {
                         .fetch(DrawComponent.class)
                         .ifPresentOrElse(
                             dc -> {
-                              java.lang.System.out.println(
-                                  "Updating DrawComponent for entity "
-                                      + drawUpdate.entityId()
-                                      + " with animation "
-                                      + drawUpdate.animationName()
-                                      + " and tint color "
-                                      + drawUpdate.tintColor());
                               dc.currentAnimation(drawUpdate.animationName());
                               dc.tintColor(drawUpdate.tintColor());
                             },
@@ -379,6 +372,5 @@ public final class GameLoop extends ScreenAdapter {
     ECSManagment.add(new InputSystem());
     ECSManagment.add(new FrictionSystem());
     ECSManagment.add(new MoveSystem());
-    ECSManagment.add(new InputSystem());
   }
 }
