51086c7139b0ba00264d2541a47a1427a1113c29
Andre Matutat <andre.matutat@hsbi.de>
2025-08-12 12:08:03 +0200

    checkstyle

--- escapeRoom/src/starter/Tutorial.java
+++ escapeRoom/src/starter/Tutorial.java
@@ -1,6 +1,12 @@
 package starter;
 
+/** Tutorial starter für das Escape Room Projekt. */
 public class Tutorial {
+  /**
+   * Main methode.
+   *
+   * @param args command line arguments
+   */
   public static void main(String[] args) {
     System.out.println("Hell World");
   }
