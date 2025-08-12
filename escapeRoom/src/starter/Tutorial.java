0570602c32a9a8e715bda1a6cf20ee39bdb7c59c
Andre Matutat <andre.matutat@hsbi.de>
2025-08-12 12:06:31 +0200

    spotless

--- escapeRoom/src/starter/Tutorial.java
+++ escapeRoom/src/starter/Tutorial.java
@@ -1,7 +1,7 @@
 package starter;
 
 public class Tutorial {
-    public static void main(String[]args){
-        System.out.println("Hell World");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hell World");
+  }
 }
