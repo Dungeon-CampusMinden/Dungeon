eb81b72be3398f9aa3758377365dddb6756e2988
M. Tarek Dawalibi <mohammed_tarek.dawalibi@hsbi.de>
2025-08-12 21:21:38 +0200

    ItemWoodenBow.java: Apply Spotless formatting to drop()

--- dungeon/src/contrib/item/concreteItem/ItemWoodenBow.java
+++ dungeon/src/contrib/item/concreteItem/ItemWoodenBow.java
@@ -70,13 +70,17 @@ public class ItemWoodenBow extends Item {
   @Override
   public boolean drop(final Point position) {
     Game.hero()
-      .flatMap(hero -> hero.fetch(PlayerComponent.class))
-      .ifPresent(pc -> pc.removeCallback(KeyboardConfig.SECOND_SKILL.value()));
+        .flatMap(hero -> hero.fetch(PlayerComponent.class))
+        .ifPresent(pc -> pc.removeCallback(KeyboardConfig.SECOND_SKILL.value()));
 
     return Game.tileAT(position)
-      .filter(FloorTile.class::isInstance)
-      .map(tile -> { Game.add(WorldItemBuilder.buildWorldItem(this, position)); return true; })
-      .orElse(false);
+        .filter(FloorTile.class::isInstance)
+        .map(
+            tile -> {
+              Game.add(WorldItemBuilder.buildWorldItem(this, position));
+              return true;
+            })
+        .orElse(false);
   }
 
   @Override
