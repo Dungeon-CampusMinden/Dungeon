5734a16413e3513ba8bca586d307935937c9a936
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2025-08-12 12:14:22 +0200

    Escaperoom: Setup basic project structure (#2314)
    Basic escape-room struktur
    
    Projekt umbenannt von escape-room in escapeRoom
    Grundstruktur des Projekts eingerichtet
    
    ---------
    
    Co-authored-by: Andre Matutat <andre.matutat@hsbi.de>

---
+++ escapeRoom/src/starter/Tutorial.java
@@ -0,0 +1,13 @@
+package starter;
+
+/** Tutorial starter für das Escape Room Projekt. */
+public class Tutorial {
+  /**
+   * Main methode.
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
