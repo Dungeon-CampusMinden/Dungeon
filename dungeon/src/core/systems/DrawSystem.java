88f70f387614d1f44344842902a49c2104b87b1c
Hannes Ru <viddie@hotmail.de>
2025-09-05 11:37:41 +0200

    Forgot spotless pass

--- dungeon/src/core/systems/DrawSystem.java
+++ dungeon/src/core/systems/DrawSystem.java
@@ -1,6 +1,5 @@
 package core.systems;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import core.Entity;
