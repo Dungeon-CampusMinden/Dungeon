43dc080c12d78dbf582bd382c06cc319ae203913
Flamtky <niklas.schumann+github@proton.me>
2025-09-10 16:59:07 +0200

    send components on spawn | fix render for real now

--- dungeon/src/core/network/messages/s2c/EntitySpawnEvent.java
+++ dungeon/src/core/network/messages/s2c/EntitySpawnEvent.java
@@ -1,5 +1,7 @@
 package core.network.messages.s2c;
 
+import core.components.DrawComponent;
+import core.components.PositionComponent;
 import core.network.messages.NetworkMessage;
 import core.utils.Direction;
 import core.utils.Point;
@@ -9,11 +11,8 @@ import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
 
 public record EntitySpawnEvent(
-    int entityId,
-    Point position,
-    Direction viewDirection,
-    String texturePath,
-    String initialState,
-    int tintColor)
+  int entityId,
+  PositionComponent positionComponent,
+  DrawComponent drawComponent)
     implements NetworkMessage {
 }
