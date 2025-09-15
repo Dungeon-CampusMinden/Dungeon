79cc0c81c7ad2b135f17d2dfffb7293add4b41a0
David Vehlow <david.vehlow@fh-bielefeld.de>
2025-09-15 14:12:29 +0200

    Add first draft for BombComponent

---
+++ dungeon/src/contrib/components/BombComponent.java
@@ -0,0 +1,29 @@
+package contrib.components;
+
+import core.Component;
+
+public class BombComponent implements Component {
+
+  private final float radius;
+  private final int damage;
+  private final long fuseMs;
+
+  public BombComponent(float radius, int damage, long fuseMs) {
+    this.radius = radius;
+    this.damage = damage;
+    this.fuseMs = fuseMs;
+  }
+
+  public float radius() {
+    return radius;
+  }
+
+  public int damage() {
+    return damage;
+  }
+
+  public long fuseMs() {
+    return fuseMs;
+  }
+
+}
