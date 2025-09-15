b7cefc7a2d36de25a00f82e4c72d2c8fd988093c
David Vehlow <david.vehlow@fh-bielefeld.de>
2025-09-15 13:58:55 +0200

    Add first draft for BombSkillComponent

---
+++ dungeon/src/contrib/components/BombSkillComponent.java
@@ -0,0 +1,37 @@
+package contrib.components;
+
+import core.Component;
+
+public final class BombSkillComponent implements Component {
+
+  public enum BombMode {
+    PLACE,
+    THROW
+  }
+
+  public enum BombType {
+    TIMED,
+    IMPACT
+  }
+
+  private BombMode mode = BombMode.PLACE;
+  private BombType type = BombType.TIMED;
+
+
+  public BombMode mode() {
+    return mode;
+  }
+
+  public void mode(BombMode m) {
+    this.mode = m;
+  }
+
+  public BombType type() {
+    return type;
+  }
+
+  public void type(BombType t) {
+    this.type = t;
+  }
+
+}
