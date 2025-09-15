548b8db822570e8c04d82fabe82ff8646bcc3b07
Flamtky <68606032+Flamtky@users.noreply.github.com>
2025-09-15 16:34:05 +0200

    server now uses dispatcher

--- dungeon/src/core/network/messages/c2s/RequestEntitySpawn.java
+++ dungeon/src/core/network/messages/c2s/RequestEntitySpawn.java
@@ -6,6 +6,6 @@ import core.network.messages.NetworkMessage;
  * A message from a client to the server to request the spawn of an entity. This is used when the
  * client receives a snapshot for an entity it does not know yet.
  *
- * @param entityName The name of the entity to spawn.
+ * @param entityId The id of the entity to spawn.
  */
 public record RequestEntitySpawn(int entityId) implements NetworkMessage {}
