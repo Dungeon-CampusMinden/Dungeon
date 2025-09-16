fb1d6153fac8b704f435b9a6afeea0c602b7fbb7
AMatutat <32962412+AMatutat@users.noreply.github.com>
2025-09-16 14:44:55 +0200

    cleanup optional tile handling

--- blockly/src/coderunner/BlocklyCommands.java
+++ blockly/src/coderunner/BlocklyCommands.java
@@ -283,27 +283,39 @@ public class BlocklyCommands {
   private static void movePushable(boolean push) {
     Entity hero = Game.hero().orElseThrow(MissingHeroException::new);
     DISABLE_SHOOT_ON_HERO = true;
+
     PositionComponent heroPC =
         hero.fetch(PositionComponent.class)
             .orElseThrow(() -> MissingComponentException.build(hero, PositionComponent.class));
     Direction viewDirection = heroPC.viewDirection();
 
-    Tile inFront =
-        Game.tileAt(heroPC.position().translate(MAGIC_OFFSET), viewDirection).orElse(null);
-    Tile checkTile;
+    Optional<Tile> inFrontOpt =
+        Game.tileAt(heroPC.position().translate(MAGIC_OFFSET), viewDirection);
+    if (inFrontOpt.isEmpty()) {
+      DISABLE_SHOOT_ON_HERO = false;
+      return;
+    }
+    Tile inFront = inFrontOpt.get();
+
     Direction moveDirection;
+    Optional<Tile> checkTileOpt;
+
     if (push) {
-      checkTile = Game.tileAt(inFront.position(), viewDirection).orElse(null);
+      checkTileOpt = Game.tileAt(inFront.position(), viewDirection);
       moveDirection = viewDirection;
     } else {
-      checkTile =
-          Game.tileAt(heroPC.position().translate(MAGIC_OFFSET), viewDirection.opposite())
-              .orElse(null);
+      checkTileOpt =
+          Game.tileAt(heroPC.position().translate(MAGIC_OFFSET), viewDirection.opposite());
       moveDirection = viewDirection.opposite();
     }
-    if (!checkTile.isAccessible()
-        || Game.entityAtTile(checkTile).anyMatch(e -> e.isPresent(BlockComponent.class))
-        || Game.entityAtTile(checkTile).anyMatch(e -> e.isPresent(AIComponent.class))) return;
+
+    if (checkTileOpt.isEmpty()
+        || !checkTileOpt.get().isAccessible()
+        || Game.entityAtTile(checkTileOpt.get()).anyMatch(e -> e.isPresent(BlockComponent.class))
+        || Game.entityAtTile(checkTileOpt.get()).anyMatch(e -> e.isPresent(AIComponent.class))) {
+      DISABLE_SHOOT_ON_HERO = false;
+      return;
+    }
     ArrayList<Entity> toMove =
         new ArrayList<>(
             Game.entityAtTile(inFront).filter(e -> e.isPresent(PushableComponent.class)).toList());
