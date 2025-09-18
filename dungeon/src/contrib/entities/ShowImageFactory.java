1027aa364f8a0c0a6092511e5389161949a37c2c
Hannes Ru <viddie@hotmail.de>
2025-09-18 15:19:20 +0200

    Refactor to fit the dungeons code style

--- dungeon/src/contrib/entities/ShowImageFactory.java
+++ dungeon/src/contrib/entities/ShowImageFactory.java
@@ -11,6 +11,7 @@ import core.utils.Vector2;
 import core.utils.components.path.SimpleIPath;
 import java.util.function.BiConsumer;
 
+/** Factory class for creating ShowImage entities. */
 public class ShowImageFactory {
 
   private static final float INTERACTION_RADIUS = 1.5f;
@@ -18,10 +19,17 @@ public class ShowImageFactory {
   private static final float Y_OFFSET = 0.25f;
 
   /**
-   * Creates a keypad at the designated position.
+   * Creates an entity that will show the specified image when interacted with, at the designated
+   * position.
    *
-   * @param pos The position where the lever will be created.
-   * @return The created keypad entity.
+   * @param pos the position of the entity
+   * @param spriteImage the path to the sprite image representing the entity
+   * @param imagePath the path of the image to be shown
+   * @param onClose action to be performed when the image is closed
+   * @param maxSize the maximum size the image should occupy on the screen, in its biggest axis
+   * @param radius the interaction radius of the entity
+   * @param text optional text configuration for displaying text alongside the image
+   * @return the entity
    */
   public static Entity createShowImage(
       Point pos,
@@ -38,39 +46,24 @@ public class ShowImageFactory {
     entity.add(dc);
 
     ShowImageComponent sic = new ShowImageComponent(imagePath);
-    sic.onCloseAction = onClose;
-    sic.maxSize = maxSize;
-    sic.textConfig = text;
+    sic.onCloseAction(onClose);
+    sic.maxSize(maxSize);
+    sic.textConfig(text);
     entity.add(sic);
 
-    entity.add(new InteractionComponent(radius, true, (e, who) -> sic.isUIOpen = true));
+    entity.add(new InteractionComponent(radius, true, (e, who) -> sic.isUIOpen(true)));
     return entity;
   }
 
-  public static Entity createShowImage(
-      Point pos,
-      String spriteImage,
-      String imagePath,
-      BiConsumer<Entity, Entity> onClose,
-      float maxSize,
-      float radius) {
-    return createShowImage(pos, spriteImage, imagePath, onClose, maxSize, radius, null);
-  }
-
-  public static Entity createShowImage(
-      Point pos,
-      String spriteImage,
-      String imagePath,
-      BiConsumer<Entity, Entity> onClose,
-      float maxSize) {
-    return createShowImage(pos, spriteImage, imagePath, onClose, maxSize, INTERACTION_RADIUS, null);
-  }
-
-  public static Entity createShowImage(
-      Point pos, String spriteImage, String imagePath, float maxSize) {
-    return createShowImage(pos, spriteImage, imagePath, null, maxSize, INTERACTION_RADIUS, null);
-  }
-
+  /**
+   * Creates an entity that will show the specified image when interacted with, at the designated
+   * position.
+   *
+   * @param pos the position of the entity
+   * @param spriteImage the path to the sprite image representing the entity
+   * @param imagePath the path of the image to be shown
+   * @return the entity
+   */
   public static Entity createShowImage(Point pos, String spriteImage, String imagePath) {
     return createShowImage(pos, spriteImage, imagePath, null, 0.85f, INTERACTION_RADIUS, null);
   }
