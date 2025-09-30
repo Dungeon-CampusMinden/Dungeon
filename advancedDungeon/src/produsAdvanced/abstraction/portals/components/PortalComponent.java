16eb2f8ea14b1d34dae4ea6a6c12cd30a0705b08
Araqs <51331697+araqs11@users.noreply.github.com>
2025-09-30 13:55:05 +0200

    AdvancedDungeon: add Portals and Portalwalls (#2541)
    closes #2375 #2412 #2406

---
+++ advancedDungeon/src/produsAdvanced/abstraction/portals/components/PortalComponent.java
@@ -0,0 +1,6 @@
+package produsAdvanced.abstraction.portals.components;
+
+import core.Component;
+
+/** Holder component for entities are that represents a portal. */
+public class PortalComponent implements Component {}
