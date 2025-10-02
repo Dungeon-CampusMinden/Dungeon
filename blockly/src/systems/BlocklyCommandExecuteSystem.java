43e8f05248fd0237a7761e382de70c01936b22ec
AMatutat <32962412+AMatutat@users.noreply.github.com>
2025-10-02 15:58:43 +0200

    fix interaction

--- blockly/src/systems/BlocklyCommandExecuteSystem.java
+++ blockly/src/systems/BlocklyCommandExecuteSystem.java
@@ -359,25 +359,30 @@ public class BlocklyCommandExecuteSystem extends System {
    */
   private void interact(Direction direction) {
     Entity hero = Game.hero().orElseThrow(MissingHeroException::new);
+    PositionComponent pc =
+        hero.fetch(PositionComponent.class)
+            .orElseThrow(() -> MissingComponentException.build(hero, PositionComponent.class));
 
-    hero.fetch(PositionComponent.class)
-        // get the tile in the given direction
-        .flatMap(
-            pc ->
-                Game.tileAt(
-                    pc.position().translate(MAGIC_OFFSET),
-                    pc.viewDirection().applyRelative(direction)))
-        // for each entity at that tile interact
-        .ifPresent(
-            tile ->
-                Game.entityAtTile(tile)
-                    .forEach(
-                        entity ->
-                            entity
-                                .fetch(InteractionComponent.class)
-                                .ifPresent(
-                                    interactionComponent ->
-                                        interactionComponent.triggerInteraction(entity, hero))));
+    Tile inDirection;
+
+    if (direction == Direction.NONE) {
+      inDirection = Game.tileAt(pc.position().translate(MAGIC_OFFSET)).orElse(null);
+    } else {
+      inDirection =
+          Game.tileAt(
+                  pc.position().translate(MAGIC_OFFSET),
+                  pc.viewDirection().applyRelative(direction))
+              .orElse(null);
+    }
+
+    Game.entityAtTile(inDirection)
+        .forEach(
+            entity ->
+                entity
+                    .fetch(InteractionComponent.class)
+                    .ifPresent(
+                        interactionComponent ->
+                            interactionComponent.triggerInteraction(entity, hero)));
   }
 
   /**
