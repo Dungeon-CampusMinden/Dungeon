d14607ef12f89fbbf5ebe5aec7f30155ab56c270
AMatutat <32962412+AMatutat@users.noreply.github.com>
2025-10-02 16:07:59 +0200

    refix fireball

--- blockly/src/entities/BlocklyFireball.java
+++ blockly/src/entities/BlocklyFireball.java
@@ -1,9 +1,19 @@
 package entities;
 
+import contrib.components.CollideComponent;
+import contrib.components.FlyComponent;
+import contrib.components.ProjectileComponent;
 import contrib.utils.components.skill.Resource;
+import contrib.utils.components.skill.SkillTools;
 import contrib.utils.components.skill.projectileSkill.FireballSkill;
+import core.Entity;
+import core.Game;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
+import core.components.VelocityComponent;
 import core.utils.Point;
 import core.utils.Tuple;
+import core.utils.Vector2;
 import java.util.function.Supplier;
 
 /**
@@ -37,7 +47,6 @@ public class BlocklyFireball extends FireballSkill {
       boolean ignoreFirstWall,
       Tuple<Resource, Integer>... resourceCost) {
     super(NAME, target, cooldown, speed, range, damageAmount, ignoreFirstWall, resourceCost);
-    this.hitBoxSize = this.hitBoxSize.scale(0.1f);
   }
 
   /**
@@ -48,6 +57,49 @@ public class BlocklyFireball extends FireballSkill {
    */
   public BlocklyFireball(Supplier<Point> target, int cooldown) {
     super(target, cooldown);
-    this.hitBoxSize = this.hitBoxSize.scale(0.1f);
+  }
+
+  @Override
+  protected void shootProjectile(Entity caster, Point start, Point aimedOn) {
+    Entity projectile = new Entity(name() + "_projectile");
+    ignoreEntities.add(caster);
+    ignoreEntities.add(projectile);
+
+    projectile.add(new FlyComponent());
+    DrawComponent dc = new DrawComponent(texture);
+
+    dc.tintColor(tintColor);
+    projectile.add(dc);
+
+    // Target point calculation
+    Point targetPoint = SkillTools.calculateLastPositionInRange(start, aimedOn, range);
+
+    Point position = start.translate(hitBoxSize.scale(-0.5)); // +offset
+    PositionComponent pc = new PositionComponent(position);
+    projectile.add(pc);
+    // calculate rotation
+    double angleDeg = Vector2.of(position).angleToDeg(Vector2.of(targetPoint));
+    pc.rotation((float) angleDeg);
+    // Calculate velocity
+    Vector2 forceToApply = SkillTools.calculateDirection(start, targetPoint).scale(speed);
+
+    // Add components
+    VelocityComponent vc = new VelocityComponent(speed, handleProjectileWallHit(caster), true);
+
+    // this is the only difference to a normal fireball
+    vc.moveboxSize(hitBoxSize.scale(0.1));
+    vc.moveboxOffset(hitBoxOffset);
+    projectile.add(vc);
+    projectile.add(new ProjectileComponent(start, targetPoint, forceToApply, onEndReached(caster)));
+
+    CollideComponent cc =
+        new CollideComponent(
+            hitBoxOffset, hitBoxSize, onCollideEnter(caster), onCollideLeave(caster));
+    cc.onHold(onCollideHold(caster));
+    cc.isSolid(false);
+    projectile.add(cc);
+
+    Game.add(projectile);
+    onSpawn(caster, projectile);
   }
 }
