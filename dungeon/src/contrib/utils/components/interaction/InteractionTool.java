7b2244b91ac0112c98d0fa6f8228307fe2b91c89
David Vehlow <david.vehlow@fh-bielefeld.de>
2025-10-02 12:33:45 +0200

    InteractionTool now uses the Entity Center via EntityUtils#getEntityCenter

--- dungeon/src/contrib/utils/components/interaction/InteractionTool.java
+++ dungeon/src/contrib/utils/components/interaction/InteractionTool.java
@@ -1,6 +1,7 @@
 package contrib.utils.components.interaction;
 
 import contrib.components.InteractionComponent;
+import contrib.utils.EntityUtils;
 import core.Entity;
 import core.Game;
 import core.components.PositionComponent;
@@ -33,37 +34,34 @@ public final class InteractionTool {
    */
   public static void interactWithClosestInteractable(
       final Entity who, final Function<InteractionData, Boolean> iReachable) {
-    PositionComponent heroPosition =
-        who.fetch(PositionComponent.class)
-            .orElseThrow(() -> MissingComponentException.build(who, PositionComponent.class));
+
     Optional<InteractionData> data =
         Game.levelEntities()
             .filter(x -> x.isPresent(InteractionComponent.class))
-            .map(x -> convertToData(x, heroPosition))
+            .map(x -> convertToData(x, who))
             .filter(iReachable::apply)
             .min((x, y) -> Float.compare(x.dist(), y.dist()));
     data.ifPresent(x -> x.ic().triggerInteraction(x.e(), who));
   }
 
   private static InteractionData convertToData(
-      final Entity entity, final PositionComponent heroPosition) {
+      final Entity entity, final Entity who) {
 
     InteractionComponent ic =
         entity
             .fetch(InteractionComponent.class)
             .orElseThrow(() -> MissingComponentException.build(entity, InteractionComponent.class));
-    PositionComponent pc =
-        entity
-            .fetch(PositionComponent.class)
-            .orElseThrow(() -> MissingComponentException.build(entity, PositionComponent.class));
+
+    Point targetCenter = EntityUtils.getEntityCenter(entity);
+    Point heroCenter   = EntityUtils.getEntityCenter(who);
+
     return new InteractionData(
-        entity,
-        pc,
-        ic,
-        Point.calculateDistance(heroPosition.position(), pc.position()),
-        pc.position().vectorTo(heroPosition.position()).normalize());
+      entity,
+      ic,
+      Point.calculateDistance(heroCenter, targetCenter),
+      targetCenter.vectorTo(heroCenter).normalize());
   }
 
   private record InteractionData(
-      Entity e, PositionComponent pc, InteractionComponent ic, float dist, Vector2 unitDir) {}
+      Entity e, InteractionComponent ic, float dist, Vector2 unitDir) {}
 }
