f082081602506cf8e6216ec1b34dac70b0a6aa98
Simon Gartzke <simon.gartzke@icloud.com>
2025-10-06 14:12:21 +0200

    added comments

--- dungeon/src/contrib/utils/components/skill/selfSkill/MeleeAttackSkill.java
+++ dungeon/src/contrib/utils/components/skill/selfSkill/MeleeAttackSkill.java
@@ -1,7 +1,6 @@
 package contrib.utils.components.skill.selfSkill;
 
 import contrib.components.CollideComponent;
-import contrib.components.FlyComponent;
 import contrib.components.HealthComponent;
 import contrib.systems.EventScheduler;
 import contrib.utils.components.health.Damage;
@@ -17,10 +16,15 @@ import core.utils.*;
 import core.utils.components.path.IPath;
 import core.utils.components.path.SimpleIPath;
 
+/**
+ * A skill that does a melee attack in the direction the entity is looking, generates a short living
+ * entity that deals damage to all entities colliding with it.
+ */
 public class MeleeAttackSkill extends Skill {
 
   /** Name of the skill. */
   public static final String NAME = "Meele";
+
   private static final IPath TEXTURE = new SimpleIPath("skills/melee");
   private int damage;
   private DamageType damageType;
@@ -30,9 +34,20 @@ public class MeleeAttackSkill extends Skill {
   /**
    * Creates a new skill with the given parameters.
    *
-   * @param resources the resources and their required amounts, provided as {@link Tuple}s
+   * @param damage The damage the melee attack deals.
+   * @param damageType Typ of damage the attack deals.
+   * @param cooldown How frequent this attack can be used.
+   * @param offset Offset for the collision hitbox.
+   * @param hitbox Hitbox for the collision.
+   * @param resources The resources and their required amounts, provided as {@link Tuple}s
    */
-  public MeleeAttackSkill(int damage, DamageType  damageType, long cooldown, Vector2 offset, Vector2 hitbox, Tuple<Resource, Integer>... resources) {
+  public MeleeAttackSkill(
+      int damage,
+      DamageType damageType,
+      long cooldown,
+      Vector2 offset,
+      Vector2 hitbox,
+      Tuple<Resource, Integer>... resources) {
     super(NAME, cooldown, resources);
     this.damage = damage;
     this.damageType = damageType;
@@ -40,53 +55,86 @@ public class MeleeAttackSkill extends Skill {
     this.hitbox = hitbox;
   }
 
+  /**
+   * Creates a short livid entity in the direction which the caster is looking at. The damage is
+   * dealt via the collide component which uses the onCollideEnter to deal the damage. The damage
+   * takes the damagetyp into account. Removes itself after {@link #cooldown} milliseconds from the
+   * game.
+   *
+   * @param caster The entity using the skill.
+   */
   @Override
   protected void executeSkill(Entity caster) {
-    caster.fetch(PositionComponent.class)
-      .ifPresent(casterPositionComponent -> {
-        Entity attack = new Entity();
+    caster
+        .fetch(PositionComponent.class)
+        .ifPresent(
+            casterPositionComponent -> {
+              Entity attack = new Entity();
 
-        DrawComponent drawComponent = new DrawComponent(TEXTURE);
-        attack.add(drawComponent);
+              DrawComponent drawComponent = new DrawComponent(TEXTURE);
+              attack.add(drawComponent);
 
-        Point attackPosition = casterPositionComponent.position().translate(casterPositionComponent.viewDirection());
-        PositionComponent attackPositionComponent = new PositionComponent(attackPosition);
+              Point attackPosition =
+                  casterPositionComponent
+                      .position()
+                      .translate(casterPositionComponent.viewDirection());
+              PositionComponent attackPositionComponent = new PositionComponent(attackPosition);
+              System.out.println(casterPositionComponent.rotation());
+              attackPositionComponent.rotation(casterPositionComponent.rotation());
+              //        switch(casterPositionComponent.viewDirection()) {
+              //          case UP -> {
+              //            attackPositionComponent.rotation(90);
+              //          }
+              //          case RIGHT -> {
+              //            attackPositionComponent.rotation(0);
+              //          }
+              //          case DOWN -> {
+              //            attackPositionComponent.rotation(270);
+              //          }
+              //          case LEFT -> {
+              //            attackPositionComponent.rotation(180);
+              //          }
+              //          default -> {}
+              //        }
 
-        switch(casterPositionComponent.viewDirection()) {
-          case UP -> {
-            attackPositionComponent.rotation(90);
-          }
-          case RIGHT -> {
-            attackPositionComponent.rotation(0);
-          }
-          case DOWN -> {
-            attackPositionComponent.rotation(270);
-          }
-          case LEFT -> {
-            attackPositionComponent.rotation(180);
-          }
-          default -> {}
-        }
+              attack.add(attackPositionComponent);
 
-        attack.add(attackPositionComponent);
+              CollideComponent collideComponent =
+                  new CollideComponent(
+                      offset,
+                      hitbox,
+                      (entity, other, direction) -> {
+                        other
+                            .fetch(HealthComponent.class)
+                            .ifPresent(
+                                healthComponent -> {
+                                  healthComponent.receiveHit(
+                                      new Damage(damage, damageType, attack));
+                                });
+                      },
+                      CollideComponent.DEFAULT_COLLIDER);
+              collideComponent.isSolid(false);
+              attack.add(collideComponent);
 
-        CollideComponent collideComponent = new CollideComponent(offset, hitbox, this::onCollideEnter, CollideComponent.DEFAULT_COLLIDER);
-        collideComponent.isSolid(false);
-        attack.add(collideComponent);
+              VelocityComponent velocityComponent = caster.fetch(VelocityComponent.class).get();
+              velocityComponent.canEnterWalls(true);
+              attack.add(velocityComponent);
 
-        VelocityComponent velocityComponent = caster.fetch(VelocityComponent.class).get();
-        velocityComponent.canEnterWalls(true);
-        attack.add(velocityComponent);
-
-        Game.add(attack);
-        EventScheduler.scheduleAction(() ->{Game.remove(attack);},250);
-      });
+              Game.add(attack);
+              EventScheduler.scheduleAction(
+                  () -> {
+                    Game.remove(attack);
+                  },
+                  250);
+            });
   }
 
-  public void onCollideEnter(Entity attack, Entity other, Direction direction) {
-    other.fetch(HealthComponent.class)
-      .ifPresent(healthComponent -> {
-        healthComponent.receiveHit(new Damage(damage, damageType, attack));
-      });
+  private void onCollideEnter(Entity attack, Entity other, Direction direction) {
+    other
+        .fetch(HealthComponent.class)
+        .ifPresent(
+            healthComponent -> {
+              healthComponent.receiveHit(new Damage(damage, damageType, attack));
+            });
   }
 }
