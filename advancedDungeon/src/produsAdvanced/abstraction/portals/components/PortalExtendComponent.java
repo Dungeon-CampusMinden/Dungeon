c894fa8450602d4b6fd2bf5871a99e88a06e4dd0
Tobi511 <130839159+Tobi511@users.noreply.github.com>
2025-10-14 14:44:31 +0200

    AdvancedDungeon: Make beam non solid and added system to extend and trim entities through portals (#2552)
    Hier werden das `PortalExtendSystem`, ein `PortalExtendComponent` und
    das `TractorBeamComponent` eingeführt.
    Diese Komponenten sorgen dafür, dass Strahl-Entitäten, wie der
    TractorBeam, durch ein Portal erweitert werden können.
    
    Dazu benötigt die jeweilige Strahl erzeugende Factory, in diesem Fall
    die `TractorBeamFactory`, jeweils eine `extend`- und eine
    `trim`-Methode.
    Diese ermöglichen es, den Strahl bei einer Portalkollision zu erweitern
    bzw. bei einer Portalversetzung wieder zu entfernen.
    
    Das `TractorBeamComponent` kennzeichnet einen TractorBeam als solchen
    und ermöglicht das Aufrufen der `trim`- und `extend`-Methoden. Außerdem
    wird ein TractorBeam im `TractorBeamComponent` als Liste gespeichert.
    
    Das `PortalExtendComponent` und das `PortalExtendSystem` übernehmen die
    Steuerung der Interaktion mit den Portalen und speichern beispielsweise,
    durch welches Portal der TractorBeam eintritt.
    
    @araqs11 hat nach einer ersten Zusammenarbeit übernommen und das ganze
    fertiggestellt. Danke dafür!
    
    ---------
    
    Co-authored-by: Simon Gartzke <simon.gartzke@icloud.com>

---
+++ advancedDungeon/src/produsAdvanced/abstraction/portals/components/PortalExtendComponent.java
@@ -0,0 +1,75 @@
+package produsAdvanced.abstraction.portals.components;
+
+import core.Component;
+import core.Entity;
+import core.utils.Direction;
+import core.utils.Point;
+import core.utils.TriConsumer;
+import java.util.function.Consumer;
+
+/** Helper component for the PortalExtendSystem. */
+public class PortalExtendComponent implements Component {
+  private boolean throughBlue = false;
+  private boolean throughGreen = false;
+  private boolean isExtended = false;
+
+  /** Default extend method. */
+  public TriConsumer<Direction, Point, PortalExtendComponent> onExtend = (d, p, pec) -> {};
+
+  /** Default trim method. */
+  public Consumer<Entity> onTrim = (e) -> {};
+
+  /**
+   * Returns the state of throughGreen.
+   *
+   * @return true if the holding entity is colliding with the green portal, otherwise false.
+   */
+  public boolean isThroughGreen() {
+    return throughGreen;
+  }
+
+  /**
+   * Returns the state of throughBlue.
+   *
+   * @return true if the holding entity is colliding with the blue portal, otherwise false.
+   */
+  public boolean isThroughBlue() {
+    return throughBlue;
+  }
+
+  /**
+   * Returns the state of isExtended.
+   *
+   * @return true if the entity is being extended, otherwise false.
+   */
+  public boolean isExtended() {
+    return isExtended;
+  }
+
+  /**
+   * Sets the state of isExtended.
+   *
+   * @param extended Sets the state of isExtended.
+   */
+  public void setExtended(boolean extended) {
+    isExtended = extended;
+  }
+
+  /**
+   * Sets the state of throughBlue.
+   *
+   * @param throughBlue Sets the state of throughBlue.
+   */
+  public void setThroughBlue(boolean throughBlue) {
+    this.throughBlue = throughBlue;
+  }
+
+  /**
+   * Sets the state of throughGreen.
+   *
+   * @param throughGreen Sets the state of throughGreen.
+   */
+  public void setThroughGreen(boolean throughGreen) {
+    this.throughGreen = throughGreen;
+  }
+}
