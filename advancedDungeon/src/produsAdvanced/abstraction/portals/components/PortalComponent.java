7e9b59c1041c824db6445ade980ff1b80aee244c
Tobi511 <130839159+Tobi511@users.noreply.github.com>
2025-10-14 14:44:31 +0200

    AdvancedDungeon: Make beam non solid and added system to extend and trim entities through portals (#2552)
    Hier werden das `PortalExtendSystem`, ein `PortalExtendComponent` und
    das `TractorBeamComponent` eingeführt.
    Diese Komponenten sorgen dafür, dass Strahl-Entitäten, wie der
    TractorBeam, durch ein Portal erweitert werden können.
    
    Dazu benötigt die jeweilige Strahl erzeugende Factory, in diesem Fall
    die `TractorBeamFactory`, jeweils eine `extend`- und eine
    `trim`-Methode.
    Diese ermöglichen es, den Strahl bei einer Portalkollision zu erweitern
    bzw. bei einer Portalversetzung wieder zu entfernen.
    
    Das `TractorBeamComponent` kennzeichnet einen TractorBeam als solchen
    und ermöglicht das Aufrufen der `trim`- und `extend`-Methoden. Außerdem
    wird ein TractorBeam im `TractorBeamComponent` als Liste gespeichert.
    
    Das `PortalExtendComponent` und das `PortalExtendSystem` übernehmen die
    Steuerung der Interaktion mit den Portalen und speichern beispielsweise,
    durch welches Portal der TractorBeam eintritt.
    
    @araqs11 hat nach einer ersten Zusammenarbeit übernommen und das ganze
    fertiggestellt. Danke dafür!
    
    ---------
    
    Co-authored-by: Simon Gartzke <simon.gartzke@icloud.com>

--- advancedDungeon/src/produsAdvanced/abstraction/portals/components/PortalComponent.java
+++ advancedDungeon/src/produsAdvanced/abstraction/portals/components/PortalComponent.java
@@ -1,6 +1,27 @@
 package produsAdvanced.abstraction.portals.components;
 
 import core.Component;
+import core.Entity;
 
 /** Holder component for entities are that represents a portal. */
-public class PortalComponent implements Component {}
+public class PortalComponent implements Component {
+  private Entity extendedEntityThrough;
+
+  /**
+   * Stores the entity that is being extended for later purposes.
+   *
+   * @param extendedEntityThrough Entity that is being extended.
+   */
+  public void setExtendedEntityThrough(Entity extendedEntityThrough) {
+    this.extendedEntityThrough = extendedEntityThrough;
+  }
+
+  /**
+   * Returns the state of extendedEntityThrough.
+   *
+   * @return Entity that is being extended through a portal.
+   */
+  public Entity getExtendedEntityThrough() {
+    return extendedEntityThrough;
+  }
+}
