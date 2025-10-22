50795fb7b6d8ade6e04338aa22f7fa3d332c9ce0
AMatutat <32962412+AMatutat@users.noreply.github.com>
2025-10-22 08:04:19 +0200

    AdvancedDungeon: Added glasswall for the laser to pass through (#2585)
    closes #2436
    
    Das Tile mit den Buchstaben 'L' im Leveleditor nutzbar. Musste leider
    wieder in den Dungeon hinzugefügt werden weil es keine andere
    Möglichkeit gibt Tiles zum Leveleditor hinzuzufügen. Die Textur ist noch
    nicht zu 100% final, da müsste am besten @b-franklin einmal sagen ob das
    passt oder nicht, kann man ja schnell ändern. Damit ein Laser durch kann
    muss er eine VelocityComponent haben und die Variable
    `canEnterGlasswalls` muss auf true sein.

--- dungeon/src/core/level/DungeonLevel.java
+++ dungeon/src/core/level/DungeonLevel.java
@@ -51,6 +51,7 @@ public class DungeonLevel implements ILevel, ITickable {
   protected ArrayList<PitTile> pitTiles = new ArrayList<>();
   protected ArrayList<PortalTile> portalTiles = new ArrayList<>();
   protected ArrayList<GitterTile> gitterTiles = new ArrayList<>();
+  protected ArrayList<GlasswandTile> glassWallTiles = new ArrayList<>();
 
   /**
    * Create a new level.
@@ -232,6 +233,7 @@ public class DungeonLevel implements ILevel, ITickable {
       case PIT -> pitTiles.remove((PitTile) tile);
       case PORTAL -> portalTiles.remove((PortalTile) tile);
       case GITTER -> gitterTiles.remove((GitterTile) tile);
+      case GLASSWALL -> glassWallTiles.remove((GlasswandTile) tile);
     }
     this.removeFromPathfinding(tile);
   }
@@ -291,6 +293,7 @@ public class DungeonLevel implements ILevel, ITickable {
       case PIT -> pitTiles.add((PitTile) tile);
       case PORTAL -> portalTiles.add((PortalTile) tile);
       case GITTER -> gitterTiles.add((GitterTile) tile);
+      case GLASSWALL -> glassWallTiles.add((GlasswandTile) tile);
     }
     this.addToPathfinding(tile);
     tile.level(this);
