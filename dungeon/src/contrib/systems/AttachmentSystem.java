2f56f0f9aaf5cb5b4ad6f2e0b6ec8a161dbb467d
Simon Gartzke <simon.gartzke@icloud.com>
2025-10-25 15:42:50 +0200

    added melee skill and attachment system

---
+++ dungeon/src/contrib/systems/AttachmentSystem.java
@@ -0,0 +1,152 @@
+package contrib.systems;
+
+import contrib.components.AttachmentComponent;
+import contrib.components.CollideComponent;
+import core.Entity;
+import core.System;
+import core.components.PositionComponent;
+import core.utils.components.MissingComponentException;
+import java.util.HashMap;
+
+/**
+ * This system takes 2 PositionComponents and attaches them in a way where one PositionComponent
+ * copies the position of another one.
+ *
+ * <p>The copied component will be ether at a fixed point which is the {@link AttachmentComponent}
+ * offset or if the offset is Vector2.ZERO it will be infront of the origin component. The distance
+ * between these two can be scaled with the scale of the {@link AttachmentComponent}.
+ */
+public class AttachmentSystem extends System {
+
+  /** Map where the key is the copied component and the value is the origin component. */
+  private static HashMap<PositionComponent, PositionComponent> attachmentMap = new HashMap<>();
+
+  /**
+   * Creates a new AttachmentSystem.
+   *
+   * <p>Entities that are being involved in an attachment will be cleared off the HashMap when they
+   * are removed.
+   */
+  public AttachmentSystem() {
+    super(AttachmentComponent.class);
+    onEntityRemove =
+        (entity -> {
+          entity
+              .fetch(PositionComponent.class)
+              .ifPresent(
+                  pc -> {
+                    if (attachmentMap.containsKey(pc)) {
+                      attachmentMap.remove(pc);
+                    } else if (attachmentMap.containsValue(pc)) {
+                      attachmentMap.remove(
+                          attachmentMap.keySet().stream()
+                              .filter(
+                                  positionComponent ->
+                                      attachmentMap.get(positionComponent).equals(pc))
+                              .findFirst()
+                              .get());
+                    }
+                  });
+        });
+  }
+
+  /** Updates the position of all attached PositionComponents. */
+  @Override
+  public void execute() {
+    filteredEntityStream().map(this::buildDataObject).forEach(this::applyAttachment);
+  }
+
+  /**
+   * Registers two PositionComponents so the copy is attached to the origin.
+   *
+   * @param copy The PositionComponent that gets updated each frame.
+   * @param origin The PositionComponent that provides the position for the copy.
+   */
+  public static void registerAttachment(PositionComponent copy, PositionComponent origin) {
+    attachmentMap.put(copy, origin);
+  }
+
+  /**
+   * Unregisters a PositionComponent from the system so it doesn't get updated by it anymore.
+   *
+   * @param copy The PositionComponent that got updated.
+   */
+  public static void unregisterAttachment(PositionComponent copy) {
+    attachmentMap.remove(copy);
+  }
+
+  /**
+   * Builds a data object for better handling in the system.
+   *
+   * @param e The entity that has the {@link AttachmentComponent}.
+   * @return A {@link ASData} object with all relevant data.
+   */
+  private ASData buildDataObject(Entity e) {
+    PositionComponent copypc =
+        e.fetch(PositionComponent.class)
+            .orElseThrow(() -> MissingComponentException.build(e, PositionComponent.class));
+
+    AttachmentComponent ac =
+        e.fetch(AttachmentComponent.class)
+            .orElseThrow(() -> MissingComponentException.build(e, AttachmentComponent.class));
+
+    PositionComponent originpc = attachmentMap.get(copypc);
+
+    return new ASData(e, ac, copypc, originpc);
+  }
+
+  /**
+   * Applies the attachment logic.
+   *
+   * <p>if isRotatingWithOrigin from the {@link AttachmentComponent} is true the position of the
+   * copied PositionComponent gets set in front of the one origin with a distance of one tile. This
+   * can be scaled with the scale of the {@link AttachmentComponent}.
+   *
+   * <p>if isRotatingWithOrigin from the {@link AttachmentComponent} is false the position of the
+   * copied PositionComponent gets set on the origin with an offset. The distance of this can be
+   * scaled with the scale of the {@link AttachmentComponent}.
+   *
+   * @param asData The Attachment Data with the attached Components.
+   */
+  private void applyAttachment(ASData asData) {
+    if (asData.ac.isRotatingWithOrigin()) {
+      asData.copypc.position(
+          asData
+              .originpc
+              .position()
+              .translate(asData.originpc.viewDirection().scale(asData.ac.getScale())));
+      // this is here because the player doesnt rotate when so the viewdirection has to be used for
+      // melee.
+      //      asData.copypc.rotation(asData.originpc.rotation());
+      switch (asData.originpc.viewDirection()) {
+        case UP -> {
+          asData.copypc.rotation(90);
+        }
+        case RIGHT -> {
+          asData.copypc.rotation(0);
+        }
+        case DOWN -> {
+          asData.copypc.rotation(270);
+        }
+        case LEFT -> {
+          asData.copypc.rotation(180);
+        }
+        default -> {}
+      }
+
+    } else {
+      asData.copypc.position(
+          asData.originpc.position().translate(asData.ac.getOffset().scale(asData.ac.getScale())));
+    }
+    asData
+        .e
+        .fetch(CollideComponent.class)
+        .ifPresent(
+            cc -> {
+              cc.collider().position(asData.copypc.position());
+            });
+  }
+
+  private record ASData(
+      Entity e, AttachmentComponent ac, PositionComponent copypc, PositionComponent originpc) {}
+}
---
+++ dungeon/src/contrib/utils/components/skill/selfSkill/MeleeAttackSkill.java
@@ -0,0 +1,143 @@
+package contrib.utils.components.skill.selfSkill;
+
+import contrib.components.AttachmentComponent;
+import contrib.components.CollideComponent;
+import contrib.components.HealthComponent;
+import contrib.systems.EventScheduler;
+import contrib.utils.components.health.Damage;
+import contrib.utils.components.health.DamageType;
+import contrib.utils.components.skill.Resource;
+import contrib.utils.components.skill.Skill;
+import core.Entity;
+import core.Game;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
+import core.utils.*;
+import core.utils.components.path.IPath;
+import core.utils.components.path.SimpleIPath;
+
+/**
+ * A skill that does a melee attack in the direction the entity is looking, generates a short living
+ * entity that deals damage to all entities colliding with it.
+ */
+public class MeleeAttackSkill extends Skill {
+
+  /** Name of the skill. */
+  public static final String NAME = "Meele";
+
+  private static final IPath TEXTURE = new SimpleIPath("skills/melee");
+  private int damage;
+  private DamageType damageType;
+  private Vector2 hitbox;
+  private Vector2 offset;
+
+  /**
+   * Creates a new skill with the given parameters.
+   *
+   * @param damage The damage the melee attack deals.
+   * @param damageType Typ of damage the attack deals.
+   * @param cooldown How frequent this attack can be used.
+   * @param offset Offset for the collision hitbox.
+   * @param hitbox Hitbox for the collision.
+   * @param resources The resources and their required amounts, provided as {@link Tuple}s
+   */
+  public MeleeAttackSkill(
+      int damage,
+      DamageType damageType,
+      long cooldown,
+      Vector2 offset,
+      Vector2 hitbox,
+      Tuple<Resource, Integer>... resources) {
+    super(NAME, cooldown, resources);
+    this.damage = damage;
+    this.damageType = damageType;
+    this.offset = offset;
+    this.hitbox = hitbox;
+  }
+
+  /**
+   * Creates a new skill with the damage and preconfigured parameters.
+   *
+   * @param damage The damage the melee attack deals.
+   */
+  public MeleeAttackSkill(int damage) {
+    this(damage, DamageType.PHYSICAL, 500, Vector2.ZERO, Vector2.ONE);
+  }
+
+  /**
+   * Creates a short livid entity in the direction which the caster is looking at. The damage is
+   * dealt via the collide component which uses the onCollideEnter to deal the damage. The damage
+   * takes the damagetype into account. Removes itself after {@link #cooldown} milliseconds from the
+   * game.
+   *
+   * @param caster The entity using the skill.
+   */
+  @Override
+  protected void executeSkill(Entity caster) {
+    caster
+        .fetch(PositionComponent.class)
+        .ifPresent(
+            casterPositionComponent -> {
+              Entity attack = new Entity();
+
+              DrawComponent drawComponent = new DrawComponent(TEXTURE);
+              attack.add(drawComponent);
+
+              Point attackPosition =
+                  casterPositionComponent
+                      .position()
+                      .translate(casterPositionComponent.viewDirection());
+              PositionComponent attackPositionComponent = new PositionComponent(attackPosition);
+              attackPositionComponent.rotation(casterPositionComponent.rotation());
+
+              switch (casterPositionComponent.viewDirection()) {
+                case UP -> {
+                  attackPositionComponent.rotation(90);
+                }
+                case RIGHT -> {
+                  attackPositionComponent.rotation(0);
+                }
+                case DOWN -> {
+                  attackPositionComponent.rotation(270);
+                }
+                case LEFT -> {
+                  attackPositionComponent.rotation(180);
+                }
+                default -> {}
+              }
+
+              attack.add(attackPositionComponent);
+              AttachmentComponent ac =
+                  new AttachmentComponent(
+                      Vector2.ZERO, attackPositionComponent, casterPositionComponent);
+              attack.add(ac);
+
+              CollideComponent collideComponent =
+                  new CollideComponent(
+                      offset,
+                      hitbox,
+                      (entity, other, direction) -> {
+                        if (caster == other) {
+                          return;
+                        }
+                        other
+                            .fetch(HealthComponent.class)
+                            .ifPresent(
+                                healthComponent -> {
+                                  healthComponent.receiveHit(
+                                      new Damage(damage, damageType, attack));
+                                });
+                      },
+                      CollideComponent.DEFAULT_COLLIDER);
+              collideComponent.isSolid(false);
+              attack.add(collideComponent);
+
+              Game.add(attack);
+              EventScheduler.scheduleAction(
+                  () -> {
+                    Game.remove(attack);
+                  },
+                  230);
+            });
+  }
+}
