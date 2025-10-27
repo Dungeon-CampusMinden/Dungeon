506091ffceb93fde47f036d454456a1b30af97f4
Flamtky <68606032+Flamtky@users.noreply.github.com>
2025-10-27 15:16:31 +0100

    revert basicstarter

--- dungeon/src/starter/BasicStarter.java
+++ dungeon/src/starter/BasicStarter.java
@@ -1,17 +1,12 @@
 package starter;
 
 import contrib.entities.HeroFactory;
-import contrib.utils.components.Debugger;
 import core.Game;
 import core.configuration.KeyboardConfig;
 import core.level.DungeonLevel;
 import core.level.loader.DungeonLoader;
-import core.systems.SoundSystem;
 import core.utils.Tuple;
 import core.utils.components.path.SimpleIPath;
-import core.utils.logging.DungeonLogLevel;
-import core.utils.logging.DungeonLogger;
-import core.utils.logging.DungeonLoggerConfig;
 import java.io.IOException;
 
 /**
@@ -25,37 +20,29 @@ import java.io.IOException;
  */
 public class BasicStarter {
 
-  private static final DungeonLogger LOGGER = DungeonLogger.getLogger(BasicStarter.class);
-
   /**
    * Main entry point to launch the basic dungeon game.
    *
    * @param args command-line arguments (not used in this starter)
    */
   public static void main(String[] args) {
-    DungeonLoggerConfig.initWithLevel(DungeonLogLevel.INFO);
     DungeonLoader.addLevel(Tuple.of("maze", DungeonLevel.class));
     try {
       Game.loadConfig(new SimpleIPath("dungeon_config.json"), KeyboardConfig.class);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+    Game.disableAudio(true);
     Game.userOnSetup(
-        () -> {
-          try {
-            onSetup();
-            Game.add(HeroFactory.newHero());
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        });
+      () -> {
+        try {
+          Game.add(HeroFactory.newHero());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      });
     Game.frameRate(30);
     Game.windowTitle("Basic Dungeon");
     Game.run();
-  }
-
-  private static void onSetup() throws IOException {
-    Game.add(new Debugger());
-    Game.add(new SoundSystem());
   }
 }
