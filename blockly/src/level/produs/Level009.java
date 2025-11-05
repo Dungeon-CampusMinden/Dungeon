feba57a4ef3e6781793b7bf1d02154b978946526
fabianprojects <fabian.winkelmann1@gmail.com>
2025-11-05 12:07:14 +0100

    Blockly: Update info slides up to level 13
    The Informationstafel wurden bis level 13 überarbeitet. Der Text enthält
    jetzt nicht mehr nur inhaltliche Informationen, sondern auch so eine
    zauberhafte Art.

--- blockly/src/level/produs/Level009.java
+++ blockly/src/level/produs/Level009.java
@@ -69,10 +69,6 @@ public class Level009 extends BlocklyLevel {
     Game.add(MiscFactory.fireballScroll(getPoint(0)));
     Game.add(MiscFactory.fireballScroll(getPoint(1)));
 
-    showPopups();
-    Game.add(MiscFactory.fireballScroll(getPoint(0)));
-    Game.add(MiscFactory.fireballScroll(getPoint(1)));
-
     BlocklyMonster.Builder guardBuilder = BlocklyMonster.GUARD.builder().addToGame();
     guardBuilder.attackRange(5);
     guardBuilder.viewDirection(Direction.UP);
