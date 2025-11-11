fde52b61d8a354ec83e9a635dd31f84866a03e4a
b-franklin <benjamin.franke@fh-bielefeld.de>
2025-11-11 16:09:47 +0100

    create Portal Demo Room

---
+++ advancedDungeon/src/starter/PortalStarter.java
@@ -0,0 +1,117 @@
+package starter;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import contrib.components.SkillComponent;
+import contrib.entities.CharacterClass;
+import contrib.entities.EntityFactory;
+import contrib.systems.*;
+import contrib.utils.components.Debugger;
+import contrib.utils.components.skill.Resource;
+import contrib.utils.components.skill.projectileSkill.FireballSkill;
+import contrib.utils.components.skill.selfSkill.SelfHealSkill;
+import core.Entity;
+import core.Game;
+import core.level.loader.DungeonLoader;
+import core.utils.Tuple;
+import core.utils.components.path.SimpleIPath;
+import java.io.IOException;
+import level.portal.PortalDemoLevel;
+import produsAdvanced.abstraction.portals.portalSkills.BluePortalSkill;
+import produsAdvanced.abstraction.portals.portalSkills.GreenPortalSkill;
+import produsAdvanced.abstraction.portals.systems.PortalExtendSystem;
+import systems.AntiMaterialBarrierSystem;
+import systems.LasergridSystem;
+
+/**
+ * Starter for the Demo Escaperoom Dungeon.
+ *
+ * <p>Usage: run with the Gradle task {@code runDemoRoom}.
+ */
+public class PortalStarter {
+  private static final boolean DEBUG_MODE = true;
+  private static final String BACKGROUND_MUSIC = "sounds/background.wav";
+  private static final int START_LEVEL = 0;
+
+  /**
+   * Main method to start the game.
+   *
+   * @param args The arguments passed to the game.
+   * @throws IOException If an I/O error occurs.
+   */
+  public static void main(String[] args) throws IOException {
+    configGame();
+    onSetup();
+
+    Game.windowTitle("Demo-Room");
+    Game.run();
+  }
+
+  private static void onSetup() {
+    Game.userOnSetup(
+        () -> {
+          // setupMusic();
+          DungeonLoader.addLevel(Tuple.of("PortalDemo", PortalDemoLevel.class));
+          createSystems();
+          createHero();
+          DungeonLoader.loadLevel(START_LEVEL);
+        });
+  }
+
+  private static void createHero() {
+    Entity chell = EntityFactory.newHero(CharacterClass.WIZARD);
+    chell
+        .fetch(SkillComponent.class)
+        .ifPresent(
+            skillComponent -> {
+              skillComponent.addSkill(new BluePortalSkill(new Tuple<>(Resource.MANA, 0)));
+              skillComponent.addSkill(new GreenPortalSkill(new Tuple<>(Resource.MANA, 0)));
+              skillComponent.removeSkill(FireballSkill.class);
+              skillComponent.removeSkill(SelfHealSkill.class);
+            });
+    Game.add(chell);
+  }
+
+  private static void configGame() throws IOException {
+    Game.loadConfig(
+        new SimpleIPath("dungeon_config.json"),
+        contrib.configuration.KeyboardConfig.class,
+        core.configuration.KeyboardConfig.class);
+    Game.disableAudio(false);
+    Game.frameRate(30);
+  }
+
+  private static void createSystems() {
+    if (DEBUG_MODE) Game.add(new LevelEditorSystem());
+    Game.add(new CollisionSystem());
+    Game.add(new ManaBarSystem());
+    Game.add(new ManaRestoreSystem());
+    Game.add(new StaminaRestoreSystem());
+    Game.add(new StaminaBarSystem());
+    Game.add(new AISystem());
+    Game.add(new ProjectileSystem());
+    Game.add(new HealthBarSystem());
+    Game.add(new HealthSystem());
+    Game.add(new HudSystem());
+    Game.add(new SpikeSystem());
+    if (!DEBUG_MODE) Game.add(new FallingSystem());
+    Game.add(new PathSystem());
+    Game.add(new LevelTickSystem());
+    Game.add(new PitSystem());
+    Game.add(new EventScheduler());
+    Game.add(new LeverSystem());
+    Game.add(new PressurePlateSystem());
+    Game.add(new IdleSoundSystem());
+    Game.add(new PortalExtendSystem());
+    Game.add(new AntiMaterialBarrierSystem());
+    Game.add(new LasergridSystem());
+    if (DEBUG_MODE) Game.add(new Debugger());
+  }
+
+  private static void setupMusic() {
+    Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal(BACKGROUND_MUSIC));
+    backgroundMusic.setLooping(true);
+    backgroundMusic.play();
+    backgroundMusic.setVolume(.05f);
+  }
+}
