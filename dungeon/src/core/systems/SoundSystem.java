6f65bcb22351b22b7cc6cd154930064ac8f45157
Flamtky <68606032+Flamtky@users.noreply.github.com>
2025-11-20 13:42:24 +0100

    sound is for the clients

--- dungeon/src/core/systems/SoundSystem.java
+++ dungeon/src/core/systems/SoundSystem.java
@@ -44,7 +44,7 @@ public class SoundSystem extends System {
   }
 
   SoundSystem(ISoundPlayer soundPlayer) {
-    super(SoundComponent.class);
+    super(AuthoritativeSide.CLIENT, SoundComponent.class);
     this.soundPlayer = soundPlayer;
     this.onEntityRemove = this::onEntityRemoved;
   }
