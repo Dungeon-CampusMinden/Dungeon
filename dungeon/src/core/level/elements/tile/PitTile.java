d4b1328908002007ddd07fd62c0fc2b85e3af031
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2025-11-25 12:04:54 +0100

    Dungeon: update neighbour textures on tile change (#2684)
    fixes #2676 
    
    Wenn ein Tile verändert wird, werden jetzt die Nachbar-Tiles gebeten
    ihre Textur zu überprüfen und ggf upzudaten.
    
    Außerdem passe ich jetzt auch das layout Array an. Da stand früher noch
    das alte Tile drinen
    
    Before: 
    
    
    https://github.com/user-attachments/assets/1dc58701-58be-43f0-96f9-a20031d518d7
    
    After: 
    
    
    
    https://github.com/user-attachments/assets/abaf4f26-6da5-49e4-911c-b09be31c6368

--- dungeon/src/core/level/elements/tile/PitTile.java
+++ dungeon/src/core/level/elements/tile/PitTile.java
@@ -117,6 +117,7 @@ public class PitTile extends Tile {
   }
 
   /** Refreshes the texture of this pit and the pit below (if there is one). */
+  @Override
   public void refreshTexture() {
     this.texturePath(TileTextureFactory.findTexturePath(this, level.layout(), levelElement));
     if (coordinate().y() == 0) return;
