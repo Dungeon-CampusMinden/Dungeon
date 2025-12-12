cf8547b42192c6f2f6035c8009d3b5edb0b2ee8d
Jan Hokamp <jan.hokamp@gmail.com>
2025-12-12 16:10:54 +0100

    Add third portal level

--- advancedDungeon/src/starter/PortalStarter.java
+++ advancedDungeon/src/starter/PortalStarter.java
@@ -61,6 +61,7 @@ public class PortalStarter {
           DungeonLoader.addLevel(Tuple.of("PortalDemo", PortalDemoLevel.class));
           DungeonLoader.addLevel(Tuple.of("portallevel1", PortalLevel_1.class));
           DungeonLoader.addLevel(Tuple.of("portallevel2", PortalLevel_2.class));
+          DungeonLoader.addLevel(Tuple.of("portallevel3", PortalLevel_3.class));
           DungeonLoader.addLevel(Tuple.of("portallevel4", PortalLevel_4.class));
           DungeonLoader.addLevel(Tuple.of("portallevel5", PortalLevel_5.class));
 
