ec8c8c18d9465233624c575d43bab048e9c41484
André Matutat <andrematutat@MacStudio-von-Andre.local>
2026-01-08 20:13:02 +0100

    cleanup

--- dungeon/src/contrib/utils/components/Debugger.java
+++ dungeon/src/contrib/utils/components/Debugger.java
@@ -261,14 +261,12 @@ public class Debugger extends System {
     if (Gdx.input.isKeyJustPressed(KeyboardConfig.DEBUG_SPAWN_MONSTER.value())
         && !LevelEditorSystem.active()) Debugger.SPAWN_MONSTER_ON_CURSOR();
     if (Gdx.input.isKeyJustPressed(KeyboardConfig.DEBUG_OPEN_DOORS.value())) Debugger.OPEN_DOORS();
-
-    checkFrameAdvance();
     if (Gdx.input.isKeyJustPressed(core.configuration.KeyboardConfig.PAUSE.value()))
       Debugger.PAUSE_GAME();
     if (Gdx.input.isKeyJustPressed(core.configuration.KeyboardConfig.ADVANCE_FRAME.value()))
       Debugger.ADVANCE_FRAME();
-
     if (Gdx.input.isKeyJustPressed(KeyboardConfig.DEBUG_TOGGLE_HUD.value()))
       Game.system(DebugDrawSystem.class, DebugDrawSystem::toggleHUD);
+    checkFrameAdvance();
   }
 }
