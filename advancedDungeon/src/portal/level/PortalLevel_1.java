760a706e58c42bd24f9dd0bbaf13118bfb307d90
AMatutat <32962412+AMatutat@users.noreply.github.com>
2026-01-10 22:03:49 +0100

    add into text to portal level 1

--- advancedDungeon/src/portal/level/PortalLevel_1.java
+++ advancedDungeon/src/portal/level/PortalLevel_1.java
@@ -1,5 +1,6 @@
 package portal.level;
 
+import contrib.hud.DialogUtils;
 import core.level.utils.DesignLabel;
 import core.level.utils.LevelElement;
 import core.utils.Direction;
@@ -11,6 +12,11 @@ import portal.util.AdvancedLevel;
 
 /** Level in the portal dungeon. */
 public class PortalLevel_1 extends AdvancedLevel {
+
+  private static final String msg = "Schau dir mal das Portal an der Wand genauer an.";
+  private static final String title =
+      "Es scheint so, also ob die Berechnungen in MyCalculations.java noch nicht vollständig sind.";
+  private static boolean showMsg = true;
 
   /**
    * Call the parent constructor of a tile level with the given layout and design label. Set the
@@ -29,5 +35,14 @@ public class PortalLevel_1 extends AdvancedLevel {
   protected void onFirstTick() {
     PortalFactory.createPortal(namedPoints.get("portal1"), Direction.DOWN, PortalColor.GREEN);
     PortalFactory.createPortal(namedPoints.get("portal2"), Direction.LEFT, PortalColor.BLUE);
+
+    if (showMsg)
+      DialogUtils.showTextPopup(
+          msg,
+          title,
+          () -> {
+            showMsg = false;
+            DialogUtils.showTextPopup(task, title);
+          });
   }
 }
