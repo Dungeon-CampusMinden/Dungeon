31ed5affe6ee788d32c9e59c8aae03bd9ebc2c63
Hannes Ru <viddie@hotmail.de>
2026-01-27 15:23:43 +0100

    Add some skin components

--- theLastHourEscapeRoom/src/modules/trash/TrashMinigameUI.java
+++ theLastHourEscapeRoom/src/modules/trash/TrashMinigameUI.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import contrib.hud.UIUtils;
 import contrib.hud.dialogs.DialogContext;
