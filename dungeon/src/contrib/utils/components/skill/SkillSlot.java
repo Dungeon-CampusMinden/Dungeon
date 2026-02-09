14506d2031f726ef1363cd4353aa411afd399524
MatGrabert <mathis.grabert@protonmail.com>
2026-02-09 11:38:49 +0100

    Create SkillSlot.java
    Skillslots sollen ermöglichen, dass auf beiden Maustasten ein Skill liegen kann, welche innerhalb der Slots gewechselt werden können.

---
+++ dungeon/src/contrib/utils/components/skill/SkillSlot.java
@@ -0,0 +1,14 @@
+package contrib.utils.components.skill;
+
+/**
+ * Enumeration of different skill-slots.
+ *
+ * <p>
+ *   Skill slots are designed to allow a skill to be assigned to each mouse button,
+ *   which can then be switched between them as desired.
+ * </p>
+ */
+public enum SkillSlot {
+  FIRST_SLOT,
+  SECOND_SLOT
+}
