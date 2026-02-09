c96be651445e09c9a692720e5440688f5a0bf6a2
MatGrabert <mathis.grabert@protonmail.com>
2026-02-09 11:45:11 +0100

    Update SkillSlot.java
    Fehlende Kommentare ergänzt.

--- dungeon/src/contrib/utils/components/skill/SkillSlot.java
+++ dungeon/src/contrib/utils/components/skill/SkillSlot.java
@@ -3,12 +3,12 @@ package contrib.utils.components.skill;
 /**
  * Enumeration of different skill-slots.
  *
- * <p>
- *   Skill slots are designed to allow a skill to be assigned to each mouse button,
- *   which can then be switched between them as desired.
- * </p>
+ * <p>Skill slots are designed to allow a skill to be assigned to each mouse button, which can then
+ * be switched between them as desired.
  */
 public enum SkillSlot {
+  /** First slot for the left mouse button. */
   FIRST_SLOT,
+  /** Second slot for the right mouse button. */
   SECOND_SLOT
 }
