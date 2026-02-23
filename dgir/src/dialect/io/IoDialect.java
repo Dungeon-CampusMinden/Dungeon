e2ba17ab887375f413763ffe338dd7e91ece37fa
Lasse Foster <lasse.foster@web.de>
2026-02-23 22:02:59 +0100

    Missed two interfaces.

---
+++ dgir/src/dialect/io/IoDialect.java
@@ -0,0 +1,36 @@
+package dialect.io;
+
+import core.*;
+import core.ir.Attribute;
+import core.ir.Op;
+import core.ir.Type;
+import java.util.List;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+public class IoDialect extends Dialect {
+  @Contract(pure = true)
+  @Override
+  public @NotNull String getNamespace() {
+    return "io";
+  }
+
+  @Contract(pure = true)
+  @Override
+  public @NotNull @Unmodifiable List<Op> allOps() {
+    return Utils.Dialect.allOps(IoDialect.class, IO.class);
+  }
+
+  @Contract(pure = true)
+  @Override
+  public @NotNull @Unmodifiable List<Type> allTypes() {
+    return List.of();
+  }
+
+  @Contract(pure = true)
+  @Override
+  public @Unmodifiable @NotNull List<Attribute> allAttributes() {
+    return List.of();
+  }
+}
---
+++ dgir/src/dialect/scf/SCFDialect.java
@@ -0,0 +1,37 @@
+package dialect.scf;
+
+import core.Dialect;
+import core.Utils;
+import core.ir.Attribute;
+import core.ir.Op;
+import core.ir.Type;
+import java.util.List;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+public class SCFDialect extends Dialect {
+  @Contract(pure = true)
+  @Override
+  public @NotNull String getNamespace() {
+    return "scf";
+  }
+
+  @Contract(pure = true)
+  @Override
+  public @NotNull @Unmodifiable List<Op> allOps() {
+    return Utils.Dialect.allOps(SCFDialect.class, SCF.class);
+  }
+
+  @Contract(pure = true)
+  @Override
+  public @NotNull @Unmodifiable List<Type> allTypes() {
+    return List.of();
+  }
+
+  @Contract(pure = true)
+  @Override
+  public @Unmodifiable @NotNull List<Attribute> allAttributes() {
+    return List.of();
+  }
+}
