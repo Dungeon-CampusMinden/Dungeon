b8321cc28c84850b6afe7d832a91ef4b21f8af61
Lasse Foster <lasse.foster@web.de>
2026-02-28 12:05:01 +0100

    Add definition location to value class for debugging support in the future.

--- dgir/src/core/ir/Value.java
+++ dgir/src/core/ir/Value.java
@@ -1,14 +1,15 @@
 package core.ir;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import core.IRObjectWithUseList;
 import core.serialization.ValueIdGenerator;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 /**
  * A dynamic value produced by an {@link Operation} or introduced as a block/region argument. Values
@@ -22,14 +23,23 @@ public final class Value extends IRObjectWithUseList<Value, ValueOperand> implem
   // =========================================================================
 
   private final @NotNull Type type;
+  private @NotNull Location definitionLocation = Location.UNKNOWN;
 
   // =========================================================================
   // Constructors
   // =========================================================================
 
-  @JsonCreator
-  public Value(@JsonProperty("type") @NotNull Type type) {
+  public Value(@NotNull Type type) {
     this.type = type;
+  }
+
+  @JsonCreator
+  public Value(
+      @JsonProperty("type") @NotNull Type type, @JsonProperty("loc") @Nullable Location location) {
+    this.type = type;
+    if (location != null) {
+      this.definitionLocation = location;
+    }
   }
 
   // =========================================================================
@@ -39,6 +49,36 @@ public final class Value extends IRObjectWithUseList<Value, ValueOperand> implem
   @Contract(pure = true)
   public @NotNull Type getType() {
     return type;
+  }
+
+  @Contract(pure = true)
+  @JsonProperty("loc")
+  @JsonInclude(NON_NULL)
+  @Nullable
+  Location getLocationIfKnown() {
+    return definitionLocation.equals(Location.UNKNOWN) ? null : definitionLocation;
+  }
+
+  /**
+   * The location of the definition of this value. It marks the first definition of the value in the
+   * original source. This is used for error reporting and debugging, and may be {@link
+   * Location#UNKNOWN} if the value was created without a known source location.
+   *
+   * @return the location of the definition of this value.
+   */
+  @Contract(pure = true)
+  @JsonIgnore
+  public @NotNull Location getLocation() {
+    return definitionLocation;
+  }
+
+  /**
+   * Set the location of the definition of this value.
+   *
+   * @param location the new location.
+   */
+  public void setLocation(@NotNull Location location) {
+    this.definitionLocation = location;
   }
 
   // =========================================================================
