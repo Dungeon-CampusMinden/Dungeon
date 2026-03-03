4862b402a03de7ae1585d296e65368d4e2f06e19
MatGrabert <mathis.grabert@protonmail.com>
2026-03-03 18:29:23 +0100

    Add set for static-callback.
    - Es wird standard abgefragt, ob die Geschwindigkeit 0 beträgt.
    - Enitys können mit der static gesetzt werde ohne, dass die Geschwindigkeit 0 beträgt.

--- dungeon/src/contrib/components/CollideComponent.java
+++ dungeon/src/contrib/components/CollideComponent.java
@@ -4,6 +4,7 @@ import contrib.utils.components.collide.Collider;
 import contrib.utils.components.collide.Hitbox;
 import core.Component;
 import core.Entity;
+import core.components.VelocityComponent;
 import core.utils.Direction;
 import core.utils.Rectangle;
 import core.utils.TriConsumer;
@@ -109,7 +110,10 @@ public final class CollideComponent implements Component {
   private TriConsumer<Entity, Entity, Direction> collideHold;
 
   /** Optional Callback to check, if the entity is static */
-  private Function<Entity, Boolean> staticCallback;
+  private Function<Entity, Boolean> staticCallback = entity ->
+    entity.fetch(VelocityComponent.class)
+      .map(vc -> vc.maxSpeed() == 0f)
+      .orElse(true);
 
   /**
    * Creates a new {@code CollideComponent}.
@@ -359,12 +363,22 @@ public final class CollideComponent implements Component {
     this.collider = collider;
   }
 
-  /** Returns if the entity is static.
+  /**
+   * Returns if the entity is static.
    *
    * @param entity The entity that should be checked.
    * @return True if the entity is static.
    * */
   public boolean isStatic(Entity entity) {
     return staticCallback.apply(entity);
+  }
+
+  /**
+   * Sets the entity static or non-static.
+   *
+   * @param callback Function that receives an entity and returns true, if the entity is static.
+   * */
+  public void staticCallback(Function<Entity, Boolean> callback) {
+    this.staticCallback = callback != null ? callback : this.staticCallback;
   }
 }
