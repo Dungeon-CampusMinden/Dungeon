b1b04e2d3dfe91d585082ef4d115d01f89614341
Hannes Ru <viddie@hotmail.de>
2026-03-11 16:40:08 +0100

    Add emote module to dungeon

---
+++ dungeon/src/contrib/modules/emote/EmoteFactory.java
@@ -0,0 +1,22 @@
+package contrib.modules.emote;
+
+import core.Entity;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
+import core.utils.Point;
+import core.utils.components.draw.DepthLayer;
+import core.utils.components.path.SimpleIPath;
+
+public class EmoteFactory {
+
+  public static Entity createEmote(Point position, Emote emote, int duration) {
+    Entity entity = new Entity();
+    entity.add(new PositionComponent(position));
+    entity.add(new EmoteComponent(emote, duration));
+    DrawComponent dc = new DrawComponent(new SimpleIPath(emote.getPath()));
+    dc.depth(DepthLayer.AbovePlayer.depth());
+    entity.add(dc);
+    return entity;
+  }
+
+}
---
+++ dungeon/src/contrib/modules/emote/EmoteSystem.java
@@ -0,0 +1,42 @@
+package contrib.modules.emote;
+
+import core.Entity;
+import core.Game;
+import core.System;
+import core.components.PositionComponent;
+import core.utils.Point;
+
+public class EmoteSystem extends System {
+
+
+  public EmoteSystem(){
+    super(AuthoritativeSide.SERVER, EmoteComponent.class, PositionComponent.class);
+  }
+
+  @Override
+  public void execute() {
+    filteredEntityStream().map(Data::of).forEach(this::process);
+  }
+
+  private void process(Data data){
+    data.ec().update(1f / Game.frameRate());
+    if (data.ec().isDone()) {
+      Game.remove(data.e());
+    } else {
+      Point pos = data.pc.position();
+      float translate = data.ec().getWobbleMoveFrame();
+      data.pc().position(pos.translate(0, translate));
+    }
+  }
+
+  @Override
+  public void stop() {
+
+  }
+
+  private record Data(Entity e, PositionComponent pc, EmoteComponent ec){
+    static Data of(Entity e){
+      return new Data(e, e.fetch(PositionComponent.class).orElseThrow(), e.fetch(EmoteComponent.class).orElseThrow());
+    }
+  }
+}
