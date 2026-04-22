8558f5fe249dc446b4635bc63ded96d89691cebd
kzuber93 <kevinzuber93@gmail.com>
2026-04-22 20:24:39 +0200

    Fix checkstyle violations in Keys

--- dungeon/src/core/input/Keys.java
+++ dungeon/src/core/input/Keys.java
@@ -23,83 +23,147 @@ package core.input;
  */
 public final class Keys {
 
+  /** Matches any key input. */
   public static final int ANY_KEY = -1;
+  /** Represents an unknown or unrecognized key. */
   public static final int UNKNOWN = 0;
 
   // Numbers
+  /** Key code for the 0 (zero) key. */
   public static final int NUM_0 = 7;
+  /** Key code for the 1 key. */
   public static final int NUM_1 = 8;
+  /** Key code for the 2 key. */
   public static final int NUM_2 = 9;
+  /** Key code for the 3 key. */
   public static final int NUM_3 = 10;
+  /** Key code for the 4 key. */
   public static final int NUM_4 = 11;
+  /** Key code for the 5 key. */
   public static final int NUM_5 = 12;
+  /** Key code for the 6 key. */
   public static final int NUM_6 = 13;
+  /** Key code for the 7 key. */
   public static final int NUM_7 = 14;
+  /** Key code for the 8 key. */
   public static final int NUM_8 = 15;
+  /** Key code for the 9 key. */
   public static final int NUM_9 = 16;
 
   // Arrows
+  /** Key code for the Up arrow key. */
   public static final int UP = 19;
+  /** Key code for the Down arrow key. */
   public static final int DOWN = 20;
+  /** Key code for the Left arrow key. */
   public static final int LEFT = 21;
+  /** Key code for the Right arrow key. */
   public static final int RIGHT = 22;
 
   // Letters
+  /** Key code for the A key. */
   public static final int A = 29;
+  /** Key code for the B key. */
   public static final int B = 30;
+  /** Key code for the C key. */
   public static final int C = 31;
+  /** Key code for the D key. */
   public static final int D = 32;
+  /** Key code for the E key. */
   public static final int E = 33;
+  /** Key code for the F key. */
   public static final int F = 34;
+  /** Key code for the G key. */
   public static final int G = 35;
+  /** Key code for the H key. */
   public static final int H = 36;
+  /** Key code for the I key. */
   public static final int I = 37;
+  /** Key code for the J key. */
   public static final int J = 38;
+  /** Key code for the K key. */
   public static final int K = 39;
+  /** Key code for the L key. */
   public static final int L = 40;
+  /** Key code for the M key. */
   public static final int M = 41;
+  /** Key code for the N key. */
   public static final int N = 42;
+  /** Key code for the O key. */
   public static final int O = 43;
+  /** Key code for the P key. */
   public static final int P = 44;
+  /** Key code for the Q key. */
   public static final int Q = 45;
+  /** Key code for the R key. */
   public static final int R = 46;
+  /** Key code for the S key. */
   public static final int S = 47;
+  /** Key code for the T key. */
   public static final int T = 48;
+  /** Key code for the U key. */
   public static final int U = 49;
+  /** Key code for the V key. */
   public static final int V = 50;
+  /** Key code for the W key. */
   public static final int W = 51;
+  /** Key code for the X key. */
   public static final int X = 52;
+  /** Key code for the Y key. */
   public static final int Y = 53;
+  /** Key code for the Z key. */
   public static final int Z = 54;
 
   // Punctuation
+  /** Key code for the Comma (,) key. */
   public static final int COMMA = 55;
+  /** Key code for the Period (.) key. */
   public static final int PERIOD = 56;
 
   // Modifiers
+  /** Key code for the left Shift modifier key. */
   public static final int SHIFT_LEFT = 59;
+  /** Key code for the right Shift modifier key. */
   public static final int SHIFT_RIGHT = 60;
 
   // Common controls
+  /** Key code for the Tab key. */
   public static final int TAB = 61;
+  /** Key code for the Space bar key. */
   public static final int SPACE = 62;
+  /** Key code for the Enter/Return key. */
   public static final int ENTER = 66;
+  /** Key code for the Backspace key. */
   public static final int BACKSPACE = 67;
+  /** Key code for the Escape key. */
   public static final int ESCAPE = 111;
+  /** Key code for the Delete key. */
   public static final int DELETE = 112;
 
   // Function keys
+  /** Key code for the F1 function key. */
   public static final int F1 = 131;
+  /** Key code for the F2 function key. */
   public static final int F2 = 132;
+  /** Key code for the F3 function key. */
   public static final int F3 = 133;
+  /** Key code for the F4 function key. */
   public static final int F4 = 134;
+  /** Key code for the F5 function key. */
   public static final int F5 = 135;
+  /** Key code for the F6 function key. */
   public static final int F6 = 136;
+  /** Key code for the F7 function key. */
   public static final int F7 = 137;
+  /** Key code for the F8 function key. */
   public static final int F8 = 138;
+  /** Key code for the F9 function key. */
   public static final int F9 = 139;
+  /** Key code for the F10 function key. */
   public static final int F10 = 140;
+  /** Key code for the F11 function key. */
   public static final int F11 = 141;
+  /** Key code for the F12 function key. */
   public static final int F12 = 142;
 
   private Keys() {}
