79b1834645bddfcf9cf07a96726fa5a35a55695e
kzuber93 <kevinzuber93@gmail.com>
2026-04-23 20:45:50 +0200

    Remove redundant level hide state creation

--- dungeon/src/contrib/modules/levelhide/LevelHideFactory.java
+++ dungeon/src/contrib/modules/levelhide/LevelHideFactory.java
@@ -22,20 +22,7 @@ public class LevelHideFactory {
     Entity entity = new Entity("hider");
     entity.add(new PositionComponent(bottomLeft));
     entity.add(new LevelHideComponent(new Rectangle(width, height), transitionSize));
-    entity.add(new LevelHideStateComponent());
     return entity;
-  }
-
-  /**
-   * Creates a level hide entity with a default transition size of 2.
-   *
-   * @param bottomLeft the bottom left point of the hide region
-   * @param width width of the hide region
-   * @param height height of the hide region
-   * @return the created level hide entity
-   */
-  public static Entity createLevelHide(Point bottomLeft, float width, float height) {
-    return createLevelHide(bottomLeft, width, height, 2);
   }
 
   /**
@@ -50,7 +37,7 @@ public class LevelHideFactory {
   }
 
   /**
-   * Creates a level hide entity defined by bottom left and top right points.
+   * Creates a level hide entity defined by bottomed left and top right points.
    *
    * @param bottomLeft the bottom left point of the hide region
    * @param topRight the top right point of the hide region
