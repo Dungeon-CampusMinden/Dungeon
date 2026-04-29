c9391d1d066f258a5ae42a09b866e873239333c7
Hannes Ru <viddie@hotmail.de>
2026-04-29 17:02:15 +0200

    Add static render of spritesheet texture to TextureGenerator

--- dungeon/src/core/utils/components/draw/TextureGenerator.java
+++ dungeon/src/core/utils/components/draw/TextureGenerator.java
@@ -1,5 +1,7 @@
 package core.utils.components.draw;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -71,5 +73,38 @@ public class TextureGenerator {
     Pixmap pm = Pixmap.createFromFrameBuffer(0, 0, fbo.getWidth(), fbo.getHeight());
     fbo.end();
     return pm;
+  }
+
+  /**
+   * Extracts a rectangular sub-region from a spritesheet image on disk and registers it as a new
+   * texture in the {@link TextureMap} at the given output path.
+   *
+   * <p>This is useful for capturing a single animation frame (e.g. the first idle frame of a
+   * character) so it can be referenced as a standalone texture (such as a portrait in a dialog)
+   * without having to parse the spritesheet at the call site.
+   *
+   * @param sourcePath The internal asset path of the source spritesheet image.
+   * @param srcX The x coordinate (in pixels) of the top-left corner of the region to extract.
+   * @param srcY The y coordinate (in pixels) of the top-left corner of the region to extract.
+   * @param width The width of the region to extract, in pixels.
+   * @param height The height of the region to extract, in pixels.
+   * @param outPath The (virtual) path under which the extracted region is registered in the
+   *     {@link TextureMap}. By convention, generated textures use the {@code @gen/} prefix.
+   */
+  public static void registerSpritesheetRegionTexture(
+      String sourcePath, int srcX, int srcY, int width, int height, String outPath) {
+    FileHandle file = Gdx.files.internal(sourcePath);
+    if (!file.exists()) {
+      throw new IllegalArgumentException("Spritesheet not found: " + sourcePath);
+    }
+    Pixmap source = new Pixmap(file);
+    try {
+      Pixmap region = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+      region.setBlending(Pixmap.Blending.None);
+      region.drawPixmap(source, 0, 0, srcX, srcY, width, height);
+      TextureMap.instance().putPixmap(new SimpleIPath(outPath), region, false);
+    } finally {
+      source.dispose();
+    }
   }
 }
