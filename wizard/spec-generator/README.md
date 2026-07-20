2b570c33d91f85da55eb1159b30db0a6d7b08d05
viddie <viddie@hotmail.de>
2026-07-20 18:21:35 +0200

    Add spec generator setup with vite, react, typescript and tailwindcss

---
+++ wizard/spec-generator/README.md
@@ -0,0 +1,3 @@
+# Witzard Spec Generator
+
+Check the documentation in the sibling folder out to see what is happening here.
---
+++ wizard/spec-generator/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="flex items-center justify-around">
+        <h1>Wizard Spec Generator</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
