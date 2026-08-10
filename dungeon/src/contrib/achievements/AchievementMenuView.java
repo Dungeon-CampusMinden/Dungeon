de53861dd191bee50c7b974c2cab4b283ae73d77
AMatutat <32962412+AMatutat@users.noreply.github.com>
2026-08-10 08:07:48 +0200

    use real graysacle

--- dungeon/src/contrib/achievements/AchievementMenuView.java
+++ dungeon/src/contrib/achievements/AchievementMenuView.java
@@ -2,11 +2,15 @@ package contrib.achievements;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import contrib.hud.UIUtils;
 import core.language.Translation;
@@ -28,7 +32,6 @@ public final class AchievementMenuView extends Table {
   private static final String T_PROGRESS = "progress";
   private static final Color TEXT = Color.BLACK;
   private static final Color LOCKED_TEXT = new Color(0.38f, 0.38f, 0.38f, 1f);
-  private static final Color LOCKED_ICON = new Color(0.35f, 0.35f, 0.35f, 1f);
   private static final String FALLBACK_IMAGE = "animation/missing_texture.png";
   private static final float REFRESH_INTERVAL_SECONDS = 0.25f;
   private static final Translation TRANS = new Translation("achievement.menu");
@@ -121,11 +124,8 @@ public final class AchievementMenuView extends Table {
         hidden ? TRANS.text(T_HIDDEN_DESCRIPTION) : achievement.displayDescription();
     Color textColor = unlocked ? TEXT : LOCKED_TEXT;
 
-    Image icon = new Image(texture(achievement.imagePath()));
+    Image icon = new Image(iconDrawable(achievement.imagePath(), unlocked));
     icon.setScaling(com.badlogic.gdx.utils.Scaling.fit);
-    if (!unlocked) {
-      icon.setColor(LOCKED_ICON);
-    }
 
     Label nameLabel =
         Scene2dElementFactory.createLabel(name, FontSpec.of(TITLE_FONT, 24, textColor));
@@ -146,11 +146,83 @@ public final class AchievementMenuView extends Table {
     return row;
   }
 
+  private static TextureRegionDrawable iconDrawable(String path, boolean unlocked) {
+    Texture texture = texture(path);
+    TextureRegion region = new TextureRegion(texture);
+    if (unlocked) {
+      return new TextureRegionDrawable(region);
+    }
+    return new GrayscaleTextureRegionDrawable(region);
+  }
+
   private static Texture texture(String path) {
     Texture texture = TextureMap.instance().textureAt(new SimpleIPath(path));
     if (texture != null) {
       return texture;
     }
     return TextureMap.instance().textureAt(new SimpleIPath(FALLBACK_IMAGE));
+  }
+
+  private static final class GrayscaleTextureRegionDrawable extends TextureRegionDrawable {
+    private static final String VERTEX_SHADER =
+        """
+        attribute vec4 a_position;
+        attribute vec4 a_color;
+        attribute vec2 a_texCoord0;
+        uniform mat4 u_projTrans;
+        varying vec4 v_color;
+        varying vec2 v_texCoords;
+
+        void main() {
+          v_color = a_color;
+          v_color.a = v_color.a * (255.0 / 254.0);
+          v_texCoords = a_texCoord0;
+          gl_Position = u_projTrans * a_position;
+        }
+        """;
+    private static final String FRAGMENT_SHADER =
+        """
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+
+        varying vec4 v_color;
+        varying vec2 v_texCoords;
+        uniform sampler2D u_texture;
+
+        void main() {
+          vec4 texel = texture2D(u_texture, v_texCoords);
+          float luminance = dot(texel.rgb, vec3(0.299, 0.587, 0.114));
+          gl_FragColor = vec4(vec3(luminance * 0.35), texel.a) * v_color;
+        }
+        """;
+
+    private static ShaderProgram shader;
+
+    private GrayscaleTextureRegionDrawable(TextureRegion region) {
+      super(region);
+    }
+
+    @Override
+    public void draw(Batch batch, float x, float y, float width, float height) {
+      ShaderProgram previousShader = batch.getShader();
+      batch.flush();
+      batch.setShader(shader());
+      super.draw(batch, x, y, width, height);
+      batch.flush();
+      batch.setShader(previousShader);
+    }
+
+    private static ShaderProgram shader() {
+      if (shader == null) {
+        ShaderProgram.pedantic = false;
+        shader = new ShaderProgram(VERTEX_SHADER, FRAGMENT_SHADER);
+        if (!shader.isCompiled()) {
+          throw new IllegalStateException(
+              "Grayscale achievement icon shader compilation failed:\n" + shader.getLog());
+        }
+      }
+      return shader;
+    }
   }
 }
