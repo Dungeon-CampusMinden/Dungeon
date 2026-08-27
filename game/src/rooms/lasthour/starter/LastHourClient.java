05461f902994a832457a5204b1c190dbd8d990f5
Simon Gartzke <simon.gartzke@icloud.com>
2026-08-27 16:55:08 +0200

    fixed game lagging

--- game/src/rooms/lasthour/starter/LastHourClient.java
+++ game/src/rooms/lasthour/starter/LastHourClient.java
@@ -135,8 +135,6 @@ public final class LastHourClient {
                   .ifPresent(newEntity::add);
               LastHourSnapshotTranslator.keypadStateFromMetadata(event.metadata())
                   .ifPresent(newEntity::add);
-              LastHourSnapshotTranslator.textKeypadStateFromMetadata(event.metadata())
-                  .ifPresent(newEntity::add);
               LastHourSnapshotTranslator.worldTimerStateFromMetadata(event.metadata())
                   .ifPresent(newEntity::add);
               LastHourSnapshotTranslator.questLogFromMetadata(event.metadata())
