4bbeb1a67cc2ad525cc5ea3ed87263aa87aea8f1
AMatutat <32962412+AMatutat@users.noreply.github.com>
2026-09-07 12:26:40 +0200

    test shader

--- game/src/rooms/systemRecovery/entities/EntityFactory.java
+++ game/src/rooms/systemRecovery/entities/EntityFactory.java
@@ -6,6 +6,9 @@ import engine.components.PositionComponent;
 import engine.utils.Point;
 import engine.utils.components.path.SimpleIPath;
 import feature.components.CollideComponent;
+import feature.hud.DialogUtils;
+import feature.interaction.Interaction;
+import feature.interaction.InteractionComponent;
 
 public class EntityFactory {
 
@@ -16,6 +19,16 @@ public class EntityFactory {
     if (on) entity.add(new DrawComponent(new SimpleIPath("objects/tech/CryoBox.png")));
     else entity.add(new DrawComponent(new SimpleIPath("objects/tech/CryoBoxOFF.png")));
 
+    return entity;
+  }
+
+  public static Entity roomLabel(Point point, String text, String titel) {
+    Entity entity = new Entity();
+    entity.add(new PositionComponent(point));
+    entity.add(new DrawComponent(new SimpleIPath("objects/tech/Computer_1.png")));
+    entity.add(
+        new InteractionComponent(
+            new Interaction((_, _) -> DialogUtils.showTextPopup(text, titel))));
     return entity;
   }
 }
