3ccdb3b2d1cc61db595bc5e65ee5fceee37b159b
Simon Gartzke <simon.gartzke@icloud.com>
2026-09-14 04:25:51 +0200

    removed debug commands

--- game/src/rooms/lasthour/level/LastHourLevel.java
+++ game/src/rooms/lasthour/level/LastHourLevel.java
@@ -160,7 +160,6 @@ public class LastHourLevel extends DungeonLevel {
             getPoint("keypad-storage"),
             Lore.DoorCode,
             () -> {
-              r2SpawnPapers();
               storageDoor.open();
               LastHourQuestLogUtil.addStorageRoomQuestLogEntry();
               LastHourQuestLogUtil.addDoorCodeQuestLogEntry();
